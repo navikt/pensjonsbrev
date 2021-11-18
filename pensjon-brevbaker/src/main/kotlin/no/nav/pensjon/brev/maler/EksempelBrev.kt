ac816d34a6c1ba97307558b15d2ebfaf07ec6aec
Håkon Heggholmen <hakon.heggholmen@nav.no>
2021-11-18 12:28:02 +0100

    Dokumenterer ett vanlig bestillingsløp med brevbaker og pdf-bygger

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/EksempelBrev.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/EksempelBrev.kt
@@ -11,27 +11,51 @@ import no.nav.pensjon.brev.template.dsl.*
 
 object EksempelBrev : StaticTemplate {
     override val template = createTemplate(
-        name = "eksempelBrev",
+        //ID på brevet
+        name = "EKSEMPEL_BREV",
+
+        //Master-mal for brevet.
         base = PensjonLatex,
+
+        // Unik datagrunnlag/DTO for brevet
         letterDataType = EksempelBrevDto::class,
+
+        // Hvilke språk brevet støtter
         lang = languages(Language.Bokmal),
+
+        // Hovedtittel inne i brevet
         title = newText(Language.Bokmal to "Eksempelbrev"),
+
+        // Metadata knyttet til en brevmal som ikke påvirker innholdet
         letterMetadata = LetterMetadata(
-            "Dette er ett eksempel-brev",
-            false,
+
+            // Visningstittel for ulike systemer.
+            // F.eks saksbehandling brev oversikt, brukerens brev oversikt osv
+            displayTitle = "Dette er ett eksempel-brev",
+
+            // Krever brevet bankid/ nivå 4 pålogging for å vises(og ikke brukernavn/passord nivå 3)
+            isSensitiv = false,
         )
     ) {
-
+        // Tekst-innholdet i malen
         outline {
-            includePhrase(argument().select(EksempelBrevDto::pensjonInnvilget).map { TestFraseDto(it.toString()) }, TestFrase)
 
+            // Undertittel
             title1 {
-                text(Language.Bokmal to "Heisann, ")
+
+                // Tekst
                 text(Language.Bokmal to "Du har fått innvilget pensjon")
             }
+
+            // Inkluder data fra datagrunnlaget til malen inn i brevet som tekst
             eval { argument().select(EksempelBrevDto::pensjonInnvilget).str() }
 
-            eval { argument().select(EksempelBrevDto::datoInnvilget).format() }
+            // Inkludering av eksisterende frase/mini-mal for gjenbruk av elementer
+            includePhrase(
+                argument().select(EksempelBrevDto::pensjonInnvilget)
+                    .map { TestFraseDto(it.toString()) },
+                TestFrase
+            )
         }
     }
 }
