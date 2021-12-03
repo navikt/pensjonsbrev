4522ce714b4d3ca68145014c0e28741a76486577
Håkon Heggholmen <hakon.heggholmen@nav.no>
2021-12-03 14:43:17 +0100

    Retting av skrivefeil parametre til OmsorgEgenAuto

--- pensjon-brevbaker-api-model/build.gradle.kts
+++ pensjon-brevbaker-api-model/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "no.nav.pensjon.brev"
-version = "1.2.5"
+version = "1.2.6"
 
 java {
     withSourcesJar()
--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/OmsorgEgenAuto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/OmsorgEgenAuto.kt
@@ -1,5 +1,5 @@
 package no.nav.pensjon.brev.api.model.maler
 
-data class OmsorgEgenAutoDto(val arEgenerklaringOmsorgspoeng: Number, val arInnvilgetOmsorgspoeng: Number) {
+data class OmsorgEgenAutoDto(val aarEgenerklaringOmsorgspoeng: Number, val aarInnvilgetOmsorgspoeng: Number) {
     constructor() : this(2020, 2021)
 }
