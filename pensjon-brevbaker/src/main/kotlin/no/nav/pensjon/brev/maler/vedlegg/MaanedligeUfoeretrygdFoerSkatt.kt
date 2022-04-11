92ff240e796cdb9604f2e50aefd64408dace4726
Agne Hesten <agne.hesten@nav.no>
2022-04-11 15:46:20 +0200

    PL-5049 Vedlegg MaanedligeUfoeretrygdFoerSkatt with phrases

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/MaanedligeUfoeretrygdFoerSkatt.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/MaanedligeUfoeretrygdFoerSkatt.kt
@@ -1,70 +1,83 @@
 package no.nav.pensjon.brev.no.nav.pensjon.brev.maler.vedlegg
 
+import no.nav.pensjon.brev.maler.fraser.*
 import no.nav.pensjon.brev.template.LangBokmalNynorskEnglish
 import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.createAttachment
-import no.nav.pensjon.brev.template.dsl.expression.expr
-import no.nav.pensjon.brev.template.dsl.expression.format
-import no.nav.pensjon.brev.template.dsl.expression.plus
-import no.nav.pensjon.brev.template.dsl.expression.select
+import no.nav.pensjon.brev.template.dsl.expression.*
 import no.nav.pensjon.brev.template.dsl.newText
 import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.dsl.textExpr
 import java.time.LocalDate
 
+data class UfoeretrygdPerMaaned(
+    val annetBelop: Int,
+    val barnetillegg: Int?,
+    val dekningFasteUtgifter: Int,
+    val garantitilleggNordisk27: Int?,
+    val grunnbeloep: Int,
+    val ordinaerUTBeloep: Int,
+    val totalUTBeloep: Int,
+    val virkningFraOgMed: LocalDate,
+    val virkningTilOgMed: LocalDate?,
+    val avkortning: Avkortning?,
+) {
+  data class Avkortning(
+      val barnetilleggFoerAvkort: Int,
+      val garantitilleggNordisk27FoerAvkort: Int,
+      val ordinaerUTBeloepFoerAvkort: Int,
+      val totalUTBeloepFoerAvkort: Int,
+  )
+}
 data class MaanedligeUfoeretrygdFoerSkattDto(
-    val kravVirkningFraOgMed: LocalDate,
-    val kravVirkningTilOgMed: LocalDate, // TODO: Gjør om til nullable
-    val gjeldendeBeregnetUfoeretrygdPerMaaned: Int,
-)
+    val gjeldendeBeregnetUTPerMaaned: UfoeretrygdPerMaaned,
+
+    val virkningDatoFraOgMed_krav: LocalDate,
+
+    val antallBeregningsperioderPaaVedtak: Int,
+    val ufoeretrygdPerioder: List<UfoeretrygdPerMaaned>,
+
+    )
 
 val maanedligeUfoeretrygdFoerSkatt = createAttachment<LangBokmalNynorskEnglish, MaanedligeUfoeretrygdFoerSkattDto>(
+    //tekst099 tittel Obligatorisk
     title = newText(
         Bokmal to "Dette er din månedlige uføretrygd før skatt",
         Nynorsk to "Dette er den månadlege uføretrygda di før skatt",
         English to "This is your monthly disability benefit before tax",
     ),
 ) {
-    paragraph {
-        text(
-            Bokmal to "Nedenfor ser du den månedlige uføretrygden din.",
-            Nynorsk to "Nedanfor ser du den månadlege uføretrygda di.",
-            English to "Below is a presentation of your monthly disability benefit.",
-        )
+    val gjeldendeUfoeretrygd = argument().map { it.gjeldendeBeregnetUTPerMaaned }
 
-        table {
-            title {
-                val virkningsDatoFraOgMed =
-                    argument().select(MaanedligeUfoeretrygdFoerSkattDto::kravVirkningFraOgMed).format(short = true)
-                val virkningsDatoTilOgMed =
-                    argument().select(MaanedligeUfoeretrygdFoerSkattDto::kravVirkningTilOgMed).format(short = true)
+    //tekst100 beskrivelse tabell 1 og 2 obligatorisk
+    includePhrase(vedleggBelopUT_001)
 
-                textExpr(
-                    Bokmal to "Den månedlige uføretrygden fra ".expr() + virkningsDatoFraOgMed + " til " + virkningsDatoTilOgMed,
-                    Nynorsk to "Den månadlege uføretrygda frå ".expr() + virkningsDatoFraOgMed + " til " + virkningsDatoTilOgMed,
-                    English to "Your monthly disability benefit from ".expr() + virkningsDatoFraOgMed + " to " + virkningsDatoTilOgMed,
-                )
+    includePhrase(tabellBeregnetUTHele, gjeldendeUfoeretrygd)
 
-                newline()
+    showIf(argument().map {
+        it.antallBeregningsperioderPaaVedtak > 1
+    }) {
+        //tekst114 tittel flere perioder
+        val virkDato = argument().map { it.virkningDatoFraOgMed_krav }
+        title1 {
+            textExpr(
+                Bokmal to "Oversikt over uføretrygdens størrelse fra ".expr() + virkDato.format(),
+                Nynorsk to "Oversikt over storleik på uføretrygda frå ".expr() + virkDato.format(),
+                English to "Disability benefit payment specifications as of ".expr() + virkDato.format(),
+            )
+        }
 
-                val beregnetUfoeretrygd = argument().select(MaanedligeUfoeretrygdFoerSkattDto::gjeldendeBeregnetUfoeretrygdPerMaaned).format()
+        //tekst115 beskrivelse tabell flere perioder (obligatorisk)
+        paragraph {
+            text(
+                Bokmal to "Nedenfor ser du den månedlige uføretrygden din for tidligere perioder.",
+                Nynorsk to "Nedanfor ser du uføretrygda di kvar månad for tidligare periodar.",
+                English to "Below is a presentation of your monthly disability benefit for your previous periods.",
+            )
+        }
 
-                textExpr(
-                    Bokmal to "Folketrygdens grunnbeløp (G) benyttet i beregningen er ".expr() + beregnetUfoeretrygd,
-                    Nynorsk to "Grunnbeløpet i folketrygda (G) nytta i utrekninga er ".expr() + beregnetUfoeretrygd,
-                    English to "The National Insurance basic amount (G) applied in the calculation is NOK ".expr() + beregnetUfoeretrygd,
-                )
-            }
-            row {
-              cell {
-                  text(
-                  Bokmal to "",
-                  Nynorsk to "",
-                  English to "",
-              )
-              }
-            }
+        forEach(argument().map { it.ufoeretrygdPerioder }){
+            includePhrase(tabellBeregnetUTHele, it)
         }
     }
-
 }
--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/MaanedligeUfoeretrygdFoerSkatt.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/MaanedligeUfoeretrygdFoerSkatt.kt
@@ -1,8 +1,14 @@
 package no.nav.pensjon.brev.maler.vedlegg
 
+import kotlinx.coroutines.runBlocking
 import no.nav.pensjon.brev.Fixtures
+import no.nav.pensjon.brev.PDF_BUILDER_URL
+import no.nav.pensjon.brev.TestTags
 import no.nav.pensjon.brev.api.model.LetterMetadata
+import no.nav.pensjon.brev.latex.LaTeXCompilerService
+import no.nav.pensjon.brev.latex.PdfCompilationInput
 import no.nav.pensjon.brev.no.nav.pensjon.brev.maler.vedlegg.MaanedligeUfoeretrygdFoerSkattDto
+import no.nav.pensjon.brev.no.nav.pensjon.brev.maler.vedlegg.UfoeretrygdPerMaaned
 import no.nav.pensjon.brev.no.nav.pensjon.brev.maler.vedlegg.maanedligeUfoeretrygdFoerSkatt
 import no.nav.pensjon.brev.template.Language.Bokmal
 import no.nav.pensjon.brev.template.Letter
@@ -10,9 +16,12 @@ import no.nav.pensjon.brev.template.base.PensjonLatex
 import no.nav.pensjon.brev.template.dsl.createTemplate
 import no.nav.pensjon.brev.template.dsl.expression.expr
 import no.nav.pensjon.brev.template.dsl.newText
+import no.nav.pensjon.brev.writeTestPDF
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
+@Tag(TestTags.PDF_BYGGER)
 class MaanedligeUfoeretrygdFoerSkatt {
 
     @Test
@@ -31,9 +40,48 @@ class MaanedligeUfoeretrygdFoerSkatt {
 
             }
 
+            val ufoeretrygdGjeldende = UfoeretrygdPerMaaned(
+                annetBelop = 1503,
+                barnetillegg = 2503,
+                dekningFasteUtgifter = 1503,
+                garantitilleggNordisk27 = 9003,
+                grunnbeloep = 91543,
+                ordinaerUTBeloep = 15003,
+                totalUTBeloep = 5503,
+                virkningFraOgMed = LocalDate.of(2017, 2, 3),
+                virkningTilOgMed = LocalDate.of(2017, 5, 5),
+                avkortning = null
+            )
+
+            val ufoeretrygdPerMaaned = UfoeretrygdPerMaaned(
+                annetBelop = 1500,
+                barnetillegg = 2500,
+                dekningFasteUtgifter = 1500,
+                garantitilleggNordisk27 = 9000,
+                grunnbeloep = 91540,
+                ordinaerUTBeloep = 15000,
+                totalUTBeloep = 5500,
+                virkningFraOgMed = LocalDate.of(2017, 2, 3),
+                virkningTilOgMed = LocalDate.of(2017, 5, 5),
+                avkortning = UfoeretrygdPerMaaned.Avkortning(
+                    barnetilleggFoerAvkort = 2800,
+                    garantitilleggNordisk27FoerAvkort = 9999,
+                    ordinaerUTBeloepFoerAvkort = 12000,
+                    totalUTBeloepFoerAvkort = 6600,
+                )
+            )
+
             includeAttachment(
                 maanedligeUfoeretrygdFoerSkatt,
-                MaanedligeUfoeretrygdFoerSkattDto(LocalDate.now(), LocalDate.now(), 91543).expr()
+                MaanedligeUfoeretrygdFoerSkattDto(
+                    ufoeretrygdGjeldende,
+                    LocalDate.now(),
+                    2,
+                    listOf(
+                        ufoeretrygdPerMaaned,
+                        ufoeretrygdPerMaaned.copy(avkortning = null),
+                    )
+                ).expr()
             )
         }
 
@@ -43,5 +91,8 @@ class MaanedligeUfoeretrygdFoerSkatt {
             Bokmal,
             Fixtures.fellesAuto
         ).render()
+            .let { PdfCompilationInput(it.base64EncodedFiles()) }
+            .let { runBlocking { LaTeXCompilerService(PDF_BUILDER_URL).producePDF(it, "test").base64PDF } }
+            .also { writeTestPDF("UT_DOD_ENSLIG_AUTO", it) }
     }
 }
