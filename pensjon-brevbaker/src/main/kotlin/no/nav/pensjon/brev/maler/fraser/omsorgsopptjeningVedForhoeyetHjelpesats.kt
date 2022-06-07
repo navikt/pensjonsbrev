6654aef99d7948946eedbedd3f8c783b156e14af
Jeremy Elsom <jeremy.elsom@nav.no>
2022-06-07 14:53:44 +0200

    PL-5574: Phrases file work in progress

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/omsorgsopptjeningVedForhoeyetHjelpesats.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/omsorgsopptjeningVedForhoeyetHjelpesats.kt
@@ -1,9 +1,13 @@
 package no.nav.pensjon.brev.maler.fraser
 
-import no.nav.pensjon.brev.api.model.maler.OmsorgsopptjeningVedForhoeyetHjelpesats
-import no.nav.pensjon.brev.maler.fraser.Constants
-import no.nav.pensjon.brev.maler.fraser.common.*
-import no.nav.pensjon.brev.template.*
+import no.nav.pensjon.brev.api.model.Kroner
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brev.template.LangBokmalNynorskEnglish
 import no.nav.pensjon.brev.template.Language.*
-import no.nav.pensjon.brev.template.dsl.*
-import no.nav.pensjon.brev.template.dsl.expression.*
+import no.nav.pensjon.brev.template.OutlinePhrase
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.format
+import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brev.template.dsl.textExpr
+
