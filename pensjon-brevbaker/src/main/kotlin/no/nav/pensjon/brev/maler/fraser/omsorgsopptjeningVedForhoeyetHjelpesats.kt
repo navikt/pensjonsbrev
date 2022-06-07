35783d09c6de7032063cb29f9dbb6b59b1224f05
Jeremy Elsom <jeremy.elsom@nav.no>
2022-06-07 15:56:38 +0200

    PL-5574: Phrases file work in progress

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/omsorgsopptjeningVedForhoeyetHjelpesats.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/omsorgsopptjeningVedForhoeyetHjelpesats.kt
@@ -38,12 +38,32 @@ val omsorgsopptjenHjelpestKap20Hjemmel_001 = OutlinePhrase<LangBokmalNynorskEngl
     }
 }
 
-val omsorgsopptjenHjelpestønadHjemmel2_001 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+val omsorgsopptjenHjelpestKap3Hjemmel_001 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
     paragraph {
         text(
-            Bokmal to "",
-            Nynorsk to "",
-            English to ""
+            Bokmal to "Vedtaket er gjort etter folketrygdloven § 3-16 første ledd bokstav b.",
+            Nynorsk to "Vedtaket er gjort etter folketrygdlova § 3-16 første ledd bokstav b.",
+            English to "This decision was made pursuant to the provisions of section 3-16, first paragraph, litra b of the National Insurance Act."
+        )
+    }
+}
+
+val omsorgsopptjenInfoOverskrift_001 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+    title1 {
+        text(
+            Bokmal to "Hva er pensjonsopptjening for omsorgsarbeid?",
+            Nynorsk to "Kva er pensjonsopptening for omsorgsarbeid?",
+            English to "What are pension earnings for care work?"
+        )
+    }
+}
+
+val omsorgsopptjenInfo_001 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+    paragraph {
+        text(
+            Bokmal to "Pensjonsopptjeningen fra omsorgsarbeidet tilsvarer det du ville fått av en inntekt på 4,5 ganger grunnbeløpet for det aktuelle året. Du kan lese mer om dette på nav.no, og få oversikt over pensjonsopptjeningen din på <felles.kontaktinformasjon.nettsted>. Her finner du også omsorgsopptjening som du har fått godkjent.",
+            Nynorsk to "Pensjonsoppteninga frå omsorgsarbeidet svarer til det du ville ha fått av ei inntekt på 4,5 gonger grunnbeløpet for det aktuelle året. Du kan lese meir om dette på nav.no, og få oversikt over pensjonsoppteninga di på <felles.kontaktinformasjon.nettsted>. Her finn du også omsorgsopptening som du har fått godkjent.",
+            English to "The pension savings you earn for care work are equivalent to what you would have earned on an income of 4.5 times the National Insurance basic amount (“G”) for the year in question. You can read more about this at nav.no and see your earned pension savings at <felles.kontaktinformasjon.nettsted>. Here you will also see your approved pension earnings for care work. "
         )
     }
 }
