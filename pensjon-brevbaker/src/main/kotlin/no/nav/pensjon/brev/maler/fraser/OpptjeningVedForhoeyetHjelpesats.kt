891f1c42952de846a4c2f3afb78b7eaa5f1a416a
Jeremy Elsom <jeremy.elsom@nav.no>
2022-06-13 13:52:40 +0200

    PL-5574: Phrases file work in progress

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OpptjeningVedForhoeyetHjelpesats.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OpptjeningVedForhoeyetHjelpesats.kt
@@ -12,3 +12,25 @@ import no.nav.pensjon.brev.template.dsl.*
 import no.nav.pensjon.brev.template.dsl.expression.*
 
 // BrevTypeKode: MF_000094
+object UfoerOmregningEnslig : StaticTemplate {
+    override val template = createTemplate(
+        name = "MF_000094",
+        base = PensjonLatex,
+        letterDataType = UfoerOmregningEnsligDto::class,
+        languages = languages(Bokmal, Nynorsk, English),
+        letterMetadata = LetterMetadata(
+            "Vedtak – omregning til enslig uføretrygdet (automatisk)", isSensitiv = true
+        )
+    ) {
+        val harMinsteytelseVedVirk = argument().map { it.minsteytelseVedvirk_sats != null }
+        val inntektFoerUfoereErSannsynligEndret = argument().map { it.inntektFoerUfoerhetVedVirk.erSannsynligEndret }
+
+        title {
+            text(
+                Bokmal to "NAV har regnet om uføretrygden din",
+                Nynorsk to "NAV har rekna om uføretrygda di",
+                English to "NAV has altered your disability benefit"
+            )
+        }
+
+        outline {
