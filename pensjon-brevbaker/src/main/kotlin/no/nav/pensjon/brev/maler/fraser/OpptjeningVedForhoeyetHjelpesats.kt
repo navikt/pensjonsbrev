2dd407d3eb3c38b725dd781eeb5a28422ec59f83
Jeremy Elsom <jeremy.elsom@nav.no>
2022-06-13 12:37:24 +0200

    PL-5574: Phrases file work in progress

---
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OpptjeningVedForhoeyetHjelpesats.kt
@@ -0,0 +1,14 @@
+package no.nav.pensjon.brev.maler
+
+import no.nav.pensjon.brev.api.model.LetterMetadata
+import no.nav.pensjon.brev.api.model.maler.OmsorggsopptjeningVedForhoeyetHjelpesats
+import no.nav.pensjon.brev.maler.fraser.common.*
+import no.nav.pensjon.brev.maler.fraser.omregning.ufoeretrygd.Ufoeretrygd
+import no.nav.pensjon.brev.maler.fraser.vedtak.Vedtak
+import no.nav.pensjon.brev.template.*
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.base.PensjonLatex
+import no.nav.pensjon.brev.template.dsl.*
+import no.nav.pensjon.brev.template.dsl.expression.*
+
+// BrevTypeKode: MF_000094
