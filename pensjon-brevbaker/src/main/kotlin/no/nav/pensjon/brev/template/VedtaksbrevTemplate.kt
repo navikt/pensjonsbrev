726f113825460e5e796edb6dddf4b7d387f4cee3
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2022-07-08 11:08:04 +0200

    Fjern kode som ikke skulle med

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/VedtaksbrevTemplate.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.pensjon.brev.template
-
-import no.nav.pensjon.brev.api.model.maler.Brevkode
-
-interface VedtaksbrevTemplate {
-    val template: LetterTemplate<*, *>
-    val kode: Brevkode.Vedtak
-}
