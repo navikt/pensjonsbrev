557c4c2d298eaf0c779401cc805608f80c216622
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2022-07-19 10:52:15 +0200

    PL-5574: Legg til model og fødselsdato til felles.bruker

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/Brevkode.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/Brevkode.kt
@@ -5,6 +5,7 @@ object Brevkode {
         UNG_UFOER_AUTO("PE_BA_04_505", "UP_FULLTT_BELOPENDR"),
         OMSORG_EGEN_AUTO("OMSORG_EGEN_AUTO", "OMSORGP_EGENMLD"),
         UFOER_OMREGNING_ENSLIG("UT_DOD_ENSLIG_AUTO", "TILST_DOD_UT"),
+        OMSORGP_GODSKRIVING("OMSORG_HJST_AUTO")
         ;
 
         val brevkoder: Set<String> = koder.toSet()
---
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/OmsorgsopptjeningVedForhoeyetHjelpesats.kt
@@ -0,0 +1,5 @@
+package no.nav.pensjon.brev.api.model.maler
+
+import no.nav.pensjon.brev.api.model.Year
+
+data class OpptjeningVedForhoeyetHjelpesatsDto(val aarInnvilgetOmrsorgspoeng: Year)
