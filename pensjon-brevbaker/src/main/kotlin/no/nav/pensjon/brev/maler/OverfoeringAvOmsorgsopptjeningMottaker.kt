1a9008fd27cc956d91845a0a65be406f381d2d83
Jeremy Elsom <jeremy.elsom@nav.no>
2022-07-26 16:13:19 +0200

    PL-5112: Created new feature branch containing OverfoeringAvOmsorgsopptjening.kt and OverfoeringAvOmsorgsopptjeningMottaker.kt

---
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/OverfoeringAvOmsorgsopptjeningMottaker.kt
@@ -0,0 +1,16 @@
+package no.nav.pensjon.brev.maler
+
+import no.nav.pensjon.brev.api.model.*
+import no.nav.pensjon.brev.api.model.maler.*
+import no.nav.pensjon.brev.maler.fraser.*
+import no.nav.pensjon.brev.maler.fraser.vedtak.Vedtak
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.VedtaksbrevTemplate
+import no.nav.pensjon.brev.template.base.PensjonLatex
+import no.nav.pensjon.brev.template.dsl.*
+import no.nav.pensjon.brev.template.dsl.expression.*
+import java.time.LocalDate
+
+object OverfoeringAvOmsorgsopptjeningMottaker {
+}
