eea6a08eb8645c274359c94135afd16431c26baa
Håkon <32221678+hconhegg@users.noreply.github.com>
2022-07-29 11:20:30 +0200

    splitt felter i ufoeretrygdPeraMaaned i netto og brutto. (#62)
    Splitter felter i ufoeretrygdPeraMaaned i netto og brutto.

--- pensjon-brevbaker-api-model/build.gradle.kts
+++ pensjon-brevbaker-api-model/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "no.nav.pensjon.brev"
-version = "3.5.2"
+version = "3.5.3"
 
 java {
     withSourcesJar()
--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/MaanedligUfoeretrygdFoerSkatt.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/MaanedligUfoeretrygdFoerSkatt.kt
@@ -10,17 +10,26 @@ data class MaanedligUfoeretrygdFoerSkattDto(
 ) {
     data class UfoeretrygdPerMaaned(
         val annetBelop: Kroner,
-        val barnetillegg: BeloepMedAvkortning?,
+        val barnetillegg: BeloepMedAvkortning?, // TODO remove in next version
+        val barnetilleggBrutto: Kroner?,
+        val barnetilleggNetto: Kroner?,
         val dekningFasteUtgifter: Kroner?,
-        val garantitilleggNordisk27: BeloepMedAvkortning?,
+        val erAvkortet: Boolean,
+        val garantitilleggNordisk27: BeloepMedAvkortning?, // TODO remove in next version
+        val garantitilleggNordisk27Brutto: Kroner?,
+        val garantitilleggNordisk27Netto: Kroner?,
         val grunnbeloep: Kroner,
-        val ordinaerUTBeloep: BeloepMedAvkortning,
-        val totalUTBeloep: BeloepMedAvkortning,
+        val ordinaerUTBeloep: BeloepMedAvkortning, // TODO remove in next version
+        val ordinaerUTBeloepBrutto: Kroner,
+        val ordinaerUTBeloepNetto: Kroner,
+        val totalUTBeloep: BeloepMedAvkortning, // TODO remove in next version
+        val totalUTBeloepBrutto: Kroner,
+        val totalUTBeloepNetto: Kroner,
         val virkningFraOgMed: LocalDate,
         val virkningTilOgMed: LocalDate?,
-        val erAvkortet: Boolean,
     ) {
 
+        // TODO remove in next version
         data class BeloepMedAvkortning(
             val netto: Kroner,
             val brutto: Kroner,
