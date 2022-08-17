ef728fc60ad74a4c5148c17c69488b472bc99c68
Jeremy Elsom <jeremy.elsom@nav.no>
2022-08-17 08:34:17 +0200

    PL-5105: Work in progress

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OpphoerBarnetillegg.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OpphoerBarnetillegg.kt
@@ -304,5 +304,156 @@ object OpphoerBarnetilleggAuto {
             )
         }
     }
-
+    val TBU1286_1 = OutlinePhrase<LangBokmalNynorskEnglish, Unit>   {
+        paragraph {
+            text(
+                Bokmal to "Barnetillegget for barnetbarna som ikke bor sammen med begge foreldrene, blir ikke utbetalt fordi du alene har en samlet inntekt som er høyere enn <BarnetilleggSerkull.inntektstak> kroner. Inntekten din er over grensen for å få utbetalt barnetillegg.",
+                Nynorsk to "Barnetillegget for barnet/barna som ikkje bur saman med begge foreldra sine, blir ikkje utbetalt fordi du åleine har ei samla inntekt som er høgare enn <BarnetilleggSerkull.inntektstak> kroner. Inntekta di er over grensa for å få utbetalt barnetillegg.",
+                English to "You will not receive child supplement for the child/children who do not live together with both parents because your total income on its own is higher than NOK <BarnetilleggSerkull.inntektstak>. You will not receive child supplement because your income exceeds the income limit."
+            )
+        }
+    }
+    val TBU1286_2 = OutlinePhrase<LangBokmalNynorskEnglish, Unit>   {
+        paragraph {
+            text(
+                Bokmal to "Barnetillegget for barnetbarna som bor med begge sine foreldre, blir ikke utbetalt fordi dere har en samlet inntekt som er høyere enn <BarnetilleggFelles.inntektstak> kroner. De samlede inntektene er over grensen for å få utbetalt barnetillegg.",
+                Nynorsk to "Barnetillegget for barnet/barna som bur saman med begge foreldra sine, blir ikkje utbetalt fordi dei har ei samla inntekt som er høgare enn <BarnetilleggFelles.inntektstak> kroner. Dei samla inntektene er over grensa for å få utbetalt barnetillegg.",
+                English to "You will not receive child supplement for the child/children who lives together with both parents because your total income on its own is higher than NOK <BarnetilleggFelles.inntektstak>. You will not receive child supplement because your combined incomes exceed the income limit."
+            )
+        }
+    }
+    val TBU2490 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        paragraph {
+            text(
+                Bokmal to "Barnetillegget for barnetbarna som bor med begge sine foreldre, blir ikke utbetalt fordi de samlede inntektene er høyere enn <BarnetilleggFelles.inntektstak> kroner. Barnetillegget for barnetbarna som ikke bor sammen med begge foreldrene, blir heller ikke utbetalt fordi inntekten din alene er høyere enn <BarnetilleggSerkull.inntektstak> kroner. Inntektene er over grensen for å få utbetalt barnetillegg.",
+                Nynorsk to "Barnetillegget for barnet/barna som bur saman med begge foreldra sine, blir ikkje utbetalt fordi dei samla inntektene er høgare enn <BarnetilleggFelles.inntektstak> kroner. Barnetillegget for barnet/barna som ikkje bur saman med begge foreldra, blir heller ikkje utbetalt fordi inntekta di åleine er høgare enn <BarnetilleggSerkull.inntektstak> kroner. Inntektene er over grensa for å få utbetalt barnetillegg.",
+                English to "You will not receive child supplement for the child/children who lives together with both parents because your total income is higher than NOK <BarnetilleggFelles.inntektstak>. You will not receive child supplement for the child/children who do not live together with both parents because your income alone is higher than NOK <BarnetilleggSerkull.inntektstak>. You will not receive child supplement because your income exceeds the income limit."
+            )
+        }
+    }
+    val TBU1288 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        paragraph {
+            text(
+                Bokmal to "Du kan lese mer om beregningen av barnetillegg i vedlegget «Opplysninger om beregningen».",
+                Nynorsk to "Du kan lese meir om berekninga av barnetillegg i vedlegget «Opplysningar om berekninga».",
+                English to "Read more about how child supplements are calculated in the attachment called \"Information about calculations \".",
+            )
+        }
+    }
+    val TBU2364 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Du må melde fra om eventuell inntekt",
+                Nynorsk to "Du må melde frå om eventuell inntekt",
+                English to "Report any income"
+            )
+        }
+    }
+    val TBU2365 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        paragraph {
+            text(
+                Bokmal to "Dersom du er i jobb eller har planer om å jobbe, må du melde fra om eventuelle endringer i inntekten din. Det er viktig at du melder fra så tidlig som mulig, slik at du får riktig utbetaling av uføretrygd. Dette kan du gjøre under menyvalget «uføretrygd» når du logger deg inn på nav.no. Her kan du legge inn hvor mye du forventer å tjene i løpet av året. Du vil da kunne se hvor mye du vil få utbetalt i uføretrygd ved siden av inntekten din.",
+                Nynorsk to "Dersom du er i jobb eller har planar om å jobbe, må du melde frå om eventuelle endringar i inntekta di. Det er viktig at du melder frå så tidleg som råd, slik at du får rett utbetaling av uføretrygd. Dette kan du gjere under menyvalet «uføretrygd» når du logger deg inn på nav.no. Her kan du leggje inn kor mykje du forventar å tene i løpet av året. Du vil då kunne sjå kor mykje du kjem til å få betalt ut i uføretrygd ved sida av inntekta di.",
+                English to "If you are working or are planning to work, you must report any changes in your income. It is important that you report this as soon as possible, so that you receive the correct disability benefit payments. You can register your change in income under the option “uføretrygd” at nav.no. You can register how much you expect to earn in the calendar year. You will then be able to see how much disability benefit you will receive in addition to your income."
+            )
+        }
+    }
+    val TBU2212 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Du må melde fra om endringer",
+                Nynorsk to "Du må melde frå om endringar",
+                English to "You must notify any changes"
+            )
+        }
+        paragraph {
+            text(
+                Bokmal to "Skjer det endringer, må du melde fra til oss med en gang. I vedlegget «Orientering om rettigheter og plikter» ser du hvilke endringer du må si fra om.",
+                Nynorsk to "Skjer det endringar, må du melde frå til oss med ein gong. I vedlegget «Orientering om rettar og plikter» ser du kva endringar du må seie frå om.",
+                English to "You must notify us immediately of any changes in your situation. In the attachment “Information about rights and obligations” you will see which changes you must report."
+            )
+        }
+    }
+    val TBU2213 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Du har rett til å klage",
+                Nynorsk to "Du har rett til å klage",
+                English to "You have the right of appeal"
+            )
+        }
+        paragraph {
+            text(
+                Bokmal to "Hvis du mener vedtaket er feil, kan du klage. Fristen for å klage er seks uker fra den datoen du mottok vedtaket. I vedlegget «Orientering om rettigheter og plikter» får du vite mer om hvordan du går fram. Du finner skjema og informasjon på nav.no/klage.",
+                Nynorsk to "Viss du meiner vedtaket er feil, kan du klage. Fristen for å klage er seks veker frå den datoen du fekk vedtaket. I vedlegget «Orientering om rettar og plikter» får du vite meir om korleis du går fram. Du finn skjema og informasjon på nav.no/klage.",
+                English to "If you believe the decision is wrong, you may appeal. The deadline for appeal is six weeks from the date you received the decision. In the attachment “Information about rights and obligations”, you can find out more about how to proceed. You will find forms and information at nav.no/klage."
+            )
+        }
+    }
+    val TBU1074 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Du har rett til innsyn",
+                Nynorsk to "Du har rett til innsyn",
+                English to "You have right of access"
+            )
+        }
+    }
+    val TBU2242 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        paragraph {
+            text(
+                Bokmal to "Du har rett til å se dokumentene i saken din. Se vedlegg «Orientering om rettigheter og plikter» for informasjon om hvordan du går fram.",
+                Nynorsk to "Du har rett til å sjå dokumenta i saka di. Sjå vedlegg «Orientering om rettar og plikter» for informasjon om korleis du går fram.",
+                English to "You are entitled to see your case documents. Refer to the attachment “Rights and obligations” for information about how to proceed."
+            )
+        }
+    }
+    val TBU1227 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Sjekk utbetalingene dine",
+                Nynorsk to "Sjekk utbetalingane dine",
+                English to "Check your disability benefit payments"
+            )
+        }
+        paragraph {
+            text(
+                Bokmal to "Du får uføretrygd utbetalt den 20 hver måned eller senest siste virkedag før denne datoen. Se alle utbetalinger du har mottatt: nav.no/uforetrygd. Her kan du også endre kontonummer.",
+                Nynorsk to "Du får uføretrygd utbetalt den 20. i kvar månad eller seinast siste vyrkedag før denne datoen. Sjå alle utbetalingar du har fått: nav.no/uforetrygd. Her kan du også endre kontonummer.",
+                English to "Your disability benefit will be paid on the 20th of each month or no later than the last business day before this date. To see all the payments you have received, go to: nav.no/uforetrygd. You may also change your account number here."
+            )
+        }
+    }
+    val TBU1228 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Skattekort",
+                Nynorsk to "Skattekort",
+                English to "Tax card"
+            )
+        }
+        paragraph {
+            text(
+                Bokmal to "Uføretrygd skattlegges som lønnsinntekt. Du trenger ikke levere skattekortet ditt til NAV fordi skatteopplysningene dine sendes elektronisk fra Skatteetaten. Du bør likevel sjekke at du har riktig skattekort. Skattekortet kan du endre på skatteetaten.no. Under menyvalget «uføretrygd» når du logger deg inn på nav.no, kan du se hvilket skattetrekk som er registrert hos NAV.",
+                Nynorsk to "Uføretrygd blir skattlagd som lønsinntekt. Du treng ikkje levere skattekortet ditt til NAV, fordi skatteopplysningane dine blir sende elektronisk frå Skatteetaten. Du bør likevel sjekke at du har rett skattekort. Skattekortet kan du endre på skatteetaten.no. Under menyvalet «uføretrygd» når du logger deg inn på nav.no, kan du sjå kva skattetrekk som er registrert hos NAV.",
+                English to "You do not need to submit your tax card to NAV because your tax details are sent electronically from the Norwegian Tax Administration. However, you should check that you have the correct tax card. You may change your tax card under skatteetaten.no. You may see your registered income tax rate under the option “uføretrygd” at nav.no."
+            )
+        }
+    }
+    val TBU3730 = OutlinePhrase<LangBokmalNynorskEnglish, Unit> {
+        title1 {
+            text(
+                Bokmal to "Skatt for deg som bor i utlandet",
+                Nynorsk to "Skatt for deg som bur i utlandet",
+                English to "Tax for people who live abroad"
+            )
+        }
+        paragraph {
+            text(
+                Bokmal to "Bor du i utlandet og betaler kildeskatt, finner du mer informasjon om kildeskatt på skatteetaten.no. Hvis du er bosatt i utlandet og betaler skatt i annet land enn Norge, kan du kontakte skattemyndighetene der du bor.",
+                Nynorsk to "Bur du i utlandet og betaler kjeldeskatt, finn du meir informasjon om kjeldeskatt på skatteetaten.no. Viss du er busett i utlandet og betaler skatt i eit anna land enn Noreg, kan du kontakte skattemyndigheitene der du bur.",
+                English to "You can find more information about withholding tax to Norway at skatteetaten.no. For information about taxation from your country of residence, you can contact the locale tax authorities."
+            )
+        }
+    }
 }
