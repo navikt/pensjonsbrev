5255a8bff0efeab377dd22098b973cf4e74b0326
Håkon Heggholmen <hakon.heggholmen@nav.no>
2022-08-18 14:30:05 +0200

    PL-5812: Skiller ut EgenerklaeringOmsorgsarbeidDto inn i egen dto og legger til returadresse.

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/OmsorgEgenAuto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/OmsorgEgenAuto.kt
@@ -1,6 +1,11 @@
 package no.nav.pensjon.brev.api.model.maler
 
 import no.nav.pensjon.brev.api.model.*
+import no.nav.pensjon.brev.api.model.vedlegg.EgenerklaeringOmsorgsarbeidDto
 
 @Suppress("unused")
-data class OmsorgEgenAutoDto(val aarEgenerklaringOmsorgspoeng: Year, val aarInnvilgetOmsorgspoeng: Year)
+data class OmsorgEgenAutoDto(
+    val aarEgenerklaringOmsorgspoeng: Year,
+    val aarInnvilgetOmsorgspoeng: Year,
+    val egenerklaeringOmsorgsarbeidDto: EgenerklaeringOmsorgsarbeidDto
+)
