914d2897e547125866b57f02ff66d56cec538712
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2022-08-30 13:31:23 +0200

    Endre writeTestHTML

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/template/base/PensjonHTMLTest.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/template/base/PensjonHTMLTest.kt
@@ -1,8 +1,6 @@
 package no.nav.pensjon.brev.template.base
 
 import no.nav.pensjon.brev.*
-import no.nav.pensjon.brev.api.model.maler.UfoerOmregningEnsligDto
-import no.nav.pensjon.brev.maler.UfoerOmregningEnslig
 import no.nav.pensjon.brev.maler.example.*
 import no.nav.pensjon.brev.template.*
 import no.nav.pensjon.brev.template.render.PensjonHTMLRenderer
@@ -17,19 +15,6 @@ class PensjonHTMLTest {
             Language.Bokmal,
             Fixtures.fellesAuto
         ).let { PensjonHTMLRenderer.render(it) }
-            .let { it.base64EncodedFiles()["index.html"]!! }
             .also { writeTestHTML("DESIGN_REFERENCE_LETTER", it) }
-    }
-
-    @Test
-    fun renderUfoerOmregningEnslig() {
-        Letter(
-            UfoerOmregningEnslig.template,
-            Fixtures.create<UfoerOmregningEnsligDto>(),
-            Language.Bokmal,
-            Fixtures.fellesAuto
-        ).let { PensjonHTMLRenderer.render(it) }
-            .let { it.base64EncodedFiles()["index.html"]!! }
-            .also { writeTestHTML("UFOER_OMREGNING_ENSLIG", it) }
     }
 }
