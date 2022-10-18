39af0f3bc352895b265c19ab7bcdded7a1cc3742
Jeremy Elsom <jeremy.elsom@nav.no>
2022-10-18 11:01:28 +0200

    PL-6066: Updated api-model with new version of snapshot for harInnvilgetBarnetillegg

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/OrienteringOmRettigheterUfoere.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/OrienteringOmRettigheterUfoere.kt
@@ -5,9 +5,10 @@ import no.nav.pensjon.brev.api.model.Kroner
 import no.nav.pensjon.brev.api.model.Sivilstand
 
 data class OrienteringOmRettigheterUfoereDto(
-    val bruker_borINorge: Boolean,
-    val institusjon_gjeldende: Institusjon,
     val avdoed_sivilstand: Sivilstand,
-    val ufoeretrygdPerMaaned_barnetilleggGjeldende: Kroner?,
+    val bruker_borINorge: Boolean,
+    val harInnvilgetBarnetillegg: Boolean,
+    val institusjon_gjeldende: Institusjon,
+    val ufoeretrygdPerMaaned_barnetilleggGjeldende: Kroner?
 )
 
