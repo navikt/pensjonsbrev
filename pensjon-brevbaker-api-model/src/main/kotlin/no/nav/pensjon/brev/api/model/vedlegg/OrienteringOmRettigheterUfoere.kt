cd5d7f9724705688b378c7d3211cc26a72e815df
Jeremy Elsom <jeremy.elsom@nav.no>
2022-10-28 16:05:54 +0200

    PL-6066:

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/OrienteringOmRettigheterUfoere.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/OrienteringOmRettigheterUfoere.kt
@@ -1,10 +1,11 @@
 package no.nav.pensjon.brev.api.model.vedlegg
 
 import no.nav.pensjon.brev.api.model.Institusjon
-import no.nav.pensjon.brev.api.model.Kroner
 import no.nav.pensjon.brev.api.model.Sivilstand
 
 data class OrienteringOmRettigheterUfoereDto(
+    val antallFellesbarnInnvilget: Int,
+    val antallSaerkullsbarnInnvilget: Int,
     val avdoed_sivilstand: Sivilstand,
     val bruker_borINorge: Boolean,
     val harInnvilgetBarnetillegg: Boolean,
