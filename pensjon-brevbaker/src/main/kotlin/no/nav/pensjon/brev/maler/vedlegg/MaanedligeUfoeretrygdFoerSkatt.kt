3c0923a64bdf4c362121e82337e78660603c0c36
Håkon Heggholmen <hakon.heggholmen@nav.no>
2022-11-23 13:25:35 +0100

    PL-6176: Endrer innhold og styling av vedlegg MaanedligeUfoeretrygdFoerSkatt. Bruker includeVedleggIfNotNull istedenfor å vurdere om vedlegget skal være med som en del av malen. Det er nå bestiller som velger om vedlegg skal være med.

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/MaanedligeUfoeretrygdFoerSkatt.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/MaanedligeUfoeretrygdFoerSkatt.kt
@@ -1,17 +1,13 @@
 package no.nav.pensjon.brev.maler.vedlegg
 
 import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDto
-import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDtoSelectors.gjeldendeBeregnetUTPerMaaned
-import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDtoSelectors.krav_virkningsDatoFraOgMed
-import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDtoSelectors.tidligereUfoeretrygdPerioder
+import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDtoSelectors.ufoeretrygdPerioder
 import no.nav.pensjon.brev.maler.fraser.VedleggMaanedligeUfoeretrgdFoerSkatt
 import no.nav.pensjon.brev.template.LangBokmalNynorskEnglish
 import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.createAttachment
 import no.nav.pensjon.brev.template.dsl.expression.*
 import no.nav.pensjon.brev.template.dsl.newText
-import no.nav.pensjon.brev.template.dsl.text
-import no.nav.pensjon.brev.template.dsl.textExpr
 
 val vedleggMaanedligUfoeretrygdFoerSkatt = createAttachment<LangBokmalNynorskEnglish, MaanedligUfoeretrygdFoerSkattDto>(
     title = newText(
@@ -23,26 +19,8 @@ val vedleggMaanedligUfoeretrygdFoerSkatt = createAttachment<LangBokmalNynorskEng
 
     includePhrase(VedleggMaanedligeUfoeretrgdFoerSkatt.VedleggBelopUT_001)
 
-    includePhrase(VedleggMaanedligeUfoeretrgdFoerSkatt.TabellBeregnetUTHele(gjeldendeBeregnetUTPerMaaned))
-
-    showIf(tidligereUfoeretrygdPerioder.isNotEmpty()) {
-        title1 {
-            textExpr(
-                Bokmal to "Oversikt over uføretrygdens størrelse fra ".expr() + krav_virkningsDatoFraOgMed.format(),
-                Nynorsk to "Oversikt over storleik på uføretrygda frå ".expr() + krav_virkningsDatoFraOgMed.format(),
-                English to "Disability benefit payment specifications as of ".expr() + krav_virkningsDatoFraOgMed.format(),
-            )
-        }
-
-        paragraph {
-            text(
-                Bokmal to "Nedenfor ser du den månedlige uføretrygden din for tidligere perioder.",
-                Nynorsk to "Nedanfor ser du uføretrygda di kvar månad for tidligare periodar.",
-                English to "Below is a presentation of your monthly disability benefit for your previous periods.",
-            )
-        }
-
-        forEach(tidligereUfoeretrygdPerioder) {
+    showIf(ufoeretrygdPerioder.isNotEmpty()) {
+        forEach(ufoeretrygdPerioder) {
             includePhrase(VedleggMaanedligeUfoeretrgdFoerSkatt.TabellBeregnetUTHele(it))
         }
     }
