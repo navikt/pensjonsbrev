f36e28c310e06e4b7283d57b2a082d16cfb2586a
Håkon Heggholmen <hakon.heggholmen@nav.no>
2022-11-23 14:20:32 +0100

    PL-6176: Unit test.

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/template/dsl/IncludeAttachmentTest.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/template/dsl/IncludeAttachmentTest.kt
@@ -4,23 +4,26 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import no.nav.pensjon.brev.template.*
 import no.nav.pensjon.brev.template.Language.Nynorsk
+import no.nav.pensjon.brev.template.dsl.NullDataSelectors.test
 import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.notNull
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import org.junit.jupiter.api.Test
 
 class IncludeAttachmentTest {
-    private val testVedlegg = createAttachment<LangNynorsk, Unit>(
-        title = newText(
-            Nynorsk to "Test vedlegg",
-        ),
-        includeSakspart = true
-    ) {
-        paragraph {
-            text(Nynorsk to "test")
-        }
-    }
-
     @Test
     fun `attachment is included in template`() {
+        val testVedlegg = createAttachment<LangNynorsk, Unit>(
+            title = newText(
+                Nynorsk to "Test vedlegg",
+            ),
+            includeSakspart = true
+        ) {
+            paragraph {
+                text(Nynorsk to "test")
+            }
+        }
+
         val expected = LetterTemplate(
             name = "test",
             title = listOf(newText(Nynorsk to "tittel")),
@@ -44,6 +47,50 @@ class IncludeAttachmentTest {
             includeAttachment(testVedlegg, Unit.expr(), false.expr())
         }
 
-        assertThat(expected, equalTo(actual))
+        assertThat(actual, equalTo(expected))
+    }
+
+    data class NullData(val test: String?)
+
+    @TemplateModelHelpers
+    object Helpers : HasModel<NullData>
+
+    @Test
+    fun `attachment is not included when using includeAttachmentIfNotNull and attachmentData is null`() {
+        val selector: Expression<String?> = Expression.FromScope(ExpressionScope<NullData, *>::argument).test
+        val testVedlegg = createAttachment<LangNynorsk, String>(
+            title = newText(
+                Nynorsk to "Test vedlegg",
+            ),
+            includeSakspart = true
+        ) {
+            paragraph {
+                text(Nynorsk to "test")
+            }
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        val expected = LetterTemplate(
+            name = "test",
+            title = listOf(newText(Nynorsk to "tittel")),
+            letterDataType = NullData::class,
+            language = languages(Nynorsk),
+            outline = emptyList(),
+            attachments = listOf(
+                IncludeAttachment(selector as Expression<String>, testVedlegg, selector.notNull())
+            ), letterMetadata = testLetterMetadata
+        )
+        val actual = createTemplate(
+            name = "test",
+            letterDataType = NullData::class,
+            languages = languages(Nynorsk),
+            letterMetadata = testLetterMetadata,
+        ) {
+            title { text(Nynorsk to "tittel") }
+            outline {}
+            includeAttachmentIfNotNull(testVedlegg, test)
+        }
+
+        assertThat(actual, equalTo(expected))
     }
 }
