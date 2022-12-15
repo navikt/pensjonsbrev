9625a5b4e3d4e53a3e5fbd4bc8f3501368915288
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2022-12-15 16:12:49 +0100

    Legg til collection mapping og lokalistert opplisting

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/template/OperationsTest.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/template/OperationsTest.kt
@@ -15,4 +15,31 @@ class OperationsTest {
     fun `different operations are not considered equal`() {
         assertNotEquals(BinaryOperation.Equal<Int>(), UnaryOperation.ToString<String>())
     }
+
+    @Test
+    fun `LocalizedCollectionFormat uses comma and localized separator before last item`() {
+        val list = listOf("Alexander", "Jeremy", "Håkon", "Agne")
+
+        assertEquals("Alexander, Jeremy, Håkon og Agne", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.Bokmal))
+        assertEquals("Alexander, Jeremy, Håkon og Agne", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.Nynorsk))
+        assertEquals("Alexander, Jeremy, Håkon and Agne", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.English))
+    }
+
+    @Test
+    fun `LocalizedCollectionFormat has no separator for only one item`() {
+        val list = listOf("Agne")
+
+        assertEquals("Agne", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.Bokmal))
+        assertEquals("Agne", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.Nynorsk))
+        assertEquals("Agne", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.English))
+    }
+
+    @Test
+    fun `LocalizedCollectionFormat has no comma for only two items`() {
+        val list = listOf("Agne", "Jeremy")
+
+        assertEquals("Agne og Jeremy", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.Bokmal))
+        assertEquals("Agne og Jeremy", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.Nynorsk))
+        assertEquals("Agne and Jeremy", BinaryOperation.LocalizedCollectionFormat.apply(list, Language.English))
+    }
 }
