25e737665f7badd35ae31eee9330080081aa823d
Jeremy Elsom <jeremy.elsom@nav.no>
2023-01-13 14:27:05 +0100

    Updated Brevkode.kt with UT_OPPHOER_BT_AUTO and UT_ENDRING_OPPTJENING_AUTO Created EndringOpptjeningAuto.kt

---
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/EndringOpptjeningAuto.kt
@@ -0,0 +1,56 @@
+package no.nav.pensjon.brev.api.model.maler
+
+import no.nav.pensjon.brev.api.model.Kroner
+import java.time.LocalDate
+
+@Suppress("unused")
+data class EndringOpptjeningAutoDto(
+    val barnetilleggFellesbarn: BarnetilleggFellesbarn?,
+    val barnetilleggSaerkullsbarn: BarnetilleggSaerkullsbarn?,
+    val endringIOpptjening: EndringIOpptjening,
+    val ufoeretrygd: Ufoeretrygd, // in use
+    val virkningsDato: LocalDate,  // in use
+) {
+
+    data class EndringIOpptjening(
+        val ufoerBeloepOekt: Boolean,  // in use
+        val ufoerBeloepRedusert: Boolean,  // in use
+    )
+
+    data class Ufoeretrygd(
+        val ektefelletilleggUtbeltalt: Kroner?,
+        val gjenlevendetilleggUtbetalt: Kroner?,
+        val harUtbetalingsgrad: Boolean,
+        val innvilgetEktefelleTillegg: Boolean,  // in use
+        val innvilgetGjenlevendeTillegg: Boolean,  // in use
+        val ufoertrygdUtbetalt: Kroner,
+        val utbetaltPerMaaned: Kroner,
+        val yrkesskadeGrad: Int,  // in use
+    )
+
+    data class BarnetilleggFellesbarn(
+        val beloepBrutto: Kroner,
+        val beloepNetto: Kroner,
+        val fribeloep: Kroner,
+        val gjelderFlereBarn: Boolean,
+        val harFradrag: Boolean,
+        val harFratrukketBeloepFraAnnenForelder: Boolean,
+        val harJusteringsbeloep: Boolean,
+        val inntektAnnenForelder: Kroner,
+        val inntektBruktIAvkortning: Kroner,
+        val inntektstak: Kroner,
+        val innvilgetTilleggFellesbarn: Boolean,  // in use
+    )
+
+    data class BarnetilleggSaerkullsbarn(
+        val beloepBrutto: Kroner,
+        val beloepNetto: Kroner,
+        val fribeloep: Kroner,
+        val gjelderFlereBarn: Boolean,
+        val harFradrag: Boolean,
+        val harJusteringsbeloep: Boolean,
+        val inntektBruktIAvkortning: Kroner,
+        val inntektstak: Kroner,
+        val innvilgetTilleggSaerkullsbarn: Boolean,  // in use
+    )
+}
