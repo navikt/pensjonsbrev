387c7089c39c64d6fa7b3ef14fa5dff8b9ae5cdc
Håkon Heggholmen <hakon.heggholmen@nav.no>
2023-01-24 13:10:22 +0100

    lager funksjon for å lage test-template for vedlegg.

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/OpplysningerBruktIBeregningUT.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/OpplysningerBruktIBeregningUT.kt
@@ -4,15 +4,12 @@ import kotlinx.coroutines.runBlocking
 import no.nav.pensjon.brev.Fixtures
 import no.nav.pensjon.brev.PDF_BUILDER_URL
 import no.nav.pensjon.brev.TestTags
-import no.nav.pensjon.brev.api.model.LetterMetadata
 import no.nav.pensjon.brev.api.model.vedlegg.OpplysningerBruktIBeregningUTDto
 import no.nav.pensjon.brev.latex.LaTeXCompilerService
 import no.nav.pensjon.brev.template.Language
 import no.nav.pensjon.brev.template.Letter
-import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.createVedleggTestTemplate
 import no.nav.pensjon.brev.template.dsl.expression.expr
-import no.nav.pensjon.brev.template.dsl.languages
-import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.render.PensjonLatexRenderer
 import no.nav.pensjon.brev.writeTestPDF
 import org.junit.jupiter.api.Tag
@@ -23,32 +20,10 @@ class OpplysningerBruktIBeregningUTTest {
 
     @Test
     fun testVedlegg() {
-        val template = createTemplate(
-            name = "test-template",
-            letterDataType = Unit::class,
-            languages = languages(Language.Bokmal, Language.Nynorsk, Language.English),
-            letterMetadata = LetterMetadata(
-                "test mal",
-                isSensitiv = false,
-                distribusjonstype = LetterMetadata.Distribusjonstype.ANNET,
-            ),
-        ) {
-            title {
-                text(
-                    Language.Bokmal to "Tittel",
-                    Language.Nynorsk to "Tittel",
-                    Language.English to "Title"
-                )
-            }
-
-            outline {
-            }
-
-            includeAttachment(
-                createVedleggOpplysningerBruktIBeregningUT(skalViseMinsteytelse = true, skalViseBarnetillegg = true),
-                Fixtures.create(OpplysningerBruktIBeregningUTDto::class).expr()
-            )
-        }
+        val template = createVedleggTestTemplate(
+            createVedleggOpplysningerBruktIBeregningUT(skalViseMinsteytelse = true, skalViseBarnetillegg = true),
+            Fixtures.create(OpplysningerBruktIBeregningUTDto::class).expr()
+        )
         Letter(
             template,
             Unit,
--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/OrienteringOmRettigheterUfoere.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/OrienteringOmRettigheterUfoere.kt
@@ -4,15 +4,12 @@ import kotlinx.coroutines.runBlocking
 import no.nav.pensjon.brev.Fixtures
 import no.nav.pensjon.brev.PDF_BUILDER_URL
 import no.nav.pensjon.brev.TestTags
-import no.nav.pensjon.brev.api.model.LetterMetadata
 import no.nav.pensjon.brev.api.model.vedlegg.OrienteringOmRettigheterUfoereDto
 import no.nav.pensjon.brev.latex.LaTeXCompilerService
 import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.Letter
-import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.createVedleggTestTemplate
 import no.nav.pensjon.brev.template.dsl.expression.expr
-import no.nav.pensjon.brev.template.dsl.languages
-import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.render.PensjonLatexRenderer
 import no.nav.pensjon.brev.writeTestPDF
 import org.junit.jupiter.api.Tag
@@ -23,42 +20,19 @@ class OrienteringOmRettigheterUfoereTest {
 
     @Test
     fun testVedlegg() {
-        val template = createTemplate(
-            name = "test-template",
-            letterDataType = Unit::class,
-            languages = languages(Bokmal, Nynorsk, English),
-            letterMetadata = LetterMetadata(
-                "test mal",
-                isSensitiv = false,
-                distribusjonstype = LetterMetadata.Distribusjonstype.ANNET,
-            ),
-        ) {
-            title {
-                text(
-                    Bokmal to "Tittel",
-                    Nynorsk to "Tittel",
-                    English to "Title"
-                )
-            }
-            outline {
-
-            }
-
-
-            includeAttachment(
-                vedleggOrienteringOmRettigheterOgPlikterUfoere,
-                Fixtures.create(OrienteringOmRettigheterUfoereDto::class).expr()
-            )
-        }
-            Letter(
-                template,
-                Unit,
-                Nynorsk,
-                Fixtures.fellesAuto
-            )
-                .let { PensjonLatexRenderer.render(it) }
-                .let { runBlocking { LaTeXCompilerService(PDF_BUILDER_URL).producePDF(it, "test").base64PDF } }
-                .also { writeTestPDF("OrienteringOmRettigheterUfoere", it) }
+        val template = createVedleggTestTemplate(
+            vedleggOrienteringOmRettigheterOgPlikterUfoere,
+            Fixtures.create(OrienteringOmRettigheterUfoereDto::class).expr()
+        )
+        Letter(
+            template,
+            Unit,
+            Nynorsk,
+            Fixtures.fellesAuto
+        )
+            .let { PensjonLatexRenderer.render(it) }
+            .let { runBlocking { LaTeXCompilerService(PDF_BUILDER_URL).producePDF(it, "test").base64PDF } }
+            .also { writeTestPDF("OrienteringOmRettigheterUfoere", it) }
 
     }
 }
