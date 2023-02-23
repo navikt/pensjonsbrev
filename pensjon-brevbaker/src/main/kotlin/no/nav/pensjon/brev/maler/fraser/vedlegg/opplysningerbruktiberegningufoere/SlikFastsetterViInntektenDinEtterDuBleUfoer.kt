7d9f195a8eddf1077983978ce3fc51467d1c757e
Jeremy Elsom <jeremy.elsom@nav.no>
2023-02-23 10:35:04 +0100

    Updated SlikFastsetterViInntektenDinEtterDuBleUfoer.kt

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/vedlegg/opplysningerbruktiberegningufoere/SlikFastsetterViInntektenDinEtterDuBleUfoer.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/vedlegg/opplysningerbruktiberegningufoere/SlikFastsetterViInntektenDinEtterDuBleUfoer.kt
@@ -6,7 +6,7 @@ import no.nav.pensjon.brev.template.OutlinePhrase
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
 import no.nav.pensjon.brev.template.dsl.text
 
-//TBU054V
+//TBU054V, TBU055V
 // IF brevkode not(PE_UT_04_108) and not(PE_UT_04_109), THEN INCLUDE
 object SlikFastsetterViInntektenDinEtterDuBleUfoer : OutlinePhrase<LangBokmalNynorskEnglish>() {
     override fun OutlineOnlyScope<LangBokmalNynorskEnglish, Unit>.template() {
@@ -29,6 +29,21 @@ object SlikFastsetterViInntektenDinEtterDuBleUfoer : OutlinePhrase<LangBokmalNyn
                 Bokmal to "Folketrygdens grunnbeløp endres hvert år, og inntekten din før og etter at du ble ufør blir justert ut fra dette.",
                 Nynorsk to "Grunnbeløpet i folketrygda blir endra kvart år, og inntekta di før og etter at du blei ufør, blir justert ut frå dette.",
                 English to "The National Insurance basic amount changes every year, and your income prior to and after your disability will be adjusted in accordance with this."
+            )
+        }
+
+        title1 {
+            text(
+                Bokmal to "Slik fastsetter vi uføregraden din",
+                Nynorsk to "Slik fastset vi uføregraden din",
+                English to "This is how we establish your degree of disability"
+            )
+        }
+        paragraph {
+            text(
+                Bokmal to "Uføregraden din fastsetter vi ved å sammenligne inntekten din før og etter at du ble ufør. Har du ikke inntektsevne, setter vi uføregraden til 100 prosent. Har du fortsatt inntektsevne, vil uføregraden tilsvare inntektsevnen din som du har tapt. Uføregraden din graderes i trinn på fem prosentpoeng. Vi vurderer alltid om uføregraden skal settes lavere enn 100 prosent.",
+                Nynorsk to "Uføregraden din fastset vi ved å samanlikne inntekta di før og etter at du blei ufør. Dersom du ikkje har inntektsevne, set vi uføregraden til 100 prosent. Har du framleis inntektsevne, tilsvarer uføregraden den inntektsevna di som du har tapt. Uføregraden din blir gradert i trinn på fem prosentpoeng. Vi vurderer alltid om uføregraden skal setjast lågare enn 100 prosent.",
+                English to "Your degree of disability is established by comparing your income before and after your disability. If you have no earning ability, your degree of disability will be fixed at 100 percent. If you still have some earning ability, your degree of disability will correspond to your loss of earning ability. Your degree of disability is graduated on a scale in increments of 5 percentage points. We always evaluate whether the degree of disability should be reduced from 100 percent."
             )
         }
     }
