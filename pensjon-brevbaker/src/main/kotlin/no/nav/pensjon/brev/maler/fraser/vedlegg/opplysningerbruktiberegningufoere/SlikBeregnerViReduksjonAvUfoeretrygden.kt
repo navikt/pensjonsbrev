82a67284cc8e2f3f18b6e058c4cd4783533b7fd1
Jeremy Elsom <jeremy.elsom@nav.no>
2023-05-05 22:00:00 +0200

    WIP

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/vedlegg/opplysningerbruktiberegningufoere/SlikBeregnerViReduksjonAvUfoeretrygden.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/vedlegg/opplysningerbruktiberegningufoere/SlikBeregnerViReduksjonAvUfoeretrygden.kt
@@ -1,17 +1,10 @@
 package no.nav.pensjon.brev.maler.fraser.vedlegg.opplysningerbruktiberegningufoere
 
 import no.nav.pensjon.brev.api.model.KravAarsakType
-import no.nav.pensjon.brev.api.model.vedlegg.InntektsAvkortingGjeldendeSelectors.forventetInntektAar
-import no.nav.pensjon.brev.api.model.vedlegg.InntektsAvkortingGjeldendeSelectors.inntektsgrenseAar
-import no.nav.pensjon.brev.api.model.vedlegg.InntektsAvkortingGjeldendeSelectors.inntektstak
 import no.nav.pensjon.brev.api.model.vedlegg.OpplysningerBruktIBeregningUTDto
-import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdGjeldendeSelectors.kompensasjonsgrad
-import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdOrdinaerSelectors.harGammelUTBeloepUlikNyUTBeloep
-import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdOrdinaerSelectors.harGammelUTBeloepUlikNyUTBeloep_safe
-import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdOrdinaerSelectors.harNyUTBeloep
-import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdOrdinaerSelectors.harNyUTBeloep_safe
 import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdOrdinaerSelectors.nettoPerAarReduksjonUT_safe
 import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdOrdinaerSelectors.overskytendeInntekt_safe
+import no.nav.pensjon.brev.api.model.vedlegg.UfoeretrygdSelectors.kompensasjonsgrad
 import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.template.LangBokmalNynorskEnglish
 import no.nav.pensjon.brev.template.Expression
@@ -22,55 +15,40 @@ import no.nav.pensjon.brev.template.dsl.expression.*
 import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.dsl.textExpr
 
-/* IF KravAarsakType = "ENDRET_INNTEKT"
-AND
-harGammelUTBeloepUlikNyUTBeloep
-AND
-harInntektsgrenseLessThanInntektstak,
-AND
-brevkode <> PE_UT_04_108, PE_UT_04_109,
-AND
-harNyUTBeloep */
+// brevkode <> PE_UT_04_108, PE_UT_04_109,
+
 data class SlikBeregnerViReduksjonAvUfoeretrygden(
-    val inntektsAvkortingGjeldende: Expression<OpplysningerBruktIBeregningUTDto.InntektsAvkortingGjeldende>,
     val kravAarsakType: Expression<KravAarsakType>,
-    val ufoeretrygdGjeldende: Expression<OpplysningerBruktIBeregningUTDto.UfoeretrygdGjeldende>,
+    val ufoeretrygd: Expression<OpplysningerBruktIBeregningUTDto.Ufoeretrygd>,
     val ufoeretrygdOrdinaer: Expression<OpplysningerBruktIBeregningUTDto.UfoeretrygdOrdinaer>,
 
     ) : OutlinePhrase<LangBokmalNynorskEnglish>() {
     override fun OutlineOnlyScope<LangBokmalNynorskEnglish, Unit>.template() {
-        val kompensasjonsgrad = ufoeretrygdGjeldende.kompensasjonsgrad.format()
-        val harInntektsgrenseLessThanInntektstak =
-            inntektsAvkortingGjeldende.inntektsgrenseAar.lessThan(inntektsAvkortingGjeldende.inntektstak)
+        val kompensasjonsgrad = ufoeretrygd.kompensasjonsgrad.format()
 
-        showIf(
-            (kravAarsakType.isOneOf(KravAarsakType.ENDRET_INNTEKT)) and ufoeretrygdOrdinaer.harGammelUTBeloepUlikNyUTBeloep
-                    and inntektsAvkortingGjeldende.forventetInntektAar.greaterThanOrEqual(inntektsAvkortingGjeldende.inntektsgrenseAar)
-                    and harInntektsgrenseLessThanInntektstak and ufoeretrygdOrdinaer.harNyUTBeloep
-        ) {
-            title1 {
-                text(
-                    Bokmal to "Slik beregner vi reduksjonen av uføretrygden",
-                    Nynorsk to "Slik bereknar vi reduksjonen av uføretrygda",
-                    English to "This is how the reduction in your disability benefit is calculated"
+        title1 {
+            text(
+                Bokmal to "Slik beregner vi reduksjonen av uføretrygden",
+                Nynorsk to "Slik bereknar vi reduksjonen av uføretrygda",
+                English to "This is how the reduction in your disability benefit is calculated"
+            )
+        }
+        ifNotNull(
+            ufoeretrygdOrdinaer.overskytendeInntekt_safe,
+            ufoeretrygdOrdinaer.nettoPerAarReduksjonUT_safe
+        ) { overskytendeInntekt, nettoPerAarReduksjonUT ->
+            paragraph {
+                textExpr(
+                    Bokmal to "".expr() + overskytendeInntekt.format() + " kr x ".expr() + kompensasjonsgrad + " % = ".expr() +
+                            nettoPerAarReduksjonUT.format() + " kroner i reduksjon for året".expr(),
+                    Nynorsk to "".expr() + overskytendeInntekt.format() + " kr x ".expr() + kompensasjonsgrad + " % = ".expr() +
+                            nettoPerAarReduksjonUT.format() + " kroner i reduksjon for året".expr(),
+                    English to "NOK ".expr() + overskytendeInntekt.format() + " kr x ".expr() + kompensasjonsgrad + " % = NOK ".expr() +
+                            nettoPerAarReduksjonUT.format() + " in reductions for the year".expr()
                 )
-            }
-            ifNotNull(
-                ufoeretrygdOrdinaer.overskytendeInntekt_safe,
-                ufoeretrygdOrdinaer.nettoPerAarReduksjonUT_safe
-            ) { overskytendeInntekt, nettoPerAarReduksjonUT ->
-                paragraph {
-                    textExpr(
-                        Bokmal to "".expr() + overskytendeInntekt.format() + " kr x ".expr() + kompensasjonsgrad + " % = ".expr() +
-                                nettoPerAarReduksjonUT.format() + " kroner i reduksjon for året".expr(),
-                        Nynorsk to "".expr() + overskytendeInntekt.format() + " kr x ".expr() + kompensasjonsgrad + " % = ".expr() +
-                                nettoPerAarReduksjonUT.format() + " kroner i reduksjon for året".expr(),
-                        English to "NOK ".expr() + overskytendeInntekt.format() + " kr x ".expr() + kompensasjonsgrad + " % = NOK ".expr() +
-                                nettoPerAarReduksjonUT.format() + " in reductions for the year".expr()
-                    )
-                }
             }
         }
     }
 }
+
 
