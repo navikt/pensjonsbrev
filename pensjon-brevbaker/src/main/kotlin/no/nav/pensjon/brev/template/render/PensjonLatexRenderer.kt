de6f388a21d1fc21f9a02849ae149fb4b2e9109b
Håkon <32221678+hconhegg@users.noreply.github.com>
2023-05-11 13:02:54 +0200

    Bugfix/saksbehandler signatur (#146)
    Escape saksbehandler navn, legg til space mellom \feltclosingsaksbehandlersuffix og par.

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/render/PensjonLatexRenderer.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/render/PensjonLatexRenderer.kt
@@ -91,14 +91,16 @@ object PensjonLatexRenderer : LetterRenderer<RenderedLatexLetter>() {
     private fun LatexAppendable.signaturCommands(saksbehandlere: SignerendeSaksbehandlere?, brevtype: LetterMetadata.Brevtype) {
         appendNewCmd("closingbehandlet") {
             if (saksbehandlere != null) {
-                saksbehandlere.attesterendeSaksbehandler?.takeIf { brevtype == LetterMetadata.Brevtype.VEDTAKSBREV }
-                    ?.let { attestant ->
-                        appenCmd("doublesignature") {
-                            arg { append(attestant) }
-                            arg { append(saksbehandlere.saksbehandler) }
-                        }
-                    } ?: append("""${saksbehandlere.saksbehandler} \\ \feltclosingsaksbehandlersuffix""",escape = false)
-
+                val attestant = saksbehandlere.attesterendeSaksbehandler?.takeIf { brevtype == LetterMetadata.Brevtype.VEDTAKSBREV }
+                if (attestant != null) {
+                    appenCmd("doublesignature") {
+                        arg { append(attestant) }
+                        arg { append(saksbehandlere.saksbehandler) }
+                    }
+                } else {
+                    append(saksbehandlere.saksbehandler)
+                    appendln(""" \\ \feltclosingsaksbehandlersuffix """, escape = false)
+                }
                 appenCmd("par")
                 appenCmd("vspace*{12pt}")
                 appenCmd("feltnavenhet")
