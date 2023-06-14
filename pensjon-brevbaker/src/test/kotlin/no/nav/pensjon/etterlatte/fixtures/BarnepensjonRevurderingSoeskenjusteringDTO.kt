2475105e956314836ef3b550d611191276d138d4
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2023-06-14 10:03:55 +0200

    Oppdaterer fixture for revurdering søskenjustering

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/BarnepensjonRevurderingSoeskenjusteringDTO.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/BarnepensjonRevurderingSoeskenjusteringDTO.kt
@@ -1,31 +1,30 @@
 package no.nav.pensjon.etterlatte.fixtures
 
 import no.nav.pensjon.brevbaker.api.model.Kroner
-import no.nav.pensjon.etterlatte.maler.BarnepensjonInnvilgelseDTO
-import no.nav.pensjon.etterlatte.maler.BarnepensjonRevurderingSoeskenjusteringDTO
-import no.nav.pensjon.etterlatte.maler.BarnepensjonSoeskenjusteringGrunn
+import no.nav.pensjon.etterlatte.maler.*
 import java.time.LocalDate
 
 fun createBarnepensjonRevurderingSoeskenjusteringDTO() =
     BarnepensjonRevurderingSoeskenjusteringDTO(
         grunnForJustering = BarnepensjonSoeskenjusteringGrunn.NYTT_SOESKEN,
-        utbetalingsinfo = BarnepensjonInnvilgelseDTO.Utbetalingsinfo(
+        utbetalingsinfo = Utbetalingsinfo(
             antallBarn = 3,
             beloep = Kroner(1231),
             soeskenjustering = true,
+            grunnbeloep = Kroner(118000),
             virkningsdato = LocalDate.of(2022, 8, 1),
             beregningsperioder = listOf(
-                BarnepensjonInnvilgelseDTO.Beregningsperiode(
+                Beregningsperiode(
                     datoFOM = LocalDate.now(),
                     datoTOM = LocalDate.now(),
                     grunnbeloep = Kroner(106003),
                     antallBarn = 1,
                     utbetaltBeloep = Kroner(495),
                 ),
-                BarnepensjonInnvilgelseDTO.Beregningsperiode(
+                Beregningsperiode(
                     datoFOM = LocalDate.now(),
                     datoTOM = null,
-                    grunnbeloep = Kroner(106003),
+                    grunnbeloep = Kroner(118000),
                     antallBarn = 1,
                     utbetaltBeloep = Kroner(495),
                 )
