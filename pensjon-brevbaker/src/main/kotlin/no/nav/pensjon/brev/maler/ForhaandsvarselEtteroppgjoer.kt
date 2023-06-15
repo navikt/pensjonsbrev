6b0f9b8201ecdf9f8fd2305a88e277f6f427be5d
Jeremy Elsom <jeremy.elsom@nav.no>
2023-06-15 08:35:15 +0200

    VedleggPraktiskInformasjon complete

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/ForhaandsvarselEtteroppgjoer.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/ForhaandsvarselEtteroppgjoer.kt
@@ -4,19 +4,16 @@ import no.nav.pensjon.brev.api.model.maler.*
 import no.nav.pensjon.brev.maler.fraser.common.*
 import no.nav.pensjon.brev.maler.fraser.common.Felles
 import no.nav.pensjon.brev.maler.fraser.ufoer.*
-import no.nav.pensjon.brev.maler.fraser.ufoer.Ufoeretrygd
 import no.nav.pensjon.brev.maler.vedlegg.*
-import no.nav.pensjon.brev.template.AutobrevTemplate
-import no.nav.pensjon.brev.template.Language
+import no.nav.pensjon.brev.template.*
 import no.nav.pensjon.brev.template.Language.*
-import no.nav.pensjon.brev.template.LanguageSupport
-import no.nav.pensjon.brev.template.LetterTemplate
 import no.nav.pensjon.brev.template.dsl.*
 import no.nav.pensjon.brev.template.dsl.expression.*
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brevbaker.api.model.*
+
 @TemplateModelHelpers
-object ForhaandsvarselEtteroppgjoer: AutobrevTemplate<ForhaandsvarselEtteroppgjoerDto> {
+object ForhaandsvarselEtteroppgjoer : AutobrevTemplate<ForhaandsvarselEtteroppgjoerDto> {
 
     override val kode: Brevkode.AutoBrev = Brevkode.AutoBrev.UT_FORHAANDSVARSEL_EO_AUTO
 
@@ -26,12 +23,28 @@ object ForhaandsvarselEtteroppgjoer: AutobrevTemplate<ForhaandsvarselEtteroppgjo
             letterDataType = ForhaandsvarselEtteroppgjoerDto::class,
             languages = languages(Bokmal, Nynorsk, English),
             letterMetadata = LetterMetadata(
-                displayTitle = "Varsel - etteroppgjør av uføretrygd ved feilutbetaling",
+                displayTitle = "Varsel - etteroppgjør av uføretrygd",
                 isSensitiv = false,
-                distribusjonstype = LetterMetadata.Distribusjonstype.VEDTAK,
+                distribusjonstype = LetterMetadata.Distribusjonstype.VIKTIG,
                 brevtype = LetterMetadata.Brevtype.VEDTAKSBREV,
             )
         ) {
+            val harTilbakePenger: Expression<Boolean>,
+            val tidligereEOIverksatt: Expression<Boolean>,
+            val endretPersonGrunnlagInntekt: Expression<Boolean>,
 
+            title {
+                showIf(harEtterbetaling) {
+                    text(
+                        Bokmal to "Nytt forhåndsvarsel om etteroppgjør av uføretrygd for <PeriodeFom>",
+                        Nynorsk to "Nytt førehandsvarsel om etteroppgjer av uføretrygd for <PeriodeFom>",
+                        English to "New advance notice of settlement of disability benefit for <PeriodeFom>"
+                    )
+                }.orShow {
+                Bokmal to "Forhåndsvarsel om etteroppgjør av uføretrygd for <PeriodeFom>",
+                    Nynorsk to "Førehandsvarsel om etteroppgjer av uføretrygd for <PeriodeFom>",
+                    English to "Advance notice of settlement of disability benefit for <PeriodeFom>"
+
+                }
+            }
         }
-}
