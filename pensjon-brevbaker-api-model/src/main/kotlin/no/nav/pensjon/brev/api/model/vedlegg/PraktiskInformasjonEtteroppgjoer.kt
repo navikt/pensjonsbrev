cedd38cbcef1fe857580df58cd567f23bffe5ff8
Jeremy Elsom <jeremy.elsom@nav.no>
2023-06-30 09:13:01 +0200

    WIP

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/PraktiskInformasjonEtteroppgjoer.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/PraktiskInformasjonEtteroppgjoer.kt
@@ -2,7 +2,7 @@ package no.nav.pensjon.brev.api.model.vedlegg
 
 import java.time.LocalDate
 
-
+// Conditional for showing the attachment: The attachment is mandatory to letter
 @Suppress("unused")
 data class PraktiskInformasjonEtteroppgjoerDto(
     val ufoeretrygdEtterOppgjoerPeriodeFom: LocalDate
