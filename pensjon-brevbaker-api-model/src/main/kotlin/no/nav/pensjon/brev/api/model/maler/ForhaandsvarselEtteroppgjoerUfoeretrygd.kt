abca82a9bd62ac5f9d2a570ed09940947f1d495e
Jeremy Elsom <jeremy.elsom@nav.no>
2023-08-03 10:17:42 +0200

    WIP after merge from master

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ForhaandsvarselEtteroppgjoerUfoeretrygd.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ForhaandsvarselEtteroppgjoerUfoeretrygd.kt
@@ -1,7 +1,9 @@
 package no.nav.pensjon.brev.api.model.maler
 
 import no.nav.pensjon.brev.api.model.vedlegg.OpplysningerOmEtteroppgjoeretDto
+import no.nav.pensjon.brev.api.model.vedlegg.OrienteringOmRettigheterUfoereDto
 import no.nav.pensjon.brevbaker.api.model.Kroner
+import no.nav.pensjon.brevbaker.api.model.Year
 
 @Suppress("unused")
 data class ForhaandsvarselEtteroppgjoerUfoeretrygdDto(
@@ -10,4 +12,8 @@ data class ForhaandsvarselEtteroppgjoerUfoeretrygdDto(
     val harTjentOver80prosentAvOIFU: Boolean,
     val kanSoekeOmNyInntektsgrense: Boolean,
     val opplysningerOmEtteroppgjoret: OpplysningerOmEtteroppgjoeretDto,
+    val orienteringOmRettigheterUfoere: OrienteringOmRettigheterUfoereDto,
+    // TODO: Include attachment BeregningAvEtteroppgjoeret
+    // TODO: Include attachment OpplysningerOmBeregningen
+    // TODO: Include attachment PraktiskInformasjonEtteroppgjoeret
 )
