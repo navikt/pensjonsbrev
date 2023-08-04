1bcb99acf7e5b013ebeb8db0f49f52f34fd0979b
Jeremy Elsom <jeremy.elsom@nav.no>
2023-08-04 13:59:33 +0200

    WIP after merge from master

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/fixtures/OrienteringOmRettigheterUfoereDto.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/fixtures/OrienteringOmRettigheterUfoereDto.kt
@@ -6,11 +6,9 @@ import no.nav.pensjon.brev.api.model.vedlegg.OrienteringOmRettigheterUfoereDto
 fun createOrienteringOmRettigheterUfoereDto() =
     OrienteringOmRettigheterUfoereDto(
         bruker_borINorge = false,
-        harInnvilgetBarnetillegg = false,
         harInnvilgetBarnetilleggFellesBarn = false,
         harInnvilgetBarnetilleggSaerkullsbarn = false,
         harTilleggForFlereBarn = false,
         institusjon_gjeldende = Institusjon.INGEN,
         sivilstand = Sivilstand.ENSLIG,
-        harInnvilgetBarnetillegg = true,
     )
