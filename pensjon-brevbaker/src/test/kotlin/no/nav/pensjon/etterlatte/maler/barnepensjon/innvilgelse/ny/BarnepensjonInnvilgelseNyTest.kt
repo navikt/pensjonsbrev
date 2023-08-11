d2dcf3afe08f224ce62ace57fc0b4e8d85539127
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2023-08-11 12:29:07 +0200

    EY-2526: Ny innvilgelsesmal for barnepensjon (#212)
    * EY-2526: Ny versjon av malen for innvilgelse barnepensjon
    
    * EY-2526: Legg med det samme opp til at den nye innvilgelsesmalen skal bli ein hovedmal
    
    * EY-2526: Testar også for ny innvilgelsesmal
    
    * EY-2526: Oppdaterer frasene i det nye innvilgelsesbrevet til å matche med det som er på sharepoint

---
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/maler/barnepensjon/innvilgelse/ny/BarnepensjonInnvilgelseNyTest.kt
@@ -0,0 +1,44 @@
+package no.nav.pensjon.etterlatte.maler.barnepensjon.innvilgelse.ny
+
+import kotlinx.coroutines.runBlocking
+import no.nav.pensjon.brev.PDF_BUILDER_URL
+import no.nav.pensjon.brev.TestTags
+import no.nav.pensjon.brev.latex.LaTeXCompilerService
+import no.nav.pensjon.brev.template.Language
+import no.nav.pensjon.brev.template.Letter
+import no.nav.pensjon.brev.template.render.PensjonHTMLRenderer
+import no.nav.pensjon.brev.template.render.PensjonLatexRenderer
+import no.nav.pensjon.brev.writeTestHTML
+import no.nav.pensjon.brev.writeTestPDF
+import no.nav.pensjon.etterlatte.Fixtures
+import no.nav.pensjon.etterlatte.maler.barnepensjon.ny.BarnepensjonInnvilgelseNy
+import no.nav.pensjon.etterlatte.maler.barnepensjon.ny.BarnepensjonInnvilgelseNyDTO
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+
+@Tag(TestTags.INTEGRATION_TEST)
+class BarnepensjonInnvilgelseNyTest {
+
+    @Test
+    fun pdftest() {
+        Letter(
+            BarnepensjonInnvilgelseNy.template,
+            Fixtures.create<BarnepensjonInnvilgelseNyDTO>(),
+            Language.Bokmal,
+            Fixtures.felles,
+        ).let { PensjonLatexRenderer.render(it) }
+            .let { runBlocking { LaTeXCompilerService(PDF_BUILDER_URL).producePDF(it, "test").base64PDF } }
+            .also { writeTestPDF("BARNEPENSJON_INNVILGELSE_NY", it) }
+    }
+
+    @Test
+    fun testHtml() {
+        Letter(
+            BarnepensjonInnvilgelseNy.template,
+            Fixtures.create<BarnepensjonInnvilgelseNyDTO>(),
+            Language.Bokmal,
+            Fixtures.felles,
+        ).let { PensjonHTMLRenderer.render(it) }
+            .also { writeTestHTML("BARNEPENSJON_INNVILGELSE_NY", it) }
+    }
+}
