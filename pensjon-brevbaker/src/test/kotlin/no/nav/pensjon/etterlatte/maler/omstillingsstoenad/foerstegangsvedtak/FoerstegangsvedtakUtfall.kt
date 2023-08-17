a519cab4a7c659d28b6d1afff439565632da2a0d
Emil Schrøder <emil.schroder@nav.no>
2023-08-17 14:53:46 +0200

    Bruk testmal

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/foerstegangsvedtak/FoerstegangsvedtakUtfall.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/foerstegangsvedtak/FoerstegangsvedtakUtfall.kt
@@ -1,43 +1,16 @@
 package no.nav.pensjon.etterlatte.maler.omstillingsstoenad.foerstegangsvedtak
 
-import kotlinx.coroutines.runBlocking
-import no.nav.pensjon.brev.PDF_BUILDER_URL
 import no.nav.pensjon.brev.TestTags
-import no.nav.pensjon.brev.latex.LaTeXCompilerService
 import no.nav.pensjon.brev.template.*
 import no.nav.pensjon.brev.template.render.*
-import no.nav.pensjon.brev.writeTestHTML
-import no.nav.pensjon.brev.writeTestPDF
 import no.nav.pensjon.etterlatte.EtterlatteBrevKode
 import no.nav.pensjon.etterlatte.Fixtures
 import no.nav.pensjon.etterlatte.maler.*
 import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.Test
 
 @Tag(TestTags.INTEGRATION_TEST)
-class OMSFoerstegangsvedtakUtfallITest {
-
-    @Test
-    fun pdftest() {
-        Letter(
-            FoerstegangsvedtakUtfall.template,
-            Fixtures.create<FoerstegangsvedtakUtfallDTO>(),
-            Language.Bokmal,
-            Fixtures.felles
-        ).let { PensjonLatexRenderer.render(it) }
-            .let { runBlocking { LaTeXCompilerService(PDF_BUILDER_URL).producePDF(it, "test").base64PDF } }
-            .also { writeTestPDF(EtterlatteBrevKode.OMS_FOERSTEGANGSVEDTAK_INNVILGELSE_UTFALL.name, it) }
-    }
-
-    @Test
-    fun testHtml() {
-        Letter(
-            FoerstegangsvedtakUtfall.template,
-            Fixtures.create<FoerstegangsvedtakUtfallDTO>(),
-            Language.Bokmal,
-            Fixtures.felles
-        ).let { PensjonHTMLRenderer.render(it) }
-            .also { writeTestHTML(EtterlatteBrevKode.OMS_FOERSTEGANGSVEDTAK_INNVILGELSE_UTFALL.name, it) }
-    }
-
-}
+class OMSFoerstegangsvedtakUtfallITest : EtterlatteMalTest<FoerstegangsvedtakUtfallDTO>(
+    FoerstegangsvedtakUtfall.template,
+    EtterlatteBrevKode.OMS_FOERSTEGANGSVEDTAK_INNVILGELSE_UTFALL,
+    Fixtures.create<FoerstegangsvedtakUtfallDTO>(),
+)
--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/foerstegangsvedtak/InnvilgelseFoerstegangsvedtakTest.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/foerstegangsvedtak/InnvilgelseFoerstegangsvedtakTest.kt
@@ -1,42 +1,14 @@
 package no.nav.pensjon.etterlatte.maler.omstillingsstoenad.foerstegangsvedtak
 
-import kotlinx.coroutines.runBlocking
-import no.nav.pensjon.brev.PDF_BUILDER_URL
 import no.nav.pensjon.brev.TestTags
-import no.nav.pensjon.brev.latex.LaTeXCompilerService
-import no.nav.pensjon.brev.template.*
-import no.nav.pensjon.brev.template.render.*
-import no.nav.pensjon.brev.writeTestHTML
-import no.nav.pensjon.brev.writeTestPDF
 import no.nav.pensjon.etterlatte.EtterlatteBrevKode
 import no.nav.pensjon.etterlatte.Fixtures
+import no.nav.pensjon.etterlatte.maler.EtterlatteMalTest
 import org.junit.jupiter.api.Tag
-import org.junit.jupiter.api.Test
 
 @Tag(TestTags.INTEGRATION_TEST)
-class OMSInnvilgelseFoerstegangsvedtakITest {
-
-    @Test
-    fun pdftest() {
-        Letter(
-            InnvilgelseFoerstegangsvedtak.template,
-            Fixtures.create<OMSInnvilgelseFoerstegangsvedtakDTO>(),
-            Language.Bokmal,
-            Fixtures.felles
-        ).let { PensjonLatexRenderer.render(it) }
-            .let { runBlocking { LaTeXCompilerService(PDF_BUILDER_URL).producePDF(it, "test").base64PDF } }
-            .also { writeTestPDF(EtterlatteBrevKode.OMS_FOERSTEGANGSVEDTAK_INNVILGELSE.name, it) }
-    }
-
-    @Test
-    fun testHtml() {
-        Letter(
-            InnvilgelseFoerstegangsvedtak.template,
-            Fixtures.create<OMSInnvilgelseFoerstegangsvedtakDTO>(),
-            Language.Bokmal,
-            Fixtures.felles
-        ).let { PensjonHTMLRenderer.render(it) }
-            .also { writeTestHTML(EtterlatteBrevKode.OMS_FOERSTEGANGSVEDTAK_INNVILGELSE.name, it) }
-    }
-
-}
+class OMSInnvilgelseFoerstegangsvedtakITest : EtterlatteMalTest<OMSInnvilgelseFoerstegangsvedtakDTO>(
+    InnvilgelseFoerstegangsvedtak.template,
+    EtterlatteBrevKode.OMS_FOERSTEGANGSVEDTAK_INNVILGELSE,
+    Fixtures.create<OMSInnvilgelseFoerstegangsvedtakDTO>(),
+)
