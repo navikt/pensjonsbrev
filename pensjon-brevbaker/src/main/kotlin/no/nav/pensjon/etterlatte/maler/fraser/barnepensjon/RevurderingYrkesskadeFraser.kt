ac462206672fb3af1baa0c77d4c65abaa62eb3c8
Trond Valen <trond.valen@nav.no>
2023-08-29 12:32:08 +0200

    EY-2373: Justert etter malendring for yrkesskade dokumentert, men ingen endring i stønad.

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/fraser/barnepensjon/RevurderingYrkesskadeFraser.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/fraser/barnepensjon/RevurderingYrkesskadeFraser.kt
@@ -8,6 +8,7 @@ import no.nav.pensjon.brev.template.Language.English
 import no.nav.pensjon.brev.template.Language.Nynorsk
 import no.nav.pensjon.brev.template.OutlinePhrase
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
+import no.nav.pensjon.brev.template.dsl.expression.equalTo
 import no.nav.pensjon.brev.template.dsl.expression.expr
 import no.nav.pensjon.brev.template.dsl.expression.format
 import no.nav.pensjon.brev.template.dsl.expression.plus
@@ -26,74 +27,88 @@ object RevurderingYrkesskadeFraser {
     ) : OutlinePhrase<LangBokmalNynorskEnglish>() {
         override fun OutlineOnlyScope<LangBokmalNynorskEnglish, Unit>.template() {
             showIf(yrkesskadeErDokumentert) {
-                paragraph {
-                    val virkningsdatoExpr = virkningsdato.format()
-                    showIf(stoenadHarOekt) {
-                        textExpr(
-                            Bokmal to "Barnepensjonen din øker fra ".expr() + virkningsdatoExpr
-                                    + " fordi det nå er dokumentert at ".expr() + dinForelder
-                                    + " døde som følge av yrkesskade. ",
-                            Nynorsk to "".expr(),
-                            English to "".expr()
-                        )
-                    }.orShow {
-                        textExpr(
-                            Bokmal to "Det er nå dokumentert at ".expr() + dinForelder
-                                    + " døde som følge av yrkesskade. ",
-                            Nynorsk to "".expr(),
-                            English to "".expr()
-                        )
-                    }
+                yrkesskadeDokumentert()
+            }.orShow {
+                yrkesskadeIkkeDokumentert()
+            }
+        }
+
+        private fun OutlineOnlyScope<LangBokmalNynorskEnglish, Unit>.yrkesskadeDokumentert() {
+            paragraph {
+                showIf(stoenadHarOekt) {
                     textExpr(
-                        Bokmal to "Du får ".expr() + kronebeloep.format() + " kroner hver måned før skatt.",
+                        Bokmal to "Barnepensjonen din øker fra ".expr() + virkningsdato.format()
+                                + " fordi det nå er dokumentert at " + dinForelder
+                                + " døde som følge av yrkesskade. "
+                                + "Du får " + kronebeloep.format() + " kroner hver måned før skatt.",
                         Nynorsk to "".expr(),
-                        English to "".expr(),
+                        English to "".expr()
+                    )
+                }.orShow {
+                    textExpr(
+                        Bokmal to "Barnepensjonen din er vurdert på nytt fra ".expr() + virkningsdato.format()
+                                + " fordi det nå er dokumentert at " + dinForelder
+                                + " døde som følge av yrkesskade. Du får fortsatt " + kronebeloep.format()
+                                + " kroner hver måned før skatt.",
+                        Nynorsk to "".expr(),
+                        English to "".expr()
                     )
                 }
-                paragraph {
+            }
+            paragraph {
+                text(
+                    Bokmal to "Yrkesskaden/yrkessykdommen er dokumentert med vedtak fra NAV. " +
+                            "Barnepensjonen er derfor innvilget etter særbestemmelser, se vedlegg om yrkesskade.",
+                    Nynorsk to "",
+                    English to "",
+                )
+                showIf(stoenadHarOekt.equalTo(false)) {
                     text(
-                        Bokmal to "Yrkesskaden/yrkessykdommen er dokumentert med vedtak fra NAV. " +
-                                "Barnepensjonen er derfor innvilget etter særbestemmelser, se vedlegg om yrkesskade.",
+                        Bokmal to " Dette fører imidlertid ikke til endring av din pensjon da vilkårene "
+                                + "for full barnepensjon er oppfylt uavhengig av at dødsfallet skyldes "
+                                + "yrkesskade/yrkessykdom.",
                         Nynorsk to "",
                         English to "",
                     )
                 }
-                paragraph {
-                    text(
-                        Bokmal to "Vedtaket er gjort etter bestemmelsene om barnepensjon i " +
-                                "folketrygdloven § 18-2, § 18-3, § 18-4, § 18-5, § 18-11 og § 22-13.",
-                        Nynorsk to "",
-                        English to ""
-                    )
-                }
-            }.orShow {
-                paragraph {
-                    textExpr(
-                        Bokmal to "Barnepensjonen din er vurdert på nytt. ".expr()
-                                + "Det blir ingen endring av din pensjon. "
-                                + "Du får fortsatt " + kronebeloep.format() + " kroner hver måned før skatt.",
-                        Nynorsk to "".expr(),
-                        English to "".expr()
-                    )
-                }
-                paragraph {
-                    textExpr(
-                        Bokmal to "Det er dokumentert med vedtak fra NAV at ".expr() + dinForelder
-                                + " ikke døde som følge av yrkesskade/yrkessykdom. " +
-                                "Det blir derfor ingen endring av barnepensjonen.",
-                        Nynorsk to "".expr(),
-                        English to "".expr()
-                    )
-                }
-                paragraph {
-                    text(
-                        Bokmal to "Vedtaket er gjort etter bestemmelsene om barnepensjon i " +
-                                "folketrygdloven § 18-2, § 18-3, § 18-4, § 18-5 og § 18-11.",
-                        Nynorsk to "",
-                        English to ""
-                    )
-                }
+            }
+            paragraph {
+                text(
+                    Bokmal to "Vedtaket er gjort etter bestemmelsene om barnepensjon i " +
+                            "folketrygdloven § 18-2, § 18-3, § 18-4, § 18-5, § 18-11 og § 22-13.",
+                    Nynorsk to "",
+                    English to ""
+                )
             }
         }
+        private fun OutlineOnlyScope<LangBokmalNynorskEnglish, Unit>.yrkesskadeIkkeDokumentert() {
+            paragraph {
+                textExpr(
+                    Bokmal to "Barnepensjonen din er vurdert på nytt. ".expr()
+                            + "Det blir ingen endring av din pensjon. "
+                            + "Du får fortsatt " + kronebeloep.format() + " kroner hver måned før skatt.",
+                    Nynorsk to "".expr(),
+                    English to "".expr()
+                )
+            }
+            paragraph {
+                textExpr(
+                    Bokmal to "Det er dokumentert med vedtak fra NAV at ".expr() + dinForelder
+                            + " ikke døde som følge av yrkesskade/yrkessykdom. " +
+                            "Det blir derfor ingen endring av barnepensjonen.",
+                    Nynorsk to "".expr(),
+                    English to "".expr()
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "Vedtaket er gjort etter bestemmelsene om barnepensjon i " +
+                            "folketrygdloven § 18-2, § 18-3, § 18-4, § 18-5 og § 18-11.",
+                    Nynorsk to "",
+                    English to ""
+                )
+            }
+        }
+
     }
 }
