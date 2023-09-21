612e281a2f2c5b852b821f20d6e3e350c0afabd1
Jeremy Elsom <jeremy.elsom@nav.no>
2023-09-21 13:00:00 +0200

    PL-6766 created for AdhocRegelendretGjenlevendetillegg

--- pensjon-brevbaker-api-model/build.gradle.kts
+++ pensjon-brevbaker-api-model/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "no.nav.pensjon.brev"
-version = "48"
+version = "49"
 
 java {
     withSourcesJar()
--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocTest.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocTest.kt
@@ -24,26 +24,13 @@ class AdhocTest {
 
     @Test
     fun `testGjenlevendeFoer1970 pdf`() {
-        testAdhocPdf(GjenlevendeInfoFoer1971.template, "ADHOC_GJENLEVENDEINFOFOER1971", Bokmal)
-        testAdhocPdf(GjenlevendeInfoFoer1971.template, "ADHOC_GJENLEVENDEINFOFOER1971", Nynorsk)
-        testAdhocPdf(GjenlevendeInfoFoer1971.template, "ADHOC_GJENLEVENDEINFOFOER1971", English)
+        testAdhocPdf(AdhocRegelendretGjenlevendetillegg.template, "ADHOC_AdhocRegelendretGjenlevendetillegg", Bokmal)
+        testAdhocPdf(AdhocRegelendretGjenlevendetillegg.template, "ADHOC_AdhocRegelendretGjenlevendetillegg", Nynorsk)
+        testAdhocPdf(AdhocRegelendretGjenlevendetillegg.template, "ADHOC_AdhocRegelendretGjenlevendetillegg", English)
     }
     @Test
     fun `testGjenlevendeFoer1970 html`() {
-        testHtml(GjenlevendeInfoFoer1971.template, "ADHOC_GJENLEVENDEINFOFOER1971", Bokmal)
+        testHtml(AdhocRegelendretGjenlevendetillegg.template, "ADHOC_AdhocRegelendretGjenlevendetillegg", Bokmal)
     }
 
-    @Test
-    fun `testGjenlevendeEtter1970 pdf`() {
-        testAdhocPdf(GjenlevendeInfoEtter1970.template, "ADHOC_GJENLEVENDEINFOETTER1970", Bokmal)
-        testAdhocPdf(GjenlevendeInfoEtter1970.template, "ADHOC_GJENLEVENDEINFOETTER1970", Nynorsk)
-        testAdhocPdf(GjenlevendeInfoEtter1970.template, "ADHOC_GJENLEVENDEINFOETTER1970", English)
-    }
-
-    @Test
-    fun `testGjenlevendeEtter1970 html`() {
-        testHtml(GjenlevendeInfoEtter1970.template, "ADHOC_GJENLEVENDEINFOETTER1970", Bokmal)
-        testHtml(GjenlevendeInfoEtter1970.template, "ADHOC_GJENLEVENDEINFOETTER1970", Nynorsk)
-        testHtml(GjenlevendeInfoEtter1970.template, "ADHOC_GJENLEVENDEINFOETTER1970", English)
-    }
 }
