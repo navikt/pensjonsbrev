4486afaa8d21197e03b4877528ddbf5ff63f098a
Jeremy Elsom <jeremy.elsom@nav.no>
2023-09-25 11:00:38 +0200

    PL-6766: Reverted and bumped up api to 50

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/common/Constants.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/common/Constants.kt
@@ -9,6 +9,7 @@ object Constants {
     const val ETTERSENDELSE_URL = "$NAV_URL/ettersendelse"
     const val FULLMAKT_URL = "$NAV_URL/fullmakt"
     const val GJENLEVENDEPENSJON_URL = "$NAV_URL/gjenlevendepensjon"
+    const val GJENLEVENDETILLEGG_URL = "$NAV_URL/gjenlevendetillegg"
     const val GJENLEVENDE_SKJEMA_URL = "$NAV_URL/gjenlevendeektefelle"
     const val GJENLEVENDE_TILLEGGSSTOENADER_URL= "$NAV_URL/tilleggsstonader-gjenlevende"
     const val HELSE_URL= "$NAV_URL/helse"
