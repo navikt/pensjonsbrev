b11b33d05435db3e711b0625fec7809b03dde308
Jeremy Elsom <jeremy.elsom@nav.no>
2023-10-26 10:15:06 +0200

    Tekstlig endringer på engelsk i AdhocGjenlevenderettFoed1954Til1957.kt

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocGjenlevenderettFoed1954Til1957.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocGjenlevenderettFoed1954Til1957.kt
@@ -77,7 +77,7 @@ object AdhocGjenlevenderettFoed1954Til1957 : AutobrevTemplate<EmptyBrevdata> {
                 text(
                     Bokmal to "Gjenlevendetillegget i alderspensjon etter nye regler skal fases ut.",
                     Nynorsk to "Attlevandetillegget i alderspensjon etter nye reglar skal fasast ut.",
-                    English to "Survivor's supplement in retirement pension calculated after the new Chapter 20 rules phases out."
+                    English to "Survivor's supplement in retirement pension calculated after the new Chapter 20 rules will be phased out."
                 )
             }
             paragraph {
@@ -97,8 +97,8 @@ object AdhocGjenlevenderettFoed1954Til1957 : AutobrevTemplate<EmptyBrevdata> {
                             + " Ved senere økning av uttaksgrad vil ikke gjenlevendetillegget økes.",
                     Nynorsk to "Viss du tar ut mindre enn 100 prosent alderspensjon, verkar det inn på attlevandetillegget ditt."
                             + " Viss du aukar uttaksgraden seinare, vil ikkje attlevandetillegget auke.",
-                    English to "If you take out less than 100 percent retirement pension, this will affect your survivor's supplement."
-                            + " If you should later increase the withdrawal rate of your retirement pension, the survivor's supplement wil not increase."
+                    English to "If you take out less than 100 percent retirement pension, has significance for your survivor's supplement."
+                            + " If you should later increase the withdrawal rate of your retirement pension, the survivor's supplement will not increase."
                 )
             }
 
