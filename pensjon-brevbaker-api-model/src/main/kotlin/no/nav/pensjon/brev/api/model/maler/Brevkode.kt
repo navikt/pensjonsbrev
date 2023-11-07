fb49bbd051df65fd86e626a05f9dfdda32fdede7
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2023-11-07 15:53:39 +0100

    Legacy-koder er flyttet til pesys

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/Brevkode.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/Brevkode.kt
@@ -1,32 +1,18 @@
 package no.nav.pensjon.brev.api.model.maler
 
 object Brevkode {
-    enum class AutoBrev(vararg koder: String) {
-        PE_AP_ADHOC_2023_REGLERENDRET_INNVGJT_20_19A("ADHOC_2023_AP1"),
-        PE_OMSORG_HJELPESTOENAD_AUTO("OMSORG_HJST_AUTO"),
-        PE_OMSORG_EGEN_AUTO("OMSORG_EGEN_AUTO", "OMSORGP_EGENMLD"),
-        UT_OMREGNING_ENSLIG_AUTO("UT_DOD_ENSLIG_AUTO", "TILST_DOD_UT"),
-        UT_OPPHOER_BT_AUTO("PE_UT_07_200", "OPPHOR_ENDRING_UT_BT"),
-        UT_UNG_UFOER_20_AAR_AUTO("PE_BA_04_505", "UP_FULLTT_BELOPENDR"),
-        UT_EO_FORHAANDSVARSEL_FEILUTBETALING_AUTO("PE_UT_23_001", "UT_EO_VARSEL_FU"),
+    enum class AutoBrev {
+        PE_AP_ADHOC_2023_REGLERENDRET_INNVGJT_20_19A,
+        PE_OMSORG_HJELPESTOENAD_AUTO,
+        PE_OMSORG_EGEN_AUTO,
+        UT_OMREGNING_ENSLIG_AUTO,
+        UT_OPPHOER_BT_AUTO,
+        UT_UNG_UFOER_20_AAR_AUTO,
+        UT_EO_FORHAANDSVARSEL_FEILUTBETALING_AUTO,
         ;
-
-        val brevkoder: Set<String> = koder.toSet()
-
-        companion object {
-            @Suppress("unused")
-            fun findByKode(kode: String): AutoBrev? =
-                AutoBrev.values().find { it.brevkoder.contains(kode) || it.name == kode }
-        }
     }
-    enum class Redigerbar(val kode: String) {
-        INFORMASJON_OM_SAKSBEHANDLINGSTID("AP_INFO_STID_MAN"),
+    enum class Redigerbar() {
+        INFORMASJON_OM_SAKSBEHANDLINGSTID,
         ;
-
-        companion object {
-            @Suppress("unused")
-            fun findByKode(kode: String): Redigerbar? =
-                Redigerbar.values().firstOrNull { it.kode == kode || it.name == kode}
-        }
     }
 }
