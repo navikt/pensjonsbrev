f2836e1aaf43b09a7061c704034061392c908ada
johannbm <johannes.bredholt.moskvil@nav.no>
2023-11-09 13:27:19 +0100

    Add minimal frontend code

---
+++ skribenten-web/frontend/src/main.tsx
@@ -0,0 +1,10 @@
+import "@navikt/ds-css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
+  <React.StrictMode>
+    <div>Hei</div>
+  </React.StrictMode>,
+);
