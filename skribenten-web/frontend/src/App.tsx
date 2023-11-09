a224be17face52099f24f15eeb31eacb1198358c
johannbm <johannes.bredholt.moskvil@nav.no>
2023-11-09 15:30:18 +0100

    test backend

---
+++ skribenten-web/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const FULLMAKTSREGISTER_API_BASE_PATH = "/skribenten-backend";
+
+export function App() {
+  return <button onClick={testBackend}>Test backend API</button>;
+}
+
+const testBackend = async () => {
+  await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/test/pen`);
+};
--- skribenten-web/frontend/src/main.tsx
+++ skribenten-web/frontend/src/main.tsx
@@ -3,8 +3,12 @@ import "@navikt/ds-css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import { App } from "./App";
+
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
-    <div>Hei</div>
+    <div>
+      <App />
+    </div>
   </React.StrictMode>,
 );
