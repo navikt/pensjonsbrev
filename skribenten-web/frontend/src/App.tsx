ff788dc10bdf1d2e5ee6496d047ea49dda7a9060
johannbm <johannes.bredholt.moskvil@nav.no>
2023-11-13 16:21:13 +0100

    test more urls

--- skribenten-web/frontend/src/App.tsx
+++ skribenten-web/frontend/src/App.tsx
@@ -7,6 +7,7 @@ export function App() {
 const testBackend = async () => {
   // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/pen/sak/22972355`);
   // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/pdl/navn/21119343981`);
-  await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/kodeverk/kommune`);
+  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/pen/sak/22972355`);
+  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/test/pen`, { method: "POST" });
   // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/lettertemplates/UFOREP`);
 };
