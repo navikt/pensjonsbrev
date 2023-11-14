02795d168b567e34c64cf7d5bad9e1ebffce9076
johannbm <johannes.bredholt.moskvil@nav.no>
2023-11-14 18:21:43 +0100

    base page

--- skribenten-web/frontend/src/App.tsx
+++ skribenten-web/frontend/src/App.tsx
@@ -1,16 +1,15 @@
-const FULLMAKTSREGISTER_API_BASE_PATH = "/skribenten-backend";
+const SKRIBENTEN_BACKEND_BASE_PATH = "/skribenten-backend";
 
 export function App() {
-  return <button onClick={testBackend}>Test backend API</button>;
+  return <div>Skribenten web!</div>;
 }
 
 const testBackend = async () => {
-  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/pen/sak/22972355`);
-  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/pdl/navn/19499332788`);
-  await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/pen/sak/22972355`);
-  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/test/pen`, { method: "POST" });
-  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/lettertemplates/UFOREP`);
-
-  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/foretrukketSpraak/01520276730`);
-  // await fetch(`${FULLMAKTSREGISTER_API_BASE_PATH}/adresse/01520276730`);
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/pen/sak/22972355`);
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/pdl/navn/19499332788`);
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/pen/sak/22972355`);
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/test/pen`, { method: "POST" });
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/lettertemplates/UFOREP`);
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/foretrukketSpraak/01520276730`);
+  // await fetch(`${SKRIBENTEN_BACKEND_BASE_PATH}/adresse/01520276730`);
 };
