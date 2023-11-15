e3a2e90dc0a09ffeea0fcaec475ac779eafd6bfa
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2023-11-15 13:09:56 +0100

    EY-3060 Vedtaksbrev omregning barnepensjon (#313)
    * Vedtaksbrev omregning barnepensjon
    
    * Oppdatert versjon forhåndsvarselet
    
    * Oppdatert versjon vedtaksmal

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/barnepensjon/migrering/ForhaandsvarselOmregningBP.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/barnepensjon/migrering/ForhaandsvarselOmregningBP.kt
@@ -37,21 +37,21 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
         outline {
             paragraph {
                 text(
-                    Language.Bokmal to "Dette brevet er et forhåndsvarsel om at NAV vil fatte nytt vedtak om barnepensjon fordi Stortinget har vedtatt nye regler for barnepensjon. Du får høyere barnepensjon. ",
+                    Language.Bokmal to "Dette er et forhåndsvarsel om at NAV vil fatte nytt vedtak om barnepensjon fordi Stortinget har vedtatt nye regler for barnepensjon. De nye reglene gjelder fra 1. januar 2024.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
             }
             paragraph {
                 text(
-                    Language.Bokmal to "Du kan se hvordan vi har beregnet barnepensjonen din og nytt månedsbeløp i vedlegget “Vedtak - endring av barnepensjon”.",
+                    Language.Bokmal to "Du kan se nytt månedsbeløp i vedlegget “Utkast til vedtak- endring av barnepensjon”.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
             }
             paragraph {
                 text(
-                    Language.Bokmal to "Du trenger ikke å søke eller gi beskjed til NAV om at du ønsker høyere barnepensjon. Det nye beløpet får du fra januar 2024.",
+                    Language.Bokmal to "Du trenger ikke å gi beskjed til NAV om du ønsker høyere barnepensjon. Det nye beløpet får du fra januar 2024.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -66,14 +66,21 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
             }
             paragraph {
                 text(
-                    Language.Bokmal to "Vi forhåndsvarsler deg slik at du kan sjekke om beregningen av pensjonen din er korrekt. Dette er en foreløpig beregning som NAV har gjort på bakgrunn av opplysningene vi har om barnepensjonen din.",
+                    Language.Bokmal to "Vi forhåndsvarsler deg slik at du kan sjekke om beregningen av pensjonen din er korrekt. Dette er en foreløpig beregning som NAV har gjort på bakgrunn av opplysningene vi allerede har om barnepensjonen din.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
             }
             paragraph {
                 text(
-                    Language.Bokmal to "Beregningen trer ikke i kraft som et vedtak før etter fire uker fra du mottar dette forhåndsvarselet. På denne måten har du mulighet til å gi oss nye opplysninger slik at vi kan rette opp eventuelle feil i beregningen.",
+                    Language.Bokmal to "Beregningen av barnepensjonen din trer i kraft som et vedtak fra fire uker fra du mottar dette forhåndsvarselet. I løpet av denne perioden kan du gi oss nye opplysninger slik at vi kan rette opp eventuelle feil i beregningen.",
+                    Language.Nynorsk to "",
+                    Language.English to ""
+                )
+            }
+            paragraph {
+                text(
+                    Language.Bokmal to "Du kan se hvordan vi har beregnet ny barnepensjon i vedlegget “Utkast til vedtak – endring av barnepensjon”.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -88,7 +95,7 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
             }
             paragraph {
                 text(
-                    Language.Bokmal to "Hvis du har innsigelser på vedtaket, har du en frist på tre uker til å komme med nye opplysninger. Tre-ukers fristen regnes fra datoen du mottar dette forhåndsvarselet. Om du kommer med nye opplysninger vil du motta et nytt vedtak.",
+                    Language.Bokmal to "Hvis opplysningene ikke stemmer, må du gi beskjed til NAV innen fire uker fra du får dette forhåndsvarselet. Hvis opplysningene gjør at NAV må beregne barnepensjonen din på nytt, får du et nytt vedtak.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -96,7 +103,7 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
 
             paragraph {
                 text(
-                    Language.Bokmal to "Hvis du ikke har innsigelser, vil vedlagte vedtak gjelde fra 1. januar 2024.",
+                    Language.Bokmal to "Hvis du ikke har nye opplysninger eller innsigelser til “Utkast til vedtak – endring av barnepensjon” som er vedlagt, vil vedlegget anses som et vedtak fra 1. januar 2024. Du vil altså ikke få et nytt vedtak fordi utkastet da er omgjort til “Vedtak - endring av barnepensjon”.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -104,7 +111,7 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
 
             paragraph {
                 text(
-                    Language.Bokmal to "Etter at vedtaket er tredd i kraft 1. januar 2024 har du seks ukers klagefrist. Du finner skjema og informasjon på nav.no/klage.",
+                    Language.Bokmal to "Etter at vedtaket har tredd i kraft (altså 1. januar 2024) har du seks ukers klagefrist på vedtaket. Du finner informasjon om hvordan du klager i “Utkast til vedtak – endring av barnepensjon”.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -112,7 +119,7 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
 
             title2 {
                 text(
-                    Language.Bokmal to "Ta kontakt med NAV hvis dødsfallet skyldes yrkesskade eller yrkessykdom",
+                    Language.Bokmal to "Døde din forelder av yrkesskade eller yrkessykdom?",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -120,7 +127,7 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
 
             paragraph {
                 text(
-                    Language.Bokmal to "Hvis forelderen din døde av yrkesskade eller yrkessykdom kan det ha betydning for barnepensjonen din. Vi ber deg ta kontakt med NAV slik at vi er sikre på at vi har beregnet barnepensjonen din riktig.",
+                    Language.Bokmal to "Hvis forelderen din døde av yrkesskade eller yrkessykdom, kan det ha betydning for barnepensjonen din. Da må du ta kontakt med NAV slik at vi er sikre på at vi har beregnet barnepensjonen din riktig.",
                     Language.Nynorsk to "",
                     Language.English to ""
                 )
@@ -153,7 +160,7 @@ object ForhaandsvarselOmregningBP : EtterlatteTemplate<BarnepensjonOmregnetNyttR
                 list {
                     item {
                         text(
-                            Language.Bokmal to "Vedtak - endring av barnepensjon",
+                            Language.Bokmal to "Utkast til vedtak - endring av barnepensjon",
                             Language.Nynorsk to "",
                             Language.English to ""
                         )
