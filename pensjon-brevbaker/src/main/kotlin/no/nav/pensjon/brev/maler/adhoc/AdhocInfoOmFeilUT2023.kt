4d6be99b6a191852421b3465f18a226b1fb1b1fd
Håkon Heggholmen <hakon.heggholmen@nav.no>
2023-12-05 12:26:18 +0100

    Legger inn tekst i malen.

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocInfoOmFeilUT2023.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocInfoOmFeilUT2023.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.brev.maler.adhoc
 
 import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.maler.fraser.ufoer.Ufoeretrygd
 import no.nav.pensjon.brev.template.AutobrevTemplate
 import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.LetterTemplate
@@ -23,17 +24,78 @@ object AdhocInfoOmFeilUT2023 : AutobrevTemplate<EmptyBrevdata> {
             distribusjonstype = LetterMetadata.Distribusjonstype.VIKTIG,
             brevtype = LetterMetadata.Brevtype.INFORMASJONSBREV,
         )
-    ){
+    ) {
         title {
             text(
-            Bokmal to "Bla",
-        ) }
+                Bokmal to "Inntektsgrensen i uføretrygdsaken din blir satt ned",
+            )
+        }
         outline {
-            title1 {
+            paragraph {
                 text(
-                    Bokmal to "bla",
+                    Bokmal to "Fra 1. januar 2024 vil NAV sette ned grensen for hva du kan tjene før utbetalingen av uføretrygden din blir redusert. " +
+                            "Du vil få et eget vedtak med informasjon om den nye inntektsgrensen din fra 1. januar 2024. " +
+                            "Du vil få dette brevet før utbetalingen i januar måned.",
                 )
             }
+            title1 {
+                text(
+                    Bokmal to "Hvorfor setter vi ned inntektsgrensen?",
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "Ved en feil ble ikke inntektsgrensen din endret ved overgang fra uførepensjon til uføretrygd fra 2015. " +
+                            "Da du mottok uførepensjon hadde du rett til en høyere inntektsgrense fordi du hadde rett til et tillegg på 10 prosent av grunnbeløpet.",
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "Fra 1. januar 2015 skulle dette tillegget i inntektsgrensen ikke vært videreført i uføretrygden. " +
+                            "Fra denne datoen ble forskriften som ga rett på dette tillegget opphevet.",
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "NAV reduserte ikke inntektsgrensen din etter at forskriften om økt inntektsgrense ble opphevet." +
+                            " Vi vil sette inntektsgrensen riktig framover i tid.",
+                )
+            }
+            title1 {
+                text(
+                    Bokmal to "Hvis du vil vite mer om regelverket",
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "Før 1. januar 2015 var grensen for hva du kunne tjene før uførepensjonen ble avkortet, økt med 10 prosent av grunnbeløpet. " +
+                            "Dette fulgte av en forskrift til folketrygdloven § 25-13.",
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "Forskriften ble opphevet fra 1. januar 2015, samtidig som uførepensjon ble omregnet til uføretrygd. " +
+                            "Store deler av regelverket for uføreytelser ble da endret.",
+                )
+                list {
+                    item {
+                        text(
+                            Bokmal to "Folketrygdloven § 12-14 - Reduksjon av uføretrygd på grunn av inntekt",
+                        )
+                    }
+                    item {
+                        text(
+                            Bokmal to "Folketrygdloven § 25-13 – Forsøksvirksomhet – avvik ved loven",
+                        )
+                    }
+                    item {
+                        text(
+                            Bokmal to "Forskrift om forsøksvirksomhet for reaktivisering av uførepensjonister (gjaldt i perioden 1. september 2001 til 31. desember 2014)",
+                        )
+                    }
+                }
+            }
+            includePhrase(Ufoeretrygd.HarDuSpoersmaalUfoeretrygd)
         }
     }
 }
