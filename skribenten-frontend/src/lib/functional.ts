d04c44246387bdf98aecaff76eaeac1fe2ffc2c8
johannbm <johannes.bredholt.moskvil@nav.no>
2023-12-13 09:46:34 +0100

    Delete skribenten-frontend folder

--- skribenten-frontend/src/lib/functional.ts
+++
@@ -1,9 +0,0 @@
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-export function compose<Args extends any[], State, Out>(
-    f1: (...args: Args) => State,
-    f2: (state: State) => Out
-): (...args: Args) => Out {
-    return (...args: Args) => f2(f1(...args))
-}
--- skribenten-frontend/test/testUtils.ts
+++
@@ -1,5 +0,0 @@
-import {BoundAction} from "../src/lib/actions"
-
-/* eslint-disable */
-export const boundActionStub: BoundAction<any> = () => {}
-/* eslint-enable */
