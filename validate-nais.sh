d39c01eea37c1866a1b1f3934766b3134ad6a005
Alexander Hoem Rosbach <82815189+routsi@users.noreply.github.com>
2023-12-13 14:11:21 +0100

    Legg til skript for å validere nais-apper og endre skribenten-web til template (#388)
    * Legg til skript for å validere nais-apper
    
    * Fjern eksplisitt tenant

---
+++ validate-nais.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+tabs 4
+
+function validateApp() {
+    local app="$1"
+    local name=$(dirname "$app")
+    echo "$(basename "$name"):"
+
+    local envs=$(find "$app" -type f \( -iname \*.yaml -o -iname \*.yml \) -not -iname \*azure\* -not -iname nais.yaml)
+
+    for env in $envs ; do
+      local result
+      local status
+      result=$(nais validate --vars "$env" --var image=placeholder "$app/nais.yaml" 2>&1)
+      status=$?
+      if [ $status -eq 0 ] ; then
+        echo -e -n "\e[0;32m ✓ \e[00m"
+      else
+        echo -e -n "\e[1;31m ❌\e[00m"
+      fi
+      echo -e "\t $(basename "$env")"
+      if [ $status -gt 0 ] ; then
+        echo "$result" | sed 's/^/        /'
+      fi
+    done
+    echo ""
+}
+
+apps=$(find . -type d -name .nais)
+
+for app in $apps ; do
+  validateApp $app
+done
