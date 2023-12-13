599ef0b3801a5fb6946b35e84058d465f4f87406
Johannes Moskvil <johannbm@users.noreply.github.com>
2023-12-13 13:29:27 +0100

    Delete old frontend (#386)

--- skribenten-frontend/src/modules/LetterEditor/actions/updateLetter.ts
+++
@@ -1,8 +0,0 @@
-import {Action} from "../../../lib/actions"
-import {RenderedLetter} from "../../../lib/model/skribenten"
-import produce from "immer"
-import {LetterEditorState} from "../model/state"
-
-export const updateLetter: Action<LetterEditorState, [letter: RenderedLetter]> = produce((draft, letter) => {
-    draft.editedLetter.letter = letter
-})
