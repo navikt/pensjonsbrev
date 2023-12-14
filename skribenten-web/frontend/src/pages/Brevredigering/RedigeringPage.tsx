19b04e10eaa2f582fc0dacf8d4d0deb14060c112
johannbm <johannes.bredholt.moskvil@nav.no>
2023-12-14 14:27:02 +0100

    Adjust size

--- skribenten-web/frontend/src/pages/Brevredigering/RedigeringPage.tsx
+++ skribenten-web/frontend/src/pages/Brevredigering/RedigeringPage.tsx
@@ -8,7 +8,7 @@ export function RedigeringPage() {
       css={css`
         background: var(--a-white);
         display: grid;
-        grid-template-columns: minmax(380px, 400px) minmax(758px, 1fr);
+        grid-template-columns: minmax(380px, 400px) 1fr;
         flex: 1;
         border-left: 1px solid var(--a-gray-400);
         border-right: 1px solid var(--a-gray-400);
