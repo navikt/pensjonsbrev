0410ee6819d90322c74cbc89e937c479be73e219
Mo Amini <mo.amini@nav.no>
2024-01-05 09:28:49 +0100

    Legger til application/auth.json til i config loadingen

--- tjenestebuss-integrasjon/src/main/kotlin/no/nav/pensjon/brev/tjenestebuss/tjenestebussintegrasjon/TjenestebussIntegrasjonApplication.kt
+++ tjenestebuss-integrasjon/src/main/kotlin/no/nav/pensjon/brev/tjenestebuss/tjenestebussintegrasjon/TjenestebussIntegrasjonApplication.kt
@@ -16,6 +16,7 @@ fun main() {
             .resolveWith(getVaultSecretConfig(), ConfigResolveOptions.defaults().setAllowUnresolved(true))
             .resolveWith(ConfigFactory.load("sts/auth"), ConfigResolveOptions.defaults().setAllowUnresolved(true))
             .resolveWith(ConfigFactory.load("azuread"))
+            .resolveWith(ConfigFactory.load("application/auth"))
     embeddedServer(Netty, port = tjenestebussIntegrasjonConfig.getInt("port"), host = "0.0.0.0") {
         tjenestebussIntegrationApi(tjenestebussIntegrasjonConfig)
     }.start(wait = true)
