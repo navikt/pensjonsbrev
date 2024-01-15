89ebfc1462f3fe08d239b18e446799635d39123b
Håkon Heggholmen <hakon.heggholmen@nav.no>
2024-01-15 16:41:04 +0100

    refactoring.

--- tjenestebuss-integrasjon/src/main/kotlin/no/nav/pensjon/brev/tjenestebuss/tjenestebussintegrasjon/services/dokumentsproduksjon/DokumentProduksjonService.kt
+++ tjenestebuss-integrasjon/src/main/kotlin/no/nav/pensjon/brev/tjenestebuss/tjenestebussintegrasjon/services/dokumentsproduksjon/DokumentProduksjonService.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.brev.tjenestebuss.tjenestebussintegrasjon.services.dokume
 
 import com.typesafe.config.Config
 import no.nav.pensjon.brev.tjenestebuss.tjenestebussintegrasjon.RedigerDoksysDokumentRequestDto
+import no.nav.pensjon.brev.tjenestebuss.tjenestebussintegrasjon.services.dokumentsproduksjon.RedigerDoksysDokumentResponseDto.Failure
 import no.nav.pensjon.brev.tjenestebuss.tjenestebussintegrasjon.services.soap.STSSercuritySOAPHandler
 import no.nav.pensjon.brev.tjenestebuss.tjenestebussintegrasjon.services.soap.TjenestebussService
 import no.nav.tjeneste.virksomhet.dokumentproduksjon.v3.RedigerDokumentDokumentIkkeFunnet
@@ -34,42 +35,28 @@ class DokumentproduksjonService(config: Config, securityHandler: STSSercuritySOA
             logger.info("Henter metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
             return RedigerDoksysDokumentResponseDto.Success(response.metaforceURI)
         } catch (ex: RedigerDokumentPessimistiskLaasing) {
-            logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
-            return RedigerDoksysDokumentResponseDto.Failure(
-                RedigerDoksysDokumentResponseDto.Failure.FailureType.LASING,
-                ex
-            )
+            logError(requestDto)
+            return Failure(Failure.FailureType.LASING, ex)
         } catch (ex: RedigerDokumentRedigeringIkkeTillatt) {
-            logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
-            return RedigerDoksysDokumentResponseDto.Failure(
-                RedigerDoksysDokumentResponseDto.Failure.FailureType.IKKE_TILLATT,
-                ex
-            )
+            logError(requestDto)
+            return Failure(Failure.FailureType.IKKE_TILLATT, ex)
         } catch (ex: RedigerDokumentInputValideringFeilet) {
-            logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
-            return RedigerDoksysDokumentResponseDto.Failure(
-                RedigerDoksysDokumentResponseDto.Failure.FailureType.VALIDERING_FEILET,
-                ex
-            )
+            logError(requestDto)
+            return Failure(Failure.FailureType.VALIDERING_FEILET, ex)
         } catch (ex: RedigerDokumentDokumentIkkeFunnet) {
-            logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
-            return RedigerDoksysDokumentResponseDto.Failure(
-                RedigerDoksysDokumentResponseDto.Failure.FailureType.IKKE_FUNNET,
-                ex
-            )
+            logError(requestDto)
+            return Failure(Failure.FailureType.IKKE_FUNNET, ex)
         } catch (ex: RedigerDokumentSikkerhetsbegrensning) {
-            logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
-            return RedigerDoksysDokumentResponseDto.Failure(
-                RedigerDoksysDokumentResponseDto.Failure.FailureType.IKKE_TILGANG,
-                ex
-            )
+            logError(requestDto)
+            return Failure(Failure.FailureType.IKKE_TILGANG, ex)
         } catch (ex: RedigerDokumentMetaforceInstanceClosed) {
-            logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.journalpostId} i journalpostId: ${requestDto.dokumentId}")
-            return RedigerDoksysDokumentResponseDto.Failure(
-                RedigerDoksysDokumentResponseDto.Failure.FailureType.LUKKET,
-                ex
-            )
+            logError(requestDto)
+            return Failure(Failure.FailureType.LUKKET, ex)
         }
+    }
+
+    private fun logError(requestDto: RedigerDoksysDokumentRequestDto) {
+        logger.error("En feil oppstod under henting av metaforce URI for dokumentId: ${requestDto.dokumentId} i journalpostId: ${requestDto.journalpostId}")
     }
 }
 
