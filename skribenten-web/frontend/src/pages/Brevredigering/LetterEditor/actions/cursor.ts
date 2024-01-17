15608196e9fd3638d7f8e2501b6c5f3100965f58
johannbm <johannes.bredholt.moskvil@nav.no>
2024-01-17 13:42:10 +0100

    Refactor arrowRight

--- skribenten-web/frontend/src/pages/Brevredigering/LetterEditor/actions/cursor.ts
+++ skribenten-web/frontend/src/pages/Brevredigering/LetterEditor/actions/cursor.ts
@@ -26,95 +26,79 @@ export const cursor: Action<
       case "ArrowUp":
       case "ArrowDown": {
         console.log("UP and DOWN not implemented");
-        return draft;
+        break;
       }
       case "ArrowRight": {
-        const isAtLastContentInBlock = block.content.length - 1 === literalIndex.contentIndex;
         const isAtEndOfContent = content.text.length === cursorPosition;
-
         if (!isAtEndOfContent) break;
 
-        if (isAtLastContentInBlock) {
-          const isAtLastBlock = draft.editedLetter.letter.blocks.length - 1 === literalIndex.blockIndex;
-          if (isAtLastBlock) {
-            break; // We are at the end of the document, there is nowhere to go.
-          }
-          const nextBlock = draft.editedLetter.letter.blocks[literalIndex.blockIndex + 1];
-          const firstLiteralContent = nextBlock.content.findIndex((content) => content.type === "LITERAL");
+        // TODO: POC, refactor this
+        let searchingInBlockIndex = literalIndex.blockIndex;
+        let searchAfterContentIndex = literalIndex.contentIndex + 1;
+        let nextLiteralIndex = -1;
 
-          draft.focus = {
-            blockIndex: literalIndex.blockIndex + 1,
-            contentIndex: firstLiteralContent,
-            cursorPosition: 0,
-          };
-          event.preventDefault();
-        } else {
-          // TODO: POC, refactor this
-          let searchingInBlockIndex = literalIndex.blockIndex;
-          let searchAfterContentIndex = literalIndex.contentIndex + 1;
-          let nextLiteralIndex = -1;
-
-          while (true) {
-            nextLiteralIndex = draft.editedLetter.letter.blocks[searchingInBlockIndex].content.findIndex(
-              (content, index) => index >= searchAfterContentIndex && content.type === "LITERAL",
-            );
-            if (nextLiteralIndex === -1) {
-              searchAfterContentIndex = 0;
-              searchingInBlockIndex = searchingInBlockIndex + 1;
-            } else {
+        while (true) {
+          nextLiteralIndex = draft.editedLetter.letter.blocks[searchingInBlockIndex].content.findIndex(
+            (content, index) => index >= searchAfterContentIndex && content.type === "LITERAL",
+          );
+          if (nextLiteralIndex === -1) {
+            searchingInBlockIndex = searchingInBlockIndex + 1;
+            if (searchingInBlockIndex >= draft.editedLetter.letter.blocks.length) {
               break;
             }
-            if (searchingInBlockIndex > draft.editedLetter.letter.blocks.length) {
-              break;
-            }
+            searchAfterContentIndex = 0;
+          } else {
+            break;
           }
-
-          draft.focus = {
-            blockIndex: searchingInBlockIndex,
-            contentIndex: nextLiteralIndex,
-            cursorPosition: 0,
-          };
-          event.preventDefault();
         }
+
+        // We ran out of blocks, that means we were at the end of the editable document
+        if (nextLiteralIndex === -1) break;
+
+        draft.focus = {
+          blockIndex: searchingInBlockIndex,
+          contentIndex: nextLiteralIndex,
+          cursorPosition: 0,
+        };
+        event.preventDefault();
+
         break;
       }
       case "ArrowLeft": {
-        {
-          const isAtStartOfContent = cursorPosition === 0;
-          if (!isAtStartOfContent) break;
+        const isAtStartOfContent = cursorPosition === 0;
+        if (!isAtStartOfContent) break;
 
-          // TODO: POC, refactor this
-          let searchingInBlockIndex = literalIndex.blockIndex;
-          let searchBeforeContentIndex = literalIndex.contentIndex;
-          let previousLiteralIndex = -1;
+        // TODO: POC, refactor this
+        let searchingInBlockIndex = literalIndex.blockIndex;
+        let searchBeforeContentIndex = literalIndex.contentIndex;
+        let previousLiteralIndex = -1;
 
-          while (true) {
-            previousLiteralIndex = draft.editedLetter.letter.blocks[searchingInBlockIndex].content.findLastIndex(
-              (content, index) => index < searchBeforeContentIndex && content.type === "LITERAL",
-            );
-            if (previousLiteralIndex === -1) {
-              searchingInBlockIndex = searchingInBlockIndex - 1;
+        while (true) {
+          previousLiteralIndex = draft.editedLetter.letter.blocks[searchingInBlockIndex].content.findLastIndex(
+            (content, index) => index < searchBeforeContentIndex && content.type === "LITERAL",
+          );
+          if (previousLiteralIndex === -1) {
+            searchingInBlockIndex = searchingInBlockIndex - 1;
 
-              if (searchingInBlockIndex < 0) {
-                break;
-              }
-              searchBeforeContentIndex = draft.editedLetter.letter.blocks[searchingInBlockIndex].content.length;
-            } else {
+            if (searchingInBlockIndex < 0) {
               break;
             }
+            searchBeforeContentIndex = draft.editedLetter.letter.blocks[searchingInBlockIndex].content.length;
+          } else {
+            break;
           }
-
-          // We ran out of blocks, that means we were at the end of the editable document
-          if (previousLiteralIndex === -1 || searchingInBlockIndex === -1) break;
-
-          draft.focus = {
-            blockIndex: searchingInBlockIndex,
-            contentIndex: previousLiteralIndex,
-            cursorPosition:
-              draft.editedLetter.letter.blocks[searchingInBlockIndex].content[previousLiteralIndex].text.length,
-          };
-          event.preventDefault();
         }
+
+        // We ran out of blocks, that means we were at the beginning of the editable document
+        if (previousLiteralIndex === -1) break;
+
+        draft.focus = {
+          blockIndex: searchingInBlockIndex,
+          contentIndex: previousLiteralIndex,
+          cursorPosition:
+            draft.editedLetter.letter.blocks[searchingInBlockIndex].content[previousLiteralIndex].text.length,
+        };
+        event.preventDefault();
         break;
       }
     }
