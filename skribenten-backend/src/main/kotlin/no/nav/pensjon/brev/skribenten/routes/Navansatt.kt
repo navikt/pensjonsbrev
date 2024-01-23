1a3933a72572316fed7cfe7e38cb0db528e260c4
Mo Amini <mo.amini@nav.no>
2024-01-23 14:36:42 +0100

    Henter nav identen for navansatt enheter fra tokenet isteden for pathparam

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Navansatt.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Navansatt.kt
@@ -1,17 +1,19 @@
 package no.nav.pensjon.brev.skribenten.routes
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.util.*
+import no.nav.pensjon.brev.skribenten.getLoggedInNavIdent
 import no.nav.pensjon.brev.skribenten.services.NavansattService
-import no.nav.pensjon.brev.skribenten.services.respondWithResult
 
 fun Route.navansattRoute(navansattService: NavansattService) {
     route("/navansatt") {
 
-        get("/{ansattId}/enheter") {
-            val ansattId = call.parameters.getOrFail<String>("ansattId")
-            respondWithResult(navansattService.hentNavAnsattEnhetListe(ansattId))
+        get("/enheter") {
+            getLoggedInNavIdent()?.let {
+                call.respond(navansattService.hentNavAnsattEnhetListe(it))
+            } ?: call.respond(HttpStatusCode.Unauthorized)
         }
     }
 
