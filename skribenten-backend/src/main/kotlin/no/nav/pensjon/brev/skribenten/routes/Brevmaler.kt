ae86e624741c9ad7484ce0207b65943142607908
johannbm <johannes.bredholt.moskvil@nav.no>
2024-02-07 14:07:43 +0100

    Add field to dto

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Brevmaler.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Brevmaler.kt
@@ -1,23 +1,20 @@
 package no.nav.pensjon.brev.skribenten.routes
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
-import no.nav.pensjon.brev.skribenten.auth.UnauthorizedException
 import no.nav.pensjon.brev.skribenten.auth.UserPrincipal
 import no.nav.pensjon.brev.skribenten.services.BrevmetadataService
+import no.nav.pensjon.brev.skribenten.services.LetterMetadata
 
 fun Route.brevmalerRoute(
     brevmetadataService: BrevmetadataService
 ) {
     get("/lettertemplates/e-blanketter") {
         val hasAccess = call.principal<UserPrincipal>()?.isPensjonUtlandGroup() ?: false
-        if (!hasAccess)  call.respond(HttpStatusCode.Forbidden, "Ingen tilgang til e-blanketter")
-
-        call.respond(brevmetadataService.getEblanketter())
+        if (hasAccess) call.respond(brevmetadataService.getEblanketter()) else emptyList<LetterMetadata>()
     }
     get("/lettertemplates/{sakType}") {
         val sakType = call.parameters.getOrFail("sakType")
