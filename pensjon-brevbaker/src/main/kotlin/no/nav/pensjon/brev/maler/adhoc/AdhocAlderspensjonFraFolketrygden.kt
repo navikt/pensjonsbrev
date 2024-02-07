781d0d7fe0191e3fb8d70884f287261d2cf50f9c
Jeremy Elsom <jeremy.elsom@nav.no>
2024-02-07 12:17:33 +0100

    Updated AdhocAlderspensjonFraFolketrygden.kt

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocAlderspensjonFraFolketrygden.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocAlderspensjonFraFolketrygden.kt
@@ -41,32 +41,41 @@ object AdhocAlderspensjonFraFolketrygden : AutobrevTemplate<EmptyBrevdata> {
                     English to "The Norwegian Parliament has decided new rules regarding survivor's rights in the retirement pension from the 1st of January 2024."
                 )
             }
+            paragraph {
+                text(
+                    Bokmal to "Reglene har ingen praktisk betydning for deg. Samlet pensjon før skatt er den samme som før. "
+                            + "Vi har bare endret metoden for beregningen av alderspensjonen din.",
+                    Nynorsk to "",
+                    English to ""
+                )
+            }
             title1 {
                 text(
-                    Bokmal to "Hva betyr dette for deg?",
-                    Nynorsk to "Kva betyr dette for deg?",
-                    English to "What does this mean for you?"
+                    Bokmal to "Endret metode for beregning",
+                    Nynorsk to "",
+                    English to ""
                 )
             }
             paragraph {
                 text(
-                    Bokmal to "Vi har endret metoden for beregningen av alderspensjonen din. Dette gjør at du nå får et gjenlevendetillegg. Dette erstatter det som før het «minstenivåtillegg individuelt».",
-                    Nynorsk to "Vi har endra metoden for berekninga av alderspensjonen din. Dette gjer at du no får eit attlevandetillegg. Dette erstattar det som før heitte «minstenivåtillegg individuelt».",
-                    English to "We have changed the method for calculating your retirement pension. This means that you now receive a survivor’s supplement. This replaces what was previously called ‘minimum pension supplement’."
+                    Bokmal to "Metoden for beregningen av alderspensjonen din gjør at du nå får et gjenlevendetillegg. "
+                            + "Dette erstatter det som før het «minstenivåtillegg individuelt».",
+                    Nynorsk to "",
+                    English to ""
+                )
+            }
+            title1 {
+                text(
+                    Bokmal to "Lurer du på hva du har utbetalt?",
+                    Nynorsk to "",
+                    English to ""
                 )
             }
             paragraph {
                 text(
-                    Bokmal to "Samlet pensjon før skatt er den samme som før.",
-                    Nynorsk to "Samla pensjon før skatt er den same som før.",
-                    English to "The total pension amount before tax is the same as before."
-                )
-            }
-            paragraph {
-                text(
-                    Bokmal to "Alderspensjonen reguleres hvert år med gjennomsnittet av pris- og lønnsveksten.",
-                    Nynorsk to "Alderspensjonen blir regulert kvart år med gjennomsnittet av pris- og lønnsveksten.",
-                    English to "Retirement pension is adjusted annually by the average of consumer price and wage growth."
+                Bokmal to "Du kan logge deg på ${Constants.UTBETALINGSOVERSIKT_URL} for å se utbetalingene dine.",
+                Nynorsk to "",
+                English to ""
                 )
             }
             title1 {
@@ -78,9 +87,13 @@ object AdhocAlderspensjonFraFolketrygden : AutobrevTemplate<EmptyBrevdata> {
             }
             paragraph {
                 text(
-                    Bokmal to "Hvis du får endringer i familiesituasjon eller planlegger opphold i et annet land, kan det påvirke utbetalingen din. I slike tilfeller må du derfor straks melde fra til NAV. Se hva du må melde fra om på nav.no/pensjon-endring.",
-                    Nynorsk to "Viss du får endringar i familiesituasjon eller planlegg opphald i eit anna land, kan det påverke utbetalinga di. I slike tilfelle må du derfor straks melde frå til NAV. Sjå kva du må melde frå om på nav.no/pensjon-endring. ",
-                    English to "If you have changes in your family situation or you plan to live abroad, this may influence your benefits. You are obliged to notify NAV as soon as you are aware of any of these changes. You can find out what you are required to report at nav.no/pensjon-endring."
+                    Bokmal to "Hvis du får endringer i familiesituasjon eller planlegger opphold i et annet land, kan det påvirke utbetalingen din. "
+                            + "I slike tilfeller må du derfor straks melde fra til NAV. Se hva du må melde fra om på ${Constants.PENSJON_ENDRING_URL}.",
+                    Nynorsk to "Viss du får endringar i familiesituasjon eller planlegg opphald i eit anna land, kan det påverke utbetalinga di. "
+                            + "I slike tilfelle må du derfor straks melde frå til NAV. Sjå kva du må melde frå om på ${Constants.PENSJON_ENDRING_URL}.",
+                    English to "If you have changes in your family situation or you plan to live abroad, this may influence your benefits. "
+                            + "You are obliged to notify NAV as soon as you are aware of any of these changes. "
+                            +"You can find out what you are required to report at ${Constants.PENSJON_ENDRING_URL}."
                 )
             }
             title1 {
@@ -92,16 +105,16 @@ object AdhocAlderspensjonFraFolketrygden : AutobrevTemplate<EmptyBrevdata> {
             }
             paragraph {
                 text(
-                    Bokmal to "Du finner mer informasjon på ${Constants.ALDERSPENSJON_GJENLEVENDE_URL}."
+                    Bokmal to "Du finner mer informasjon på ${Constants.REGULERING_ALDERSPENSJON_URL}."
                             + " På ${Constants.KONTAKT_URL} kan du chatte eller skrive til oss."
-                            + " Hvis du ikke finner svar på $NAV_URL, kan du ringe oss på telefon ${Constants.NAV_KONTAKTSENTER_TELEFON},"
+                            + " Hvis du ikke finner svar på $NAV_URL, kan du ringe oss på telefon ${Constants.NAV_KONTAKTSENTER_TELEFON_PENSJON},"
                             + " hverdager kl. ${Constants.NAV_KONTAKTSENTER_AAPNINGSTID}.",
-                    Nynorsk to "Du finn meir informasjon på ${Constants.ALDERSPENSJON_GJENLEVENDE_URL}."
-                            + " Om du ikkje finn svar på $NAV_URL, kan du ringe oss på telefon ${Constants.NAV_KONTAKTSENTER_TELEFON},"
+                    Nynorsk to "Du finn meir informasjon på ${Constants.REGULERING_ALDERSPENSJON_URL}."
+                            + " Om du ikkje finn svar på $NAV_URL, kan du ringe oss på telefon ${Constants.NAV_KONTAKTSENTER_TELEFON_PENSJON},"
                             + " kvardagar kl. ${Constants.NAV_KONTAKTSENTER_AAPNINGSTID}.",
-                    English to "You can find more information at ${Constants.ALDERSPENSJON_GJENLEVENDE_URL}."
+                    English to "You can find more information at ${Constants.REGULERING_ALDERSPENSJON_URL}."
                             + " At ${Constants.KONTAKT_URL}, you can chat or write to us."
-                            + " If you do not find the answer at $NAV_URL, you can call us at: +47 ${Constants.NAV_KONTAKTSENTER_TELEFON},"
+                            + " If you do not find the answer at $NAV_URL, you can call us at: +47 ${Constants.NAV_KONTAKTSENTER_TELEFON_PENSJON},"
                             + " weekdays ${Constants.NAV_KONTAKTSENTER_AAPNINGSTID}."
                 )
             }
