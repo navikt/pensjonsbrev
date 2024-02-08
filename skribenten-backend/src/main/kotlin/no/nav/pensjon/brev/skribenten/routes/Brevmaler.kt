f02267d31fa6d9321c16d0df005612ba454ddcc7
Håkon Heggholmen <hakon.heggholmen@nav.no>
2024-02-08 10:42:17 +0100

    Sjekker groups claim for pensjon utland.

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Brevmaler.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Brevmaler.kt
@@ -2,17 +2,28 @@ package no.nav.pensjon.brev.skribenten.routes
 
 import com.typesafe.config.Config
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
+import no.nav.pensjon.brev.skribenten.auth.UserPrincipal
 import no.nav.pensjon.brev.skribenten.isInGroup
 import no.nav.pensjon.brev.skribenten.services.BrevmetadataService
+import org.slf4j.LoggerFactory
 
 fun Route.brevmalerRoute(
     brevmetadataService: BrevmetadataService,
     groupsConfig: Config
 ) {
+    val logger = LoggerFactory.getLogger("no.nav.pensjon.brev.skribenten.routes.brevmalerRoute")
     get("/lettertemplates/e-blanketter") {
+        logger.info("DEBUG_TMP: ${groupsConfig.getString("pensjon_utland")}")
+        logger.info(
+            "DEBUG_TMP2: ${
+                call.authentication.principal<UserPrincipal>()?.jwtPayload?.getClaim("groups")
+                    ?.asList(String::class.java)
+            }"
+        )
         val hasAccess = isInGroup(groupsConfig.getString("pensjon_utland"))
         val eblanketter = if (hasAccess) brevmetadataService.getEblanketter() else emptyList()
         call.respond(eblanketter)
