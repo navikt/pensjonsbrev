da2d8619b1d3b79c3965fd1f0a0291d25cc12c59
johannbm <johannes.bredholt.moskvil@nav.no>
2024-02-12 13:46:29 +0100

    Delete bff

--- autodokumentasjon-web/bff/src/index.ts
+++
@@ -1,9 +0,0 @@
-import config from "./config.js";
-import { server } from "./server.js";
-
-const hostName = config.app.host;
-const port = config.app.port;
-
-server.listen(port, hostName, () => {
-  console.log(`Starting skribenten-web-bff at ${hostName}:${port}`);
-});
