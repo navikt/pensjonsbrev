b84096216377e271bfa05bed93eb1fd14dd83deb
johannbm <johannes.bredholt.moskvil@nav.no>
2024-02-13 10:14:17 +0100

    Propagate requestid to frontend

--- autodokumentasjon-web/frontend/src/routes/index.tsx
+++ autodokumentasjon-web/frontend/src/routes/index.tsx
@@ -1,5 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => <div>App</div>,
+  component: () => (
+    <div>
+      <button onClick={() => fetch("/brevbaker")}>Hent litt data</button>
+    </div>
+  ),
 });
