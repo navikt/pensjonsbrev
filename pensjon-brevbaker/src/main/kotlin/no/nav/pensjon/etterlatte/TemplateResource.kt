541a226689548c645a6f91e8be5c73c3c3bdb7db
Espen Schei <espen.schei@outlook.com>
2024-02-16 13:48:13 +0100

    Nytt informasjonsbrev til barn ved dødsfall av forelder (#569)
    EY-3452

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/TemplateResource.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/TemplateResource.kt
@@ -7,6 +7,7 @@ import no.nav.pensjon.etterlatte.maler.andre.TomMalInformasjonsbrev
 import no.nav.pensjon.etterlatte.maler.andre.UtsattKlagefrist
 import no.nav.pensjon.etterlatte.maler.barnepensjon.avslag.BarnepensjonAvslag
 import no.nav.pensjon.etterlatte.maler.barnepensjon.avslag.BarnepensjonAvslagRedigerbartUtfall
+import no.nav.pensjon.etterlatte.maler.barnepensjon.informasjon.BarnepensjonInformasjonDoedsfall
 import no.nav.pensjon.etterlatte.maler.barnepensjon.innvilgelse.BarnepensjonInnvilgelse
 import no.nav.pensjon.etterlatte.maler.barnepensjon.innvilgelse.BarnepensjonInnvilgelseForeldreloes
 import no.nav.pensjon.etterlatte.maler.barnepensjon.innvilgelse.BarnepensjonInnvilgelseForeldreloesRedigerbartUfall
@@ -56,7 +57,7 @@ val prodAutobrevTemplates: Set<EtterlatteTemplate<*>> =
         BarnepensjonVarselRedigerbartUtfall,
         BarnepensjonVedleggBeregningTrygdetidRedigerbartUtfall,
         BarnepensjonVedleggForhaandsvarselRedigerbartUtfall,
-
+        BarnepensjonInformasjonDoedsfall,
         // Omstillingsstønad
         OmstillingsstoenadAvslag,
         OmstillingsstoenadAvslagRedigerbartUtfall,
@@ -70,21 +71,17 @@ val prodAutobrevTemplates: Set<EtterlatteTemplate<*>> =
         OmstillingsstoenadVedleggForhaandsvarselRedigerbartUtfall,
         OmstillingsstoenadVarsel,
         OmstillingsstoenadVarselRedigerbartUtfall,
-
         // Tilbakekreving
         TilbakekrevingInnhold,
         TilbakekrevingFerdig,
-
         // Klage
         AvvistKlageInnhold,
         AvvistKlageFerdigstilling,
-
         // Informasjonsbrev
         TomMal,
         TomDelmal,
         TomMalInformasjonsbrev,
         UtsattKlagefrist,
-
         // Div migrering mm.
         ForhaandsvarselOmregningBP,
         EnkeltVedtakOmregningNyttRegelverk,
@@ -103,15 +100,11 @@ class TemplateResource(
     private val redigerbareBrevMap: Map<EtterlatteBrevKode, EtterlatteTemplate<*>> =
         redigerbareTemplates.associateBy { it.kode }
 
-    fun getAutoBrev(): Set<EtterlatteBrevKode> =
-        autoBrevMap.keys
+    fun getAutoBrev(): Set<EtterlatteBrevKode> = autoBrevMap.keys
 
-    fun getAutoBrev(kode: EtterlatteBrevKode): LetterTemplate<*, *>? =
-        autoBrevMap[kode]?.template
+    fun getAutoBrev(kode: EtterlatteBrevKode): LetterTemplate<*, *>? = autoBrevMap[kode]?.template
 
-    fun getRedigerbareBrev(): Set<EtterlatteBrevKode> =
-        redigerbareBrevMap.keys
+    fun getRedigerbareBrev(): Set<EtterlatteBrevKode> = redigerbareBrevMap.keys
 
-    fun getRedigerbartBrev(kode: EtterlatteBrevKode): LetterTemplate<*, *>? =
-        redigerbareBrevMap[kode]?.template
+    fun getRedigerbartBrev(kode: EtterlatteBrevKode): LetterTemplate<*, *>? = redigerbareBrevMap[kode]?.template
 }
---
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/BarnepensjonInformasjonDoedsfall.kt
@@ -0,0 +1,9 @@
+package no.nav.pensjon.etterlatte.fixtures
+
+import no.nav.pensjon.etterlatte.maler.barnepensjon.informasjon.BarnepensjonInformasjonDoedsfallDTO
+
+fun createBarnepensjonInformasjonDoedsfallDTO() =
+    BarnepensjonInformasjonDoedsfallDTO(
+        innhold = emptyList(),
+        avdoedNavn = "Ola Nordmann",
+    )
