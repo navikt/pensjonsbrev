471dc7e2fba6920206bf3f119a5320e5460792ef
johannbm <johannes.bredholt.moskvil@nav.no>
2024-02-22 10:39:27 +0100

    Map failuretypes

--- skribenten-web/frontend/src/components/ApiError.tsx
+++ skribenten-web/frontend/src/components/ApiError.tsx
@@ -4,6 +4,9 @@ import { Alert, CopyButton, Heading, HStack, Link } from "@navikt/ds-react";
 import { ErrorComponent } from "@tanstack/react-router";
 import { AxiosError } from "axios";
 
+import type { FailureType } from "~/types/apiTypes";
+import { FAILURE_TYPES } from "~/types/apiTypes";
+
 const PORTEN_URL = "https://jira.adeo.no/plugins/servlet/desk/portal/541";
 export function ApiError({ error, title }: { error: unknown; title: string }) {
   if (error instanceof AxiosError) {
@@ -22,27 +25,26 @@ export function ApiError({ error, title }: { error: unknown; title: string }) {
         </Heading>
         <div>
           {correlationId && (
-            <>
-              <HStack align="center" gap="1">
-                <span>
-                  Hvis det skjer igjen, kopier ID nedenfor og{" "}
-                  <Link href={PORTEN_URL} target="_blank">
-                    meld feil i Porten
-                  </Link>
-                </span>
-                <CopyButton
-                  copyText={correlationId}
-                  css={css`
-                    border-radius: 4px;
-                    border: 2px solid var(--a-border-default);
-                  `}
-                  icon={<FilesIcon />}
-                  size="small"
-                  text="Kopier ID"
-                  variant="action"
-                />
-              </HStack>
-            </>
+            <HStack align="center" gap="1">
+              <span>{mapErrorMessage(error.message)}</span>
+              <span>
+                Hvis det skjer igjen, kopier ID nedenfor og{" "}
+                <Link href={PORTEN_URL} target="_blank">
+                  meld feil i Porten
+                </Link>
+              </span>
+              <CopyButton
+                copyText={correlationId}
+                css={css`
+                  border-radius: 4px;
+                  border: 2px solid var(--a-border-default);
+                `}
+                icon={<FilesIcon />}
+                size="small"
+                text="Kopier ID"
+                variant="action"
+              />
+            </HStack>
           )}
         </div>
       </Alert>
@@ -51,4 +53,43 @@ export function ApiError({ error, title }: { error: unknown; title: string }) {
 
   // If the error is not an axios error, then there is a programming error. Fallback to default Router Error
   return <ErrorComponent error={error} />;
+}
+
+function mapErrorMessage(errorMessage: string) {
+  if (isOfFailureType(errorMessage)) {
+    return mapFailureTypes(errorMessage);
+  }
+
+  return "";
+}
+
+function isOfFailureType(error: string): error is FailureType {
+  return FAILURE_TYPES.includes(error as FailureType);
+}
+
+function mapFailureTypes(failureType: FailureType) {
+  switch (failureType) {
+    case "DOKSYS_BESTILLING_ADDRESS_NOT_FOUND":
+    case "EXSTREAM_BESTILLING_ADRESSE_MANGLER": {
+      return "Fant ikke adresse på brukeren.";
+    }
+    case "DOKSYS_BESTILLING_PERSON_NOT_FOUND": {
+      return "Fant ikke bruker.";
+    }
+    case "EXSTREAM_BESTILLING_MANGLER_OBLIGATORISK_INPUT": {
+      return "Mangler i data-grunnlaget til brukeren for å kunne bestille brevet.";
+    }
+    case "DOKSYS_BESTILLING_UNAUTHORIZED": {
+      return "Har ikke tilgang til å produsere brev på brukeren.";
+    }
+    case "DOKSYS_REDIGERING_IKKE_TILGANG": {
+      return "Prøver å redigere ett brev hvor man ikke har tilgang til bruker/enhet.";
+    }
+    case "DOKSYS_REDIGERING_UNDER_REDIGERING": {
+      return "Brevet er allerede åpent for redigering. Det kan hende det er noen andre som har åpnet den eller at du allerede har den åpnet.";
+    }
+    default: {
+      return "Teknisk feil.";
+    }
+  }
 }
