bd86a036ac511f734ac04923381a7fec9050b5c9
Øyvind Stette Haarberg <oyvind.haarberg@bekk.no>
2024-02-29 10:18:28 +0100

    EY-3559: Oversendelsesbrev til bruker for klage (#615)
    * Oversendelsesbrev til bruker for klage
    
    * Fikser opp i mindre rot

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/KlageOversendelseDTO.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/KlageOversendelseDTO.kt
@@ -1,13 +1,14 @@
 package no.nav.pensjon.etterlatte.fixtures
 
 import no.nav.pensjon.etterlatte.maler.klage.Formkrav
-import no.nav.pensjon.etterlatte.maler.klage.KlageOversendelseDTO
+import no.nav.pensjon.etterlatte.maler.klage.KlageOversendelseBlankettDTO
+import no.nav.pensjon.etterlatte.maler.klage.KlageOversendelseBrukerDTO
 import no.nav.pensjon.etterlatte.maler.klage.VedtakKlagenGjelder
 import no.nav.pensjon.etterlatte.maler.klage.VedtakType
 import no.nav.pensjon.etterlatte.maler.tilbakekreving.SakType
 import java.time.LocalDate
 
-fun createKlageOversendelseDTO() = KlageOversendelseDTO(
+fun createKlageOversendelseBlankettDTO() = KlageOversendelseBlankettDTO(
     innhold = emptyList(),
     formkrav = Formkrav(
         vedtaketKlagenGjelder = VedtakKlagenGjelder(
@@ -31,3 +32,19 @@ fun createKlageOversendelseDTO() = KlageOversendelseDTO(
     sakType = SakType.BARNEPENSJON
 
     )
+
+fun createKlageOversendelseBrukerDTO() = KlageOversendelseBrukerDTO(
+    sakType = SakType.OMSTILLINGSSTOENAD,
+    klageDato = LocalDate.now().minusWeeks(3),
+    vedtakDato = LocalDate.now().minusMonths(2),
+    innstillingTekst = """
+        En innstillingstekst
+        
+        Med flere linjer
+        
+        og noe annet
+        """.trimIndent(),
+    under18Aar = false,
+    harVerge = false,
+    bosattIUtlandet = false
+)
