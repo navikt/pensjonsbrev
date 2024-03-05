358fbab9cc73bd2b34e7187e45fc2abf37749ff2
Mo Amini <moh_amini@hotmail.com>
2024-03-05 14:52:36 +0100

    Modifisert VarselSaksbehandlingstidAutoDto (#636)
    Modifisert VarselSaksbehandlingstidAutoDto

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/fixtures/ufoere/VarselSaksbehandlingstidAuto.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/fixtures/ufoere/VarselSaksbehandlingstidAuto.kt
@@ -1,12 +1,10 @@
 package no.nav.pensjon.brev.fixtures.ufoere
 
-import no.nav.pensjon.brev.Fixtures
 import no.nav.pensjon.brev.api.model.maler.ufoerApi.VarselSaksbehandlingstidAutoDto
 import java.time.LocalDate
 
 fun createVarselSaksbehandlingstidAutoDto() =
     VarselSaksbehandlingstidAutoDto(
-        mottattDatoMinus2Dager = LocalDate.now(),
+        dagensDatoMinus2Dager = LocalDate.now(),
         utvidetBehandlingstid = false,
-        orienteringOmRettigheterUfoere = Fixtures.create(),
     )
