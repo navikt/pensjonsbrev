060c9e7693a33f480ef7928f4d9086422ff1a9db
Mo Amini <moh_amini@hotmail.com>
2024-03-05 14:52:36 +0100

    Modifisert VarselSaksbehandlingstidAutoDto (#636)
    Modifisert VarselSaksbehandlingstidAutoDto

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ufoerApi/VarselSaksbehandlingstidAuto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ufoerApi/VarselSaksbehandlingstidAuto.kt
@@ -1,13 +1,11 @@
 package no.nav.pensjon.brev.api.model.maler.ufoerApi
 
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
-import no.nav.pensjon.brev.api.model.vedlegg.OrienteringOmRettigheterUfoereDto
 import java.time.LocalDate
 
 
 @Suppress("unused")
 data class VarselSaksbehandlingstidAutoDto(
-    val mottattDatoMinus2Dager: LocalDate,
+    val dagensDatoMinus2Dager: LocalDate,
     val utvidetBehandlingstid: Boolean,
-    val orienteringOmRettigheterUfoere: OrienteringOmRettigheterUfoereDto,
 ) : BrevbakerBrevdata
