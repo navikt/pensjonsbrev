88b385520d39bd839f14ab5fde165704ab1abe8d
Håkon Heggholmen <hakon.heggholmen@nav.no>
2024-03-11 10:35:15 +0100

    Legger til manglende kategori overrides.

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/BrevmetadataService.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/BrevmetadataService.kt
@@ -160,7 +160,7 @@ private val kategoriOverrides: Map<String, String> = mapOf(
     "Vedtak - endring og revurdering" to listOf("PE_AF_04_020","PE_AF_04_114","AP_AVSL_ENDR","AP_ENDR_GRAD_MAN","AP_ENDR_STANS_MAN","AP_ENDR_OPPTJ_MAN","AP_ENDR_FT_MAN","AP_ENDR_EPS_MAN","AP_ENDR_INST_MAN","AP_ENDR_GJRETT_MAN","AP_OPPH_FT_MAN","AP_AVSL_GJRETT_MAN","AP_AVSL_FT_MAN","PE_AP_04_020","PE_AP_04_214","PE_AP_04_215","PE_AP_04_216","PE_AP_04_220","PE_AP_04_920","PE_FT_01_006","PE_FT_01_007","BP_OPPH_MAN","PE_BP_04_020","PE_BP_04_021","PE_BP_04_031","PE_GP_04_020","PE_GP_04_028","PE_GP_04_029","PE_GP_04_030","PE_UT_06_300","PE_UT_04_100","PE_UT_04_102","PE_UT_04_109","PE_UT_04_114","PE_UT_07_100"),
     "Vedtak - flytte mellom land" to listOf("AP_ENDR_FLYTT_MAN","AP_STANS_FLYTT_MAN","PE_AP_04_223","PE_AP_04_224","PE_AP_04_225","PE_IY_04_125","PE_IY_04_126","PE_IY_04_127"),
     "Sluttbehandling" to listOf("PE_AP_04_903","PE_AP_04_904","PE_AP_04_912","PE_GP_04_025","PE_GP_04_032","PE_UT_04_106","PE_UT_04_107","INFO_P1"),
-    "Informasjonsbrev" to listOf("PE_IY_03_175","AP_INFO_STID_MAN","PE_IY_03_174","PE_BP_01_001","PE_BP_01_002","PE_IY_03_169","PE_UP_07_105","PE_UT_04_001","PE_UT_04_004","PE_IY_05_041","PE_IY_03_163","PE_IY_03_164","PE_IY_03_180","PE_IY_03_170","PE_IY_03_172"),
+    "Informasjonsbrev" to listOf("PE_IY_03_175","AP_INFO_STID_MAN","PE_IY_03_174","PE_BP_01_001","PE_BP_01_002","PE_IY_03_169","PE_UP_07_105","PE_UT_04_001","PE_UT_04_004","PE_IY_05_041","PE_IY_03_163","PE_IY_03_164","PE_IY_03_180","PE_IY_03_170","PE_IY_03_172", "DOD_INFO_RETT_MAN", "PE_IY_03_173", "PE_IY_03_171", "PE_AP_04_922", "PE_AP_04_922"),
     "Varsel" to listOf("VARSEL_REVURD","PE_IY_03_051","PE_IY_03_179"),
     "Vedtak - eksport" to listOf("PE_GP_04_022","PE_UT_04_103","PE_UT_04_115"),
     "Omsorgsopptjening" to listOf("OMSORG_EGEN_MAN","PE_IY_04_010"),
@@ -169,7 +169,7 @@ private val kategoriOverrides: Map<String, String> = mapOf(
     "Leveattest" to listOf("PE_IY_03_176","PE_IY_03_177","PE_IY_05_301","PE_IY_05_411","PE_IY_05_510","PE_IY_05_410","PE_IY_05_511"),
     "Feilutbetaling" to listOf("VARSEL_TILBAKEBET","VEDTAK_TILBAKEKREV","VEDTAK_TILBAKEKREV_MIDL","PE_IY_04_060","PE_IY_04_061","PE_IY_05_027"),
     "Klage og anke" to listOf("PE_IY_03_151","PE_IY_03_152","PE_IY_03_158","PE_IY_03_159","PE_IY_03_160","PE_IY_04_050","PE_IY_04_051","PE_IY_03_150","PE_IY_03_153","PE_IY_03_154","PE_IY_03_157","PE_IY_03_161","PE_IY_03_162"),
-    "Posteringsgrunnlag" to listOf("PE_OK_06_100","PE_OK_06_101"),
+    "Posteringsgrunnlag" to listOf("PE_OK_06_100","PE_OK_06_101", "PE_OK_06_102"),
     "Fritekstbrev" to listOf("PE_IY_03_156","PE_IY_05_300"),
 ).flatMap { kategori -> kategori.value.map { it to kategori.key } }.toMap()
 
