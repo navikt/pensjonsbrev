77e669ec1e9703999ddc95a5124fc19e31b2977f
johannbm <johannes.bredholt.moskvil@nav.no>
2024-03-12 13:02:39 +0100

    lazy load devtool

--- skribenten-web/frontend/src/routes/__root.tsx
+++ skribenten-web/frontend/src/routes/__root.tsx
@@ -1,11 +1,20 @@
 import { QueryClient } from "@tanstack/react-query";
 import { Outlet, rootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import React from "react";
 
 import { AppHeader } from "~/components/AppHeader";
 
 export const queryClient = new QueryClient();
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((response) => ({
+          default: response.TanStackRouterDevtools,
+        })),
+      );
 
 export const Route = rootRouteWithContext<{
   queryClient: typeof queryClient;
