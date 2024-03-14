7d9520bf906fa2205b25054d3f8cbf64d597f64a
Mo Amini <mo.amini@nav.no>
2024-03-14 10:13:30 +0100

    Bytter fra antall måneder behandlingstid for VarselSaksbehandlingstidAuto

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/ufoereBrev/VarselSaksbehandlingstidAuto.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/ufoereBrev/VarselSaksbehandlingstidAuto.kt
@@ -4,7 +4,6 @@ import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.api.model.maler.ufoerApi.VarselSaksbehandlingstidAutoDto
 import no.nav.pensjon.brev.api.model.maler.ufoerApi.VarselSaksbehandlingstidAutoDtoSelectors.dagensDatoMinus2Dager
 import no.nav.pensjon.brev.api.model.maler.ufoerApi.VarselSaksbehandlingstidAutoDtoSelectors.utvidetBehandlingstid
-import no.nav.pensjon.brev.maler.fraser.common.Felles
 import no.nav.pensjon.brev.maler.fraser.ufoer.Ufoeretrygd
 import no.nav.pensjon.brev.template.AutobrevTemplate
 import no.nav.pensjon.brev.template.Language.Bokmal
@@ -58,13 +57,13 @@ object VarselSaksbehandlingstidAuto : AutobrevTemplate<VarselSaksbehandlingstidA
             paragraph {
                 textExpr(
                     Bokmal to "Søknaden din blir behandlet så snart som mulig, og senest innen ".expr()
-                            + ifElse(utvidetBehandlingstid, ifFalse = "4", ifTrue = "12") + " måneder. "
+                            + ifElse(utvidetBehandlingstid, ifFalse = "6", ifTrue = "20") + " måneder. "
                             + "Blir ikke saken din ferdigbehandlet innen denne fristen, vil vi gi deg beskjed om ny svartid.",
                     Nynorsk to "Søknaden din vert handsama så snart som mogleg, og seinast innan ".expr()
-                            + ifElse(utvidetBehandlingstid, ifFalse = "4", ifTrue = "12") + " månader. "
+                            + ifElse(utvidetBehandlingstid, ifFalse = "6", ifTrue = "20") + " månader. "
                             + "Vert ikkje saka di handsama innan denne fristen, vil vi gje deg melding om ny svartid.",
                     English to "Your application will be processed as soon as possible, and no later than within ".expr()
-                            + ifElse(utvidetBehandlingstid, ifFalse = "4", ifTrue = "12") + " months. "
+                            + ifElse(utvidetBehandlingstid, ifFalse = "6", ifTrue = "20") + " months. "
                             + "If your case is not processed within this deadline, we will notify you of a new response time."
                 )
             }
