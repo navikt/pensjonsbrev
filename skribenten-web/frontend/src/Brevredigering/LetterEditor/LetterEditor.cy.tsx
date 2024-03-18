8340a0268c54270f1f987113d2650198f2cef92c
johannbm <johannes.bredholt.moskvil@nav.no>
2024-03-18 15:49:17 +0100

    rewrite lots of tests

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/LetterEditor.cy.tsx
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/LetterEditor.cy.tsx
@@ -9,29 +9,123 @@ import exampleLetter1 from "./example-letter-1.json";
 import { LetterEditor } from "./LetterEditor";
 
 describe("<LetterEditor />", () => {
-  it("Vertical navigation", () => {
+  beforeEach(() => {
     cy.viewport(800, 1400);
+  });
+  it("ArrowUp works within sibling contenteditables", () => {
     cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
-    cy.contains("punkt 3").click();
-    move("{upArrow}", 3);
-    move("{rightArrow}", 20);
+
+    cy.contains("CP1-3").click();
+    move("{leftArrow}", 10);
     move("{upArrow}", 1);
-    assertCaret("CP1", 0);
-    move("{leftArrow}", 1);
+    assertCaret("[CP1-2]", 31);
+  });
+  it("ArrowDown works within sibling contenteditables", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP1-1").click();
     move("{downArrow}", 1);
-    move("{rightArrow}", 4);
+    assertCaret("[CP1-2]", 67);
+  });
+  it("ArrowUp moves to the right of a variable if that is closest", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP1-3").click();
+    move("{leftArrow}", 45);
     move("{upArrow}", 1);
-    assertCaret("fra trygdemyndighetene i", 26);
+    assertCaret("[CP1-2]", 0);
+  });
+  it("ArrowUp moves to the left of a variable if that is closest", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP1-3").click();
+    move("{home}", 45);
+
+    move("{rightArrow}", 30);
+    move("{upArrow}", 1);
+    assertCaret("[CP1-1]", 21);
+  });
+
+  it("ArrowUp works between paragraphs", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP2-2").click();
+    move("{upArrow}", 1);
+    assertCaret("[CP2-1]", 17);
+  });
+  it("ArrowDown works between paragraphs", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP2-1").click();
+    move("{downArrow}", 1);
+    assertCaret("[CP2-3]", 7);
+  });
+  it("ArrowDown moves between paragraphs and to the nearest side of a variable [LEFT]", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP2-1").click();
+    move("{leftArrow}", 20);
+    move("{downArrow}", 1);
+    assertCaret("[CP2-2]", 17);
+  });
+  it("ArrowDown moves between paragraphs and to the nearest side of a variable [RIGHT]", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP2-1").click();
+    move("{leftArrow}", 10);
+    move("{downArrow}", 1);
+    assertCaret("[CP2-3]", 0);
+  });
+  it("Can move up an itemlist", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+    // CP3
+    cy.contains("CP3-3").click();
+    move("{upArrow}", 1);
+    assertCaret("[CP3-2]", 28);
+    move("{upArrow}", 1);
+    assertCaret("[CP3-1]", 126);
+    move("{upArrow}", 1);
+    assertCaret("[CP3-1]", 31);
+    move("{upArrow}", 1);
+    assertCaret("Tittel over punktliste", 22);
+  });
+  it("Can move down an itemlist", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+    // CP3
+    cy.contains("CP3-1").click();
+    move("{home}", 1);
+    assertCaret("[CP3-1]", 97);
+    move("{downArrow}", 1);
+    assertCaret("[CP3-2]", 0);
+    move("{downArrow}", 1);
+    assertCaret("[CP3-3]", 0);
+    move("{downArrow}", 1);
+    assertCaret("Tittel under punktliste", 5);
+  });
+  it("ArrowUp at first node moves caret to the beginning", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP1-1").click();
+    move("{upArrow}", 1);
+    assertCaret("[CP1-1]", 0);
+  });
+  it("ArrowDown at last node moves caret to the end", () => {
+    cy.mount(<LetterEditor initialState={exampleLetter1 as RenderedLetter} />);
+
+    cy.contains("CP4-1").click();
+    move("{leftArrow}", 10);
+    move("{downArrow}", 1);
+    assertCaret("CP4-1", 68);
   });
 });
 
 function move(key: string, times: number) {
-  cy.focused().type(key.repeat(times));
+  cy.focused().type(Cypress._.repeat(key, times));
 }
 
 function assertCaret(content: string, caretOffset: number) {
   cy.get(".editor").then(() => {
-    cy.contains(content).focused();
+    cy.focused().contains(content);
     expect(getRange()?.startOffset).to.eq(caretOffset);
   });
 }
