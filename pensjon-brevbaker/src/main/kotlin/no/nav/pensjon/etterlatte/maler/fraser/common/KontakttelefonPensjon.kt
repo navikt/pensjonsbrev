bc503c85906790c74955c7d0854d928ee36d0618
Emil Schrøder <emil.schroder@nav.no>
2024-04-05 12:25:39 +0200

    Fiks skrivefeil i kontakttelefon (#701)

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/fraser/common/KontakttelefonPensjon.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/fraser/common/KontakttelefonPensjon.kt
@@ -5,14 +5,14 @@ import no.nav.pensjon.brev.template.Language
 import no.nav.pensjon.brev.template.LanguageSupport
 import no.nav.pensjon.brev.template.dsl.ParagraphOnlyScope
 import no.nav.pensjon.brev.template.dsl.text
-import no.nav.pensjon.etterlatte.maler.fraser.common.Constants.KONTATTELEFON_PENSJON_MED_LANDKODE
+import no.nav.pensjon.etterlatte.maler.fraser.common.Constants.KONTAKTTELEFON_PENSJON_MED_LANDKODE
 
 fun ParagraphOnlyScope<LanguageSupport.Triple<Language.Bokmal, Language.Nynorsk, Language.English>, out Any>.kontakttelefonPensjon(utland: Expression<Boolean>) =
     showIf(utland) {
         text(
-            Language.Bokmal to KONTATTELEFON_PENSJON_MED_LANDKODE,
-            Language.Nynorsk to KONTATTELEFON_PENSJON_MED_LANDKODE,
-            Language.English to KONTATTELEFON_PENSJON_MED_LANDKODE
+            Language.Bokmal to KONTAKTTELEFON_PENSJON_MED_LANDKODE,
+            Language.Nynorsk to KONTAKTTELEFON_PENSJON_MED_LANDKODE,
+            Language.English to KONTAKTTELEFON_PENSJON_MED_LANDKODE
         )
     } orShow {
         text(
