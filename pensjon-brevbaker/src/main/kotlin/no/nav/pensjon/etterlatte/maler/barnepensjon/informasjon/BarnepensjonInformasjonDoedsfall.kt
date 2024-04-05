54171cafa48ebefde857429bc595d257e365fc98
Emil Schrøder <emil.schroder@nav.no>
2024-04-05 08:05:35 +0200

    Fiks skrivefeil i kontakttelefon

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/barnepensjon/informasjon/BarnepensjonInformasjonDoedsfall.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/barnepensjon/informasjon/BarnepensjonInformasjonDoedsfall.kt
@@ -20,7 +20,7 @@ import no.nav.pensjon.etterlatte.maler.barnepensjon.informasjon.BarnepensjonInfo
 import no.nav.pensjon.etterlatte.maler.fraser.common.Constants
 import no.nav.pensjon.etterlatte.maler.fraser.common.Constants.BARNEPENSJON_URL
 import no.nav.pensjon.etterlatte.maler.fraser.common.Constants.KONTAKTTELEFON_PENSJON
-import no.nav.pensjon.etterlatte.maler.fraser.common.Constants.KONTATTELEFON_PENSJON_MED_LANDKODE
+import no.nav.pensjon.etterlatte.maler.fraser.common.Constants.KONTAKTTELEFON_PENSJON_MED_LANDKODE
 
 data class BarnepensjonInformasjonDoedsfallDTO(
     override val innhold: List<Element>,
@@ -211,9 +211,9 @@ object BarnepensjonInformasjonDoedsfall : EtterlatteTemplate<BarnepensjonInforma
                 showIf(borIutland.and(erOver18aar.not())) {
                     paragraph {
                         text(
-                            Bokmal to "Du finner mer informasjon på $BARNEPENSJON_URL. Hvis du ikke finner svar på spørsmålet ditt, kan du ringe oss på telefon $KONTATTELEFON_PENSJON_MED_LANDKODE hverdager kl. 09.00-15.00. Om du oppgir fødselsnummer til barnet, kan vi lettere gi deg rask og god hjelp.",
-                            Nynorsk to "Du finn meir informasjon på $BARNEPENSJON_URL. Dersom du ikkje finn svar på spørsmålet ditt der, kan du ringje oss på telefon $KONTATTELEFON_PENSJON_MED_LANDKODE, kvardagar kl. 09.00–15.00. Det vil gjere det enklare for oss å gi deg rask og god hjelp om du oppgir fødselsnummeret til barnet.",
-                            English to "For more information, visit us online: ${Constants.Engelsk.BARNEPENSJON_URL}. If you cannot find the answer to your question, you can call us by phone ($KONTATTELEFON_PENSJON_MED_LANDKODE) weekdays 9-15. If you provide your child's national identity number, we can more easily provide you with quick and good help.",
+                            Bokmal to "Du finner mer informasjon på $BARNEPENSJON_URL. Hvis du ikke finner svar på spørsmålet ditt, kan du ringe oss på telefon $KONTAKTTELEFON_PENSJON_MED_LANDKODE hverdager kl. 09.00-15.00. Om du oppgir fødselsnummer til barnet, kan vi lettere gi deg rask og god hjelp.",
+                            Nynorsk to "Du finn meir informasjon på $BARNEPENSJON_URL. Dersom du ikkje finn svar på spørsmålet ditt der, kan du ringje oss på telefon $KONTAKTTELEFON_PENSJON_MED_LANDKODE, kvardagar kl. 09.00–15.00. Det vil gjere det enklare for oss å gi deg rask og god hjelp om du oppgir fødselsnummeret til barnet.",
+                            English to "For more information, visit us online: ${Constants.Engelsk.BARNEPENSJON_URL}. If you cannot find the answer to your question, you can call us by phone ($KONTAKTTELEFON_PENSJON_MED_LANDKODE) weekdays 9-15. If you provide your child's national identity number, we can more easily provide you with quick and good help.",
                         )
                     }
                     showIf(borIutland.not().and(erOver18aar)) {
@@ -228,9 +228,9 @@ object BarnepensjonInformasjonDoedsfall : EtterlatteTemplate<BarnepensjonInforma
                     showIf(borIutland.and(erOver18aar)) {
                         paragraph {
                             text(
-                                Bokmal to "Du finner mer informasjon på $BARNEPENSJON_URL. Hvis du ikke finner svar på spørsmålet ditt, kan du ringe oss på telefon $KONTATTELEFON_PENSJON_MED_LANDKODE hverdager kl. 09.00-15.00. Om du oppgir fødselsnummeret ditt, kan vi lettere gi deg rask og god hjelp.",
-                                Nynorsk to "Du finn meir informasjon på $BARNEPENSJON_URL. Dersom du ikkje finn svar på spørsmålet ditt der, kan du ringje oss på telefon $KONTATTELEFON_PENSJON_MED_LANDKODE, kvardagar kl. 09.00–15.00. Det vil gjere det enklare for oss å gi deg rask og god hjelp om du oppgir fødselsnummeret ditt.",
-                                English to "For more information, visit us online: ${Constants.Engelsk.BARNEPENSJON_URL}. If you cannot find the answer to your question, you can call us by phone ($KONTATTELEFON_PENSJON_MED_LANDKODE) weekdays 9-15. If you provide your national identity number, we can more easily provide you with quick and good help.",
+                                Bokmal to "Du finner mer informasjon på $BARNEPENSJON_URL. Hvis du ikke finner svar på spørsmålet ditt, kan du ringe oss på telefon $KONTAKTTELEFON_PENSJON_MED_LANDKODE hverdager kl. 09.00-15.00. Om du oppgir fødselsnummeret ditt, kan vi lettere gi deg rask og god hjelp.",
+                                Nynorsk to "Du finn meir informasjon på $BARNEPENSJON_URL. Dersom du ikkje finn svar på spørsmålet ditt der, kan du ringje oss på telefon $KONTAKTTELEFON_PENSJON_MED_LANDKODE, kvardagar kl. 09.00–15.00. Det vil gjere det enklare for oss å gi deg rask og god hjelp om du oppgir fødselsnummeret ditt.",
+                                English to "For more information, visit us online: ${Constants.Engelsk.BARNEPENSJON_URL}. If you cannot find the answer to your question, you can call us by phone ($KONTAKTTELEFON_PENSJON_MED_LANDKODE) weekdays 9-15. If you provide your national identity number, we can more easily provide you with quick and good help.",
                             )
                         }
                     }
