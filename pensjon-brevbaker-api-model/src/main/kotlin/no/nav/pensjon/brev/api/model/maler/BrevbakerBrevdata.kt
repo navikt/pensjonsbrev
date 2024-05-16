d4f124c63128ad4781e318b0cde6ca9b952bdfef
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2024-05-16 15:48:29 +0200

    Smått

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/BrevbakerBrevdata.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/BrevbakerBrevdata.kt
@@ -3,9 +3,8 @@ package no.nav.pensjon.brev.api.model.maler
 /**
  * Top-level interface for all Brevbaker Dto classes.
  */
-interface BrevbakerBrevdata {
-}
-//
+interface BrevbakerBrevdata
+
 interface RedigerbarBrevdata<SaksbehandlerValg : BrevbakerBrevdata, PesysData : BrevbakerBrevdata> : BrevbakerBrevdata {
     val saksbehandlerValg: SaksbehandlerValg
     val pesysData: PesysData
