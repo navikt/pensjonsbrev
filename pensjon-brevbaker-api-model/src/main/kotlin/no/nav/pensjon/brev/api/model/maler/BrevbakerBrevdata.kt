75bf07ff5be022855b95764e1a94ffa967d0f02c
Alexander Hoem Rosbach <82815189+routsi@users.noreply.github.com>
2024-05-21 13:03:58 +0200

    Legg til grensesnitt for skille mellom saksbehandler-valg og data fra pesys for redigerbare maler (#746)
    * Fiks svar fra rendre redigerbart brev + litt smått
    
    * Interface for LetterData til redigerbare maler som skiller på saksbehandlerValg og pesysData
    
    * Fiks TemplateModelSpecification for EmptyBrevdata
    
    * Smått

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/BrevbakerBrevdata.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/BrevbakerBrevdata.kt
@@ -3,5 +3,9 @@ package no.nav.pensjon.brev.api.model.maler
 /**
  * Top-level interface for all Brevbaker Dto classes.
  */
-interface BrevbakerBrevdata {
+interface BrevbakerBrevdata
+
+interface RedigerbarBrevdata<SaksbehandlerValg : BrevbakerBrevdata, PesysData : BrevbakerBrevdata> : BrevbakerBrevdata {
+    val saksbehandlerValg: SaksbehandlerValg
+    val pesysData: PesysData
 }
--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/AutobrevTemplate.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.pensjon.brev.template
-
-import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
-import no.nav.pensjon.brev.api.model.maler.Brevkode
-
-interface AutobrevTemplate<LetterData : BrevbakerBrevdata>: HasModel<LetterData> {
-    val template: LetterTemplate<*, LetterData>
-    val kode: Brevkode.AutoBrev
-}
