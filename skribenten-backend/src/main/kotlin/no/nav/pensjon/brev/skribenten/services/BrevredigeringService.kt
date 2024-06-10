9af50724100658719e81336b23f37cf44d6102c8
Mo Amini <mo.amini@nav.no>
2024-06-10 08:34:05 +0200

    Brevredigering CRUD uten ferdigstill

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/BrevredigeringService.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/BrevredigeringService.kt
@@ -1,79 +1,137 @@
 package no.nav.pensjon.brev.skribenten.services
 
+import io.ktor.http.*
+import io.ktor.server.application.*
+import kotlinx.coroutines.coroutineScope
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
-import no.nav.pensjon.brev.skribenten.services.Brevredigering.saksbehandlerValg
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import no.nav.pensjon.brev.api.model.maler.Brevkode
+import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.skribenten.db.Brevredigering
+import no.nav.pensjon.brev.skribenten.db.BrevredigeringTable
+import no.nav.pensjon.brev.skribenten.letter.Edit
+import no.nav.pensjon.brev.skribenten.letter.toEdit
+import no.nav.pensjon.brev.skribenten.letter.updateEditedLetter
+import no.nav.pensjon.brev.skribenten.principal
+import no.nav.pensjon.brev.skribenten.routes.GeneriskRedigerbarBrevdata
+import no.nav.pensjon.brev.skribenten.routes.OppprettBrevResponse
 import org.jetbrains.exposed.sql.transactions.transaction
+import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
-import java.util.UUID
 
-class BrevredigeringService {
-    fun hentBrevredigering(opprettetAvNavident: String, saksid: String, brevkode: String): List<BrevredigeringDto> =
-        transaction {
-            Brevredigering.select {
-                Brevredigering.opprettetAvNavident eq opprettetAvNavident
-                Brevredigering.saksid eq saksid
-                Brevredigering.brevkode eq brevkode
-            }.map { 
-                BrevredigeringDto(
-                it[Brevredigering.saksid], 
-                it[Brevredigering.opprettetAvNavident],
-                it[Brevredigering.brevkode],
-                it[saksbehandlerValg])
-            }
-        }
-    fun hentBrevredigering(navident: String): List<String> =
-        transaction {
-            Brevredigering.select { Brevredigering.opprettetAvNavident eq navident }.map { row -> row[Brevredigering.brevkode] }
-        }
+class BrevredigeringService(private val brevbakerService: BrevbakerService) {
 
-    fun lagreBrevredigering(
-        saksid: String,
-        navident: String,
-        brevkode: String,
-        redigeresAvNavident: String?,
-        saksbehandlerValg: BrevbakerBrevdata
-    ) {
-        transaction {
-            Brevredigering.insert {
-                it[eksternId] = UUID.randomUUID().toString()
-                it[this.saksid] = saksid
-                it[this.opprettetAvNavident] = navident
-                it[this.brevkode] = brevkode
-                it[this.saksbehandlerValg] = saksbehandlerValg
-                it[laastForRedigering] = false
-                it[this.redigeresAvNavident] = redigeresAvNavident
-                it[this.opprettet] = LocalDateTime.now().toString()
-                it[this.sistredigert] = LocalDateTime.now().toString()
+    private val logger = LoggerFactory.getLogger(BrevredigeringService::class.java)
 
+    suspend fun <T : Any> opprettBrev(
+        call: ApplicationCall,
+        sak: PenService.SakSelection,
+        brevkode: Brevkode.Redigerbar,
+        saksbehandlerValg: BrevbakerBrevdata,
+        mapper: Brevredigering.() -> T,
+    ): ServiceResult<T> {
+        //TODO hent pesys data
+        val pesysData = hentPesysData(brevkode = brevkode, saksId = sak.saksId)
+
+        return brevbakerService.renderLetter(
+            call = call,
+            brevkode = brevkode,
+            brevdata = GeneriskRedigerbarBrevdata(
+                pesysData = pesysData,
+                saksbehandlerValg = saksbehandlerValg
+            )
+        ).map { letter ->
+            transaction {
+                Brevredigering.new {
+                    saksId = sak.saksId
+                    opprettetAvNavIdent = call.principal().navIdent
+                    this.brevkode = brevkode.name
+                    this.saksbehandlerValg = saksbehandlerValg
+                    laastForRedigering = false
+                    redigeresAvNavIdent = null
+                    opprettet = LocalDateTime.now()
+                    sistredigert = LocalDateTime.now()
+                    redigertBrev = letter.toEdit()
+                    sistRedigertAvNavIdent = call.principal().navIdent
+                }.mapper()
             }
         }
     }
 
-    fun slettBrevredigering(
-        uuid: String,
-        saksid: String,
-        navident: String,
-        brevkode: String,
-        saksbehandlerValg: BrevbakerBrevdata
-    ) {
-        transaction {
-            Brevredigering.deleteWhere {
-                this.eksternId eq uuid
-                this.saksid eq saksid
-                this.opprettetAvNavident eq navident
-                this.brevkode eq brevkode
-                this.saksbehandlerValg eq saksbehandlerValg
+
+    suspend fun <T : Any> oppdaterBrev(
+        call: ApplicationCall,
+        brevId: Long,
+        brevkode: Brevkode.Redigerbar,
+        saksbehandlerValg: BrevbakerBrevdata,
+        redigertBrev: Edit.Letter,
+        mapper: Brevredigering.() -> T,
+    ): ServiceResult<T?> = coroutineScope {
+        val eksisterende = transaction { Brevredigering.findById(brevId) }
+
+        return@coroutineScope if (eksisterende != null) {
+            val pesysData = hentPesysData(brevkode = brevkode, saksId = eksisterende.saksId)
+
+            brevbakerService.renderLetter(
+                call = call,
+                brevkode = brevkode,
+                brevdata = GeneriskRedigerbarBrevdata(
+                    pesysData = pesysData,
+                    saksbehandlerValg = saksbehandlerValg,
+                )
+            ).map { redigertBrev.updateEditedLetter(it) }
+                .map { brev ->
+                    transaction {
+                        Brevredigering.findByIdAndUpdate(brevId) {
+                            it.redigertBrev = brev
+                            it.sistredigert = LocalDateTime.now()
+                            it.saksbehandlerValg = saksbehandlerValg
+                            it.sistRedigertAvNavIdent = call.principal().navIdent
+                        }?.mapper()
+                    }
+                }
+        } else {
+            ServiceResult.Error("Fant ikke brev med id: $brevId", HttpStatusCode.NotFound)
+        }
+    }
+
+    fun slettBrev(id: Long): ServiceResult<*> {
+        return transaction {
+            val brev = Brevredigering.findById(id)
+            if (brev != null) {
+                brev.delete()
+                return@transaction ServiceResult.Ok("Brev med id: $id slettet")
+            } else {
+                return@transaction ServiceResult.Error("Kunne ikke finnne brev med id: $id", HttpStatusCode.NotFound)
             }
         }
     }
+
+    //TODO implementer felter
+    private fun hentPesysData(brevkode: Brevkode.Redigerbar, saksId: Long): BrevbakerBrevdata = EmptyBrevdata
+
+    fun hentBrev(brevId: Long, mapper: Brevredigering.() -> OppprettBrevResponse): OppprettBrevResponse? {
+        return try {
+            transaction {
+                Brevredigering.findById(brevId)?.mapper() ?: throw IllegalStateException("Feil ved henting av brev med brevId: $brevId")
+            }
+        } catch (e: Exception) {
+            logger.error("Feil ved henting av brev med brevId: $brevId", e)
+            throw e
+        }
+    }
+
+    fun hentSaksbehandlersBrev(
+        navIdent: String,
+        mapper: Brevredigering.() -> OppprettBrevResponse
+    ): List<OppprettBrevResponse?>? {
+        return try {
+            transaction {
+                Brevredigering.find { BrevredigeringTable.opprettetAvNavIdent eq navIdent }.map(mapper)
+            }
+        } catch (e: Exception) {
+            logger.error("Feil ved henting av brev med brevId: $navIdent", e)
+            throw e
+        }
+    }
+
 }
-
-data class BrevredigeringDto(
-    val saksid: String,
-    val navident: String,
-    val brevkode: String,
-    val saksbehandlerValg: BrevbakerBrevdata
-)
-
