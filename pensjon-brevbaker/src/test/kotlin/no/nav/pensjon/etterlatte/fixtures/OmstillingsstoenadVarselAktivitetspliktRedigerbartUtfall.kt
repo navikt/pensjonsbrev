d40b65c1fbd3f72489e91e5028c18df6f9a950f0
Espen Schei <espen.schei@outlook.com>
2024-06-18 12:40:56 +0200

    Nytt varselbrev for omstillingsstønad (#768)
    * Nytt varselbrev for omstillingsstønad
    
    EY-4063
    
    * Liten bokstav i lista på nynorsk
    
    EY-4063

---
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadVarselAktivitetspliktRedigerbartUtfall.kt
@@ -0,0 +1,8 @@
+package no.nav.pensjon.etterlatte.fixtures
+
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.varsel.OmstillingsstoenadVarselAktivitetspliktRedigerbartUtfallDTO
+
+
+fun createOmstillingsstoenadVarselAktivitetspliktRedigerbartUtfallDTO() = OmstillingsstoenadVarselAktivitetspliktRedigerbartUtfallDTO(
+    innhold = emptyList()
+)
