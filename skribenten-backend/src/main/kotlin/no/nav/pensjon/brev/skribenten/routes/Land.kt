ba1cf37836151b50655fbce3cb28651f55323b8c
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-07-05 16:23:12 +0200

    endepunkt for å hente landkoder med tilhørende navn

---
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/Land.kt
@@ -0,0 +1,24 @@
+package no.nav.pensjon.brev.skribenten.routes
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.*
+
+internal val norskBokmål: Locale = Locale("NB", "NO")
+internal val landKoder: Set<String> = Locale.getISOCountries(Locale.IsoCountryCode.PART1_ALPHA2)
+internal val land = landKoder.map {
+    LandJson(it, Locale("", it).getDisplayCountry(norskBokmål))
+}
+
+internal data class LandJson(
+    val kode: String,
+    val navn: String
+)
+
+//TODO - test
+fun Route.landRoute() {
+    get("/land") {
+        call.respond(land)
+    }
+}
