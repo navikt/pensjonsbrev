390bc31ffc7c6697c43e66bc43aaca1e910e1fff
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-07-15 16:07:14 +0200

    oppdater pdfviewer style

---
+++ skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevbehandler/$brevId/-components/PDFViewer.tsx
@@ -0,0 +1,318 @@
+import "react-pdf/dist/Page/TextLayer.css";
+import "react-pdf/dist/Page/AnnotationLayer.css";
+
+import type { SerializedStyles } from "@emotion/react";
+import { css } from "@emotion/react";
+import { ChevronDownIcon, ChevronUpIcon, TrashIcon, ZoomMinusIcon, ZoomPlusIcon } from "@navikt/aksel-icons";
+import { BodyShort, Button, HStack, Modal, TextField } from "@navikt/ds-react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "@tanstack/react-router";
+import { useEffect, useState } from "react";
+import { pdfjs } from "react-pdf";
+import { Document, Page } from "react-pdf";
+
+import { hentAlleBrevForSak, slettBrev } from "~/api/sak-api-endpoints";
+import type { BrevInfo } from "~/types/brev";
+
+import { Route } from "../route";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
+
+/**
+ * @param properties.maxHeightLimit - optional, hvis satt, vil den begrense høyden vil selve PDF-visningen.
+ *  Dette er nyttig fordi at i fleste tilfeller vil vi at PDF'en skal være i en scrollable div, og ikke gjør hele siden scrollable.
+ *  For å gjøre div'en scrollable bruker man overflow: auto, men da må vi sette en høyde på div'en.
+ *  Optional i de tilfelle vi vil vise alt uansett. Vi får se.
+ */
+const PDFViewer = (properties: { sakId: string; brevId: string; pdf: Blob; maxHeightLimit?: number }) => {
+  const [scale, setScale] = useState<number>(1);
+  const [totalNumberOfPages, setTotalNumberOfPages] = useState<number>(1);
+
+  return (
+    <div>
+      <PDFViewerTopBar
+        brevId={properties.brevId}
+        sakId={properties.sakId}
+        scale={scale}
+        setScale={setScale}
+        totalNumberOfPages={totalNumberOfPages}
+      />
+      <div
+        css={css`
+          // 48px er høyden på topbaren
+          height: ${properties.maxHeightLimit ? `${properties.maxHeightLimit - 48}px` : "100%"};
+          overflow: auto;
+        `}
+      >
+        <Document
+          css={css`
+            background-color: var(--a-gray-300);
+            padding: 0 3rem 1rem 3rem;
+          `}
+          file={properties.pdf}
+          onLoadSuccess={(pdf) => setTotalNumberOfPages(pdf.numPages)}
+        >
+          {Array.from({ length: totalNumberOfPages }, (_, index) => (
+            <div id={`page_${index + 1}`} key={`page_${index + 1}`}>
+              <Page
+                css={css`
+                  margin-bottom: 1rem;
+                `}
+                pageNumber={index + 1}
+                scale={scale}
+              />
+            </div>
+          ))}
+        </Document>
+      </div>
+    </div>
+  );
+};
+
+export default PDFViewer;
+
+const SlettBrev = (properties: { sakId: string; brevId: string }) => {
+  const [vilSletteBrev, setVilSletteBrev] = useState(false);
+
+  return (
+    <div>
+      {vilSletteBrev && (
+        <SlettBrevModal
+          brevId={properties.brevId}
+          onClose={() => setVilSletteBrev(false)}
+          sakId={properties.sakId}
+          åpen={vilSletteBrev}
+        />
+      )}
+      <Button onClick={() => setVilSletteBrev(true)} size="small" type="button" variant="danger">
+        <HStack gap="1">
+          <TrashIcon fontSize="1.5rem" title="slett-ikon" /> <BodyShort>Slett</BodyShort>
+        </HStack>
+      </Button>
+    </div>
+  );
+};
+
+const SlettBrevModal = (properties: { sakId: string; brevId: string; åpen: boolean; onClose: () => void }) => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate({ from: Route.fullPath });
+
+  const slett = useMutation({
+    mutationFn: () => slettBrev(properties.sakId, properties.brevId),
+    onSuccess: () => {
+      queryClient.setQueryData(hentAlleBrevForSak.queryKey, (currentBrevInfo: BrevInfo[]) =>
+        currentBrevInfo.filter((brev) => brev.id.toString() !== properties.brevId),
+      );
+      navigate({ to: "/saksnummer/$saksId/brevbehandler", params: { saksId: properties.sakId } });
+    },
+  });
+
+  return (
+    <Modal
+      header={{ heading: "Vil du slette brevet?" }}
+      onClose={properties.onClose}
+      open={properties.åpen}
+      portal
+      width={450}
+    >
+      <Modal.Body>
+        <BodyShort>Brevet vil bli slettet, og du kan ikke angre denne handlingen.</BodyShort>
+      </Modal.Body>
+      <Modal.Footer>
+        <HStack gap="4">
+          <Button onClick={properties.onClose} type="button" variant="tertiary">
+            Nei, behold brevet
+          </Button>
+          <Button loading={slett.isPending} onClick={() => slett.mutate()} type="button" variant="danger">
+            Ja, slett brevet
+          </Button>
+        </HStack>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+const scrollToPage = (pageNumber: number) => {
+  const pageElement = document.querySelector(`#page_${pageNumber}`);
+  if (pageElement) {
+    pageElement.scrollIntoView();
+  }
+};
+
+const PDFViewerTopBar = (properties: {
+  sakId: string;
+  brevId: string;
+  totalNumberOfPages: number;
+  scale: number;
+  setScale: (n: number) => void;
+}) => {
+  return (
+    <HStack
+      align="center"
+      css={css`
+        border-top: 1px solid var(--a-gray-200);
+        border-bottom: 1px solid var(--a-gray-200);
+        padding: 0 1rem;
+        height: 48px;
+      `}
+      justify="space-between"
+    >
+      <HStack align="center" gap="2">
+        <TopBarNavigation totalNumberOfPages={properties.totalNumberOfPages} />
+        <TopBarZoom scale={properties.scale} setScale={properties.setScale} />
+      </HStack>
+      <SlettBrev brevId={properties.brevId} sakId={properties.sakId} />
+    </HStack>
+  );
+};
+
+const TopBarNavigation = (properties: { totalNumberOfPages: number }) => {
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+  const [textFieldValue, setTextFieldValue] = useState("1");
+
+  useEffect(() => {
+    setTextFieldValue(currentPageNumber.toString());
+  }, [currentPageNumber]);
+
+  const handlePageInputChange = () => {
+    const parsedValue = Number.parseInt(textFieldValue, 10);
+    if (parsedValue >= 1 && parsedValue <= properties.totalNumberOfPages) {
+      setCurrentPageNumber(parsedValue);
+      scrollToPage(parsedValue);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (currentPageNumber < properties.totalNumberOfPages) {
+      setCurrentPageNumber(currentPageNumber + 1);
+      scrollToPage(currentPageNumber + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPageNumber > 1) {
+      setCurrentPageNumber(currentPageNumber - 1);
+      scrollToPage(currentPageNumber - 1);
+    }
+  };
+
+  return (
+    <HStack
+      align="end"
+      css={css`
+        border-right: 1px solid var(--a-gray-200);
+        padding-right: 1rem;
+      `}
+      gap="2"
+    >
+      <BasicPDFViewerButton
+        cssOveride={css`
+          padding: 0;
+          height: 24px;
+          widht: 24px;
+        `}
+        disabled={currentPageNumber === 1}
+        onClick={goToPreviousPage}
+      >
+        <ChevronUpIcon fontSize="24px" title="forrige side" />
+      </BasicPDFViewerButton>
+      <BasicPDFViewerButton
+        cssOveride={css`
+          padding: 0;
+          height: 24px;
+          widht: 24px;
+        `}
+        disabled={currentPageNumber === properties.totalNumberOfPages}
+        onClick={goToNextPage}
+      >
+        <ChevronDownIcon fontSize="24px" title="neste side" />
+      </BasicPDFViewerButton>
+      <HStack gap="1">
+        <TextField
+          css={css`
+            input {
+              width: 36px;
+              height: 32px;
+              min-height: 32px;
+            }
+          `}
+          hideLabel
+          label="Side"
+          //legg på onchange som sjekker at det er et tall
+          onChange={(event) => {
+            const value = event.target.value;
+            const parsedValue = Number.parseInt(value, 10);
+            if (!Number.isNaN(parsedValue) || value === "") {
+              setTextFieldValue(value);
+            }
+          }}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handlePageInputChange();
+            }
+          }}
+          value={textFieldValue}
+        />
+        <BodyShort
+          css={css`
+            align-self: center;
+            font-size: 20px;
+          `}
+        >
+          / {properties.totalNumberOfPages}
+        </BodyShort>
+      </HStack>
+    </HStack>
+  );
+};
+
+const TopBarZoom = (properties: { scale: number; setScale: (n: number) => void }) => {
+  return (
+    <HStack gap="2">
+      <BasicPDFViewerButton
+        onClick={() => {
+          if (properties.scale <= 2) {
+            properties.setScale(properties.scale + 0.1);
+          }
+        }}
+      >
+        <ZoomPlusIcon fontSize="24px" title="zoom-in" />
+      </BasicPDFViewerButton>
+
+      <BasicPDFViewerButton
+        onClick={() => {
+          if (properties.scale >= 1) {
+            properties.setScale(properties.scale - 0.1);
+          }
+        }}
+      >
+        <ZoomMinusIcon fontSize="24px" title="zoom-ut" />
+      </BasicPDFViewerButton>
+    </HStack>
+  );
+};
+
+const BasicPDFViewerButton = (properties: {
+  cssOveride?: SerializedStyles;
+  disabled?: boolean;
+  onClick: () => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <Button
+      css={
+        properties.cssOveride ??
+        css`
+          padding: 0;
+          height: fit-content;
+        `
+      }
+      disabled={properties.disabled}
+      onClick={properties.onClick}
+      type="button"
+      variant="tertiary-neutral"
+    >
+      {properties.children}
+    </Button>
+  );
+};
--- skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevbehandler/$brevId/route.tsx
+++ skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevbehandler/$brevId/route.tsx
@@ -1,21 +1,12 @@
-import "react-pdf/dist/Page/TextLayer.css";
-import "react-pdf/dist/Page/AnnotationLayer.css";
+import { useMutation } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+import { useEffect, useRef, useState } from "react";
 
-import { css } from "@emotion/react";
-import { BodyShort, Button, HStack, Modal, TextField, VStack } from "@navikt/ds-react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { useRef, useState } from "react";
-import { pdfjs } from "react-pdf";
-import { Document, Page } from "react-pdf";
-
-import { hentAlleBrevForSak, lagPdfForBrev, slettBrev } from "~/api/sak-api-endpoints";
-import type { BrevInfo } from "~/types/brev";
+import { hentPdfForBrevFunction, lagPdfForBrev } from "~/api/sak-api-endpoints";
 import type { Nullable } from "~/types/Nullable";
 
+import PDFViewer from "./-components/PDFViewer";
 import pdf from "./-multipage.pdf";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString();
 
 export const Route = createFileRoute("/saksnummer/$saksId/brevbehandler/$brevId")({
   component: BrevForhåndsvisning,
@@ -23,234 +14,44 @@ export const Route = createFileRoute("/saksnummer/$saksId/brevbehandler/$brevId"
 
 function BrevForhåndsvisning() {
   const { saksId, brevId } = Route.useParams();
-  const [scale, setScale] = useState<number>(1);
-  const [numberPages, setNumberPages] = useState<Nullable<number>>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const inputReference = useRef(null);
-
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
-    setNumberPages(numPages);
-  }
+  const pageContainerReference = useRef<HTMLDivElement>(null);
+  const [containerHeight, setContainerHeight] = useState<Nullable<number>>(null);
 
   //TODO - vil vi alltid kanskje lage PDF'en ved switchen i parent route, og bare hente den her?
   const lagPdf = useMutation({
     mutationFn: () => lagPdfForBrev(saksId, brevId),
     onSuccess: (pdfData) => {
-      console.log(pdfData);
       window.open(URL.createObjectURL(pdfData));
     },
   });
 
-  const scrollToPage = (pageNumber) => {
-    const pageElement = document.querySelector(`#page_${pageNumber}`);
-    if (pageElement) {
-      pageElement.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+  const hentPdf = useMutation({
+    mutationFn: () => hentPdfForBrevFunction(saksId, brevId),
+  });
 
-  const handlePageInputChange = (event) => {
-    const newPageNumber = Number(event.target.value);
-    if (!Number.isNaN(newPageNumber) && newPageNumber >= 1 && newPageNumber <= numberPages) {
-      setCurrentPage(newPageNumber);
-      scrollToPage(newPageNumber);
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      handlePageInputChange(event);
-    }
-  };
-
-  const goToNextPage = () => {
-    if (currentPage < numberPages) {
-      setCurrentPage(currentPage + 1);
-      scrollToPage(currentPage + 1);
-    }
-  };
-
-  const goToPreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-      scrollToPage(currentPage - 1);
-    }
-  };
+  useEffect(() => {
+    setContainerHeight(pageContainerReference?.current?.getBoundingClientRect().height ?? null);
+  }, [pageContainerReference]);
 
   return (
-    <div
-      css={css`
-        max-height: 85vh;
-      `}
-    >
-      <VStack>
+    <div ref={pageContainerReference}>
+      {/*       <VStack>
         <HStack>
           <Button loading={lagPdf.isPending} onClick={() => lagPdf.mutate()} type="button">
             Lag pdf
           </Button>
+          <Button loading={hentPdf.isPending} onClick={() => hentPdf.mutate()} type="button">
+            Hent pdf
+          </Button>
         </HStack>
       </VStack>
-      -------------------
-      <div>
-        <HStack>
-          <Button
-            onClick={() => {
-              if (scale <= 2) {
-                setScale(scale + 0.1);
-              }
-            }}
-            size="small"
-            type="button"
-          >
-            +
-          </Button>
-          <Button
-            onClick={() => {
-              if (scale >= 1) {
-                setScale(scale - 0.1);
-              }
-            }}
-            size="small"
-            type="button"
-          >
-            -
-          </Button>
-          <SlettBrev brevId={brevId} sakId={saksId} />
-          <HStack>
-            <TextField
-              css={css`
-                width: 50px;
-              `}
-              hideLabel
-              label="Side"
-              onKeyDown={handleKeyDown}
-              ref={inputReference}
-            />
-            / {numberPages}
-          </HStack>
-          <button disabled={currentPage === 1} onClick={goToPreviousPage}>
-            Previous Page
-          </button>
-          <button disabled={currentPage === numberPages} onClick={goToNextPage}>
-            Next Page
-          </button>
-        </HStack>
-        {/* 
-        <div
-          css={css`
-            overflow: auto;
-            max-height: 75vh;
-          `}
-        >
-          <Document
-            css={css`
-              background-color: var(--a-gray-300);
-              padding: 1rem 3rem;
-            `}
-            file={pdf}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            {Array.from(new Array(totalPages), (element, index) => (
-              <Page
-                css={css`
-                  margin: 2rem 0;
-                `}
-                key={`page_${index + 1}`}
-                pageNumber={index + 1}
-                scale={scale}
-              />
-            ))}
-          </Document>
-        </div>
-*/}
-        <div
-          css={css`
-            overflow: auto;
-            max-height: 75vh;
-          `}
-        >
-          <Document
-            css={css`
-              background-color: var(--a-gray-300);
-              padding: 1rem 3rem;
-            `}
-            file={pdf}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            {Array.from(new Array(numberPages), (element, index) => (
-              <div id={`page_${index + 1}`} key={`page_${index + 1}`}>
-                <Page
-                  css={css`
-                    margin: 2rem 0;
-                  `}
-                  pageNumber={index + 1}
-                  scale={scale}
-                />
-              </div>
-            ))}
-          </Document>
-        </div>
-      </div>
-      -------------------
+      -------------------*/}
+      {hentPdf.isSuccess && (
+        <PDFViewer brevId={brevId} maxHeightLimit={containerHeight ?? undefined} pdf={hentPdf.data} sakId={saksId} />
+      )}
+      {hentPdf.isIdle && (
+        <PDFViewer brevId={brevId} maxHeightLimit={containerHeight ?? undefined} pdf={pdf} sakId={saksId} />
+      )}
     </div>
   );
 }
-
-const SlettBrev = (properties: { sakId: string; brevId: string }) => {
-  const [vilSletteBrev, setVilSletteBrev] = useState(false);
-
-  return (
-    <div>
-      {vilSletteBrev && (
-        <SlettBrevModal
-          brevId={properties.brevId}
-          onClose={() => setVilSletteBrev(false)}
-          sakId={properties.sakId}
-          åpen={vilSletteBrev}
-        />
-      )}
-      <Button onClick={() => setVilSletteBrev(true)} size="small" type="button" variant="danger">
-        Slett brev
-      </Button>
-    </div>
-  );
-};
-
-const SlettBrevModal = (properties: { sakId: string; brevId: string; åpen: boolean; onClose: () => void }) => {
-  const queryClient = useQueryClient();
-  const navigate = useNavigate({ from: Route.fullPath });
-
-  const slett = useMutation({
-    mutationFn: () => slettBrev(properties.sakId, properties.brevId),
-    onSuccess: () => {
-      queryClient.setQueryData(hentAlleBrevForSak.queryKey, (currentBrevInfo: BrevInfo[]) =>
-        currentBrevInfo.filter((brev) => brev.id.toString() !== properties.brevId),
-      );
-      navigate({ to: "/saksnummer/$saksId/brevbehandler", params: { saksId: properties.sakId } });
-    },
-  });
-
-  return (
-    <Modal
-      header={{ heading: "Vil du slette brevet?" }}
-      onClose={properties.onClose}
-      open={properties.åpen}
-      portal
-      width={450}
-    >
-      <Modal.Body>
-        <BodyShort>Brevet vil bli slettet, og du kan ikke angre denne handlingen.</BodyShort>
-      </Modal.Body>
-      <Modal.Footer>
-        <HStack gap="4">
-          <Button onClick={properties.onClose} type="button" variant="tertiary">
-            Nei, behold brevet
-          </Button>
-          <Button loading={slett.isPending} onClick={() => slett.mutate()} type="button" variant="danger">
-            Ja, slett brevet
-          </Button>
-        </HStack>
-      </Modal.Footer>
-    </Modal>
-  );
-};
