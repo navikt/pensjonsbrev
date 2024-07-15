42b86cf1ae148de6bd1f4592d3a5cad7aab83dd0
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-07-15 17:12:50 +0200

    /brevbehandler må sette høyden til PDF'viewer
    Tekniske detaljer gjør at pdfviewer er en egen route. Det er ikke helt feil å anse at /brevbehandler er den faktiske parenten

--- skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevbehandler/$brevId/route.tsx
+++ skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevbehandler/$brevId/route.tsx
@@ -1,12 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect, useRef, useState } from "react";
 
-import { hentPdfForBrevFunction, lagPdfForBrev } from "~/api/sak-api-endpoints";
-import type { Nullable } from "~/types/Nullable";
+import { hentPdfForBrev, hentPdfForBrevFunction } from "~/api/sak-api-endpoints";
 
 import PDFViewer from "./-components/PDFViewer";
-import pdf from "./-multipage.pdf";
 
 export const Route = createFileRoute("/saksnummer/$saksId/brevbehandler/$brevId")({
   component: BrevForhåndsvisning,
@@ -14,44 +11,20 @@ export const Route = createFileRoute("/saksnummer/$saksId/brevbehandler/$brevId"
 
 function BrevForhåndsvisning() {
   const { saksId, brevId } = Route.useParams();
-  const pageContainerReference = useRef<HTMLDivElement>(null);
-  const [containerHeight, setContainerHeight] = useState<Nullable<number>>(null);
 
+  /*
   //TODO - vil vi alltid kanskje lage PDF'en ved switchen i parent route, og bare hente den her?
   const lagPdf = useMutation({
     mutationFn: () => lagPdfForBrev(saksId, brevId),
     onSuccess: (pdfData) => {
       window.open(URL.createObjectURL(pdfData));
     },
+  });*/
+
+  const hentPdfQuery = useQuery({
+    queryKey: hentPdfForBrev.queryKey,
+    queryFn: () => hentPdfForBrevFunction(saksId, brevId),
   });
 
-  const hentPdf = useMutation({
-    mutationFn: () => hentPdfForBrevFunction(saksId, brevId),
-  });
-
-  useEffect(() => {
-    setContainerHeight(pageContainerReference?.current?.getBoundingClientRect().height ?? null);
-  }, [pageContainerReference]);
-
-  return (
-    <div ref={pageContainerReference}>
-      {/*       <VStack>
-        <HStack>
-          <Button loading={lagPdf.isPending} onClick={() => lagPdf.mutate()} type="button">
-            Lag pdf
-          </Button>
-          <Button loading={hentPdf.isPending} onClick={() => hentPdf.mutate()} type="button">
-            Hent pdf
-          </Button>
-        </HStack>
-      </VStack>
-      -------------------*/}
-      {hentPdf.isSuccess && (
-        <PDFViewer brevId={brevId} maxHeightLimit={containerHeight ?? undefined} pdf={hentPdf.data} sakId={saksId} />
-      )}
-      {hentPdf.isIdle && (
-        <PDFViewer brevId={brevId} maxHeightLimit={containerHeight ?? undefined} pdf={pdf} sakId={saksId} />
-      )}
-    </div>
-  );
+  return <div>{hentPdfQuery.isSuccess && <PDFViewer brevId={brevId} pdf={hentPdfQuery.data} sakId={saksId} />}</div>;
 }
