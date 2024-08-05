3ef0afb65b47e82544b5ebc76dc12f4c57f22632
Emil Schrøder <emil.schroder@nav.no>
2024-08-05 09:24:18 +0200

    Fiks skrivefeil mot -> not

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/vedlegg/omstillingsstoenad/BeregningAvOmstillingsstoenad.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/vedlegg/omstillingsstoenad/BeregningAvOmstillingsstoenad.kt
@@ -281,7 +281,7 @@ private fun OutlineOnlyScope<LangBokmalNynorskEnglish, OmstillingsstoenadBeregni
                     "“Utbetaling per måned” er derfor satt til 0 fra gjeldende tidspunkt, slik det fremgår i tabellen over.",
             Nynorsk to "Då du ikkje har oppfylt kravet om aktivitetsplikt, har omstillingsstønaden din blitt stansa. " +
                     "«Utbetaling per månad» er difor sett til 0 frå gjeldande tidspunkt, slik det går fram av tabellen over.",
-            English to "Your Adjustment Allowance has been stopped because you have mot met the mandatory activity requirements. " +
+            English to "Your Adjustment Allowance has been stopped because you have not met the mandatory activity requirements. " +
                     "«Payment per month is therefore set at «0» from the relevant date, as evident in the table above.",
         )
     }
