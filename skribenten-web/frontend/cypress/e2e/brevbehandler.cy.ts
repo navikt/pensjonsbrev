f28b685c98490eaf856fea31aeec6d675adbeed8
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-08-20 13:48:03 +0200

    refaktorerer brevbehandler reg-tester

--- skribenten-web/frontend/cypress/e2e/brevbehandler.cy.ts
+++ skribenten-web/frontend/cypress/e2e/brevbehandler.cy.ts
@@ -1,30 +1,40 @@
 describe("Brevbehandler", () => {
+  const kladdBrev = {
+    id: 1,
+    opprettetAv: "Ola Nordmann",
+    opprettet: "2021-09-01T12:00:00",
+    sistredigertAv: "Ola Nordmann",
+    sistredigert: "2021-09-01T12:00:00",
+    brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
+    status: { type: "Kladd" },
+    distribusjonstype: "SENTRALPRINT",
+  };
+
+  const klarBrev = { ...kladdBrev, status: { type: "Klar" } };
+
+  const brevSomSendesSomLokalPrint = {
+    ...klarBrev,
+    brevkode: "BREV_SOM_SENDES_SOM_LOKALPRINT",
+    id: 2,
+    distribusjonstype: "LOKALPRINT",
+    status: { type: "Klar" },
+  };
+
   beforeEach(() => {
     cy.setupSakStubs();
     cy.viewport(1200, 1400);
     cy.visit("/saksnummer/123456/brevbehandler");
   });
+
   it("saken inneholder ingen brev", () => {
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", { body: [] });
     cy.contains("Fant ingen brev som er under behandling").should("be.visible");
   });
+
   it("kan ferdigstille og sende brev med sentralprint", () => {
     cy.intercept("POST", "/bff/skribenten-backend/sak/123456/brev/1/pdf/send", (request) => {
       request.reply({ journalpostId: 80_912, error: null });
     });
-
-    const kladdBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      status: { type: "Kladd" },
-      distribusjonstype: "SENTRALPRINT",
-    };
-
-    const klarBrev = { ...kladdBrev, status: { type: "Klar" } };
 
     const brevResponse = [kladdBrev, klarBrev];
 
@@ -36,7 +46,6 @@ describe("Brevbehandler", () => {
 
     cy.intercept("PATCH", "/bff/skribenten-backend/sak/123456/brev/1", (request) => {
       expect(request.body).contains({ sakId: "123456", brevId: 1, laastForRedigering: true });
-
       //er ikke interesert i innholdet i redigert brev + saksbehandlerValg
       request.reply({ info: klarBrev, redigertBrev: {}, saksbehandlerValg: {} });
     });
@@ -72,41 +81,30 @@ describe("Brevbehandler", () => {
   });
 
   it("kan ferdigstille og sende brev med lokalprint", () => {
-    cy.intercept("POST", "/bff/skribenten-backend/sak/123456/brev/1/pdf/send", (request) => {
-      request.reply({ journalpostId: 80_912, error: null });
-    });
-    cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1/pdf", (request) => {
-      request.reply(new Blob(["%PDF-1.4\n%..."], { type: "application/pdf" }));
-    });
-    const kladdBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      status: { type: "Kladd" },
-      distribusjonstype: "SENTRALPRINT",
-    };
-
-    const klarBrev = { ...kladdBrev, status: { type: "Klar" } };
+    //ser ikke ut til å være en god måte å gi ulik respons på hvert kall, så vi må ha en teller
+    let hentBrevRequestNr = 0;
+    let patchBrevRequestNr = 0;
     const lokalprintBrev = { ...kladdBrev, distribusjonstype: "LOKALPRINT", status: { type: "Klar" } };
 
     const brevResponse = [kladdBrev, lokalprintBrev];
     const patchResponse = [klarBrev, lokalprintBrev];
 
-    let hentBrevRequestNr = 0;
+    cy.intercept("POST", "/bff/skribenten-backend/sak/123456/brev/1/pdf/send", (request) => {
+      request.reply({ journalpostId: 80_912, error: null });
+    });
+    cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1/pdf", (request) => {
+      request.reply({ fixture: "helloWorldPdf.txt", headers: { "content-type": "application/pdf" } });
+    });
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", (request) => {
       request.reply([brevResponse[hentBrevRequestNr++]]);
     });
-
-    let patchBrevRequestNr = 0;
     cy.intercept("PATCH", "/bff/skribenten-backend/sak/123456/brev/1", (request) => {
       if (patchBrevRequestNr === 0) {
         expect(request.body).contains({ sakId: "123456", brevId: 1, laastForRedigering: true });
       } else {
         expect(request.body).contains({ sakId: "123456", brevId: 1, distribusjonstype: "LOKALPRINT" });
       }
+      //er ikke interesert i innholdet i redigert brev + saksbehandlerValg
       request.reply({ info: patchResponse[patchBrevRequestNr++], redigertBrev: {}, saksbehandlerValg: {} });
     });
 
@@ -139,6 +137,7 @@ describe("Brevbehandler", () => {
     cy.contains("80912").should("be.visible");
     cy.contains("Åpne brevet i ny fane").should("be.visible");
   });
+
   it("kan sende flere ferdigstilte brev samtidig", () => {
     cy.intercept("POST", "/bff/skribenten-backend/sak/123456/brev/1/pdf/send", (request) => {
       request.reply({ journalpostId: 80_912, error: null });
@@ -147,29 +146,10 @@ describe("Brevbehandler", () => {
       request.reply({ journalpostId: 80_913, error: null });
     });
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/2/pdf", (request) => {
-      request.reply(new Blob(["%PDF-1.4\n%..."], { type: "application/pdf" }));
+      request.reply({ fixture: "helloWorldPdf.txt", headers: { "content-type": "application/pdf" } });
     });
-
-    const klarBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      distribusjonstype: "SENTRALPRINT",
-      status: { type: "Klar" },
-    };
-    const lokalprintBrev = {
-      ...klarBrev,
-      brevkode: "BREV_SOM_SENDES_SOM_LOKALPRINT",
-      id: 2,
-      distribusjonstype: "LOKALPRINT",
-      status: { type: "Klar" },
-    };
-
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", (request) => {
-      request.reply([klarBrev, lokalprintBrev]);
+      request.reply([klarBrev, brevSomSendesSomLokalPrint]);
     });
 
     cy.contains("Send ferdigstilte brev").click("left");
@@ -190,37 +170,22 @@ describe("Brevbehandler", () => {
     cy.contains("Sendt til lokalprint").should("be.visible");
     cy.contains("BREV_SOM_SENDES_SOM_LOKALPRINT").click();
 
+    //contains tar alltid den første forekomsten av teksten, så den treffer den som er over
+    //vi bruker get på classen til å finne riktig element
     cy.get(".css-16pei6b-Oppsummeringspar").eq(2).contains("Distribueres via").should("be.visible");
     cy.contains("Lokal print").should("be.visible");
+    // -- || --
     cy.get(".css-16pei6b-Oppsummeringspar").eq(3).contains("Journalpost ID").should("be.visible");
     cy.contains("80913").should("be.visible");
     cy.contains("Åpne brevet i ny fane").should("be.visible");
   });
+
   it("velger hvilke brev som skal sendes", () => {
     cy.intercept("POST", "/bff/skribenten-backend/sak/123456/brev/1/pdf/send", (request) => {
       request.reply({ journalpostId: 80_912, error: null });
     });
-
-    const klarBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      distribusjonstype: "SENTRALPRINT",
-      status: { type: "Klar" },
-    };
-    const lokalprintBrev = {
-      ...klarBrev,
-      brevkode: "BREV_SOM_SENDES_SOM_LOKALPRINT",
-      id: 2,
-      distribusjonstype: "LOKALPRINT",
-      status: { type: "Klar" },
-    };
-
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", (request) => {
-      request.reply([klarBrev, lokalprintBrev]);
+      request.reply([klarBrev, brevSomSendesSomLokalPrint]);
     });
 
     cy.contains("Send ferdigstilte brev").click("left");
@@ -242,179 +207,37 @@ describe("Brevbehandler", () => {
   });
 
   it("viser pdf når er brev er valgt", () => {
-    const kladdBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      status: { type: "Kladd" },
-      distribusjonstype: "SENTRALPRINT",
-    };
-
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1/pdf", (request) => {
-      const pdfContent = `
-%PDF-1.1
-%¥±ë
-
-1 0 obj
-  << /Type /Catalog
-     /Pages 2 0 R
-  >>
-endobj
-
-2 0 obj
-  << /Type /Pages
-     /Kids [3 0 R]
-     /Count 1
-     /MediaBox [0 0 300 144]
-  >>
-endobj
-
-3 0 obj
-  <<  /Type /Page
-      /Parent 2 0 R
-      /Resources
-       << /Font
-           << /F1
-               << /Type /Font
-                  /Subtype /Type1
-                  /BaseFont /Times-Roman
-               >>
-           >>
-       >>
-      /Contents 4 0 R
-  >>
-endobj
-
-4 0 obj
-  << /Length 55 >>
-stream
-  BT
-    /F1 18 Tf
-    0 0 Td
-    (Hello World) Tj
-  ET
-endstream
-endobj
-
-xref
-0 5
-0000000000 65535 f 
-0000000018 00000 n 
-0000000077 00000 n 
-0000000178 00000 n 
-0000000457 00000 n 
-trailer
-  <<  /Root 1 0 R
-      /Size 5
-  >>
-startxref
-565
-%%EOF`;
-
-      request.reply(pdfContent);
+      request.reply({ fixture: "helloWorldPdf.txt" });
     });
-
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", (request) => {
       request.reply([kladdBrev]);
     });
 
-    //åpner brevet
     cy.contains("INFORMASJON_OM_SAKSBEHANDLINGSTID").click();
-    //brev med id 1
     cy.url().should("eq", "http://localhost:5173/saksnummer/123456/brevbehandler?brevId=%221%22");
-
     cy.contains("Hello World").should("be.visible");
   });
-  it("sletter et brev", () => {
-    const kladdBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      status: { type: "Kladd" },
-      distribusjonstype: "SENTRALPRINT",
-    };
 
+  it("sletter et brev", () => {
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", (request) => {
       request.reply([kladdBrev]);
     });
-
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1/pdf", (request) => {
-      const pdfContent = `
-  %PDF-1.1
-  %¥±ë
-  
-  1 0 obj
-    << /Type /Catalog
-       /Pages 2 0 R
-    >>
-  endobj
-  
-  2 0 obj
-    << /Type /Pages
-       /Kids [3 0 R]
-       /Count 1
-       /MediaBox [0 0 300 144]
-    >>
-  endobj
-  
-  3 0 obj
-    <<  /Type /Page
-        /Parent 2 0 R
-        /Resources
-         << /Font
-             << /F1
-                 << /Type /Font
-                    /Subtype /Type1
-                    /BaseFont /Times-Roman
-                 >>
-             >>
-         >>
-        /Contents 4 0 R
-    >>
-  endobj
-  
-  4 0 obj
-    << /Length 55 >>
-  stream
-    BT
-      /F1 18 Tf
-      0 0 Td
-      (Hello World) Tj
-    ET
-  endstream
-  endobj
-  
-  xref
-  0 5
-  0000000000 65535 f 
-  0000000018 00000 n 
-  0000000077 00000 n 
-  0000000178 00000 n 
-  0000000457 00000 n 
-  trailer
-    <<  /Root 1 0 R
-        /Size 5
-    >>
-  startxref
-  565
-  %%EOF`;
-
-      request.reply(pdfContent);
+      request.reply({ fixture: "helloWorldPdf.txt" });
+    });
+    cy.intercept("DELETE", "/bff/skribenten-backend/sak/123456/brev/1", (request) => {
+      request.reply({});
     });
 
-    cy.intercept("DELETE", "/bff/skribenten-backend/sak/123456/brev/1");
     cy.contains("INFORMASJON_OM_SAKSBEHANDLINGSTID").click();
     cy.url().should("eq", "http://localhost:5173/saksnummer/123456/brevbehandler?brevId=%221%22");
     cy.contains("Slett").click();
     cy.contains("Vil du slette brevet?").should("be.visible");
     cy.contains("Ja, slett brevet").click();
+    cy.url().should("eq", "http://localhost:5173/saksnummer/123456/brevbehandler");
   });
+
   it("får error i responsen for ferdigstill & prøver igjen", () => {
     let requestNo = 0;
     cy.intercept("POST", "/bff/skribenten-backend/sak/123456/brev/1/pdf/send", (request) => {
@@ -433,18 +256,6 @@ startxref
         request.reply({ journalpostId: 80_912, error: null });
       }
     });
-
-    const klarBrev = {
-      id: 1,
-      opprettetAv: "Ola Nordmann",
-      opprettet: "2021-09-01T12:00:00",
-      sistredigertAv: "Ola Nordmann",
-      sistredigert: "2021-09-01T12:00:00",
-      brevkode: "INFORMASJON_OM_SAKSBEHANDLINGSTID",
-      distribusjonstype: "SENTRALPRINT",
-      status: { type: "Klar" },
-    };
-
     cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev", (request) => {
       request.reply([klarBrev]);
     });
