8c3fb4729c423255fdca635db2788fd992dbae01
ramziabuqassim <ramzi.abu.qassim@nav.no>
2024-10-03 11:40:55 +0200

    fiks test

--- skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevvelger/-components/endreMottaker/UtfyllingAvManuellAdresseForm.tsx
+++ skribenten-web/frontend/src/routes/saksnummer_/$saksId/brevvelger/-components/endreMottaker/UtfyllingAvManuellAdresseForm.tsx
@@ -121,6 +121,7 @@ const UtfyllingAvManuellAdresseForm = (properties: {
                         top: auto;
                       }
                     `}
+                    data-cy="land-combobox"
                     error={fieldState.error?.message}
                     label="Land *"
                     onToggleSelected={(option) => {
