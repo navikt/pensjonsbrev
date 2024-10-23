3498c4d83338b5bcff11e5dd4aaa95a3cbab8ba4
Henrik Gundersen <henrik.gundersen@nav.no>
2024-10-23 11:33:46 +0200

    EY-4555: Fikser bosattUtland på avvist klagebrev (#994)

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/klage/AvvistKlageFerdigstilling.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/klage/AvvistKlageFerdigstilling.kt
@@ -4,6 +4,7 @@ import no.nav.pensjon.brev.template.Language
 import no.nav.pensjon.brev.template.dsl.createTemplate
 import no.nav.pensjon.brev.template.dsl.expression.equalTo
 import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.not
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
@@ -18,6 +19,7 @@ import no.nav.pensjon.etterlatte.maler.fraser.common.SakType
 import no.nav.pensjon.etterlatte.maler.fraser.omstillingsstoenad.OmstillingsstoenadFellesFraser
 import no.nav.pensjon.etterlatte.maler.klage.AvvistKlageFerdigDTOSelectors.data
 import no.nav.pensjon.etterlatte.maler.klage.AvvistKlageFerdigDTOSelectors.innhold
+import no.nav.pensjon.etterlatte.maler.klage.AvvistKlageInnholdDTOSelectors.bosattUtland
 import no.nav.pensjon.etterlatte.maler.klage.AvvistKlageInnholdDTOSelectors.sakType
 import no.nav.pensjon.etterlatte.maler.konverterElementerTilBrevbakerformat
 import no.nav.pensjon.etterlatte.maler.vedlegg.klageOgAnke
@@ -25,7 +27,7 @@ import no.nav.pensjon.etterlatte.maler.vedlegg.klageOgAnke
 
 data class AvvistKlageFerdigDTO(
     override val innhold: List<Element>,
-    val data: AvvistKlageInnholdDTO
+    val data: AvvistKlageInnholdDTO,
 ) : FerdigstillingBrevDTO
 
 @TemplateModelHelpers
@@ -57,14 +59,18 @@ object AvvistKlageFerdigstilling : EtterlatteTemplate<AvvistKlageFerdigDTO>, Hov
             showIf(data.sakType.equalTo(SakType.BARNEPENSJON)) {
                 includePhrase(BarnepensjonFellesFraser.DuHarRettTilAaKlage)
                 includePhrase(BarnepensjonFellesFraser.DuHarRettTilInnsyn)
-                includePhrase(BarnepensjonFellesFraser.HarDuSpoersmaal(true.expr(), false.expr()))
+                includePhrase(BarnepensjonFellesFraser.HarDuSpoersmaal(true.expr(), data.bosattUtland))
             } orShow {
                 includePhrase(OmstillingsstoenadFellesFraser.DuHarRettTilAaKlage)
                 includePhrase(OmstillingsstoenadFellesFraser.DuHarRettTilInnsyn)
                 includePhrase(OmstillingsstoenadFellesFraser.HarDuSpoersmaal)
             }
         }
-        // Vet ikke hva utlandstilknytning er. Går for bosatt utland for nå
-        includeAttachment(klageOgAnke(bosattUtland = true), innhold)
+
+        // Nasjonal
+        includeAttachment(klageOgAnke(bosattUtland = true), innhold, data.bosattUtland)
+
+        // Bosatt utland
+        includeAttachment(klageOgAnke(bosattUtland = false), innhold, data.bosattUtland.not())
     }
 }
