6eeb6927ac4ee46fbebe3f54fe720d46ad6e0257
Håkon Heggholmen <hakon.heggholmen@nav.no>
2024-10-24 09:23:53 +0200

    Legger inn tom mal som kan oppdateres med innhold så snart for feil i etteroppgjør.

---
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/AdhocFeilEtteroppgjoer2023.kt
@@ -0,0 +1,39 @@
+package no.nav.pensjon.brev.maler.adhoc
+
+import no.nav.pensjon.brev.api.model.maler.Brevkode
+import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.maler.fraser.common.Constants
+import no.nav.pensjon.brev.maler.vedlegg.dineRettigheterOgMulighetTilAaKlagePensjonStatisk
+import no.nav.pensjon.brev.template.AutobrevTemplate
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.LetterTemplate
+import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+
+
+object AdhocFeilEtteroppgjoer2023 : AutobrevTemplate<EmptyBrevdata> {
+    override val kode: Brevkode.AutoBrev = Brevkode.AutoBrev.PE_ADHOC_2024_FEIL_ETTEROPPGJOER_2023
+    override val template: LetterTemplate<*, EmptyBrevdata> = createTemplate(
+        name = kode.name,
+        letterDataType = EmptyBrevdata::class,
+        languages = languages(Bokmal),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Feil i etteroppgjørsbrev fra NAV",
+            isSensitiv = false,
+            distribusjonstype = LetterMetadata.Distribusjonstype.VIKTIG,
+            brevtype = LetterMetadata.Brevtype.INFORMASJONSBREV,
+        )
+    ) {
+        // TODO legg inn innhold så snart det er klart. (Haste-oppdrag)
+        title {
+            text(
+                Bokmal to "",
+            )
+        }
+        outline {
+
+        }
+    }
+}
