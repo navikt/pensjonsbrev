d404cbcaf9b8d63dd0c73655af232417959ec750
Alexander Hoem Rosbach <82815189+routsi@users.noreply.github.com>
2024-10-28 11:03:55 +0100

    Legg til dto for AvslagUfoeretrygd (#1002)
    * Legg til dto for AvslagUfoeretrygd
    
    * Endre type og bump versjon

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/vedtaksbrev/vedtaksdata/beregningsdata/beregninguforeperiode/BeregningUforePeriode.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/vedtaksbrev/vedtaksdata/beregningsdata/beregninguforeperiode/BeregningUforePeriode.kt
@@ -1,7 +1,9 @@
 package no.nav.pensjon.brev.api.model.maler.legacy.vedtaksbrev.vedtaksdata.beregningsdata.beregninguforeperiode
 
 import no.nav.pensjon.brev.api.model.maler.legacy.vedtaksbrev.vedtaksdata.beregningsdata.beregningufore.beregningytelseskomp.BeregningYtelsesKomp
+import no.nav.pensjon.brev.api.model.maler.legacy.vedtaksbrev.vedtaksdata.beregningsdata.beregningufore.uforetrygdberegning.Uforetrygdberegning
 
 data class BeregningUforePeriode(
-    val beregningytelseskomp: BeregningYtelsesKomp?
+    val beregningytelseskomp: BeregningYtelsesKomp?,
+    val uforetrygdberegning: Uforetrygdberegning?,
 )
