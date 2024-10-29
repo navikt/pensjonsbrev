d3f31b5daf8628eefb87a266e97185d983b8de4d
Håkon Heggholmen <hakon.heggholmen@nav.no>
2024-10-29 13:16:51 +0100

    Endrer til vedlegg dine rettihgeter og muligheter å klage.

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/vedlegg/DineRettigheterOgPlikterUfoereStatisk.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/vedlegg/DineRettigheterOgPlikterUfoereStatisk.kt
@@ -10,29 +10,12 @@ import no.nav.pensjon.brev.template.dsl.newText
 val vedleggDineRettigheterOgPlikterUfoereStatisk =
     createAttachment<LangBokmalNynorskEnglish, EmptyBrevdata>(
         title = newText(
-            Bokmal to "Dine rettigheter og plikter",
-            Nynorsk to "Rettane og pliktene dine",
-            English to "Your rights and obligations"
+            Bokmal to "Dine rettigheter og mulighet til å klage",
+            Nynorsk to "Rettane dine og høve til å klage",
+            English to "Your rights and how to appeal"
         ),
         includeSakspart = false,
     ) {
-        includePhrase(VedleggPlikter)
-
-        paragraph {
-            list {
-                item { includePhrase(VedleggPlikterUT1) }
-                item { includePhrase(VedleggPlikterUT2) }
-                item { includePhrase(VedleggPlikterUT3) }
-                item { includePhrase(VedleggPlikterUT4) }
-                item { includePhrase(VedleggPlikterUT5) }
-                item { includePhrase(VedleggPlikterEndretSivilstatus)}
-                item { includePhrase(VedleggPlikterUT8) }
-                item { includePhrase(VedleggPlikterUT9) }
-                item { includePhrase(VedleggPlikterUT10) }
-                item { includePhrase(VedleggPlikterUT11) }
-                item { includePhrase(VedleggPlikterUT12) }
-            }
-        }
         includePhrase(VedleggVeiledning)
         includePhrase(VedleggInnsynSakUfoeretrygdPesys)
         includePhrase(VedleggHjelpFraAndre)
