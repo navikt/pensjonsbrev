3a7b62bb2715f46e3561ce0400449784fd6750f7
Mads Opheim <mads.opheim@nav.no>
2024-11-08 12:17:03 +0100

    Konstantar for telefonnummer

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/alderspensjon/Alderspensjon.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/alderspensjon/Alderspensjon.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.brev.maler.fraser.alderspensjon
 
 import no.nav.pensjon.brev.maler.fraser.common.Constants.KONTAKT_ENG_URL
 import no.nav.pensjon.brev.maler.fraser.common.Constants.KONTAKT_URL
+import no.nav.pensjon.brev.maler.fraser.common.Constants.NAV_KONTAKTSENTER_TELEFON_PENSJON
 import no.nav.pensjon.brev.maler.fraser.common.Constants.NAV_URL
 import no.nav.pensjon.brev.maler.fraser.common.Constants.PENSJON_URL
 import no.nav.pensjon.brev.template.LangBokmalEnglish
@@ -22,8 +23,8 @@ class Alderspensjon {
             }
             paragraph {
                 text(
-                    Bokmal to "Du finner mer informasjon på $PENSJON_URL. På $KONTAKT_URL kan du chatte eller skrive til oss. Hvis du ikke finner svar på $NAV_URL, kan du ringe oss på telefon 55 55 33 34, hverdager 09.00-15.00.",
-                    English to "You can find more information at $PENSJON_URL. At $KONTAKT_ENG_URL you can chat or write to us. If you do not find the answer at $NAV_URL, you can call us at: +47 55 55 33 34, weekdays 09:00-15:00.",
+                    Bokmal to "Du finner mer informasjon på $PENSJON_URL. På $KONTAKT_URL kan du chatte eller skrive til oss. Hvis du ikke finner svar på $NAV_URL, kan du ringe oss på telefon $NAV_KONTAKTSENTER_TELEFON_PENSJON, hverdager 09.00-15.00.",
+                    English to "You can find more information at $PENSJON_URL. At $KONTAKT_ENG_URL you can chat or write to us. If you do not find the answer at $NAV_URL, you can call us at: +47 $NAV_KONTAKTSENTER_TELEFON_PENSJON, weekdays 09:00-15:00.",
                 )
             }
         }
