1fba4c0d858b2f39b3b2b645680fc1669b7a0ebf
andreasbalevik <andreasbalevik@gmail.com>
2024-11-08 14:19:34 +0100

    EY-4358 rettet byggefeil

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadInntektsjusteringVarselDTO.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadInntektsjusteringVarselDTO.kt
@@ -1,11 +1,14 @@
 package no.nav.pensjon.etterlatte.fixtures
 
+import no.nav.pensjon.brevbaker.api.model.NAVEnhet
+import no.nav.pensjon.brevbaker.api.model.Telefonnummer
 import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.inntektsjustering.OmstillingsstoenadInntektsjusteringVarselDTO
+import java.time.LocalDate
 
 fun createOmstillingsstoenadInntektsjusteringVarselDTO() = OmstillingsstoenadInntektsjusteringVarselDTO(
     innhold = createPlaceholderForRedigerbartInnhold(),
-    inntektsaar = TODO(),
-    bosattUtland = TODO(),
-    virkningstidspunkt = TODO(),
-    avsenderEnhet = TODO(),
+    inntektsaar = 2025,
+    bosattUtland = false,
+    virkningstidspunkt = LocalDate.of(2025, 1, 1),
+    avsenderEnhet = NAVEnhet("test", "test", Telefonnummer("test")),
 )
--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadInntektsjusteringVedtakRedigerbartUtfallDTO.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadInntektsjusteringVedtakRedigerbartUtfallDTO.kt
@@ -1,10 +1,11 @@
 package no.nav.pensjon.etterlatte.fixtures
 
 import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.inntektsjustering.OmstillingsstoenadVedtakInntektsjusteringRedigerbartUtfallDTO
+import java.time.LocalDate
 
 fun createOmstillingsstoenadInntektsjusteringVedtakRedigerbartUtfallDTO() = OmstillingsstoenadVedtakInntektsjusteringRedigerbartUtfallDTO(
     innhold = createPlaceholderForRedigerbartInnhold(),
-    opphoerFoerDesemberNesteAar = TODO(),
-    inntektsbeloep = TODO(),
-    opphoerDato = TODO(),
+    opphoerFoerDesemberNesteAar = false,
+    inntektsbeloep = 20000,
+    opphoerDato = LocalDate.of(2024, 1,1),
 )
