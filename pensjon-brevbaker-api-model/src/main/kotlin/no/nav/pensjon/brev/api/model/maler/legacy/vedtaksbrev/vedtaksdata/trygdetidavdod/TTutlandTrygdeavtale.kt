5ee98fe256f119119ee9dd444cfb39bcda1b86d4
Håkon Heggholmen <hakon.heggholmen@nav.no>
2024-11-11 11:46:13 +0100

    Trekker ut api-modell i egen branch.

---
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/vedtaksbrev/vedtaksdata/trygdetidavdod/TTutlandTrygdeavtale.kt
@@ -0,0 +1,8 @@
+package no.nav.pensjon.brev.api.model.maler.legacy.vedtaksbrev.vedtaksdata.trygdetidavdod
+
+data class TTutlandTrygdeavtale(
+    val ttnevnerbilateral: Int?,
+    val fattbilateral: Int?,
+    val framtidigttavtaleland: Int?,
+    val tttellerbilateral: Int?,
+)
