cd03e1c9131533396e225c1a96c25ec1daebf22f
Mads Opheim <mads.opheim@nav.no>
2024-11-14 16:42:22 +0100

    Opplegg for å kunne bestille brev utan å måtte sende inn brevkode som allereie fins som enum i api-modellen vår

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/BrevTemplate.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/BrevTemplate.kt
@@ -4,6 +4,7 @@ import no.nav.pensjon.brev.api.model.Sakstype
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.Brevkode
+import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.toCode
 import no.nav.pensjon.brev.template.dsl.TextScope
@@ -43,4 +44,13 @@ interface AutobrevTemplate<out LetterData : BrevbakerBrevdata> : BrevTemplate<Le
             languages = template.language.all().map { it.toCode() },
             metadata = template.letterMetadata,
         )
+}
+
+interface BrevUtenKode<E: Enum<E>>: BrevTemplate<EmptyBrevdata, E> {
+    override fun description(): TemplateDescription.Autobrev = TemplateDescription.Autobrev(
+        name = template.name,
+        letterDataClass = template.letterDataType.java.name,
+        languages = template.language.all().map { it.toCode() },
+        metadata = template.letterMetadata,
+    )
 }
