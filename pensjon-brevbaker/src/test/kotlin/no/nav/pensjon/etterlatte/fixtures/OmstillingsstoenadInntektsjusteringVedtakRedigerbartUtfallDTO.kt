7995fc5a297c6b0bf0054899599815688f4f7479
andreasbalevik <andreasbalevik@gmail.com>
2024-11-19 09:16:46 +0100

    rette tekst under Inntekten din i beregningsvedlegget for inntektsjustering

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadInntektsjusteringVedtakRedigerbartUtfallDTO.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadInntektsjusteringVedtakRedigerbartUtfallDTO.kt
@@ -6,5 +6,5 @@ import java.time.LocalDate
 
 fun createOmstillingsstoenadInntektsjusteringVedtakRedigerbartUtfallDTO() = OmstillingsstoenadVedtakInntektsjusteringRedigerbartUtfallDTO(
     inntektsbeloep = Kroner(20000),
-    opphoerDato = LocalDate.of(2025, 8, 1),
+    opphoerDato = LocalDate.of(2025, 11, 1),
 )
