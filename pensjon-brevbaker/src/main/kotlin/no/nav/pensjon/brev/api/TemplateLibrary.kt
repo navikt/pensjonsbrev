944277886822290430bcd590986cd911ac9b8e1e
Mads Opheim <mads.opheim@nav.no>
2024-11-26 11:07:05 +0100

    Går over frå å krevje brevkode som enum til å krevje det som implementasjon av interface. Da kan vi frikople oss frå dei konkrete kodane

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/api/TemplateLibrary.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/api/TemplateLibrary.kt
@@ -7,14 +7,16 @@ import no.nav.pensjon.brev.maler.redigerbar.OrienteringOmSaksbehandlingstidV2
 import no.nav.pensjon.brev.maler.ufoereBrev.VarselSaksbehandlingstidAutoV2
 import no.nav.pensjon.brev.template.BrevTemplate
 
-class TemplateLibrary<Kode : Enum<Kode>, out T : BrevTemplate<BrevbakerBrevdata, Kode>>(templates: Set<T>) {
-    private val templates: Map<Kode, T> = templates.associateBy { it.kode }
+class TemplateLibrary<Kode : Brevkode<Kode>, out T : BrevTemplate<BrevbakerBrevdata, Kode>>(templates: Set<T>) {
+    private val templates: Map<String, T> = templates.associateBy { it.kode.kode() }
 
     fun listTemplatesWithMetadata() = templates.map { getTemplate(it.key)!!.description() }
 
     fun listTemplatekeys() = templates.keys
 
-    fun getTemplate(kode: Kode) = when {
+    fun getTemplate(kode: Kode) = getTemplate(kode.kode())
+
+    fun getTemplate(kode: String) = when {
         // Legg inn her hvis du ønsker å styre forskjellige versjoner, feks
         // kode == DinBrevmal.kode && FeatureToggles.dinToggle.isEnabled() -> DinBrevmalV2
         kode == Brevkode.Redigerbar.UT_ORIENTERING_OM_SAKSBEHANDLINGSTID.kode() && FeatureToggles.pl7231ForventetSvartid.isEnabled() -> OrienteringOmSaksbehandlingstidV2
