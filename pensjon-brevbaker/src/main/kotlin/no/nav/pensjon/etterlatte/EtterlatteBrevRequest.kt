73cd1a59d802d4bfd21d91df439e44a55ce1417f
Mads Opheim <mads.opheim@nav.no>
2024-11-27 13:42:24 +0100

    I etterlatte-requesten kan vi fint bruke brevkode-interface på samme måte som elles

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteBrevRequest.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteBrevRequest.kt
@@ -1,11 +1,12 @@
 package no.nav.pensjon.etterlatte
 
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
+import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brevbaker.api.model.Felles
 import no.nav.pensjon.brevbaker.api.model.LanguageCode
 
 data class EtterlatteBrevRequest(
-    val kode: EtterlatteBrevKode,
+    val kode: Brevkode.Automatisk,
     val letterData: BrevbakerBrevdata,
     val felles: Felles,
     val language: LanguageCode,
