af85c4eb43f1b16a13cd4353414258d0032f5a3f
Mads Opheim <mads.opheim@nav.no>
2024-11-27 13:58:47 +0100

    EtterlatteTemplate-interfacet kan i grunn berre vera ei vidarekopling til AutobrevTemplate. Tenkjer dette interfacet på sikt bør ut av vår kode

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteTemplate.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteTemplate.kt
@@ -1,11 +1,6 @@
 package no.nav.pensjon.etterlatte
 
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
-import no.nav.pensjon.brev.api.model.maler.Brevkode
-import no.nav.pensjon.brev.template.HasModel
-import no.nav.pensjon.brev.template.LetterTemplate
+import no.nav.pensjon.brev.template.AutobrevTemplate
 
-interface EtterlatteTemplate<LetterData : BrevbakerBrevdata>: HasModel<LetterData> {
-    val template: LetterTemplate<*, LetterData>
-    val kode: Brevkode.Automatisk
-}
+interface EtterlatteTemplate<LetterData : BrevbakerBrevdata> : AutobrevTemplate<LetterData>
