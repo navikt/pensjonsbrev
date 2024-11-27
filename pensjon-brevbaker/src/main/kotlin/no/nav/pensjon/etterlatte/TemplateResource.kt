2a66ae482861f8ea43011acdb6e5c6f7afff19b9
Mads Opheim <mads.opheim@nav.no>
2024-11-27 13:23:25 +0100

    Slettar ubrukt etterlatte-kode

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/TemplateResource.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/TemplateResource.kt
@@ -125,23 +125,12 @@ val prodAutobrevTemplates: Set<EtterlatteTemplate<*>> =
         EnkeltVedtakOmregningNyttRegelverkFerdig,
     )
 
-val prodRedigerbareTemplates: Set<EtterlatteTemplate<*>> = emptySet()
-
 class TemplateResource(
     autobrevTemplates: Set<EtterlatteTemplate<*>> = prodAutobrevTemplates,
-    redigerbareTemplates: Set<EtterlatteTemplate<*>> = prodRedigerbareTemplates,
 ) {
     private val autoBrevMap: Map<EtterlatteBrevKode, EtterlatteTemplate<*>> =
         autobrevTemplates.associateBy { it.kode }
 
-    private val redigerbareBrevMap: Map<EtterlatteBrevKode, EtterlatteTemplate<*>> =
-        redigerbareTemplates.associateBy { it.kode }
-
-    fun getAutoBrev(): Set<EtterlatteBrevKode> = autoBrevMap.keys
-
     fun getAutoBrev(kode: EtterlatteBrevKode): LetterTemplate<*, *>? = autoBrevMap[kode]?.template
 
-    fun getRedigerbareBrev(): Set<EtterlatteBrevKode> = redigerbareBrevMap.keys
-
-    fun getRedigerbartBrev(kode: EtterlatteBrevKode): LetterTemplate<*, *>? = redigerbareBrevMap[kode]?.template
 }
