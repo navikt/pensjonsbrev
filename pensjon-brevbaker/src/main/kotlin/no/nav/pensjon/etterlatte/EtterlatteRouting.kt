d0e4b53f101a506e23c06e75f4bef5ad22f76d75
Mads Opheim <mads.opheim@nav.no>
2024-11-27 18:10:11 +0100

    Samkøyrer implementasjon

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteRouting.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteRouting.kt
@@ -3,8 +3,7 @@ package no.nav.pensjon.etterlatte
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.post
-import io.micrometer.core.instrument.Tag
-import no.nav.pensjon.brev.Metrics
+import no.nav.pensjon.brev.api.TemplateResource
 import no.nav.pensjon.brev.api.model.BestillBrevRequest
 import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.latex.LaTeXCompilerService
@@ -14,7 +13,9 @@ import no.nav.pensjon.brevbaker.api.model.LetterMetadata
 data class LetterResponse(val base64pdf: String, val contentType: String, val letterMetadata: LetterMetadata)
 
 fun Route.etterlatteRouting(latexCompilerService: LaTeXCompilerService) {
-    val letterResource = LetterResource(EtterlatteMaler, latexCompilerService)
+    val letterResource = LetterResource(
+        latexCompilerService, TemplateResource("", EtterlatteMaler.hentAutobrevmaler(), latexCompilerService)
+    )
 
     post<BestillBrevRequest<Brevkode.Automatisk>>("/pdf") { letterRequest ->
         call.respond(letterResource.renderPDF(letterRequest))
