e83ad22bdc7fd69285a5265ff87269987b021bcd
Mads Opheim <mads.opheim@nav.no>
2024-12-12 15:59:07 +0100

    Gjer etterlatte-strukturen mellom route og resource lik som elles

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteRouting.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/EtterlatteRouting.kt
@@ -1,49 +1,24 @@
 package no.nav.pensjon.etterlatte
 
-import io.ktor.http.ContentType
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.micrometer.core.instrument.Tag
-import no.nav.pensjon.brev.Metrics
 import no.nav.pensjon.brev.api.model.BestillBrevRequest
+import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.latex.LaTeXCompilerService
-import no.nav.pensjon.brev.template.render.LatexDocumentRenderer
-import no.nav.pensjon.brev.template.render.Letter2Markup
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata
-
-private val letterResource = LetterResource(EtterlatteMaler)
 
 data class LetterResponse(val file: String, val contentType: String, val letterMetadata: LetterMetadata)
 
 fun Route.etterlatteRouting(latexCompilerService: LaTeXCompilerService) {
+    val letterResource = LetterResource(EtterlatteMaler, latexCompilerService)
 
-    post("/pdf") {
-        val letterRequest = call.receive<BestillBrevRequest<*>>()
-
-        val letter = letterResource.create(letterRequest)
-        val pdfBase64 = Letter2Markup.render(letter)
-            .let { LatexDocumentRenderer.render(it.letterMarkup, it.attachments, letter) }
-            .let { latexCompilerService.producePDF(it) }
-
-        call.respond(LetterResponse(
-            file = pdfBase64.base64PDF,
-            contentType = ContentType.Application.Pdf.toString(),
-            letterMetadata = letter.template.letterMetadata
-        ))
-
-        // Om dere vil ha det
-        Metrics.prometheusRegistry.counter(
-            "pensjon_brevbaker_etterlatte_request_count",
-            listOf(Tag.of("brevkode", letterRequest.kode.kode()))
-        ).increment()
+    post<BestillBrevRequest<Brevkode.Automatisk>>("/pdf") { letterRequest ->
+        call.respond(letterResource.renderPDF(letterRequest))
+        letterResource.countLetter(letterRequest.kode)
     }
 
-    post("/json") {
-        val letterRequest = call.receive<BestillBrevRequest<*>>()
-        val letter = letterResource.create(letterRequest)
-
-        call.respond(Letter2Markup.render(letter).letterMarkup)
+    post<BestillBrevRequest<Brevkode.Automatisk>>("/json") {
+        call.respond(letterResource.renderJSON(it))
     }
 }
 
