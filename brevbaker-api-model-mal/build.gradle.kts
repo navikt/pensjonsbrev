ca601b8d8b9d2f4c7bdd4638907227c4e97f948b
Mads Opheim <mads.opheim@nav.no>
2025-01-06 13:51:46 +0100

    Biblioteksmodul kor tanken er å leggje det som trengs for å implementere ein mal

---
+++ brevbaker-api-model-mal/build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+val apiModelJavaTarget: String by System.getProperties()
+
+plugins {
+    kotlin("jvm")
+    `maven-publish`
+}
+
+group = "no.nav.pensjon.brevbaker"
+version = "0.0.1"
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib"))
+    api("no.nav.pensjon.brevbaker:brevbaker-api-model-common:1.7.0")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/navikt/pensjonsbrev")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(apiModelJavaTarget))
+    }
+}
+tasks {
+    compileJava {
+        targetCompatibility = apiModelJavaTarget
+    }
+    compileTestJava {
+        targetCompatibility = apiModelJavaTarget
+    }
+}
