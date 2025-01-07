f72fb8519266b117c134dbc8b1c16696802c5498
Mads Opheim <mads.opheim@nav.no>
2025-01-07 13:21:51 +0100

    Flyttar over LanguageCombination, som trengs for at vi skal kunne flytte Element

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
@@ -22,32 +22,6 @@ data class LanguageSettings(val settings: Map<String, Element.OutlineContent.Par
             ?: throw MissingLanguageSettingException(setting)
 }
 
-sealed class LanguageCombination {
-
-    data class Single<Lang : Language>(val first: Lang) : LanguageCombination(), LanguageSupport.Single<Lang>, StableHash by StableHash.of(first) {
-        override fun supports(language: Language): Boolean = language == first
-        override fun all(): Set<Language> = setOf(first)
-    }
-
-    data class Double<Lang1 : Language, Lang2 : Language>(
-        val first: Lang1,
-        val second: Lang2,
-    ) : LanguageCombination(), LanguageSupport.Double<Lang1, Lang2>, StableHash by StableHash.of(first, second) {
-        override fun supports(language: Language): Boolean = language == first || language == second
-        override fun all(): Set<Language> = setOf(first, second)
-    }
-
-    data class Triple<Lang1 : Language, Lang2 : Language, Lang3 : Language>(
-        val first: Lang1,
-        val second: Lang2,
-        val third: Lang3
-    ) : LanguageCombination(), LanguageSupport.Triple<Lang1, Lang2, Lang3>, StableHash by StableHash.of(first, second, third) {
-        override fun supports(language: Language): Boolean =
-            language == first || language == second || language == third
-        override fun all(): Set<Language> = setOf(first, second, third)
-    }
-}
-
 fun <Lang1 : Language, Lang2 : Language, LetterData : Any> OutlineOnlyScope<LanguageSupport.Double<Lang1, Lang2>, LetterData>.includePhrase(phrase: OutlinePhrase<out LanguageSupport.Triple<Lang1, *, Lang2>>) {
     // Det er trygt å caste her fordi receiver og phrase begge har Lang1 og Lang2.
     @Suppress("UNCHECKED_CAST")
