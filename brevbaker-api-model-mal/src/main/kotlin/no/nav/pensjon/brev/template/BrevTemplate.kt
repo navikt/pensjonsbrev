045f6861ea4d1599a6dbccf5fe6605f4435dbfbf
Mads Opheim <mads.opheim@nav.no>
2025-01-07 18:00:55 +0100

    Flyttar også RedigerbarTemplate. Kan no deklarere opp klassesignaturen til malane. Gjenstår createTemplate og alt rundt det

--- brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/BrevTemplate.kt
+++ brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/BrevTemplate.kt
@@ -1,8 +1,12 @@
 package no.nav.pensjon.brev.template
 
+import no.nav.pensjon.brev.api.model.Sakstype
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.Brevkode
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
+import no.nav.pensjon.brev.template.dsl.TextScope
+import no.nav.pensjon.brevbaker.api.model.ElementTags
 import no.nav.pensjon.brevbaker.api.model.LanguageCode
 
 interface BrevTemplate<out LetterData : BrevbakerBrevdata, Kode : Brevkode<Kode>> : HasModel<LetterData> {
@@ -19,6 +23,25 @@ interface AutobrevTemplate<out LetterData : BrevbakerBrevdata> : BrevTemplate<Le
             languages = template.language.all().map { it.toCode() },
             metadata = template.letterMetadata,
         )
+}
+interface RedigerbarTemplate<LetterData : RedigerbarBrevdata<out BrevbakerBrevdata, out BrevbakerBrevdata>> : BrevTemplate<LetterData, Brevkode.Redigerbart> {
+    val kategori: TemplateDescription.Brevkategori
+    val brevkontekst: TemplateDescription.Brevkontekst
+    val sakstyper: Set<Sakstype>
+
+    override fun description(): TemplateDescription.Redigerbar =
+        TemplateDescription.Redigerbar(
+            name = template.name,
+            letterDataClass = template.letterDataType.java.name,
+            languages = template.language.all().map { it.toCode() },
+            metadata = template.letterMetadata,
+            kategori = kategori,
+            brevkontekst = brevkontekst,
+            sakstyper = sakstyper,
+        )
+
+    fun TextScope<*, *>.fritekst(beskrivelse: String): Expression<String> =
+        Expression.Literal(beskrivelse, setOf(ElementTags.FRITEKST))
 }
 
 fun Language.toCode(): LanguageCode =
