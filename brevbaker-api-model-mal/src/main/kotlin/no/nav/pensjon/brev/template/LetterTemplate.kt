64dd2beeab6bf5ff8fa1f7e4fed756bbc0969772
Mads Opheim <mads.opheim@nav.no>
2025-01-07 17:20:35 +0100

    Flyttar - endeleg - over LetterTemplate også

--- brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/LetterTemplate.kt
+++ brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/LetterTemplate.kt
@@ -2,9 +2,26 @@ package no.nav.pensjon.brev.template
 
 import no.nav.pensjon.brevbaker.api.model.ElementTags
 import no.nav.pensjon.brevbaker.api.model.IntValue
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
 import no.nav.pensjon.brevbaker.api.model.Telefonnummer
 import java.time.LocalDate
+import kotlin.reflect.KClass
 
+data class LetterTemplate<Lang : LanguageSupport, out LetterData : Any>(
+    val name: String,
+    val title: List<TextElement<Lang>>,
+    val letterDataType: KClass<out LetterData>,
+    val language: Lang,
+    val outline: List<OutlineElement<Lang>>,
+    val attachments: List<IncludeAttachment<Lang, *>> = emptyList(),
+    val letterMetadata: LetterMetadata,
+) {
+    init {
+        if (title.isEmpty()) {
+            throw MissingTitleInTemplateException("Missing title in template: $name")
+        }
+    }
+}
 
 sealed class Expression<out Out> : StableHash {
 
