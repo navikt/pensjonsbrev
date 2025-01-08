16e0ca736b2cc859302c2bb05a171fd0561f7f86
Mads Opheim <mads.opheim@nav.no>
2025-01-08 12:22:16 +0100

    KSP for pensjonsmaler

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,11 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 val apiModelVersion: String by project
+val apiModelJavaTarget: String by System.getProperties()
 
 plugins {
-    kotlin("jvm") version "2.1.0"
+    kotlin("jvm")
+    id("com.google.devtools.ksp")
 }
 
 group = "no.nav.etterlatte"
@@ -14,9 +18,33 @@ repositories {
 dependencies {
     compileOnly(kotlin("stdlib"))
     api(project(":brevbaker-api-model-mal"))
-    api("no.nav.pensjon.brev:pensjon-brevbaker-api-model:$apiModelVersion")
+    api(project(":pensjon-brevbaker-api-model"))
+    ksp(project(":template-model-generator"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(apiModelJavaTarget))
+    }
+    sourceSets {
+        main {
+            kotlin.srcDir("build/generated/ksp/main/kotlin")
+        }
+        test {
+            kotlin.srcDir("build/generated/ksp/test/kotlin")
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        targetCompatibility = apiModelJavaTarget
+    }
+    compileTestJava {
+        targetCompatibility = apiModelJavaTarget
+    }
 }
