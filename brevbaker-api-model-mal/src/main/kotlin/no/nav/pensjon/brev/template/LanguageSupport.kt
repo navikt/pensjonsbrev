2f0aec395c1b0df2c0cc95d3c2067a21af4ae7a1
Mads Opheim <mads.opheim@nav.no>
2025-01-08 10:30:46 +0100

    Typealiasa for språk kan fint vera generelt

--- brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/LanguageSupport.kt
+++ brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/LanguageSupport.kt
@@ -8,3 +8,11 @@ interface LanguageSupport : StableHash {
     interface Double<Lang1 : Language, Lang2 : Language> : Single<Lang1>
     interface Triple<Lang1 : Language, Lang2 : Language, Lang3 : Language> : Double<Lang1, Lang2>
 }
+
+typealias BaseLanguages = LangBokmalNynorskEnglish
+typealias LangBokmal = LanguageSupport.Single<Language.Bokmal>
+typealias LangNynorsk = LanguageSupport.Single<Language.Nynorsk>
+typealias LangEnglish = LanguageSupport.Single<Language.English>
+typealias LangBokmalNynorsk = LanguageSupport.Double<Language.Bokmal, Language.Nynorsk>
+typealias LangBokmalEnglish = LanguageSupport.Double<Language.Bokmal, Language.English>
+typealias LangBokmalNynorskEnglish = LanguageSupport.Triple<Language.Bokmal, Language.Nynorsk, Language.English>
--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
@@ -1,15 +1,6 @@
 package no.nav.pensjon.brev.template
 
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
-import java.util.*
-
-typealias BaseLanguages = LangBokmalNynorskEnglish
-typealias LangBokmal = LanguageSupport.Single<Language.Bokmal>
-typealias LangNynorsk = LanguageSupport.Single<Language.Nynorsk>
-typealias LangEnglish = LanguageSupport.Single<Language.English>
-typealias LangBokmalNynorsk = LanguageSupport.Double<Language.Bokmal, Language.Nynorsk>
-typealias LangBokmalEnglish = LanguageSupport.Double<Language.Bokmal, Language.English>
-typealias LangBokmalNynorskEnglish = LanguageSupport.Triple<Language.Bokmal, Language.Nynorsk, Language.English>
 
 data class LanguageSettings(val settings: Map<String, Element.OutlineContent.ParagraphContent.Text.Literal<BaseLanguages>>) {
     class MissingLanguageSettingException(msg: String) : Exception(msg)
