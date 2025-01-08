8f01c5059d836e47cb71b1e5be8fe47cd3c774d3
Mads Opheim <mads.opheim@nav.no>
2025-01-08 10:44:05 +0100

    Hjelpefunksjonen for å kombinere første og tredje språk er fort også nyttig

--- brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
+++ brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
@@ -1,5 +1,6 @@
 package no.nav.pensjon.brev.template
 
+import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
 import java.util.Locale
 
 sealed class Language : StableHash {
@@ -19,4 +20,10 @@ sealed class Language : StableHash {
     object Bokmal : Language(), StableHash by StableHash.of("Language.Bokmal")
     object Nynorsk : Language(), StableHash by StableHash.of("Language.Nynorsk")
     object English : Language(), StableHash by StableHash.of("Language.English")
+}
+
+fun <Lang1 : Language, Lang2 : Language, LetterData : Any> OutlineOnlyScope<LanguageSupport.Double<Lang1, Lang2>, LetterData>.includePhrase(phrase: OutlinePhrase<out LanguageSupport.Triple<Lang1, *, Lang2>>) {
+    // Det er trygt å caste her fordi receiver og phrase begge har Lang1 og Lang2.
+    @Suppress("UNCHECKED_CAST")
+    (phrase as OutlinePhrase<LanguageSupport.Double<Lang1, Lang2>>).apply(this)
 }
--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/Language.kt
@@ -12,9 +12,3 @@ data class LanguageSettings(val settings: Map<String, Element.OutlineContent.Par
         settings[setting]?.text(language)
             ?: throw MissingLanguageSettingException(setting)
 }
-
-fun <Lang1 : Language, Lang2 : Language, LetterData : Any> OutlineOnlyScope<LanguageSupport.Double<Lang1, Lang2>, LetterData>.includePhrase(phrase: OutlinePhrase<out LanguageSupport.Triple<Lang1, *, Lang2>>) {
-    // Det er trygt å caste her fordi receiver og phrase begge har Lang1 og Lang2.
-    @Suppress("UNCHECKED_CAST")
-    (phrase as OutlinePhrase<LanguageSupport.Double<Lang1, Lang2>>).apply(this)
-}
