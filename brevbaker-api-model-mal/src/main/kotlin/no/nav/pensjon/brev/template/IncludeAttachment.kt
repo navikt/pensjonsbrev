0bc912e1431771a3e1c6604f1cdf108944313327
Mads Opheim <mads.opheim@nav.no>
2025-01-08 10:37:17 +0100

    Kan no flytte over også resten av IncludeAttachment

--- brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/IncludeAttachment.kt
+++ brevbaker-api-model-mal/src/main/kotlin/no/nav/pensjon/brev/template/IncludeAttachment.kt
@@ -1,5 +1,9 @@
 package no.nav.pensjon.brev.template
 
+import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
+import no.nav.pensjon.brev.template.dsl.TextScope
+import no.nav.pensjon.brev.template.dsl.text
+
 data class IncludeAttachment<out Lang : LanguageSupport, AttachmentData : Any>(
     val data: Expression<AttachmentData>,
     val template: AttachmentTemplate<Lang, AttachmentData>,
@@ -11,3 +15,19 @@ data class AttachmentTemplate<out Lang : LanguageSupport, AttachmentData : Any>(
     val outline: List<OutlineElement<Lang>>,
     val includeSakspart: Boolean = false,
 ): HasModel<AttachmentData>, StableHash by StableHash.of(title, StableHash.of(outline), StableHash.of(includeSakspart))
+
+fun <Lang : LanguageSupport, LetterData : Any> createAttachment(
+    title: TextElement<Lang>,
+    includeSakspart: Boolean = false,
+    outline: OutlineOnlyScope<Lang, LetterData>.() -> Unit
+) = AttachmentTemplate<Lang, LetterData>(
+    title,
+    OutlineOnlyScope<Lang, LetterData>().apply(outline).elements,
+    includeSakspart
+)
+
+fun TextScope<BaseLanguages, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
+    text(Language.Bokmal to "«", Language.Nynorsk to "«", Language.English to "“")
+    addTextContent(attachment.title)
+    text(Language.Bokmal to "»", Language.Nynorsk to "»", Language.English to "”")
+}
