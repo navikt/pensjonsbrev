1310bf4deef79f4e8850b6e63574fb8440672870
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-01-08 13:30:20 +0100

    abstraher ut brevalternativ tekster

--- skribenten-web/frontend/src/routes/saksnummer_/$saksId/vedtak.$vedtakId/route.tsx
+++ skribenten-web/frontend/src/routes/saksnummer_/$saksId/vedtak.$vedtakId/route.tsx
@@ -1,14 +1,18 @@
 import { css } from "@emotion/react";
 import { ArrowRightIcon } from "@navikt/aksel-icons";
-import { BodyShort, Box, Button, Heading, HStack, Label, Switch, TextField, VStack } from "@navikt/ds-react";
+import { BodyShort, Box, Button, Heading, HStack, Label, Switch, Tabs, VStack } from "@navikt/ds-react";
 import { createFileRoute } from "@tanstack/react-router";
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 import Actions from "~/Brevredigering/LetterEditor/actions";
 import { LetterEditor } from "~/Brevredigering/LetterEditor/LetterEditor";
 import type { LetterEditorState } from "~/Brevredigering/LetterEditor/model/state";
+import { AutoSavingTextField } from "~/Brevredigering/ModelEditor/components/ScalarEditor";
+import { SaksbehandlerValgModelEditor } from "~/Brevredigering/ModelEditor/ModelEditor";
 import { Divider } from "~/components/Divider";
 import OppsummeringAvMottaker from "~/components/OppsummeringAvMottaker";
+import type { SaksbehandlerValg } from "~/types/brev";
 
 import { nyBrevResponse } from "../../../../../cypress/utils/brevredigeringTestUtils";
 
@@ -67,8 +71,41 @@ const ThreeSectionLayout = (props: { left: React.ReactNode; right: React.ReactNo
   );
 };
 
+interface VedtakSidemenyFormData {
+  signatur: string;
+  saksbehandlerValg: SaksbehandlerValg;
+}
+
 const Vedtak = () => {
-  const [editorState, setEditorState] = useState<LetterEditorState>(Actions.create(nyBrevResponse({})));
+  const { saksId } = Route.useParams();
+  const brevResponse = nyBrevResponse({});
+  const [editorState, setEditorState] = useState<LetterEditorState>(Actions.create(brevResponse));
+
+  useEffect(() => {
+    if (editorState.redigertBrevHash !== brevResponse.redigertBrevHash) {
+      setEditorState((previousState) => ({
+        ...previousState,
+        redigertBrev: brevResponse.redigertBrev,
+        redigertBrevHash: brevResponse.redigertBrevHash,
+      }));
+    }
+  }, [brevResponse.redigertBrev, brevResponse.redigertBrevHash, editorState.redigertBrevHash, setEditorState]);
+
+  const defaultValuesModelEditor = useMemo(
+    () => ({
+      saksbehandlerValg: {
+        ...brevResponse.saksbehandlerValg,
+      },
+      signatur: brevResponse.redigertBrev.signatur.saksbehandlerNavn,
+    }),
+    [brevResponse.redigertBrev.signatur.saksbehandlerNavn, brevResponse.saksbehandlerValg],
+  );
+
+  const form = useForm<VedtakSidemenyFormData>({
+    defaultValues: defaultValuesModelEditor,
+  });
+
+  console.log("formValues", form.getValues());
 
   return (
     <ThreeSectionLayout
@@ -79,37 +116,43 @@ const Vedtak = () => {
       }
       left={
         <div>
-          <VStack gap="8">
-            <Heading size="small">brev tittel</Heading>
-            <VStack gap="4">
-              <Heading size="small">brev tittel</Heading>
-              <OppsummeringAvMottaker
-                mottaker={{
-                  type: "NorskAdresse",
-                  navn: "Navn Navnesen",
-                  postnummer: "1234",
-                  poststed: "Oslo",
-                  adresselinje1: "Adresselinje 1",
-                  adresselinje2: null,
-                  adresselinje3: null,
-                }}
-                saksId={""}
-                withTitle
-              />
+          <FormProvider {...form}>
+            <VStack gap="8">
+              <Heading size="small">{brevResponse.redigertBrev.title}</Heading>
+              <VStack gap="4">
+                <Heading size="small">{brevResponse.redigertBrev.title}</Heading>
+                <OppsummeringAvMottaker mottaker={brevResponse.info.mottaker} saksId={saksId} withTitle />
+                <VStack>
+                  <Label size="small">Distribusjonstype</Label>
+                  <BodyShort size="small">TODO - type</BodyShort>
+                </VStack>
+              </VStack>
+              <Divider />
+              <VStack gap="5">
+                <Switch size="small">Marker tekst som er lagt til manuelt</Switch>
+                <Switch size="small">Vis slettet tekst</Switch>
+                <AutoSavingTextField
+                  field={"signatur"}
+                  fieldType={{
+                    type: "scalar",
+                    nullable: false,
+                    kind: "STRING",
+                  }}
+                  label="Underskrift"
+                  onSubmit={() => console.log("submit")}
+                  timeoutTimer={2500}
+                  type={"text"}
+                />
+              </VStack>
+              <Divider />
               <VStack>
-                <Label size="small">Distribusjonstype</Label>
-                <BodyShort size="small">Type</BodyShort>
+                <BrevmalAlternativer
+                  brevkode={brevResponse.info.brevkode}
+                  submitOnChange={() => console.log("submit")}
+                />
               </VStack>
             </VStack>
-            <Divider />
-            <VStack gap="5">
-              <Switch size="small">Marker tekst som er lagt til manuelt</Switch>
-              <Switch size="small">Vis slettet tekst</Switch>
-              <TextField autoComplete="name" label="Underskrift" size="small" />
-            </VStack>
-            <Divider />
-            <VStack>{/* <SaksbehandlerValgModelEditor brevkode={"testy"} fieldsToRender={"required"} /> */}</VStack>
-          </VStack>
+          </FormProvider>
         </div>
       }
       right={
@@ -123,5 +166,68 @@ const Vedtak = () => {
         />
       }
     />
+  );
+};
+
+enum BrevAlternativTab {
+  TEKSTER = "TEKSTER",
+  OVERSTYRING = "OVERSTYRING",
+}
+
+const BrevmalAlternativer = (props: { brevkode: string; submitOnChange?: () => void }) => {
+  return (
+    <Tabs
+      css={css`
+        width: 100%;
+
+        .navds-tabs__scroll-button {
+          /* vi har bare 2 tabs, så det gir ikke mening tab listen skal være scrollbar. Den tar i tillegg mye ekstra plass når skjermen er <1024px */
+          display: none;
+        }
+      `}
+      defaultValue={BrevAlternativTab.TEKSTER}
+      fill
+      size="small"
+    >
+      <Tabs.List
+        css={css`
+          display: grid;
+          grid-template-columns: repeat(2, 1fr);
+        `}
+      >
+        <Tabs.Tab label="Tekster" value={BrevAlternativTab.TEKSTER} />
+        <Tabs.Tab label="Overstyring" value={BrevAlternativTab.OVERSTYRING} />
+      </Tabs.List>
+      <Tabs.Panel
+        css={css`
+          display: flex;
+          flex-direction: column;
+          gap: var(--a-spacing-6);
+          margin-top: 12px;
+        `}
+        value={BrevAlternativTab.TEKSTER}
+      >
+        <SaksbehandlerValgModelEditor
+          brevkode={props.brevkode}
+          fieldsToRender={"optional"}
+          submitOnChange={props.submitOnChange}
+        />
+      </Tabs.Panel>
+      <Tabs.Panel
+        css={css`
+          display: flex;
+          flex-direction: column;
+          gap: var(--a-spacing-6);
+          margin-top: 12px;
+        `}
+        value={BrevAlternativTab.OVERSTYRING}
+      >
+        <SaksbehandlerValgModelEditor
+          brevkode={props.brevkode}
+          fieldsToRender={"required"}
+          submitOnChange={props.submitOnChange}
+        />
+      </Tabs.Panel>
+    </Tabs>
   );
 };
