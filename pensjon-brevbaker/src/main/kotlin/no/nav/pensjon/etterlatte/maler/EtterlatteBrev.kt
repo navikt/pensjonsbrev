7857bf00f1f62d43625fce8b6efe465aa4fbbb45
Emil Schrøder <emil.schroder@nav.no>
2025-01-14 09:50:11 +0100

    Legg til tekst om yrkesskade for OMS innvilgelse
    EY-4734

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/EtterlatteBrev.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/etterlatte/maler/EtterlatteBrev.kt
@@ -20,6 +20,7 @@ data class BarnepensjonBeregning(
     val trygdetid: List<Trygdetid>,
     val erForeldreloes: Boolean = false,
     val forskjelligTrygdetid: ForskjelligTrygdetid? = null,
+    val erYrkesskade: Boolean = false,
 ) : BrevDTO {
     val harForskjelligMetode = forskjelligTrygdetid?.harForskjelligMetode == true
 }
@@ -42,6 +43,7 @@ data class OmstillingsstoenadBeregning(
     val trygdetid: Trygdetid,
     val oppphoersdato: LocalDate? = null,
     val opphoerNesteAar: Boolean,
+    val erYrkesskade: Boolean = false,
 ) : BrevDTO
 
 data class OmstillingsstoenadBeregningRevurderingRedigertbartUtfall(
