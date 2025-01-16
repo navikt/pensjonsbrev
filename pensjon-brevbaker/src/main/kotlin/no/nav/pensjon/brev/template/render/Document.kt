3e4c95b40181b320bb6712a4e2f82c5cdb77ca92
Mads Opheim <mads.opheim@nav.no>
2025-01-16 16:22:22 +0100

    Trekkjer ut HTML og Latex i eigne filer, for oversikt

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/render/Document.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/render/Document.kt
@@ -17,40 +17,6 @@ interface Document {
 
 private val base64Encoder: Base64.Encoder = Base64.getEncoder()
 
-class LatexDocument : Document {
-    private val _files: MutableList<DocumentFile> = mutableListOf()
-    override val files: List<DocumentFile>
-        get() = _files
-
-    private fun newPlainTextFile(fileName: String, writeToFile: Appendable.() -> Unit) =
-        _files.add(DocumentFile.PlainText(fileName, writeToFile))
-
-    fun newLatexFile(fileName: String, writeToFile: LatexAppendable.() -> Unit) =
-        newPlainTextFile(fileName) {
-            LatexAppendable(this).apply(writeToFile)
-        }
-
-    fun addFiles(newFiles: List<DocumentFile>) {
-        _files.addAll(newFiles)
-    }
-}
-
-class HTMLDocument(indexHTMLBuilder: Appendable.() -> Unit) : Document {
-    private val _files: MutableList<DocumentFile> = mutableListOf()
-    override val files: List<DocumentFile>
-        get() = _files
-
-    val indexHTML: DocumentFile.PlainText = DocumentFile.PlainText("index.html", indexHTMLBuilder).also { _files.add(it) }
-
-    fun newFile(filename: String, writeToFile: Appendable.() -> Unit) {
-        _files.add(DocumentFile.PlainText(filename, writeToFile))
-    }
-
-    fun addFile(file: DocumentFile.PlainText) {
-        _files.add(file)
-    }
-}
-
 sealed class DocumentFile {
     abstract val fileName: String
     abstract fun writeTo(path: Path)
---
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/template/render/LatexDocument.kt
@@ -0,0 +1,22 @@
+package no.nav.pensjon.brev.template.render
+
+import no.nav.pensjon.brev.latex.LatexAppendable
+
+
+class LatexDocument : Document {
+    private val _files: MutableList<DocumentFile> = mutableListOf()
+    override val files: List<DocumentFile>
+        get() = _files
+
+    private fun newPlainTextFile(fileName: String, writeToFile: Appendable.() -> Unit) =
+        _files.add(DocumentFile.PlainText(fileName, writeToFile))
+
+    fun newLatexFile(fileName: String, writeToFile: LatexAppendable.() -> Unit) =
+        newPlainTextFile(fileName) {
+            LatexAppendable(this).apply(writeToFile)
+        }
+
+    fun addFiles(newFiles: List<DocumentFile>) {
+        _files.addAll(newFiles)
+    }
+}
