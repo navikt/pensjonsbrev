9e62f1c34b234d4b264b5ae4864eeb6a0841c39c
Bian <paomoo@users.noreply.github.com>
2025-01-22 09:57:39 +0100

    PL-7022: legge til begregning virkingFom for belop (#1068)
    * PL-7022: legge til begregning virkingFom for belop
    
    * PL-7022: legge til begregning virkingFom for belop
    
    * PL-7022: fikse format til mndAr.
    
    * Pl-7022:merge fra main og fix navn
    
    * Pl-7022:fjern gammel metode fra etterlate , legger til ny metode for format mnd og år.
    
    * Pl-7022: legge til unit test for formatering av måned og år.

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/fraser/TBU1121_Generated.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/fraser/TBU1121_Generated.kt
@@ -1,6 +1,7 @@
 package no.nav.pensjon.brev.maler.legacy.fraser
 
 import no.nav.pensjon.brev.api.model.maler.legacy.PE
+import no.nav.pensjon.brev.maler.legacy.vedtaksdata_beregningsdata_beregningufore_beregningvirkningdatofom
 import no.nav.pensjon.brev.maler.legacy.vedtaksdata_beregningsdata_beregningufore_totalnetto
 import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.template.Expression
@@ -9,9 +10,10 @@ import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.OutlinePhrase
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
 import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.formatMonthYear
 import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.dsl.textExpr
-
 
 data class TBU1121_Generated(
 	val pe: Expression<PE>,
@@ -21,9 +23,21 @@ data class TBU1121_Generated(
 
 		paragraph {
 			textExpr (
-				Bokmal to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og barnetillegg per måned før skatt.",
-				Nynorsk to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og barnetillegg per månad før skatt.",
-				English to "Your monthly disability benefit and child supplement payment will be NOK ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " before tax.",
+				Bokmal to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og barnetillegg per måned før skatt",
+				Nynorsk to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og barnetillegg per månad før skatt",
+				English to "Your monthly disability benefit and child supplement payment will be NOK ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " before tax",
+			)
+			ifNotNull(pe.vedtaksdata_beregningsdata_beregningufore_beregningvirkningdatofom()) {
+				textExpr (
+					Bokmal to " fra ".expr() + it.formatMonthYear(),
+					Nynorsk to " fra ".expr() + it.formatMonthYear() ,
+					English to " from ".expr() + it.formatMonthYear(),
+				)
+			}
+			text(
+				Bokmal to ".",
+				Nynorsk to ".",
+				English to ".",
 			)
 		}
     }
--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/fraser/TBU1122_Generated.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/fraser/TBU1122_Generated.kt
@@ -1,6 +1,9 @@
+
+
 package no.nav.pensjon.brev.maler.legacy.fraser
 
 import no.nav.pensjon.brev.api.model.maler.legacy.PE
+import no.nav.pensjon.brev.maler.legacy.vedtaksdata_beregningsdata_beregningufore_beregningvirkningdatofom
 import no.nav.pensjon.brev.maler.legacy.vedtaksdata_beregningsdata_beregningufore_totalnetto
 import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.template.Expression
@@ -9,7 +12,9 @@ import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.OutlinePhrase
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
 import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.formatMonthYear
 import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.dsl.textExpr
 
 
@@ -21,9 +26,21 @@ data class TBU1122_Generated(
 
 		paragraph {
 			textExpr (
-				Bokmal to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og gjenlevendetillegg per måned før skatt.",
-				Nynorsk to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og attlevandetillegg per månad før skatt.",
-				English to "Your monthly disability benefit and survivor's supplement payment will be NOK ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " before tax.",
+				Bokmal to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og gjenlevendetillegg per måned før skatt",
+				Nynorsk to "Du får ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " kroner i uføretrygd og attlevandetillegg per månad før skatt",
+				English to "Your monthly disability benefit and survivor's supplement payment will be NOK ".expr() + pe.vedtaksdata_beregningsdata_beregningufore_totalnetto().format() + " before tax",
+			)
+			ifNotNull(pe.vedtaksdata_beregningsdata_beregningufore_beregningvirkningdatofom()) {
+				textExpr (
+					Bokmal to " fra ".expr() + it.formatMonthYear(),
+					Nynorsk to " fra ".expr() + it.formatMonthYear() ,
+					English to " from ".expr() + it.formatMonthYear(),
+				)
+			}
+			text(
+				Bokmal to ".",
+				Nynorsk to ".",
+				English to ".",
 			)
 		}
     }
