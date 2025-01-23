3f1d4fa858223b26d9320596dd076d36b87f628c
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-01-23 11:06:18 +0100

    Legg til brevkode i logg mdc

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/routing/LetterRoutes.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/routing/LetterRoutes.kt
@@ -1,7 +1,9 @@
 package no.nav.pensjon.brev.routing
 
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.AttributeKey
 import no.nav.pensjon.brev.api.TemplateResource
 import no.nav.pensjon.brev.api.model.BestillBrevRequest
 import no.nav.pensjon.brev.api.model.BestillRedigertBrevRequest
@@ -9,12 +11,19 @@ import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.template.AutobrevTemplate
 import no.nav.pensjon.brev.template.RedigerbarTemplate
 
+private val BREV_KODE = AttributeKey<String>("CallStartTime")
+fun <T : Brevkode<T>> RoutingContext.installBrevkodeInCallContext(kode: Brevkode<T>) {
+    call.attributes.put(BREV_KODE, kode.kode())
+}
+fun ApplicationCall.useBrevkodeFromCallContext(): String? = attributes.getOrNull(BREV_KODE)
+
 fun Route.letterRoutes(
     autobrev: TemplateResource<Brevkode.Automatisk, AutobrevTemplate<*>>,
     redigerbareBrev: TemplateResource<Brevkode.Redigerbart, RedigerbarTemplate<*>>,
 ) {
     route("/${autobrev.name}") {
         post<BestillBrevRequest<Brevkode.Automatisk>>("/pdf") { brevbestilling ->
+            installBrevkodeInCallContext(brevbestilling.kode)
             call.respond(autobrev.renderPDF(brevbestilling))
             autobrev.countLetter(brevbestilling.kode)
         }
@@ -34,6 +43,7 @@ fun Route.letterRoutes(
             call.respond(markup)
         }
         post<BestillRedigertBrevRequest<Brevkode.Redigerbart>>("/pdf") { brevbestilling ->
+            installBrevkodeInCallContext(brevbestilling.kode)
             call.respond(redigerbareBrev.renderPDF(brevbestilling))
             redigerbareBrev.countLetter(brevbestilling.kode)
         }
