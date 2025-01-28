cb2d9c67242f44dbf10d83ef58522c4284be681f
Mads Opheim <mads.opheim@nav.no>
2025-01-28 12:57:10 +0100

    TODO

--- pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/LatexDocument.kt
+++ pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/LatexDocument.kt
@@ -3,7 +3,7 @@ package no.nav.pensjon.brev.pdfbygger
 import no.nav.pensjon.brev.template.render.Document
 import no.nav.pensjon.brev.template.render.DocumentFile
 
-
+// TODO: Fas ut denne til fordel for noko enklare
 class LatexDocument : Document {
     private val _files: MutableList<DocumentFile> = mutableListOf()
     override val files: List<DocumentFile>
