13b6428296d047aebf37e6349feaf54132e3c071
Mads Opheim <mads.opheim@nav.no>
2025-01-28 13:07:36 +0100

    Fjern unødig publish

--- brevbaker-dsl/build.gradle.kts
+++ brevbaker-dsl/build.gradle.kts
@@ -7,7 +7,6 @@ val templateModelGeneratorVersion: String by project
 
 plugins {
     kotlin("jvm")
-    `maven-publish`
     id("com.google.devtools.ksp")
 }
 
@@ -33,24 +32,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("com.natpryce:hamkrest:$hamkrestVersion")
     testImplementation(kotlin("reflect"))
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/navikt/pensjonsbrev")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
-        }
-    }
 }
 
 kotlin {
