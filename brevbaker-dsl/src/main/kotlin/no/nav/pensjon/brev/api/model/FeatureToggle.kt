fe0724c8eb81411134493c2487da84b93c71fbb8
Mads Opheim <mads.opheim@nav.no>
2025-02-04 13:23:34 +0100

    Kastar eksplisitt feil viss featuretoggling ikkje er initialisert

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/api/model/FeatureToggle.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/api/model/FeatureToggle.kt
@@ -9,5 +9,10 @@ object FeatureToggleSingleton {
         this.featureToggleService = featureToggleService
     }
 
-    fun isEnabled(toggle: FeatureToggle) = featureToggleService.isEnabled(toggle)
+    fun isEnabled(toggle: FeatureToggle): Boolean {
+        if (!::featureToggleService.isInitialized) {
+            throw IllegalStateException("Du må sette opp en FeatureToggleService med FeatureToggleSingleton::init for å kunne bruke feature toggles.")
+        }
+        return featureToggleService.isEnabled(toggle)
+    }
 }
