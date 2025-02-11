f75c8a98186b4b2a3e4d10bf9fa0ed0d5025c2ec
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-02-11 13:33:43 +0100

    fiks: cursorPosition skal nå være på riktig plass etter paste inn i en literal

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/actions/paste.ts
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/actions/paste.ts
@@ -1,6 +1,6 @@
 import type { Draft } from "immer";
 import { produce } from "immer";
-import { first } from "lodash";
+import { isEqual } from "lodash";
 
 import { newItem, newItemList, newLiteral, newParagraph, text } from "~/Brevredigering/LetterEditor/actions/common";
 import type { LiteralIndex } from "~/Brevredigering/LetterEditor/actions/model";
@@ -34,7 +34,6 @@ export function logPastedClipboard(clipboardData: DataTransfer) {
 function insertTextInLetter(draft: Draft<LetterEditorState>, literalIndex: LiteralIndex, offset: number, str: string) {
   const content = draft.redigertBrev.blocks[literalIndex.blockIndex]?.content[literalIndex.contentIndex];
   const cursorPositionUpdater = (l: number) => (draft.focus.cursorPosition = l);
-  // const cursorPositionUpdater = (l: number) => void 0;
 
   if (content?.type === ITEM_LIST && "itemContentIndex" in literalIndex) {
     const itemContent = content.items[literalIndex.itemIndex]?.content[literalIndex?.itemContentIndex];
@@ -70,6 +69,7 @@ function insertText(
     cursorPositionUpdater(text.length);
   }
 }
+
 function insertHtmlClipboardInLetter(
   draft: Draft<LetterEditorState>,
   literalIndex: LiteralIndex,
@@ -102,55 +102,84 @@ function insertHtmlClipboardInLetter(
       const firstCombinedElement = parsedAndCombinedHtml[0];
 
       if (appendingToStartOfLiteral) {
-        /**
-         * hvis firstCombinedElement er span, så pluses dette sammen med literalToBePastedIn
-         * hvis firstCombinedElement er p eller li, så lages det en ny paragraph med dette som første element, og literalToBePastedIn som en ny blokk
-         */
-
         if (firstCombinedElement.tag === "SPAN") {
+          const combinedSpanText = firstCombinedElement.content.join(" ");
+
           const newLiteralToBePastedIn = newLiteral({
             ...literalToBePastedInto,
-            text:
-              firstCombinedElement.content.join(" ") + (literalToBePastedInto.editedText ?? literalToBePastedInto.text),
+            text: combinedSpanText + (literalToBePastedInto.editedText ?? literalToBePastedInto.text),
           });
 
           const newContent = [...contentBeforeLiteral, newLiteralToBePastedIn, ...contentAfterLiteral];
           thisBlock.content = newContent;
+          draft.focus = {
+            ...draft.focus,
+            cursorPosition: combinedSpanText.length,
+          };
         } else {
           const newBeforeBlock = newParagraph({ content: contentBeforeLiteral });
-          const newThisBlock = mapCmbIlToBrevbakerTypes(parsedAndCombinedHtml);
+          const newThisBlock = traversedElementsToBrevbaker(parsedAndCombinedHtml);
+          const newNextBlock = newParagraph({ content: [literalToBePastedInto] });
           const newContentAfterLiteralBlock = newParagraph({ content: contentAfterLiteral });
+
           const newBlocks = [
             ...blocksBeforeThisBlock,
             newBeforeBlock.content.length > 0 ? newBeforeBlock : [],
             ...newThisBlock,
-            newParagraph({ content: [literalToBePastedInto] }),
+            newNextBlock,
             newContentAfterLiteralBlock.content.length > 0 ? newContentAfterLiteralBlock : [],
             ...blocksAfterThisBlock,
           ].flat();
 
+          const newBlockPosition = newBlocks.findIndex((b) => isEqual(b, newNextBlock));
+          const newContentPosition = Math.max(newNextBlock.content.length - 1, 0);
+
+          const lastEl = newNextBlock.content.at(-1);
+          const newCursorPosition =
+            //TODO - eslint og prettier clasher litt med syntax preferanse. Burde uansett gjøre denne cleanere
+            /* eslint-disable prettier/prettier */
+            (lastEl && lastEl.type === "ITEM_LIST"
+              ? lastEl.items.at(-1)?.content.at(-1)?.text.length
+              : (lastEl && "editedText" in lastEl
+                ? lastEl.editedText?.length
+                : lastEl?.text.length)) ?? 0;
+          /* eslint-enable prettier/prettier */
+
           draft.redigertBrev.blocks = newBlocks;
+          draft.focus = {
+            blockIndex: newBlockPosition,
+            contentIndex: newContentPosition,
+            cursorPosition: newCursorPosition,
+          };
         }
       } else if (appendingToMiddleOfLiteral) {
         const textBeforeOffset = (literalToBePastedInto.editedText ?? literalToBePastedInto.text).slice(0, offset);
         const textAfterOffset = (literalToBePastedInto.editedText ?? literalToBePastedInto.text).slice(offset);
 
         if (firstCombinedElement.tag === "SPAN") {
+          const combinedSpanText = firstCombinedElement.content.join(" ");
           const newLiteralToBePastedIn = newLiteral({
             ...literalToBePastedInto,
             text:
               textBeforeOffset +
-              (firstCombinedElement.content.length > 1
-                ? firstCombinedElement.content.join(" ")
-                : ` ${firstCombinedElement.content[0]}`) +
+              (firstCombinedElement.content.length > 1 ? combinedSpanText : ` ${firstCombinedElement.content[0]}`) +
               textAfterOffset,
           });
 
           const newContent = [...contentBeforeLiteral, newLiteralToBePastedIn, ...contentAfterLiteral];
+
+          const cursorPosition =
+            textBeforeOffset +
+            (firstCombinedElement.content.length > 1 ? combinedSpanText : ` ${firstCombinedElement.content[0]}`);
           thisBlock.content = newContent;
+
+          draft.focus = {
+            ...draft.focus,
+            cursorPosition: cursorPosition.length,
+          };
         } else {
           const newBeforeBlock = newParagraph({ content: contentBeforeLiteral });
-          const mappedToBrevbaker = mapCmbIlToBrevbakerTypes(parsedAndCombinedHtml);
+          const mappedToBrevbaker = traversedElementsToBrevbaker(parsedAndCombinedHtml);
           const firstMapped = mappedToBrevbaker[0];
           const restMapped = mappedToBrevbaker.slice(1);
 
@@ -186,88 +215,81 @@ function insertHtmlClipboardInLetter(
           const newBlocksAfterThisBlock = restMapped;
 
           const newContentAfterLiteralBlock = newParagraph({ content: contentAfterLiteral });
+          const restAfterBlock = newParagraph({ content: [newLiteral({ text: textAfterOffset })] });
 
           const newBlocks = [
             ...blocksBeforeThisBlock,
             newBeforeBlock.content.length > 0 ? newBeforeBlock : [],
             newThisBlock,
-            newBlocksAfterThisBlock,
-            newParagraph({ content: [newLiteral({ text: textAfterOffset })] }),
+            ...newBlocksAfterThisBlock,
+            restAfterBlock,
             newContentAfterLiteralBlock.content.length > 0 ? newContentAfterLiteralBlock : [],
             ...blocksAfterThisBlock,
           ].flat();
 
           draft.redigertBrev.blocks = newBlocks;
+
+          const newBlockPosition = newBlocks.findIndex((b) => isEqual(b, restAfterBlock));
+          const newContentPosition = Math.max((newBlocksAfterThisBlock.at(-1)?.content?.length ?? 0) - 1, 0);
+
+          draft.focus = {
+            blockIndex: newBlockPosition,
+            contentIndex: newContentPosition,
+            cursorPosition: 0,
+          };
         }
       } else {
         if (firstCombinedElement.tag === "SPAN") {
+          const combinedSpanText = firstCombinedElement.content.join(" ");
           const newLiteralToBePastedIn = newLiteral({
             ...literalToBePastedInto,
-            text:
-              (literalToBePastedInto.editedText ?? literalToBePastedInto.text) + firstCombinedElement.content.join(" "),
+            text: (literalToBePastedInto.editedText ?? literalToBePastedInto.text) + combinedSpanText,
           });
+          const newCursorPosition = (
+            (literalToBePastedInto.editedText ?? literalToBePastedInto.text) + combinedSpanText
+          ).length;
 
           const newContent = [...contentBeforeLiteral, newLiteralToBePastedIn, ...contentAfterLiteral];
           thisBlock.content = newContent;
+          draft.focus = {
+            ...draft.focus,
+            cursorPosition: newCursorPosition,
+          };
         } else {
-          const newBeforeBlock = newParagraph({ content: contentBeforeLiteral });
-          const newThisBlock = mapCmbIlToBrevbakerTypes(parsedAndCombinedHtml);
-          const newContentAfterLiteralBlock = newParagraph({ content: contentAfterLiteral });
-          const newBlocks = [
-            ...blocksBeforeThisBlock,
-            newBeforeBlock.content.length > 0 ? newBeforeBlock : [],
-            ...newThisBlock,
-            newParagraph({ content: [literalToBePastedInto] }),
-            newContentAfterLiteralBlock.content.length > 0 ? newContentAfterLiteralBlock : [],
-            ...blocksAfterThisBlock,
-          ].flat();
-
-          draft.redigertBrev.blocks = newBlocks;
-        }
-        if (firstCombinedElement.tag === "SPAN") {
-          const newLiteralToBePastedIn = newLiteral({
-            ...literalToBePastedInto,
-            text:
-              (literalToBePastedInto.editedText ?? literalToBePastedInto.text) + firstCombinedElement.content.join(" "),
-          });
-
-          const newContent = [...contentBeforeLiteral, newLiteralToBePastedIn, ...contentAfterLiteral];
-          thisBlock.content = newContent;
-        } else {
-          const mappedToBrevbaker = mapCmbIlToBrevbakerTypes(parsedAndCombinedHtml);
+          const mappedToBrevbaker = traversedElementsToBrevbaker(parsedAndCombinedHtml);
           const firstMapped = mappedToBrevbaker[0];
           const restMapped = mappedToBrevbaker.slice(1);
 
+          const theNewLiteral =
+            firstMapped.content[0].type === "LITERAL"
+              ? newLiteral({
+                  ...literalToBePastedInto,
+                  text:
+                    (literalToBePastedInto.editedText ?? literalToBePastedInto.text) +
+                    (firstMapped.content[0] as LiteralValue).text,
+                })
+              : newLiteral({
+                  text:
+                    (literalToBePastedInto.editedText ?? literalToBePastedInto.text) +
+                    (firstMapped.content[0] as ItemList).items[0].content.map((c) => c.text).join(" "),
+                });
+
+          const newLiteralContent =
+            firstMapped.content[0].type === "LITERAL"
+              ? [theNewLiteral, ...(firstMapped.content.length > 1 ? firstMapped.content.slice(1) : [])]
+              : [
+                  newItemList({
+                    items: [
+                      newItem({
+                        content: [theNewLiteral],
+                      }),
+                      ...(firstMapped.content[0] as ItemList).items.slice(1),
+                    ],
+                  }),
+                ];
+
           const newThisBlock = newParagraph({
-            content: [
-              contentBeforeLiteral,
-              firstMapped.content[0].type === "LITERAL"
-                ? [
-                    newLiteral({
-                      ...literalToBePastedInto,
-                      text:
-                        (literalToBePastedInto.editedText ?? literalToBePastedInto.text) +
-                        (firstMapped.content[0] as LiteralValue).text,
-                    }),
-                    ...(firstMapped.content.length > 1 ? firstMapped.content.slice(1) : []),
-                  ]
-                : [
-                    newItemList({
-                      items: [
-                        newItem({
-                          content: [
-                            newLiteral({
-                              text:
-                                (literalToBePastedInto.editedText ?? literalToBePastedInto.text) +
-                                (firstMapped.content[0] as ItemList).items[0].content.map((c) => c.text).join(" "),
-                            }),
-                          ],
-                        }),
-                        ...(firstMapped.content[0] as ItemList).items.slice(1),
-                      ],
-                    }),
-                  ],
-            ].flat(),
+            content: [contentBeforeLiteral, newLiteralContent].flat(),
           });
 
           const newBlocksAfterThisBlock = restMapped;
@@ -280,7 +302,36 @@ function insertHtmlClipboardInLetter(
             ...blocksAfterThisBlock,
           ].flat();
 
+          const newBlockPosition = newBlocks.findIndex((b) =>
+            isEqual(b, newBlocksAfterThisBlock.length > 0 ? newBlocksAfterThisBlock.at(-1) : newThisBlock),
+          );
+          const newContentPosition = Math.max(
+            (newBlocksAfterThisBlock.length > 0
+              ? (newBlocksAfterThisBlock.at(-1)?.content.length ?? 0)
+              : (newThisBlock.content.length ?? 0)) - 1,
+            0,
+          );
+
+          const blockToGetCursorPositionFrom =
+            newBlocksAfterThisBlock.length > 0 ? (newBlocksAfterThisBlock.at(-1) ?? newThisBlock) : newThisBlock;
+          const lastContent = blockToGetCursorPositionFrom.content.at(-1) ?? newThisBlock.content.at(-1);
+
+          const newCursorPosition =
+            lastContent?.type === "LITERAL"
+              ? lastContent.text.length
+              : ((lastContent && (lastContent as ItemList).items.at(-1)?.content.at(-1)?.text.length) ?? 0);
+
+          const itemContentIndex = lastContent?.type === "ITEM_LIST" ? 0 : undefined;
+          const itemIndex = lastContent?.type === "ITEM_LIST" ? lastContent.items.length - 1 : undefined;
+
           draft.redigertBrev.blocks = newBlocks;
+          draft.focus = {
+            blockIndex: newBlockPosition,
+            contentIndex: newContentPosition,
+            itemContentIndex: itemContentIndex,
+            itemIndex: itemIndex,
+            cursorPosition: newCursorPosition,
+          };
         }
       }
     },
@@ -301,11 +352,11 @@ interface TraversedElement {
   content: (TraversedElement | string)[];
 }
 
-const trav = (el: Element): TraversedElement[] | TraversedElement => {
+const traverseElement = (el: Element): TraversedElement[] | TraversedElement => {
   switch (el.tagName) {
     case "LI": {
       if (el.children.length > 0) {
-        const content = [...el.children].flatMap(trav).filter((el) => el.content !== null);
+        const content = [...el.children].flatMap(traverseElement).filter((el) => el.content !== null);
 
         const mapped = content.flatMap((el) => {
           if (typeof el === "string") {
@@ -322,7 +373,7 @@ const trav = (el: Element): TraversedElement[] | TraversedElement => {
     }
     case "P": {
       if (el.children.length > 0) {
-        const content = [...el.children].flatMap(trav).flatMap((el) => el.content);
+        const content = [...el.children].flatMap(traverseElement).flatMap((el) => el.content);
 
         //dersom content er tom, betyr det at den bar inneholdt en tom span, som skal være linjeskift
         return { tag: "P", content: content.length > 0 ? content : [" "] };
@@ -334,7 +385,7 @@ const trav = (el: Element): TraversedElement[] | TraversedElement => {
 
     case "SPAN": {
       if (el.children.length > 0) {
-        const content = [...el.children].flatMap(trav).flatMap((el) => el.content);
+        const content = [...el.children].flatMap(traverseElement).flatMap((el) => el.content);
         return { tag: "SPAN", content: content };
       }
       const isEmptyOrNonExisting = el.textContent?.trim() === "" || !el.textContent;
@@ -342,10 +393,10 @@ const trav = (el: Element): TraversedElement[] | TraversedElement => {
     }
 
     case "DIV": {
-      return [...el.children].flatMap(trav);
+      return [...el.children].flatMap(traverseElement);
     }
     default: {
-      return [...el.children].flatMap(trav);
+      return [...el.children].flatMap(traverseElement);
     }
   }
 };
@@ -364,8 +415,8 @@ function mergeNeighboringTags(blocks: TraversedElement[]): TraversedElement[] {
   }, []);
 }
 
-const mapCmbIlToBrevbakerTypes = (cmbIL: TraversedElement[]) =>
-  cmbIL.reduce<ParagraphBlock[]>((acc, curr) => {
+const traversedElementsToBrevbaker = (t: TraversedElement[]) =>
+  t.reduce<ParagraphBlock[]>((acc, curr) => {
     const lastParagraph = acc.at(-1) ?? null;
     const isLastParagraphLineBreak =
       lastParagraph?.content.length === 1 &&
@@ -376,7 +427,7 @@ const mapCmbIlToBrevbakerTypes = (cmbIL: TraversedElement[]) =>
       case "LI": {
         const itemList = newItemList({
           items: curr.content.map((item) => {
-            //vi forventer at ItemLists sine items er kun ren tekst strenger
+            //vi forventer at ItemLists sine items er kun rene tekst strenger
             if (typeof item !== "string") {
               throw new TypeError("item is not a string");
             }
@@ -407,7 +458,7 @@ const mapCmbIlToBrevbakerTypes = (cmbIL: TraversedElement[]) =>
   }, []);
 
 const parseAndCombineHTML = (bodyElement: HTMLElement) => {
-  const result = [...bodyElement.children].flatMap(trav);
+  const result = [...bodyElement.children].flatMap(traverseElement);
   console.log("-----result--------");
   console.dir(result, { depth: null });
   console.log("-----------------");
