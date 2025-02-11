50a0b092f50a1781b69d4db04709aa3ed78b9f6b
Mads Opheim <mads.opheim@nav.no>
2025-02-11 10:30:32 +0100

    Eksplisitt handtering av nøkkel for featuretoggle, så får vi enums med rett stil, og det er vel også litt meir eksplisitt

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/FeatureToggles.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/FeatureToggles.kt
@@ -2,10 +2,10 @@ package no.nav.pensjon.brev.api.model.maler
 
 import no.nav.pensjon.brev.api.model.FeatureToggle
 
-enum class FeatureToggles : FeatureToggle {
-    brevMedFritekst,
-    brevmal_ut_avslag,
+enum class FeatureToggles(private val key: String) : FeatureToggle {
+    BREV_MED_FRITEKST("brevMedFritekst"),
+    BREVMAL_UT_AVSLAG("brevmal_ut_avslag"),
     ;
 
-    override fun key() = name
+    override fun key() = key
 }
