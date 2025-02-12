f18aa01e4be8d57c4837e73b3b8de0aaac299873
Mads Opheim <mads.opheim@nav.no>
2025-02-12 14:56:30 +0100

    Verken etterlatte eller skribenten bør trengje avhengnad til pensjon-brevbaker-api-model, som er dto-ar og sånt for pesys

--- skribenten-backend/build.gradle.kts
+++ skribenten-backend/build.gradle.kts
@@ -1,7 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion: String by project
 val exposedVersion: String by project
 val jacksonJsr310Version: String by project
 val javaTarget: String by System.getProperties()
