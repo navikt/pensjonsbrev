5e2f012dda32047ce8afa86d4b03c3d657039441
Mads Opheim <mads.opheim@nav.no>
2025-02-12 14:58:26 +0100

    No er det berre pensjonsmaler-modulen som er avhengig av apimodellen, så da kan vi flytte versjonsnummeret direkte dit. Dermed treng ikkje uføre- (og snart alder-)teamet å ha tilgang til gradle.properties, kor vi jo gjer også andre endringar

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion: String by project
+val apiModelVersion = 121
 val apiModelJavaTarget: String by System.getProperties()
 val jupiterVersion: String by project
 
