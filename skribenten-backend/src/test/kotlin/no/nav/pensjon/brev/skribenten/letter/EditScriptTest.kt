2335b109362ee8fe185162a14a5951cb2530e7e5
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-02-14 14:57:53 +0100

    Refaktorering WIP

--- skribenten-backend/src/test/kotlin/no/nav/pensjon/brev/skribenten/letter/EditScriptTest.kt
+++ skribenten-backend/src/test/kotlin/no/nav/pensjon/brev/skribenten/letter/EditScriptTest.kt
@@ -9,16 +9,51 @@ import org.junit.jupiter.api.Test
 class EditScriptTest {
 
     val a = editedLetter(
-        Paragraph(1, true, listOf(Literal(1, "noe tekst "), Variable(2, " og noe mera")))
+        Paragraph(2, true, listOf(Literal(1, "noe tekst "), Variable(2, " og noe mera")))
     )
     val b = editedLetter(
-        Paragraph(1, true, listOf(Literal(null, "jadda"))),
+        Paragraph(1, true, listOf(Literal(1, "jadda"))),
         Paragraph(2, true, listOf(Literal(1, "noe tekst "), Variable(2, " og noe merb")))
     )
 
     @Test
     fun testDiff3() {
-        println(Diff3.shortestEditScript(a.editables, b.editables))
+        shortestEditScript(a.editables, b.editables).forEach { println(it) }
     }
 
+    @Test
+    fun applyEditScript() {
+        val editscript = shortestEditScript(a.editables, b.editables)
+    }
+
+    fun List<EditOperation.Insert<Editable>>.reduceEditScript() {
+        val queue = toMutableList()
+
+        if (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+            when (current.insert) {
+                is Editable.Block -> newBlock(current.insert, queue)
+                is Editable.ContentFont -> TODO()
+                is Editable.ContentId -> TODO()
+                is Editable.ContentText -> TODO()
+                is Editable.BlockId -> TODO()
+//                is Editable.BlockStart -> TODO()
+                is Editable.BlockType -> TODO()
+            }
+        }
+    }
+
+    fun newBlock(block: Editable.Block, remaining: MutableList<EditOperation.Insert<Editable>>) {
+        while (remaining.isNotEmpty() && remaining.first().insert.index.block == block.index.block) {
+            val current = remaining.removeFirst()
+            when (current.insert) {
+                is Editable.ContentId -> TODO()
+                is Editable.ContentFont -> TODO()
+                is Editable.ContentText -> TODO()
+                is Editable.Block -> TODO()
+                is Editable.BlockId -> TODO()
+                is Editable.BlockType -> TODO()
+            }
+        }
+    }
 }
