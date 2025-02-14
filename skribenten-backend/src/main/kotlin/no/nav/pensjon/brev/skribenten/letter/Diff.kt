48b655abd87a9e1be057bbe8e50274d80a514d81
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-02-14 14:57:53 +0100

    Refaktorering WIP

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/letter/Diff.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/letter/Diff.kt
@@ -7,23 +7,68 @@ import no.nav.pensjon.brev.skribenten.letter.Editable.*
 
 
 sealed class Editable {
-    data class BlockId(val index: Int, val id: Int?) : Editable() {
+    abstract val index: BlockIndex
+
+    interface BlockIndex { val block: Int }
+    interface ContentIndex : BlockIndex { val content: Int }
+    interface CharIndex : ContentIndex { val offset: Int }
+
+    object Index {
+        data class Block(override val block: Int) : BlockIndex
+        data class Content(override val block: Int, override val content: Int) : ContentIndex
+        data class Char(override val block: Int, override val content: Int, override val offset: Int) : CharIndex
+    }
+
+    data class Block(override val index: BlockIndex, val id: Int?, val type: Block.Type) : Editable() {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (javaClass != other?.javaClass) return false
 
-            other as BlockId
+            other as Block
 
-            return id == other.id
+            if (id != other.id) return false
+            if (type != other.type) return false
+
+            return true
         }
 
         override fun hashCode(): Int {
-            return id ?: 0
+            var result = id ?: 0
+            result = 31 * result + type.hashCode()
+            return result
         }
     }
 
-    data class BlockType(val type: Block.Type) : Editable()
-    data class ContentId(val blockIndex: Int, val contentIndex: Int, val id: Int?) : Editable() {
+//    data class BlockStart(override val index: BlockIndex) : Editable() {
+//        override fun equals(other: Any?): Boolean = javaClass == other?.javaClass
+//        override fun hashCode(): Int = javaClass.hashCode()
+//    }
+    @ConsistentCopyVisibility
+    data class BlockId private constructor(val id: Int?) : Editable() {
+        override lateinit var index: BlockIndex
+            private set
+
+        constructor(index: BlockIndex, id: Int?) : this(id) {
+            this.index = index
+        }
+    }
+
+    data class BlockType(override val index: BlockIndex, val type: Block.Type) : Editable() {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as BlockType
+
+            return type == other.type
+        }
+
+        override fun hashCode(): Int {
+            return type.hashCode()
+        }
+    }
+
+    data class ContentId(override val index: ContentIndex, val id: Int?) : Editable() {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (javaClass != other?.javaClass) return false
@@ -38,8 +83,22 @@ sealed class Editable {
         }
     }
 
-    data class ContentFont(val fontType: FontType) : Editable()
-    data class ContentText(val blockIndex: Int, val contentIndex: Int, val char: Char) : Editable() {
+    data class ContentFont(override val index: ContentIndex, val fontType: FontType) : Editable() {
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as ContentFont
+
+            return fontType == other.fontType
+        }
+
+        override fun hashCode(): Int {
+            return fontType.hashCode()
+        }
+    }
+
+    data class ContentText(override val index: CharIndex, val char: Char) : Editable() {
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
             if (javaClass != other?.javaClass) return false
@@ -61,8 +120,10 @@ val Edit.Letter.editables: Sequence<Editable>
         var contentIndex = 0
 
         override suspend fun SequenceScope<Editable>.visit(block: Block) {
-            yield(BlockId(index = blockIndex, id = block.id))
-            yield(BlockType(block.type))
+//            yield(BlockStart(Index.Block(blockIndex)))
+//            yield(BlockId(index = Index.Block(blockIndex), id = block.id))
+//            yield(BlockType(Index.Block(blockIndex), block.type))
+            yield(Block(Index.Block(blockIndex), block.id, block.type))
             block.content.forEach {
                 visit(it)
                 contentIndex++
@@ -72,14 +133,19 @@ val Edit.Letter.editables: Sequence<Editable>
         }
 
         override suspend fun SequenceScope<Editable>.visit(content: Variable) {
-            yield(ContentId(blockIndex = blockIndex, contentIndex = contentIndex, id = content.id))
-            yield(ContentFont(content.fontType))
-            content.text.forEach { yield(ContentText(blockIndex = blockIndex, contentIndex = contentIndex, char = it)) }
+            yield(ContentId(Index.Content(block = blockIndex, content = contentIndex), id = content.id))
+            yield(ContentFont(Index.Content(block = blockIndex, content = contentIndex), content.fontType))
+            yieldTextEditables(blockIndex, contentIndex, content.text)
         }
 
         override suspend fun SequenceScope<Editable>.visit(content: Edit.ParagraphContent.Text.Literal) {
-            yield(ContentId(blockIndex = blockIndex, contentIndex = contentIndex, id = content.id))
-            yield(ContentFont(content.fontType))
-            content.text.forEach { yield(ContentText(blockIndex = blockIndex, contentIndex = contentIndex, char = it)) }
+            yield(ContentId(Index.Content(block = blockIndex, content = contentIndex), id = content.id))
+            yield(ContentFont(Index.Content(block = blockIndex, content = contentIndex), content.fontType))
+            yieldTextEditables(blockIndex, contentIndex, content.text)
         }
+
+        private suspend fun SequenceScope<Editable>.yieldTextEditables(blockIndex: Int, contentIndex: Int, text: String) =
+            text.forEachIndexed {  offset, char -> yield(ContentText(Index.Char(block = blockIndex, content = contentIndex, offset = offset), char = char)) }
+
     }.build(this)
+
--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/letter/Diff3.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/letter/Diff3.kt
@@ -1,71 +1,109 @@
 package no.nav.pensjon.brev.skribenten.letter
 
-import no.nav.pensjon.brev.skribenten.letter.Diff3.Coords.Companion.origin
+import no.nav.pensjon.brev.skribenten.letter.Coords.Companion.origin
 import no.nav.pensjon.brev.skribenten.letter.EditOperation.Delete
 import no.nav.pensjon.brev.skribenten.letter.EditOperation.Insert
+
+/**
+ * Find the shortest edit script of insert and delete operations to transform
+ * old into new.
+ *
+ * Uses an implementation of Eugene W. Myers Difference Algorithm, specifically
+ * the linear space refinement from section 4b.
+ *
+ * @param T must implement an equals-method
+ */
+fun <T : Any> shortestEditScript(old: Sequence<T>, new: Sequence<T>): List<EditOperation<T>> =
+    MyersDiff(old.toList(), new.toList()).shortestEditScript()
 
 sealed class EditOperation<T : Any> {
     data class Insert<T : Any>(val insert: T) : EditOperation<T>()
     data class Delete<T : Any>(val delete: T) : EditOperation<T>()
 }
 
-object Diff3 {
+private class Vectors private constructor(private val negativeOffset: Int, private val array: IntArray) {
+    constructor(n: Int, m: Int, initialVector: Int) : this(m + n, IntArray((m + n) * 2 + 1).also { it[1 + n + m] = initialVector })
 
-    fun <T : Any> shortestEditScript(old: Sequence<T>, new: Sequence<T>): List<EditOperation<T>> =
-        shortestEditScript(Diff(old.toList(), new.toList()))
+    operator fun get(i: Int) = array[i + negativeOffset]
+    operator fun set(i: Int, v: Int) {
+        array[i + negativeOffset] = v
+    }
+}
+private data class Path(val from: Coords, val to: Coords)
+private data class Coords(val x: Int, val y: Int) {
+    companion object {
+        val origin = Coords(0, 0)
+    }
+}
+private data class MiddleSnake(val d: Int, val from: Coords, val to: Coords)
 
-    private fun <T : Any> shortestEditScript(diff: Diff<T>): List<EditOperation<T>> {
-        return if (diff.n > 0 && diff.m > 0) {
-            val snake = findMiddleSnake(diff)
+private class MyersDiff<T : Any>(val first: List<T>, val second: List<T>) {
+    val n get() = first.size
+    val m get() = second.size
+    val size = n + m
+    val delta = n - m
+    val deltaIsOdd = (delta % 2) != 0
+    val halfway = size / 2 + (if (deltaIsOdd) 1 else 0)
+    val endCoords get() = Coords(n, m)
 
-            if (snake.d > 1 || (snake.from != snake.to)) {
-                shortestEditScript(diff.slice(from = origin, to = snake.from)) +
-                        shortestEditScript(diff.slice(from = snake.to, to = diff.endCoords))
-            } else if (diff.m > diff.n) {
-                shortestEditScript(diff.slice(from = Coords(x = diff.n, y = diff.n), to = Coords(x = diff.n, y = diff.m)))
-            } else {
-                shortestEditScript(diff.slice(from = Coords(x = diff.m, y = diff.m), to = Coords(x = diff.n, y = diff.m)))
-            }
-        } else if (diff.n > 0) {
-            diff.first.map { Delete(it) }
-        } else {
-            diff.second.map { Insert(it) }
+    init {
+        if (first !is RandomAccess) {
+            throw RuntimeException("first er ikke RandomAccess (ArrayList)")
+        }
+        if (second !is RandomAccess) {
+            throw RuntimeException("second er ikke RandomAccess (ArrayList)")
         }
     }
 
-    private fun <T : Any> findMiddleSnake(diff: Diff<T>): MiddleSnake {
-        val vForward = Vectors(n = diff.n, m = diff.m, initialVector = 0)
-        val vReverse = Vectors(n = diff.n, m = diff.m, initialVector = diff.m)
+    fun slice(from: Coords, to: Coords): MyersDiff<T> =
+        MyersDiff(first.slice(from.x..<to.x), second.slice(from.y..<to.y))
 
-        for (d in 0..diff.halfway) {
-            // forward search
-            val forwards = searchForwards(d, vForward, vReverse, diff)
+    fun shortestEditScript(): List<EditOperation<T>> {
+        return if (n > 0 && m > 0) {
+            val snake = findMiddleSnake()
+
+            if (snake.d > 1 || (snake.from != snake.to)) {
+                slice(from = origin, to = snake.from).shortestEditScript() +
+                        slice(from = snake.to, to = endCoords).shortestEditScript()
+            } else if (m > n) {
+                slice(from = Coords(x = n, y = n), to = Coords(x = n, y = m)).shortestEditScript()
+            } else {
+                slice(from = Coords(x = m, y = m), to = Coords(x = n, y = m)).shortestEditScript()
+            }
+        } else if (n > 0) {
+            first.map { Delete(it) }
+        } else {
+            second.map { Insert(it) }
+        }
+    }
+
+    private fun findMiddleSnake(): MiddleSnake {
+        val vForward = Vectors(n = n, m = m, initialVector = 0)
+        val vReverse = Vectors(n = n, m = m, initialVector = m)
+
+        for (d in 0..halfway) {
+            val forwards = searchForwards(d, vForward, vReverse)
             if (forwards != null) {
                 return forwards
             }
 
-            val backwards = searchBackwards(d, vReverse, vForward, diff)
+            val backwards = searchBackwards(d, vReverse, vForward)
             if (backwards != null) {
                 return backwards
             }
         }
 
-        return MiddleSnake(d = diff.n + diff.m, from = origin, Coords(diff.n, diff.m))
+        return MiddleSnake(d = n + m, from = origin, Coords(n, m))
     }
 
     // Searches forwards for a d-path, and optimizing for x (i.e. equal or deletions)
-    private fun <T : Any> searchForwards(
-        d: Int,
-        vForward: Vectors,
-        vReverse: Vectors,
-        diff: Diff<T>,
-    ): MiddleSnake? {
+    private fun searchForwards(d: Int, vForward: Vectors, vReverse: Vectors): MiddleSnake? {
         for (k in -d..d step 2) {
-            val path = findFurthestReachingForwardDpathInDiagonalK(d, k, vForward, diff)
+            val path = findFurthestReachingForwardDpathInDiagonalK(d, k, vForward)
             vForward[k] = path.to.x
 
-            if (diff.deltaIsOdd && k in (diff.delta - (d - 1))..(diff.delta + (d - 1))) {
-                if (path.from.y >= vReverse[k - diff.delta]) {
+            if (deltaIsOdd && k in (delta - (d - 1))..(delta + (d - 1))) {
+                if (path.from.y >= vReverse[k - delta]) {
                     return MiddleSnake(d = 2 * d - 1, from = path.from, to = path.to)
                 }
             }
@@ -73,28 +111,7 @@ object Diff3 {
         return null
     }
 
-    // Searches backwards for a d-path, and optimizing for y (i.e. equal or insertions)
-    private fun <T : Any> searchBackwards(
-        d: Int,
-        vReverse: Vectors,
-        vForward: Vectors,
-        diff: Diff<T>,
-    ): MiddleSnake? {
-        for (k in -d..d step 2) {
-            val path = findFurthestReachingReverseDpathInDiagonalK(d, k, vReverse, diff)
-            vReverse[k] = path.to.y
-
-            if (!diff.deltaIsOdd && (k + diff.delta) in -d..d) {
-                if (path.to.x <= vForward[k]) {
-                    // Since the path is found through a reverse search we have to flip from and to
-                    return MiddleSnake(d = 2 * d, from = path.to, to = path.from)
-                }
-            }
-        }
-        return null
-    }
-
-    private fun <T : Any> findFurthestReachingForwardDpathInDiagonalK(d: Int, k: Int, vForward: Vectors, diff: Diff<T>): Path {
+    private fun findFurthestReachingForwardDpathInDiagonalK(d: Int, k: Int, vForward: Vectors): Path {
         val fromX = if (k == -d || k != d && vForward[k - 1] < vForward[k + 1]) {
             vForward[k + 1]
         } else {
@@ -105,7 +122,7 @@ object Diff3 {
 
         var x = fromX
         var y = fromY
-        while (x < diff.n && y < diff.m && diff.first[x] == diff.second[y]) {
+        while (x < n && y < m && first[x] == second[y]) {
             x++
             y++
         }
@@ -113,17 +130,33 @@ object Diff3 {
         return Path(from = Coords(x = fromX, y = fromY), to = Coords(x = x, y = y))
     }
 
-    private fun <T : Any> findFurthestReachingReverseDpathInDiagonalK(d: Int, k: Int, vReverse: Vectors, diff: Diff<T>): Path {
+    // Searches backwards for a d-path, and optimizing for y (i.e. equal or insertions)
+    private fun searchBackwards(d: Int, vReverse: Vectors, vForward: Vectors): MiddleSnake? {
+        for (k in -d..d step 2) {
+            val path = findFurthestReachingReverseDpathInDiagonalK(d, k, vReverse)
+            vReverse[k] = path.to.y
+
+            if (!deltaIsOdd && (k + delta) in -d..d) {
+                if (path.to.x <= vForward[k]) {
+                    // Since the path is found through a reverse search we have to flip from and to
+                    return MiddleSnake(d = 2 * d, from = path.to, to = path.from)
+                }
+            }
+        }
+        return null
+    }
+
+    private fun findFurthestReachingReverseDpathInDiagonalK(d: Int, k: Int, vReverse: Vectors): Path {
         val fromY = if (k == -d || k != d && vReverse[k - 1] > vReverse[k + 1]) {
             vReverse[k + 1]
         } else {
             vReverse[k - 1] - 1
         }
-        val fromX = fromY + (k + diff.delta)
+        val fromX = fromY + (k + delta)
 
         var x = fromX
         var y = fromY
-        while (x > 0 && y > 0 && diff.first[x - 1] == diff.second[y - 1]) {
+        while (x > 0 && y > 0 && first[x - 1] == second[y - 1]) {
             x--
             y--
         }
@@ -133,44 +166,5 @@ object Diff3 {
             to = Coords(x = x, y = y),
         )
     }
-
-    private data class Path(val from: Coords, val to: Coords)
-    private data class Coords(val x: Int, val y: Int) {
-        companion object {
-            val origin = Coords(0, 0)
-        }
-    }
-    private data class MiddleSnake(val d: Int, val from: Coords, val to: Coords)
-
-    private class Diff<T : Any>(val first: List<T>, val second: List<T>) {
-        val n get() = first.size
-        val m get() = second.size
-        val size = n + m
-        val delta = n - m
-        val deltaIsOdd = (delta % 2) != 0
-        val halfway = size / 2 + (if (deltaIsOdd) 1 else 0)
-        val endCoords get() = Coords(n, m)
-
-        init {
-            if (first !is RandomAccess) {
-                throw RuntimeException("first er ikke RandomAccess (ArrayList)")
-            }
-            if (second !is RandomAccess) {
-                throw RuntimeException("second er ikke RandomAccess (ArrayList)")
-            }
-        }
-
-        fun slice(from: Coords, to: Coords): Diff<T> =
-            Diff(first.slice(from.x..<to.x), second.slice(from.y..<to.y))
-
-    }
-
-    private class Vectors private constructor(private val negativeOffset: Int, private val array: IntArray) {
-        constructor(n: Int, m: Int, initialVector: Int) : this(m + n, IntArray((m + n) * 2 + 1).also { it[1 + n + m] = initialVector })
-
-        operator fun get(i: Int) = array[i + negativeOffset]
-        operator fun set(i: Int, v: Int) {
-            array[i + negativeOffset] = v
-        }
-    }
 }
+
