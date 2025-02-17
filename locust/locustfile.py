8c272897a974f288c73ef61633ffbe2714394ef5
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-02-17 17:27:14 +0100

    WIP

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -17,13 +17,13 @@ class BrevbakerLoadTest(FastHttpUser):
         #headers = {'Content-Type': 'application/json'}
 
         #Test av asynkront kall lokalt:
-        headers = {'Content-Type': 'application/json'}
+        #headers = {'Content-Type': 'application/json'}
 
         #Test av synkront kall på clusteret:
         #headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.access_token(), 'X-Request-ID': str(uuid.uuid4())}
 
         # Test av async på clusteret:
-        #headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.access_token(), 'X-Request-ID': str(uuid.uuid4())}
+        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.access_token(), 'X-Request-ID': str(uuid.uuid4())}
         self.client.post("/letter/autobrev/pdfAsync", payload, headers=headers)
 
     def access_token(self):
