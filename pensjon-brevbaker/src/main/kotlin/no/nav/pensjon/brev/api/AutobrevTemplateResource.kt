de1e1f4c00eda6b2095e7f3a420426097e7885c6
Mads Opheim <mads.opheim@nav.no>
2025-02-17 10:54:06 +0100

    No treng ikkje autobrev-implementasjonen lenger å ha redigerbar-kode

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/api/AutobrevTemplateResource.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/api/AutobrevTemplateResource.kt
@@ -3,14 +3,11 @@ package no.nav.pensjon.brev.api
 import no.nav.brev.brevbaker.BrevbakerHTML
 import no.nav.brev.brevbaker.BrevbakerJSON
 import no.nav.pensjon.brev.api.model.BestillBrevRequest
-import no.nav.pensjon.brev.api.model.BestillRedigertBrevRequest
 import no.nav.pensjon.brev.api.model.LetterResponse
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.latex.LaTeXCompilerService
 import no.nav.pensjon.brev.template.BrevTemplate
-import no.nav.pensjon.brev.template.render.Letter2Markup
-import no.nav.pensjon.brev.template.toScope
 import no.nav.pensjon.brevbaker.api.model.LetterMarkup
 
 class AutobrevTemplateResource<Kode : Brevkode<Kode>, out T : BrevTemplate<BrevbakerBrevdata, Kode>>(
@@ -24,19 +21,9 @@ class AutobrevTemplateResource<Kode : Brevkode<Kode>, out T : BrevTemplate<Brevb
             brevbakerPDF.lagPDF(createLetter(kode, letterData, language, felles))
         }
 
-    suspend fun renderPDF(brevbestilling: BestillRedigertBrevRequest<Kode>): LetterResponse =
-        with(brevbestilling) {
-            brevbakerPDF.lagPDF(createLetter(kode, letterData, language, felles), letterMarkup)
-        }
-
     override fun renderHTML(brevbestilling: BestillBrevRequest<Kode>): LetterResponse =
         with(brevbestilling) {
             BrevbakerHTML.renderHTML(createLetter(kode, letterData, language, felles))
-        }
-
-    fun renderHTML(brevbestilling: BestillRedigertBrevRequest<Kode>): LetterResponse =
-        with(brevbestilling) {
-            BrevbakerHTML.renderHTML(createLetter(kode, letterData, language, felles), letterMarkup)
         }
 
     fun renderJSON(brevbestilling: BestillBrevRequest<Kode>): LetterMarkup =
@@ -44,7 +31,4 @@ class AutobrevTemplateResource<Kode : Brevkode<Kode>, out T : BrevTemplate<Brevb
             BrevbakerJSON.renderJSON(createLetter(kode, letterData, language, felles))
         }
 
-    fun renderLetterMarkup(brevbestilling: BestillBrevRequest<Kode>): LetterMarkup =
-        createLetter(brevbestilling.kode, brevbestilling.letterData, brevbestilling.language, brevbestilling.felles)
-            .let { Letter2Markup.renderLetterOnly(it.toScope(), it.template) }
 }
