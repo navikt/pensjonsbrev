b6ecfdbbe4ac1e3b1bd323b7fc1a815c688e293e
Mads Opheim <mads.opheim@nav.no>
2025-02-18 11:09:16 +0100

    Køyrer intellij-formatering på fila

--- brevbaker/src/main/kotlin/no/nav/brev/brevbaker/BrevbakerPDF.kt
+++ brevbaker/src/main/kotlin/no/nav/brev/brevbaker/BrevbakerPDF.kt
@@ -15,29 +15,31 @@ private val base64Decoder = Base64.getDecoder()
 
 internal class BrevbakerPDF(private val pdfByggerService: PDFByggerService) {
     suspend fun renderPDF(letter: Letter<BrevbakerBrevdata>, redigertBrev: LetterMarkup? = null): LetterResponse =
-        renderCompleteMarkup(letter, redigertBrev)
-            .let { pdfByggerService.producePDF(
+        renderCompleteMarkup(letter, redigertBrev).let {
+            pdfByggerService.producePDF(
                 PDFRequest(
-                letterMarkup = it.letterMarkup,
-                attachments = it.attachments,
-                language = letter.language.toCode(),
-                felles = letter.felles,
-                brevtype = letter.template.letterMetadata.brevtype
-            )
-            ) }
-            .let { pdf ->
-                LetterResponse(
-                    file = base64Decoder.decode(pdf.base64PDF),
-                    contentType = ContentTypes.PDF,
-                    letterMetadata = letter.template.letterMetadata
+                    letterMarkup = it.letterMarkup,
+                    attachments = it.attachments,
+                    language = letter.language.toCode(),
+                    felles = letter.felles,
+                    brevtype = letter.template.letterMetadata.brevtype
                 )
-            }
-
-    private fun renderCompleteMarkup(letter: Letter<BrevbakerBrevdata>, redigertBrev: LetterMarkup? = null): LetterWithAttachmentsMarkup =
-        letter.toScope().let { scope ->
-            LetterWithAttachmentsMarkup(
-                redigertBrev ?: Letter2Markup.renderLetterOnly(scope, letter.template),
-                Letter2Markup.renderAttachmentsOnly(scope, letter.template),
+            )
+        }.let { pdf ->
+            LetterResponse(
+                file = base64Decoder.decode(pdf.base64PDF),
+                contentType = ContentTypes.PDF,
+                letterMetadata = letter.template.letterMetadata
             )
         }
+
+    private fun renderCompleteMarkup(
+        letter: Letter<BrevbakerBrevdata>,
+        redigertBrev: LetterMarkup? = null,
+    ): LetterWithAttachmentsMarkup = letter.toScope().let { scope ->
+        LetterWithAttachmentsMarkup(
+            redigertBrev ?: Letter2Markup.renderLetterOnly(scope, letter.template),
+            Letter2Markup.renderAttachmentsOnly(scope, letter.template),
+        )
+    }
 }
