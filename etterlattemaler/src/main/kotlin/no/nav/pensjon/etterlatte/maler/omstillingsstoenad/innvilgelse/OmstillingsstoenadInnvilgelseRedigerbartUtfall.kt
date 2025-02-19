d9abb4d72d57c739804ac7a9b206a496e1f2d29e
Øyvind Haarberg <oyvind.haarberg@bekk.no>
2025-02-19 14:43:47 +0100

    fikser import

--- etterlattemaler/src/main/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/innvilgelse/OmstillingsstoenadInnvilgelseRedigerbartUtfall.kt
+++ etterlattemaler/src/main/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/innvilgelse/OmstillingsstoenadInnvilgelseRedigerbartUtfall.kt
@@ -4,6 +4,7 @@ import no.nav.pensjon.brev.template.Language.Bokmal
 import no.nav.pensjon.brev.template.Language.English
 import no.nav.pensjon.brev.template.Language.Nynorsk
 import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.isNull
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
