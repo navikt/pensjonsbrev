6e9a36f8454d0ee9421ed1ce107bcebe066347f9
Mads Opheim <mads.opheim@nav.no>
2025-02-19 09:38:46 +0100

    Fann litt meir som bør vera internal

--- brevbaker/src/testFixtures/kotlin/no/nav/brev/brevbaker/LetterTestRenderer.kt
+++ brevbaker/src/testFixtures/kotlin/no/nav/brev/brevbaker/LetterTestRenderer.kt
@@ -6,11 +6,16 @@ import no.nav.pensjon.brev.template.LetterTemplate
 import no.nav.pensjon.brev.template.render.Letter2Markup
 import no.nav.pensjon.brev.template.render.LetterWithAttachmentsMarkup
 import no.nav.pensjon.brev.template.toScope
+import no.nav.pensjon.brevbaker.api.model.LetterMarkup
+import no.nav.pensjon.brevbaker.api.model.LetterMarkup.Attachment
 
 object LetterTestRenderer {
-    fun render(letter: Letter<*>): LetterWithAttachmentsMarkup = Letter2Markup.render(letter)
+    fun render(letter: Letter<*>): LetterAndAttachmentsMarkup = Letter2Markup.render(letter)
+        .let { LetterAndAttachmentsMarkup(it.letterMarkup, it.attachments) }
 
     fun renderLetterOnly(letter: Letter<*>) = Letter2Markup.renderLetterOnly(letter.toScope(), letter.template)
 
     fun renderAttachmentsOnly(scope: ExpressionScope<*>, template: LetterTemplate<*, *>) = Letter2Markup.renderAttachmentsOnly(scope, template)
 }
+
+data class LetterAndAttachmentsMarkup(val letterMarkup: LetterMarkup, val attachments: List<Attachment>)
