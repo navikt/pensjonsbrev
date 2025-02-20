43ff6b84847bbaba662150be4cf03973e85b4a14
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-02-20 12:13:32 +0100

    Fix locust etter merge.

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -4,3 +4,4 @@
  (cd tjenestebuss-integrasjon && ./fetch-secrets.sh)
  (cd skribenten-web/bff && python3 setup_local_azure_secrets.py)
  (cd pensjon-brevbaker && ./fetch-secrets.sh)
+ (cd pensjon-pdf-bygger && ./fetch-secrets.sh)
--- locust/locustfile.py
+++ locust/locustfile.py
@@ -9,7 +9,7 @@ import uuid
 payload = open(os.path.join(os.path.dirname(__file__), 'autobrev_request_async.json'), "r").read()
 
 class BrevbakerLoadTest(FastHttpUser):
-    token = azuread.fetch_token()
+    #token = azuread.fetch_token()
     wait_time = between(0,0)
     @task
     def load_test(self):
@@ -17,13 +17,13 @@ class BrevbakerLoadTest(FastHttpUser):
         #headers = {'Content-Type': 'application/json'}
 
         #Test av asynkront kall lokalt:
-        #headers = {'Content-Type': 'application/json'}
+        headers = {'Content-Type': 'application/json'}
 
         #Test av synkront kall på clusteret:
         #headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.access_token(), 'X-Request-ID': str(uuid.uuid4())}
 
         # Test av async på clusteret:
-        headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.access_token(), 'X-Request-ID': str(uuid.uuid4())}
+        #headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + self.access_token(), 'X-Request-ID': str(uuid.uuid4())}
         self.client.post("/letter/autobrev/pdfAsync", payload, headers=headers)
 
     def access_token(self):
