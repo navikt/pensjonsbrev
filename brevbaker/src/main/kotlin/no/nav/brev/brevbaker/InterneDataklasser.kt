51f273d131efeb3031240714bf6f81942536337b
Mads Opheim <mads.opheim@nav.no>
2025-02-21 10:06:12 +0100

    Flyttar annotasjonsklassa ut til eiga fil, var jo malplassert der ho var

---
+++ brevbaker/src/main/kotlin/no/nav/brev/brevbaker/InterneDataklasser.kt
@@ -0,0 +1,13 @@
+package no.nav.brev.brevbaker
+
+import kotlin.RequiresOptIn.Level
+
+@RequiresOptIn(
+    message = "Dette er dataklasser vi bruker internt i brevteamet, mellom applikasjonene våre." +
+            "For deg som konsument anbefaler vi at du heller lager og bruke din egen implementasjon av interfacene vi tilbyr" +
+            "Bruker du likevel våre interne implementasjoner er det på egen risiko, vi endrer disse ved behov uten å annonsere det ut.",
+    level = Level.ERROR
+)
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class InterneDataklasser
--- brevbaker/src/main/kotlin/no/nav/brev/brevbaker/template/Letter.kt
+++ brevbaker/src/main/kotlin/no/nav/brev/brevbaker/template/Letter.kt
@@ -1,5 +1,6 @@
 package no.nav.pensjon.brev.template
 
+import no.nav.brev.brevbaker.InterneDataklasser
 import no.nav.pensjon.brevbaker.api.model.Felles
 
 // TODO: Look at * projections for LetterTemplate, we have to have it for the API endpoint, but perhaps not for internal usage.
@@ -20,9 +21,6 @@ data class LetterImpl<ParameterType : Any>(
         }
     }
 }
-
-@RequiresOptIn("interneDataklasser")
-annotation class InterneDataklasser
 
 interface Letter<ParameterType : Any> {
     val template: LetterTemplate<*, ParameterType>
