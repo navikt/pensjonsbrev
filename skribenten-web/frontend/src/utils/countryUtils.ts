d4f4b35bd978a2a07e2c5d12c9ba15c858e79b8c
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2025-02-24 14:02:39 +0100

    Sørg for at landData er definert før det brukes

--- skribenten-web/frontend/src/utils/countryUtils.ts
+++ skribenten-web/frontend/src/utils/countryUtils.ts
@@ -8,7 +8,6 @@ type Land = {
  * Hent landnavn basert på landkode
  */
 export const getCountryNameByKode = (kode: Nullable<string>, landData: Land[] | undefined): Nullable<string> => {
-  if (!landData) return kode;
-  const country = landData.find((land) => land.kode === kode);
+  const country = landData?.find((land) => land.kode === kode);
   return country ? country.navn : kode;
 };
