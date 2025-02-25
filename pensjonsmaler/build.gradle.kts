722567727c3a0f5557bcec5e5a5be97b91020f07
Mads Opheim <mads.opheim@nav.no>
2025-02-25 14:02:06 +0100

    Versjon av apimodell

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 127
+val apiModelVersion = 128
 
 val apiModelJavaTarget: String by System.getProperties()
 val jupiterVersion: String by project
