da952c62e0c94b58dfffbff75d916475d62701a3
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2025-02-25 15:45:15 +0100

    Forbedret kode

--- skribenten-web/frontend/src/utils/countryUtils.ts
+++ skribenten-web/frontend/src/utils/countryUtils.ts
@@ -7,15 +7,6 @@ type Land = {
 /**
  * Hent landnavn basert på landkode
  */
-export const getCountryNameByKode = (kode: Nullable<string>, landData: Land[]): string => {
-  if (!Array.isArray(landData)) {
-    return kode || "";
-  }
-
-  if (landData) {
-    const country = landData.find((land) => land.kode === kode);
-    return country ? country.navn : kode || "";
-  }
-
-  return kode || "";
+export const getCountryNameByKode = (kode: Nullable<string>, landData: Land[]): Nullable<string> => {
+  return landData.find((land) => land.kode === kode)?.navn ?? kode;
 };
