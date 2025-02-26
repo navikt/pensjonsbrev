612dce9b8a125a73f400f2a2f03cb20f7b3a3a10
Mads Opheim <mads.opheim@nav.no>
2025-02-26 12:07:55 +0100

    Tar bort unødig dobbel override på type

--- brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LetterMarkup.kt
+++ brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LetterMarkup.kt
@@ -63,6 +63,8 @@ interface LetterMarkup {
         }
 
         interface ItemList : ParagraphContent {
+            override val type: Type
+                get() = Type.ITEM_LIST
             val items: List<Item>
             interface Item {
                 val id: Int
