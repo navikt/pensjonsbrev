ff7fb4d0d822942871c4298b9cd2b04e37213810
Håkon <32221678+hconhegg@users.noreply.github.com>
2025-02-26 14:42:06 +0100

    Omsorgspoeng -> Omsorgsopptjening (#1329)

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/EndretUforetrygdPGAOpptjeningLegacy.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/EndretUforetrygdPGAOpptjeningLegacy.kt
@@ -162,7 +162,7 @@ override val kode = Pesysbrevkoder.AutoBrev.UT_ENDRET_PGA_OPPTJENING
                     }
                     item {
                         text(
-                            Bokmal to "Omsorgspoeng",
+                            Bokmal to "Omsorgsopptjening",
                         )
                     }
                     item {
