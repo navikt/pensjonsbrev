83a9fc133a10a31e70d0df0dfbef16765a04297f
Petter Hannevold <petter.hannevold@nav.no>
2025-03-03 12:15:58 +0100

    Nytt, smalere grensesnitt for endring-pga-inntekt-2-brevet

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ufoerApi/EndretUfoeretrygdPGAInntekt2.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ufoerApi/EndretUfoeretrygdPGAInntekt2.kt
@@ -6,76 +6,46 @@ import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDto
 import no.nav.pensjon.brev.api.model.vedlegg.OrienteringOmRettigheterUfoereDto
 import java.time.LocalDate
 
-@Suppress("unused")
 data class EndretUfoeretrygdPGAInntektDto2(
-    val gammeltBelop: Int?,
-    val nyttBelop: Int?,
-    val virkningFom: LocalDate,
-    val forventetInntekt: Int?,
-    val utbetalingsgrad: Int?,
-    val uforegrad: Int,
-    val vilFylle67IlaVirkningFomAr: Boolean,
-    val gjenlevendetillegg: Gjenlevendetillegg?,
-    val instoppholdType: String?,
-    val totalNetto: Int,
-    val avkortningInntektsgrense: Int,
-    val avkortningInntektstak: Int,
-    val belopRedusert: Boolean,
-    val avkortningsbelopPerAr: Int?,
-    val grunnbelop: Int,
-    val kravarsaksType: String,
-    val totalNettoForAr: Int?,
-    val nettoAkk: Int?,
-    val nettoPerManed: Int,
-    val belopOkt: Boolean,
-    val regelverktypeBT: String?,
-    val brukerBorINorge: Boolean,
-    val ettArForVirkningstidspunkt: Int,
-    val borMed: String?,
+
+
+    val uforetrygd: Uforetrygd,
     val barnetilleggFellesbarn: BarnetilleggFellesbarn?,
     val barnetilleggSaerkullsbarn: BarnetilleggSaerkullsbarn?,
-    val endretBT: Boolean,
+    val gjenlevendetillegg: Gjenlevendetillegg?,
+    val forventetInntekt: Int?,
+    val virkningFom: LocalDate,
+    val totalNetto: Int,
 
-    // Trengs for vedlegg. Husk å fjerne når vedleggene er konvertert
-    val pe: PE,
-    val maanedligUfoeretrygdFoerSkatt: MaanedligUfoeretrygdFoerSkattDto?,
-    val orienteringOmRettigheterUfoere: OrienteringOmRettigheterUfoereDto,
 ) : BrevbakerBrevdata
 
+data class Uforetrygd(
+    val netto: Int,
+    val endringsbelop: Int,
+    val uforegrad: Int,
+    val inntektstak: Int,
+    val inntektsgrense: Int,
+    )
 
 data class Gjenlevendetillegg (
     val belop: Int,
 )
 
 data class BarnetilleggFellesbarn (
-    val endret: Boolean,
-    val gammeltBelop: Int,
-    val nyttBelop: Int,
-    val inntektBruktIAvkortningAv: Int?,
-    val periodisertInntekt: Boolean,
-    val periodisertFribelop: Boolean,
-    val brukersInntektBruktIAvkortningAv: Int?,
-    val inntektAnnenForelder: Int?,
-    val belopFratrukketAnnenForeldersInntekt: Int?,
-    val justeringsbelopPerAr: Int?,
     val netto: Int,
+    val endringsbelop: Int,
+    val inntektBruktIAvkortning: Int?,
     val fribelop: Int,
-    val avkortningInntektstak: Int,
-    val arligAvkortningsbelop: Int,
+    val inntektstak: Int,
     val antallBarn: Int,
 )
 
 data class BarnetilleggSaerkullsbarn(
-    val endret: Boolean,
-    val gammeltBelop: Int,
-    val nyttBelop: Int,
-    val inntektBruktIAvkortningAv: Int?,
-    val periodisertInntekt: Boolean,
-    val periodisertFribelop: Boolean,
+
     val netto: Int,
-    val justeringsbelopPerAr: Int?,
+    val endringsbelop: Int,
+    val inntektBruktIAvkortning: Int?,
     val fribelop: Int,
-    val avkortningInntektstak: Int,
-    val arligAvkortningsbelop: Int,
+    val inntektstak: Int,
     val antallBarn: Int,
 )
