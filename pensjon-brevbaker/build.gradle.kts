ce33683ef228196130ef2f08f60174706df1bb15
Mads Opheim <mads.opheim@nav.no>
2025-03-13 15:11:48 +0100

    Ryddar bort spor av pdfbox frå brevbaker-appen

--- pensjon-brevbaker/build.gradle.kts
+++ pensjon-brevbaker/build.gradle.kts
@@ -125,7 +125,6 @@ dependencies {
 
     implementation(libs.bundles.metrics)
 
-    testImplementation(libs.pdfbox)
     // JUnit 5
     testImplementation(libs.bundles.junit)
     testImplementation(libs.hamkrest)
