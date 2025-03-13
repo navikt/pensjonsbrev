15787b56ea9d07520c992cbab12e63ccabefc574
Chris Searle <christopher.dominic.searle@nav.no>
2025-03-13 12:12:31 +0100

    Legg til støtte for redigerbar avslag uttak før normert pensjonsalder (#1359)
    Legg til støtte for redigerbar avslag uttak før normert pensjonsalder
    
    ---------
    
    Co-authored-by: Espen Schei <espen.schei@outlook.com>
    Co-authored-by: Scott Hallvard Levkowetz <scott.hallvard.levkowetz@nav.no>

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/FeatureToggles.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/FeatureToggles.kt
@@ -11,4 +11,5 @@ object FeatureToggles {
     val brevMedFritekst = FeatureToggle("brevMedFritekst")
     val brevmalUtAvslag = FeatureToggle("brevmalUtAvslag")
     val innhentingAvInformasjonFraBruker = FeatureToggle("innhentingAvInformasjonFraBruker")
+    val apAvslagNormertPensjonsalder = FeatureToggle("pensjonsbrev.brev.ap_avslag_norm_auto")
 }
--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/alder/AvslagUttakFoerNormertPensjonsalder.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/alder/AvslagUttakFoerNormertPensjonsalder.kt
@@ -1,19 +1,23 @@
 package no.nav.pensjon.brev.maler.alder
 
+import no.nav.pensjon.brev.api.model.Sakstype
+import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
-import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDto
 import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.afpBruktIBeregning
 import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.minstePensjonssats
 import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.normertPensjonsalder
 import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.opplysningerBruktIBeregningen
 import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.totalPensjon
 import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.virkFom
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderDto
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderDtoSelectors.PesysDataSelectors.avslagUttakData
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderDtoSelectors.pesysData
 import no.nav.pensjon.brev.maler.adhoc.vedlegg.dineRettigheterOgMulighetTilAaKlagePensjonStatisk
 import no.nav.pensjon.brev.maler.alder.vedlegg.opplysningerBruktIBeregningenAP
-import no.nav.pensjon.brev.template.AutobrevTemplate
 import no.nav.pensjon.brev.template.Language.Bokmal
 import no.nav.pensjon.brev.template.Language.English
 import no.nav.pensjon.brev.template.Language.Nynorsk
+import no.nav.pensjon.brev.template.RedigerbarTemplate
 import no.nav.pensjon.brev.template.dsl.createTemplate
 import no.nav.pensjon.brev.template.dsl.expression.expr
 import no.nav.pensjon.brev.template.dsl.expression.format
@@ -25,13 +29,13 @@ import no.nav.pensjon.brevbaker.api.model.LetterMetadata
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Brevtype.VEDTAKSBREV
 
 @TemplateModelHelpers
-object AvslagUttakFoerNormertPensjonsalderAuto : AutobrevTemplate<AvslagUttakFoerNormertPensjonsalderAutoDto> {
+object AvslagUttakFoerNormertPensjonsalder : RedigerbarTemplate<AvslagUttakFoerNormertPensjonsalderDto> {
 
-    override val kode = Pesysbrevkoder.AutoBrev.PE_AP_AVSLAG_UTTAK_FOER_NORMERT_PENSJONSALDER_AUTO
+    override val kode = Pesysbrevkoder.Redigerbar.PE_AP_AVSLAG_UTTAK_FOER_NORMERT_PENSJONSALDER
 
     override val template = createTemplate(
         name = kode.name,
-        letterDataType = AvslagUttakFoerNormertPensjonsalderAutoDto::class,
+        letterDataType = AvslagUttakFoerNormertPensjonsalderDto::class,
         languages = languages(Bokmal, Nynorsk, English),
         letterMetadata = LetterMetadata(
             displayTitle = "Nav har avslått søknaden din om alderspensjon før normert pensjonsalder",
@@ -40,26 +44,36 @@ object AvslagUttakFoerNormertPensjonsalderAuto : AutobrevTemplate<AvslagUttakFoe
             brevtype = VEDTAKSBREV,
         )
     ) {
+        val avslagData = pesysData.avslagUttakData
+
         title {
             textExpr(
-                Bokmal to "Nav har avslått søknaden din om alderspensjon fra ".expr() + virkFom.format(),
-                Nynorsk to "Nav har avslått søknaden din om alderspensjon frå ".expr() + virkFom.format(),
-                English to "Nav has declined your application for retirement pension from ".expr() + virkFom.format(),
+                Bokmal to "Nav har avslått søknaden din om alderspensjon fra ".expr() + avslagData.virkFom.format(),
+                Nynorsk to "Nav har avslått søknaden din om alderspensjon frå ".expr() + avslagData.virkFom.format(),
+                English to "Nav has declined your application for retirement pension from ".expr() + avslagData.virkFom.format(),
             )
         }
 
         outline {
-            includePhrase(AvslagUttakFoerNormertPensjonsalderFelles(
-                afpBruktIBeregning = afpBruktIBeregning,
-                normertPensjonsalder = normertPensjonsalder,
-                opplysningerBruktIBeregningen = opplysningerBruktIBeregningen,
-                virkFom = virkFom,
-                minstePensjonssats = minstePensjonssats,
-                totalPensjon = totalPensjon
-            ))
+            includePhrase(
+                AvslagUttakFoerNormertPensjonsalderFelles(
+                    afpBruktIBeregning = avslagData.afpBruktIBeregning,
+                    normertPensjonsalder = avslagData.normertPensjonsalder,
+                    opplysningerBruktIBeregningen = avslagData.opplysningerBruktIBeregningen,
+                    virkFom = avslagData.virkFom,
+                    minstePensjonssats = avslagData.minstePensjonssats,
+                    totalPensjon = avslagData.totalPensjon
+                )
+            )
         }
 
         includeAttachment(dineRettigheterOgMulighetTilAaKlagePensjonStatisk)
-        includeAttachment(opplysningerBruktIBeregningenAP, opplysningerBruktIBeregningen)
+        includeAttachment(opplysningerBruktIBeregningenAP, avslagData.opplysningerBruktIBeregningen)
     }
+
+    override val kategori: TemplateDescription.Brevkategori = TemplateDescription.Brevkategori.FOERSTEGANGSBEHANDLING
+
+    override val brevkontekst: TemplateDescription.Brevkontekst = TemplateDescription.Brevkontekst.VEDTAK
+
+    override val sakstyper: Set<Sakstype> = setOf(Sakstype.ALDER)
 }
