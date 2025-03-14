098a4d7de4fcdd076aa85c72044607e4cf5ab033
Jeremy Elsom <jeremy.elsom@nav.no>
2025-03-14 09:30:50 +0100

    Oppdaterte gradle fra main

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 135
+val apiModelVersion = 137
 
 val apiModelJavaTarget: String by System.getProperties()
 
