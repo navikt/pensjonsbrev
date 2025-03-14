6c0c2346513436b8331b99ae2fa6d857ec87d6eb
Jeremy Elsom <jeremy.elsom@nav.no>
2025-03-14 09:35:23 +0100

    Lagt VarselRevurderingAvPensjon til ProductionTemplates.kt

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/ProductionTemplates.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/ProductionTemplates.kt
@@ -50,6 +50,7 @@ object ProductionTemplates : AllTemplates {
     )
     private val redigerbare: Set<RedigerbarTemplate<out RedigerbarBrevdata<*, *>>> = setOf(
         AvslagUfoeretrygd,
+        AvslagUttakFoerNormertPensjonsalder,
         BekreftelsePaaFlyktningstatus,
         ForespoerselOmDokumentasjonAvBotidINorgeAlder,
         ForespoerselOmDokumentasjonAvBotidINorgeEtterlatte,
@@ -61,7 +62,7 @@ object ProductionTemplates : AllTemplates {
         InnhentingOpplysningerFraBruker,
         OrienteringOmSaksbehandlingstid,
         VarselOmMuligAvslag,
-        AvslagUttakFoerNormertPensjonsalder,
+        VarselRevurderingAvPensjon,
     )
 
     override fun hentAutobrevmaler() = autobrev
