90b0e4e87d7c4e00f2793d67047d0fe6608dbce0
Mads Opheim <mads.opheim@nav.no>
2025-03-18 16:10:17 +0100

    Forenklar testklassa som genererer alle malane som HTML og definerte malar som PDF, så pdf-genereringa kun køyrer for dei ønska malane. Blir dermed mindre støy og lettare å halde oversikt

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/template/GenererAlleMaleneTest.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/template/GenererAlleMaleneTest.kt
@@ -17,24 +17,20 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-class GenererAlleMaleneTest {
 
-    private val filterForPDF = listOf<String>("SAMLET")
+val filterForPDF = listOf("SAMLET")
+
+class GenererAlleMaleneTest {
 
     @Tag(TestTags.MANUAL_TEST)
     @ParameterizedTest(name = "{1}, {3}")
-    @MethodSource("alleMalene")
+    @MethodSource("filtrerteMaler")
     fun <T : Any> testPdf(
         template: LetterTemplate<LanguageSupport, T>,
         brevkode: Brevkode<*>,
         fixtures: T,
         spraak: Language,
     ) {
-        if (filterForPDF.isNotEmpty()) {
-            if (!filterForPDF.any { template.name.lowercase().contains(it.lowercase()) }) {
-                return
-            }
-        }
         if (!template.language.supports(spraak)) {
             println("Mal ${template.name} fins ikke på språk $spraak, tester ikke denne")
             return
@@ -69,46 +65,33 @@ class GenererAlleMaleneTest {
 
     companion object {
         @JvmStatic
-        fun alleMalene(): List<Arguments> {
+        fun filtrerteMaler(): List<Arguments> = finnMaler(filterForPDF)
+
+        @JvmStatic
+        fun alleMalene(): List<Arguments> = finnMaler(listOf())
+
+        @JvmStatic
+        fun finnMaler(filter: List<String> = listOf()): List<Arguments> {
             FeatureToggleSingleton.init(FeatureToggleDummy)
             return listOf(Language.Nynorsk, Language.Bokmal, Language.English).flatMap { spraak ->
-                ProductionTemplates.hentAutobrevmaler().map {
-                    Arguments.of(
-                        it.template,
-                        it.kode,
-                        Fixtures.create(it.template.letterDataType),
-                        spraak,
-                    )
-                } + ProductionTemplates.hentRedigerbareMaler().map {
-                    Arguments.of(
-                        it.template,
-                        it.kode,
-                        Fixtures.create(it.template.letterDataType),
-                        spraak,
-                    )
-                } + LetterExample.let {
-                    Arguments.of(
-                        LetterExample.template,
-                        LetterExample.kode,
-                        Fixtures.create(LetterExample.template.letterDataType),
-                        spraak,
-                    )
-                } + EksempelbrevRedigerbart.let {
-                    Arguments.of(
-                        EksempelbrevRedigerbart.template,
-                        EksempelbrevRedigerbart.kode,
-                        Fixtures.create(EksempelbrevRedigerbart.template.letterDataType),
-                        spraak,
-                    )
-                }
+                (ProductionTemplates.hentAutobrevmaler() +
+                        ProductionTemplates.hentRedigerbareMaler()
+                        + LetterExample
+                        + EksempelbrevRedigerbart
+                        ).filter {
+                        filter.isEmpty() || filter.any { f ->
+                            it.template.name.lowercase().contains(f.lowercase())
+                        }
+                    }
+                    .map { Arguments.of(it.template, it.kode, Fixtures.create(it.template.letterDataType), spraak) }
             }
         }
     }
 }
 
 object FeatureToggleDummy : FeatureToggleService {
-    val toggles: MutableMap<FeatureToggle, Boolean> = mutableMapOf()
+    private val toggles: MutableMap<FeatureToggle, Boolean> = mutableMapOf()
 
-    override fun isEnabled(toggle: FeatureToggle): Boolean = toggles.get(toggle) ?: false
+    override fun isEnabled(toggle: FeatureToggle): Boolean = toggles[toggle] ?: false
 
 }
