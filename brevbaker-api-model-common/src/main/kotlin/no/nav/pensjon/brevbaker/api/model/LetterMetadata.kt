f22fd9caf9ff6367ea0d6d67e8a6462a560deab0
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-03-18 15:35:31 +0100

    Retter på dokumentasjon.

--- brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LetterMetadata.kt
+++ brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LetterMetadata.kt
@@ -1,7 +1,7 @@
 package no.nav.pensjon.brevbaker.api.model
 
 /**
- * @property displayTitle Visningstittel i arkivet. Brukes for visning til saksbehandler og bruker. Se https://confluence.adeo.no/display/BOA/QDIST010+-+DistribuerForsendelseTilDittNAV
+ * @property displayTitle Visningstittel i arkivet og brevmeny. Brukes for visning til saksbehandler og bruker. Se https://confluence.adeo.no/display/BOA/QDIST010+-+DistribuerForsendelseTilDittNAV
  * @property isSensitiv Styrer om innholdet i brevet er sensitivt. Sensitive brev skal ikke vises ved nivå 3 innlogging
  * @property distribusjonstype Styrer viktigheten og typen varsel til bruker under distribusjon. Les mer på: https://confluence.adeo.no/display/BOA/RDIST002-1.+DistribuerJournalpost+-+For+Konsumenter
  * @property brevtype Endrer på enkelte standard-tekster i grunn-malen til brevet avhengig av om brevet er ett vedtak eller ikke.
