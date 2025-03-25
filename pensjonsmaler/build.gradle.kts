e896d5c83508447e43cacee9aa734eaa4903a397
Espen Schei <espen.schei@nav.no>
2025-03-25 10:20:44 +0100

    Bump versjon til 143

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 142
+val apiModelVersion = 143
 
 val apiModelJavaTarget: String by System.getProperties()
 
