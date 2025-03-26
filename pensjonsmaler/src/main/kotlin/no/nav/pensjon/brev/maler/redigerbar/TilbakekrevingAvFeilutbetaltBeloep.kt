863f6f4f57eac720d17c24f130c18245dca85419
Jeremy Elsom <jeremy.elsom@nav.no>
2025-03-26 13:25:12 +0100

    WIP

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/redigerbar/TilbakekrevingAvFeilutbetaltBeloep.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/redigerbar/TilbakekrevingAvFeilutbetaltBeloep.kt
@@ -15,12 +15,8 @@ import no.nav.pensjon.brev.api.model.maler.redigerbar.TilbakekrevingAvFeilutbeta
 import no.nav.pensjon.brev.api.model.maler.redigerbar.TilbakekrevingAvFeilutbetaltBeloepDtoSelectors.pesysData
 import no.nav.pensjon.brev.api.model.maler.redigerbar.TilbakekrevingAvFeilutbetaltBeloepDtoSelectors.saksbehandlerValg
 import no.nav.pensjon.brev.maler.adhoc.vedlegg.dineRettigheterOgMulighetTilAaKlagePensjonStatisk
-import no.nav.pensjon.brev.maler.adhoc.vedlegg.vedleggDineRettigheterOgMulighetTilAaKlageUfoereStatisk
 import no.nav.pensjon.brev.maler.fraser.common.Felles
-import no.nav.pensjon.brev.maler.redigerbar.VarselRevurderingAvPensjon.fritekst
-import no.nav.pensjon.brev.maler.vedlegg.vedleggPraktiskInformasjonEtteroppgjoerUfoeretrygd
 import no.nav.pensjon.brev.model.format
-import no.nav.pensjon.brev.template.Expression
 import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.RedigerbarTemplate
 import no.nav.pensjon.brev.template.dsl.createTemplate
@@ -32,10 +28,7 @@ import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.template.dsl.textExpr
-import no.nav.pensjon.brevbaker.api.model.Kroner
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 @TemplateModelHelpers
 
@@ -133,7 +126,7 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                 textExpr(
                     Bokmal to "Dette er ".expr() + feilutbetaltTotalBeloep.format() + " kroner inkludert skatt.",
                     Nynorsk to "Dette er ".expr() + feilutbetaltTotalBeloep.format() + " kroner inkludert skatt.",
-                    English to "This amounts to NOK ".expr() + feilutbetaltTotalBeloep.format() + " including tax.",
+                    English to "This amounts to NOK ".expr() + feilutbetaltTotalBeloep.format() + " including tax."
 
                     )
             }
@@ -142,7 +135,7 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                     textExpr(
                         Bokmal to "Vi har kommet fram til at du skal betale tilbake hele beløpet. Det vil si ".expr() + sumTilInnkrevingTotalBeloep.format() + " kroner etter at skatten er trukket fra.",
                         Nynorsk to "Vi har kome fram til at du skal betale tilbake heile beløpet. Det vil seie ".expr() + sumTilInnkrevingTotalBeloep.format() + " kroner etter at skatten er trektfrå.",
-                        English to "We have concluded that you must repay the full excess payment you have received. This amounts to NOK ".expr() + sumTilInnkrevingTotalBeloep.format() + " after deduction of tax.",
+                        English to "We have concluded that you must repay the full excess payment you have received. This amounts to NOK ".expr() + sumTilInnkrevingTotalBeloep.format() + " after deduction of tax."
                     )
                 }
             }
@@ -151,7 +144,7 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                     textExpr(
                         Bokmal to "Vi har kommet fram til at du skal betale tilbake deler av beløpet. Det vil si ".expr() + sumTilInnkrevingTotalBeloep.format() + " kroner etter at skatten er trukket fra.",
                         Nynorsk to "Vi har kome fram til at du skalbetale tilbake delar av beløpet. Det vil seie ".expr() + sumTilInnkrevingTotalBeloep.format() + " kroner etter at skatten er trektfrå.",
-                        English to "We have concluded that you mustrepay some of the excess payment you have received. This amounts to NOK ".expr() + sumTilInnkrevingTotalBeloep.format() + " after deduction of tax.",
+                        English to "We have concluded that you mustrepay some of the excess payment you have received. This amounts to NOK ".expr() + sumTilInnkrevingTotalBeloep.format() + " after deduction of tax."
                     )
                 }
             }
@@ -159,7 +152,7 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                 text(
                     Bokmal to "Beløpet som er trukket i skatt vil Nav få tilbake av Skatteetaten.",
                     Nynorsk to "Beløpet som er trekt i skatt vil Nav få tilbakeav Skatteetaten.",
-                    English to "Nav will collect the tax amount from the Norwegian Tax Administration.",
+                    English to "Nav will collect the tax amount from the Norwegian Tax Administration."
                 )
             }
             showIf(saksbehandlerValg.harMotregning) {
@@ -195,7 +188,7 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                     text(
                         Bokmal to "Dette er tatt med i beregningen vår.",
                         Nynorsk to "Dette er teke med i berekninga vår.",
-                        English to "This has been taken into account in ourcalculations.",
+                        English to "This has been taken into account in our calculations."
                     )
                 }
             }
@@ -203,14 +196,14 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                 text(
                     Bokmal to "Vedtaket er gjort etter folketrygdloven § 22-15.",
                     Nynorsk to "Vedtaket er gjort etter folketrygdlova § 22-15.",
-                    English to "This decision was made pursuant to the provisions of section 22-15 of the National Insurance Act.",
+                    English to "This decision was made pursuant to the provisions of section 22-15 of the National Insurance Act."
                 )
             }
             paragraph {
                 text(
                     Bokmal to "I vedlegget finner du en oversikt over periodene med feilutbetalinger og beløpet du må betale tilbake.",
                     Nynorsk to "I vedlegget finn du ei oversikt overperiodane med feilutbetalingar og beløpet du må betale tilbake.",
-                    English to "The attachment contains an overview ofthe periods with payments in error and the amounts you must repay",
+                    English to "The attachment contains an overview ofthe periods with payments in error and the amounts you must repay"
                 )
             }
             title1 {
@@ -223,18 +216,39 @@ object TilbakekrevingAvFeilutbetaltBeloep : RedigerbarTemplate<TilbakekrevingAvF
                     English to "We will report the change to the Norwegian Tax Administration. They will correct your tax settlement in view of this change.",
                 )
             }
-            // Du har rett til å klage
-
+            title1 {
+                text(Bokmal to "Du har rett til å klage", Nynorsk to "Du har rett til å klage", English to "You have the right to appeal")
+            }
+            paragraph {
+                text(
+                Bokmal to "Hvis du mener vedtaket er feil, kan du klage innen seks uker fra den datoen du mottok vedtaket. " +
+                        "Klagen skal være skriftlig. Du finner skjema og informasjon på nav.no/klage. I vedlegget får du vite mer om hvordan du går fram.",
+                Nynorsk to "Dersom du meiner at vedtaket er feil, kan du klage innan seks veker frå den datoen du fekk vedtaket. " +
+                        "Klaga skal vera skriftleg. Du finn skjema og informasjon på nav.no/klage. I vedlegget får du vite meir om korleis du går fram.",
+                English to "If you think the decision is wrong, you may appeal the decision within six weeks of the date on which you received notice of the decision. " +
+                        "Your appeal must be made in writing. You will find a form and information about this at nav.no/klage. The appendix includes information on how to proceed."
+                )
+            }
+            paragraph {
+                text(
+                    Bokmal to "Selv om du klager på vedtaket, må du begynne å betale tilbake. Dette går frem av forvaltningsloven § 42 og hvordan vi må praktisere regelverket.",
+                    Nynorsk to "Sjølv om du klagar på vedtaket, må du begynne å betale tilbake. Dette går fram av forvaltningslova § 42 og korleis vi må praktisere regelverket.",
+                    English to "Even if you appeal this decision, you will need to start the repayments. " +
+                            "This follows from section 42 of the Public Administration Act and the rules on our implementation of the regulations."
+                )
+            }
             paragraph {
                 text(
                     Bokmal to "Vi kan utsette tilbakekrevingen til klagen er behandlet, for eksempel hvis det er sannsynlig at vedtaket blir omgjort. " +
-                            "Du kan også søke om utsettelse av tilbakebetaling, men vi kan ikke ta hensyn til den økonomiske situasjonen din når vi vurderer om du kan utsette og betale tilbake.",
+                            "Du kan også søke om utsettelse av tilbakebetaling, men vi kan ikke ta hensyn til den økonomiske situasjonen din når vi vurderer om du kan utsette og betale tilbake",
                     Nynorsk to "Vi kan utsetje tilbakekrevjinga til klaga er behandla, for eksempel dersom det er sannsynleg at vedtaket blir gjort om. " +
                             "Du kan også søkje om utsetjing av tilbakebetalinga, men vi kan ikkje ta omsyn til den økonomiske situasjonen din når vi vurderer om du kan utsetje å betale tilbake.",
                     English to "We may postpone sending you the claim for repayment until your appeal has been processed; for example, if it seems likely that the decision will be overturned. " +
-                            "You can also apply for deferral of the repayment, but we cannot take your financial situation into account when assessing whether you qualify for a deferral.",
+                    "You can also apply for deferral of the repayment, but we cannot take your financial situation into account when assessing whether you qualify for a deferral."
                 )
             }
+            includePhrase(Felles.RettTilInnsyn(dineRettigheterOgMulighetTilAaKlagePensjonStatisk))
+            includePhrase(Felles.HarDuSpoersmaal.alder)
         }
     }
 }
