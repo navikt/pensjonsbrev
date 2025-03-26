644d7d2648a1cd67c9ca60ac1e5758d12fd601b7
Mads Opheim <mads.opheim@nav.no>
2025-03-26 15:49:13 +0100

    Apropos unngå data class i API

--- brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/PDFVedlegg.kt
+++ brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/PDFVedlegg.kt
@@ -1,13 +1,21 @@
 package no.nav.pensjon.brevbaker.api.model
 
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
+import java.util.Objects
 
 enum class PDFVedleggType(val tittel: String) {
     P1("P1 – Samlet melding om pensjonsvedtak"),
     InformasjonOmP1("Informasjon om skjemaet P1 og hvordan det brukes")
 }
 
-data class PDFVedlegg(
+class PDFVedlegg(
     val type: PDFVedleggType,
     val data: BrevbakerBrevdata
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other !is PDFVedlegg) return false
+        return this.type == other.type && this.data == other.data
+    }
+    override fun hashCode() = Objects.hash(type, data)
+    override fun toString() = "PDFVedlegg(type=$type, data=$data)"
+}
