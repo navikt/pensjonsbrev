e8882f3312e818aafd382a606ac5ade946fa698d
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-03-27 15:10:14 +0100

    Konverterer metaforce vedlegg 3.

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 146
+val apiModelVersion = 147
 
 val apiModelJavaTarget: String by System.getProperties()
 
