dae1697b8bee784f4db529f5b6bc7e58484d8457
Espen Schei <espen.schei@outlook.com>
2025-03-27 13:17:32 +0100

    Avslagsbrev gradsendring alder før normert pensjonsalder - AP2025 (#1381)
    * Starter på avslagsbrev for gradsendring
    
    Co-authored-by: Chris Searle <christopher.dominic.searle@nav.no>
    Co-authored-by: Espen Schei <espen.schei@nav.no>
    Co-authored-by: Joakim Jacobsen <joakim.jacobsen@nav.no>
    
    * Nytt avslagsbrev for gradsendring tidlig uttak
    
    Co-authored-by: Chris Searle <christopher.dominic.searle@nav.no>
    Co-authored-by: Scott Hallvard Levkowetz <scott.hallvard.levkowetz@nav.no>
    Co-authored-by: Joakim Jacobsen <joakim.jacobsen@nav.no>
    
    * Hent ut oversetelser for søknad før ett år fra metaforce
    
    * Opprett autobrev versjon av gradsendring
    
    Co-authored-by: Chris Searle <christopher.dominic.searle@nav.no>
    Co-authored-by: Scott Hallvard Levkowetz <scott.hallvard.levkowetz@nav.no>
    Co-authored-by: Joakim Jacobsen <joakim.jacobsen@nav.no>
    
    * Legger til tekst som dekker tilfeller for ikke full uttaksgrad
    
    ---------
    
    Co-authored-by: Scott Hallvard Levkowetz <scott.hallvard.levkowetz@nav.no>
    Co-authored-by: Chris Searle <christopher.dominic.searle@nav.no>
    Co-authored-by: Joakim Jacobsen <joakim.jacobsen@nav.no>
    Co-authored-by: Chris Searle <chris@chrissearle.org>

---
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/alder/avslag/gradsendring/AvslagGradsendringFoerNormertPensjonsalderAuto.kt
@@ -0,0 +1,75 @@
+package no.nav.pensjon.brev.maler.alder.avslag.gradsendring
+
+import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDto
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.afpBruktIBeregning
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.borINorge
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.harEOSLand
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.minstePensjonssats
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.normertPensjonsalder
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.opplysningerBruktIBeregningen
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.totalPensjon
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.vedtakBegrunnelseLavOpptjening
+import no.nav.pensjon.brev.api.model.maler.alderApi.AvslagUttakFoerNormertPensjonsalderAutoDtoSelectors.virkFom
+import no.nav.pensjon.brev.maler.adhoc.vedlegg.dineRettigheterOgMulighetTilAaKlagePensjonStatisk
+import no.nav.pensjon.brev.maler.alder.vedlegg.opplysningerBruktIBeregningenAP
+import no.nav.pensjon.brev.template.AutobrevTemplate
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.format
+import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.textExpr
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Brevtype.VEDTAKSBREV
+
+@TemplateModelHelpers
+object AvslagGradsendringFoerNormertPensjonsalderAuto : AutobrevTemplate<AvslagUttakFoerNormertPensjonsalderAutoDto> {
+
+    override val kode = Pesysbrevkoder.AutoBrev.PE_AP_AVSLAG_GRAD_FOER_NORMERT_PENSJONSALDER_AUTO
+
+    override val template = createTemplate(
+        name = kode.name,
+        letterDataType = AvslagUttakFoerNormertPensjonsalderAutoDto::class,
+        languages = languages(Bokmal, Nynorsk, English),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Vedtak - avslag endring av uttaksgrad - AP2025",
+            isSensitiv = true,
+            distribusjonstype = LetterMetadata.Distribusjonstype.VIKTIG,
+            brevtype = VEDTAKSBREV,
+        )
+    ) {
+        title {
+            textExpr(
+                Bokmal to "Nav har avslått søknaden din om alderspensjon fra ".expr() + virkFom.format(),
+                Nynorsk to "Nav har avslått søknaden din om alderspensjon frå ".expr() + virkFom.format(),
+                English to "Nav has declined your application for retirement pension from ".expr() + virkFom.format(),
+            )
+        }
+
+        outline {
+            includePhrase(
+                AvslagGradsendringFoerNormertPensjonsalderFelles(
+                    afpBruktIBeregning = afpBruktIBeregning,
+                    normertPensjonsalder = normertPensjonsalder,
+                    opplysningerBruktIBeregningen = opplysningerBruktIBeregningen,
+                    virkFom = virkFom,
+                    minstePensjonssats = minstePensjonssats,
+                    totalPensjon = totalPensjon,
+                    borINorge = borINorge,
+                    harEOSLand = harEOSLand,
+                    vedtakBegrunnelseLavOpptjening = vedtakBegrunnelseLavOpptjening,
+                )
+            )
+        }
+
+        includeAttachment(dineRettigheterOgMulighetTilAaKlagePensjonStatisk)
+        includeAttachment(
+            template = opplysningerBruktIBeregningenAP,
+            attachmentData = opplysningerBruktIBeregningen,
+            predicate = vedtakBegrunnelseLavOpptjening
+        )
+    }
+}
