3f66923694fdcb5495c210bf48fceaba1537fa63
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-03-28 12:07:37 +0100

    WIP

---
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/vedlegg/OpplysningerBruktIBeregningenAlder.kt
@@ -0,0 +1,6 @@
+package no.nav.pensjon.brev.api.model.vedlegg
+
+@Suppress("unused")
+data class OpplysningerBruktIBeregningenAlder(
+    val test: Boolean,
+)
---
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/OpplysningerBruktIBeregningenAlder.kt
@@ -0,0 +1,35 @@
+package no.nav.pensjon.brev.maler.vedlegg
+
+import no.nav.pensjon.brev.api.model.vedlegg.OpplysningerBruktIBeregningenAlder
+import no.nav.pensjon.brev.template.LangBokmalNynorskEnglish
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.createAttachment
+import no.nav.pensjon.brev.template.dsl.newText
+import no.nav.pensjon.brev.template.dsl.text
+
+val vedleggOpplysningerBruktIBeregningenAlder =
+    createAttachment<LangBokmalNynorskEnglish, OpplysningerBruktIBeregningenAlder>(
+        title = newText(
+            Bokmal to "Opplysninger brukt i beregningen",
+            Nynorsk to "Opplysningar brukte i berekninga",
+            English to "Information about your calculation",
+        ),
+        includeSakspart = true,
+        outline = {
+            paragraph {
+                text(
+                    Bokmal to "I dette vedlegget finner du opplysninger om deg og din pensjonsopptjening som vi har brukt i beregningen av pensjonen din. Hvis du mener at opplysningene er feil, må du melde fra til Nav, fordi det kan ha betydning for størrelsen på pensjonen din.",
+                    Nynorsk to "I dette vedlegget finn du opplysningar om deg og pensjonsoppteninga di som vi har brukt i berekninga av pensjonen din. Dersom du meiner at opplysningane er feil, må du melde frå til Nav, fordi det kan ha noko å seie for storleiken på pensjonen din.",
+                    English to "This appendix contains information about you and your accumulated pension rights, which we have used to calculate your pension. If you think the information is incorrect, you must notify Nav, as this may affect the size of your pension.",
+                )
+            }
+
+            title1 {
+                text(
+                    Bokmal to "Sivilstand",
+                    Nynorsk to "Sivilstand",
+                    English to "Civil status",
+                )
+            }
+        }
+    )
