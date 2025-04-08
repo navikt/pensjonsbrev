ea104805c6c1d29e0eadec61822d04fc02d6afb5
Jeremy Elsom <jeremy.elsom@nav.no>
2025-04-08 12:42:59 +0200

    WIP

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/redigerbar/InnvilgetGarantitillegg.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/redigerbar/InnvilgetGarantitillegg.kt
@@ -1,7 +1,9 @@
 package no.nav.pensjon.brev.maler.redigerbar
 
+import no.nav.pensjon.brev.api.model.Sakstype
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
+import no.nav.pensjon.brev.api.model.maler.redigerbar.InnvilgetGarantitilleggDto
 import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.template.Language.*
 import no.nav.pensjon.brev.template.RedigerbarTemplate
@@ -18,4 +20,18 @@ import no.nav.pensjon.brevbaker.api.model.LetterMetadata
 object InnvilgetGarantitillegg {
     override val kode = Pesysbrevkoder.Redigerbar.PE_AP_INNVILGET_GARANTITILLEGG
     override val kategori = TemplateDescription.Brevkategori.VEDTAK_ENDRING_OG_REVURDERING
+    override val brevkontekst = TemplateDescription.Brevkontekst.VEDTAK
+    override val sakstyper = Sakstype.pensjon
+
+    override val template = createTemplate(
+        name = kode.name,
+        letterDataType =  InnvilgetGarantitilleggDto::class,
+        languages = languages(Bokmal, Nynorsk, English),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Vedtak - Innvilgelse av garantitillegg i alderspensjon",
+            isSensitiv = false,
+            distribusjonstype = LetterMetadata.Distribusjonstype.VEDTAK,
+            brevtype = LetterMetadata.Brevtype.VEDTAKSBREV,
+        )
+    )
 }
