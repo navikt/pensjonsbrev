7a04aa7dafb3a41976ee817326f9dcf6932dbd9b
Chris Searle <chris@chrissearle.org>
2025-04-08 14:44:45 +0200

    Bump to 153

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 151
+val apiModelVersion = 153
 
 val apiModelJavaTarget: String by System.getProperties()
 
