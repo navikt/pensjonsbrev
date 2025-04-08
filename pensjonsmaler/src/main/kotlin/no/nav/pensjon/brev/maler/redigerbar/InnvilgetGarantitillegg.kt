19677923206b8081bfdc3a9e7099162078ecb1c3
Jeremy Elsom <jeremy.elsom@nav.no>
2025-04-08 11:15:02 +0200

    Opprettet InnvilgetGarantitillegg.kt

---
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/redigerbar/InnvilgetGarantitillegg.kt
@@ -0,0 +1,17 @@
+package no.nav.pensjon.brev.maler.redigerbar
+
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.RedigerbarTemplate
+import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.*
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brev.template.dsl.textExpr
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+
+// MF_000102
+@TemplateModelHelpers
+object InnvilgetGarantitillegg {
+}
