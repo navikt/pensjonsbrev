a4e1fbea6655c50c2aabb8f7280c053d3b24b299
Andreas Balevik <andreasbalevik@gmail.com>
2025-04-10 14:55:13 +0200

    EY-5142 opprette forhåndsvarsel redigerbart innhold (#1433)
    * EY-5142 forhåndsvarsel etteroppgjør
    
    * EY-5142 forhåndsvarsel etteroppgjør vedlegg wip
    
    * EY-5142 forhåndsvarsel etteroppgjør vedlegg wip
    
    * EY-5142 la til rett data
    
    * EY-5142 enum for resultatType
    
    * EY-5142 opprette forhaandsvarsel redigerbart
    
    * EY-5142 la til enum som mangler
    
    * EY-5142 rename feil enum
    
    * EY-5142 endre type for rettsgebyr
    
    * endre til kroner for rett formatering

---
+++ etterlattemaler/src/main/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/etteroppgjoer/EtteroppgjoerForhaandsvarselInnhold.kt
@@ -0,0 +1,75 @@
+package no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer
+
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brev.template.Language
+import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.absoluteValue
+import no.nav.pensjon.brev.template.dsl.expression.and
+import no.nav.pensjon.brev.template.dsl.expression.equalTo
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.format
+import no.nav.pensjon.brev.template.dsl.expression.not
+import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brev.template.dsl.textExpr
+import no.nav.pensjon.brevbaker.api.model.Kroner
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+import no.nav.pensjon.etterlatte.EtterlatteBrevKode
+import no.nav.pensjon.etterlatte.EtterlatteTemplate
+import no.nav.pensjon.etterlatte.maler.Delmal
+import no.nav.pensjon.etterlatte.maler.Element
+import no.nav.pensjon.etterlatte.maler.FerdigstillingBrevDTO
+import no.nav.pensjon.etterlatte.maler.Hovedmal
+import no.nav.pensjon.etterlatte.maler.RedigerbartUtfallBrevDTO
+import no.nav.pensjon.etterlatte.maler.fraser.common.Felles
+import no.nav.pensjon.etterlatte.maler.fraser.omstillingsstoenad.OmstillingsstoenadFellesFraser
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselBrevDTOSelectors.data
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.bosattUtland
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.dagensDato
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.etteroppgjoersAar
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.norskInntekt
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.opplysningerOmEtteroppgjoeretData
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.resultatType
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.rettsgebyrBeloep
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerForhaandsvarselDTOSelectors.utbetalingData
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerUtbetalingDTOSelectors.avviksBeloep
+import no.nav.pensjon.etterlatte.maler.vedlegg.omstillingsstoenad.etteroppgjoer.OpplysningerOmEtteroppgjoeretData
+import no.nav.pensjon.etterlatte.maler.vedlegg.omstillingsstoenad.etteroppgjoer.opplysningerOmEtteroppgjoeret
+import java.time.LocalDate
+
+
+data class EtteroppgjoerForhaandsvarselRedigerbartBrevDTO(val type: String) : RedigerbartUtfallBrevDTO
+
+
+@TemplateModelHelpers
+object EtteroppgjoerForhaandsvarselInnhold : EtterlatteTemplate<EtteroppgjoerForhaandsvarselRedigerbartBrevDTO>, Delmal {
+    override val kode: EtterlatteBrevKode = EtterlatteBrevKode.OMS_EO_FORHAANDSVARSEL_INNHOLD
+
+    override val template = createTemplate(
+        name = kode.name,
+        letterDataType = EtteroppgjoerForhaandsvarselRedigerbartBrevDTO::class,
+        languages = languages(Language.Bokmal, Language.Nynorsk, Language.English),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Etteroppgjør Forhåndsvarsel Innhold",
+            isSensitiv = true,
+            distribusjonstype = LetterMetadata.Distribusjonstype.VIKTIG,
+            brevtype = LetterMetadata.Brevtype.INFORMASJONSBREV,
+        ),
+    ) {
+        title {
+            text(
+                Language.Bokmal to "",
+                Language.Nynorsk to "",
+                Language.English to "",
+            )
+        }
+
+        outline {
+
+
+        }
+
+    }
+}
