232c633bc857f0d409d330242d896ee20455b664
Håkon <32221678+hconhegg@users.noreply.github.com>
2025-04-10 12:48:55 +0200

    Gjør om double formatter til å ta i mot antall desimaler. (#1434)

---
+++ brevbaker-dsl/src/test/kotlin/no/nav/pensjon/brev/template/LocalizedFormatterTest.kt
@@ -0,0 +1,29 @@
+package no.nav.pensjon.brev.template
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import no.nav.pensjon.brev.Fixtures
+import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.format
+import org.junit.jupiter.api.Test
+
+class LocalizedFormatterTest {
+
+    private val testExpressionScope = ExpressionScope(EmptyBrevdata, Fixtures.felles, Language.Bokmal)
+
+    @Test
+    fun `double formatteres som standard til 2 desimaler`() {
+        assertThat(2.0.expr().format().eval(testExpressionScope), equalTo("2,00"))
+    }
+
+    @Test
+    fun `double formatteres ikke til negative tall`() {
+        assertThat(2.0.expr().format(-1).eval(testExpressionScope), equalTo("2"))
+    }
+
+    @Test
+    fun `double formatteres ikke til mer enn 16 plasser`() {
+        assertThat(2.0.expr().format(100).eval(testExpressionScope), equalTo("2," + "0".repeat(16)))
+    }
+}
