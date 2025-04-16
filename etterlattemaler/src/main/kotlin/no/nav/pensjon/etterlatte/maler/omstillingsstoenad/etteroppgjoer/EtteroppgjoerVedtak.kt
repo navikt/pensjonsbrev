3970842b96c9606cfc7d086af0897b4896a8eeb8
Henrik Gundersen <henrik.gundersen@nav.no>
2025-04-16 09:55:32 +0200

    EY-5150: Oppsett for vedtaksbrev etteroppgjør (#1448)

---
+++ etterlattemaler/src/main/kotlin/no/nav/pensjon/etterlatte/maler/omstillingsstoenad/etteroppgjoer/EtteroppgjoerVedtak.kt
@@ -0,0 +1,61 @@
+package no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer
+
+import no.nav.pensjon.brev.template.Language.Bokmal
+import no.nav.pensjon.brev.template.Language.English
+import no.nav.pensjon.brev.template.Language.Nynorsk
+import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+import no.nav.pensjon.etterlatte.EtterlatteBrevKode
+import no.nav.pensjon.etterlatte.EtterlatteTemplate
+import no.nav.pensjon.etterlatte.maler.Element
+import no.nav.pensjon.etterlatte.maler.FerdigstillingBrevDTO
+import no.nav.pensjon.etterlatte.maler.Hovedmal
+import no.nav.pensjon.etterlatte.maler.fraser.omstillingsstoenad.OmstillingsstoenadFellesFraser
+import no.nav.pensjon.etterlatte.maler.konverterElementerTilBrevbakerformat
+import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.etteroppgjoer.EtteroppgjoerVedtakDTOSelectors.innhold
+
+data class EtteroppgjoerVedtakDTO(
+    override val innhold: List<Element>,
+    val data: EtteroppgjoerVedtakDataDTO,
+): FerdigstillingBrevDTO
+
+data class EtteroppgjoerVedtakDataDTO(
+    val bosattUtland: Boolean = false,
+)
+
+@TemplateModelHelpers
+object EtteroppgjoerVedtak: EtterlatteTemplate<EtteroppgjoerVedtakDTO>, Hovedmal {
+    override val kode: EtterlatteBrevKode = EtterlatteBrevKode.OMS_EO_VEDTAK
+
+    override val template =
+        createTemplate(
+            name = kode.name,
+            letterDataType = EtteroppgjoerVedtakDTO::class,
+            languages = languages(Bokmal, Nynorsk, English),
+            letterMetadata =
+            LetterMetadata(
+                displayTitle = "Vedtak - Etteroppgjør",
+                isSensitiv = true,
+                distribusjonstype = LetterMetadata.Distribusjonstype.VEDTAK,
+                brevtype = LetterMetadata.Brevtype.VEDTAKSBREV,
+            ),
+        ) {
+            title {
+                text(
+                    Bokmal to "TODO",
+                    Nynorsk to "TODO",
+                    English to "TODO",
+                )
+            }
+
+            outline {
+                konverterElementerTilBrevbakerformat(innhold)
+
+                includePhrase(OmstillingsstoenadFellesFraser.DuHarRettTilAaKlage)
+                includePhrase(OmstillingsstoenadFellesFraser.HarDuSpoersmaal)
+            }
+        }
+}
