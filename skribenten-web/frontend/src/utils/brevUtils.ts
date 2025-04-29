7a0123979d11d30b01ff0e34ea1e408cb3dd12db
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2025-04-29 12:53:29 +0200

    Naming consistency

--- skribenten-web/frontend/src/utils/brevUtils.ts
+++ skribenten-web/frontend/src/utils/brevUtils.ts
@@ -8,7 +8,7 @@ export const erBrevKladdEllerUnderRedigering = (brev: BrevInfo) => erBrevKladd(b
 
 export const erBrevArkivert = (brev: BrevInfo): boolean => brev.status.type === "Arkivert";
 
-export const skalBrevAttesteres = (b: BrevInfo) => b.brevtype === "VEDTAKSBREV";
+export const skalBrevAttesteres = (brev: BrevInfo) => brev.brevtype === "VEDTAKSBREV";
 export const erBrevKlarTilAttestering = (brev: BrevInfo) => brev.status.type === "Attestering";
 
 export const erBrevLaastForRedigering = (brev: BrevInfo) =>
