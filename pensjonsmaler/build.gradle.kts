7ee2ebd2c07a1641990de73237dce397388b0c9b
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2025-04-30 12:12:28 +0200

    Validerer at API-a våre er binært kompatible (#1480)
    * Startar å innføre validatoren
    
    * Oppsett
    
    * Versjonar
    
    * apiDump-utgangspunkt
    
    * Oppdaterer readme
    
    * Api-model

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 168
+val apiModelVersion = 169
 
 val apiModelJavaTarget: String by System.getProperties()
 
