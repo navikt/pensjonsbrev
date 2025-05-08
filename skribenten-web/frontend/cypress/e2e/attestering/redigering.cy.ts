4e1aae58c8d63c9514160cff2e33ee07ff110ae7
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2025-05-08 16:04:59 +0200

    E2e test fiks attestant redigering

--- skribenten-web/frontend/cypress/e2e/attestering/redigering.cy.ts
+++ skribenten-web/frontend/cypress/e2e/attestering/redigering.cy.ts
@@ -13,9 +13,9 @@ describe("attestant redigering", () => {
       fixture: "modelSpecification.json",
     }).as("modelSpecification");
 
-    cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1?reserver=true", (req) => req.reply(defaultBrev)).as(
-      "brev",
-    );
+    cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1/attestering?reserver=true", (req) =>
+      req.reply(defaultBrev),
+    ).as("loadBrev");
     cy.intercept("put", "/bff/skribenten-backend/brev/1/redigertBrev?frigiReservasjon=*", (req) => {
       req.reply(defaultBrev);
     }).as("hurtiglagreRedigertBrev");
@@ -36,7 +36,7 @@ describe("attestant redigering", () => {
       );
     });
 
-    cy.visit("/saksnummer/123456/vedtak/1/redigering");
+    cy.visit("/saksnummer/123456/attester/1/redigering");
     cy.contains("Lagret 25.09.2024 ").should("exist");
     cy.contains("weeks.").click();
     cy.focused().type(" hello!");
@@ -61,7 +61,7 @@ describe("attestant redigering", () => {
       req.reply(newBrev);
     }).as("lagreUnderskrift");
 
-    cy.visit("/saksnummer/123456/vedtak/1/redigering");
+    cy.visit("/saksnummer/123456/attester/1/redigering");
     cy.contains("Underskrift").click().type("{selectall}{backspace}").type("Den nye attestanten");
     cy.wait("@lagreUnderskrift");
 
@@ -69,7 +69,7 @@ describe("attestant redigering", () => {
   });
 
   it("Blokkerer redigering om brev er reservert av noen andre", () => {
-    cy.visit("/saksnummer/123456/vedtak/1/redigering");
+    cy.visit("/saksnummer/123456/attester/1/redigering");
     cy.intercept("GET", "/bff/skribenten-backend/brev/1/reservasjon", { fixture: "brevreservasjon_opptatt.json" }).as(
       "brevreservasjon_opptatt",
     );
@@ -81,14 +81,14 @@ describe("attestant redigering", () => {
   });
 
   it("Gjenoppta redigering", () => {
-    cy.visit("/saksnummer/123456/vedtak/1/redigering");
+    cy.visit("/saksnummer/123456/attester/1/redigering");
     cy.intercept("GET", "/bff/skribenten-backend/brev/1/reservasjon", { fixture: "brevreservasjon_opptatt.json" }).as(
       "brevreservasjon_opptatt",
     );
     cy.wait("@brevreservasjon_opptatt");
     cy.contains("Brevet er utilgjengelig for deg fordi Hugo Weaving har brevet åpent.").should("exist");
 
-    cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1?reserver=true", {
+    cy.intercept("GET", "/bff/skribenten-backend/sak/123456/brev/1/attestering?reserver=true", {
       fixture: "brevResponse_ny_hash.json",
     }).as("brev_ny_hash");
     cy.intercept("GET", "/bff/skribenten-backend/brev/1/reservasjon", { fixture: "brevreservasjon_ny_hash.json" }).as(
