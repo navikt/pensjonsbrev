97945a510f0af3338c3552df8366e082f02bbeb0
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-05-13 12:43:36 +0200

    Bump api model

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 175
+val apiModelVersion = 176
 
 val apiModelJavaTarget: String by System.getProperties()
 
