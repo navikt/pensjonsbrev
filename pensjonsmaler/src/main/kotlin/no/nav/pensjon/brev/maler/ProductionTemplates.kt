2d970ae9a0233d7d7d87e45ac35c55c42f1c7b11
Hanne Æsøy Nes <hanneasoynes@gmail.com>
2025-05-21 15:01:41 +0200

    UFO-17: Vedtaksbrev GJP årskull 1962 til 1970

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/ProductionTemplates.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/ProductionTemplates.kt
@@ -4,6 +4,12 @@ import no.nav.brev.brevbaker.AllTemplates
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.maler.adhoc.*
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VedtakOpphor1960til70
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VarselGjpForlengetArskull6061
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VedtakForlengelse6061
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VarselGjpOpphorArskill6070
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VedtakForlengelse1962til70
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VarselGjpForlengetArskull6270
 import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.*
 import no.nav.pensjon.brev.maler.alder.AvslagUttakFoerNormertPensjonsalderAuto
 import no.nav.pensjon.brev.maler.alder.AvslagUttakFoerNormertPensjonsalder
@@ -72,6 +78,7 @@ object ProductionTemplates : AllTemplates {
         VarselGjpOpphorArskill6070,
         VedtakOpphor1960til70,
         VedtakForlengelse6061,
+        VedtakForlengelse1962til70,
         VarselGjpForlengetArskull6061Utland,
     )
 
