d0f060c05c74c92f2da1046b688b85cc1b134527
Andreas Balevik <andreas.balevik@nav.no>
2025-05-22 12:17:17 +0200

    rette inntekt grunnlag beregningvedlegg

--- etterlattemaler/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/EtteroppgjoerVedtakDTO.kt
+++ etterlattemaler/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/EtteroppgjoerVedtakDTO.kt
@@ -27,7 +27,8 @@ fun createEtteroppgjoerVedtakBrevDTO() =
                 Kroner(4),
                 Kroner(40),
                 Kroner(400),
-                Kroner(4000)
+                Kroner(4000),
+                Kroner(4444)
             ),
             vedleggInnhold = emptyList(),
             utbetaltBeloep = Kroner(4444)
