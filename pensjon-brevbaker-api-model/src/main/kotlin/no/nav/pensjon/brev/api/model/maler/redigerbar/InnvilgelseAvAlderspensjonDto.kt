b31080320226bf0d900433c4fca81395fb989b4c
Jeremy Elsom <jeremy.elsom@nav.no>
2025-05-23 13:03:06 +0200

    WIP

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/InnvilgelseAvAlderspensjonDto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/InnvilgelseAvAlderspensjonDto.kt
@@ -1,34 +1,58 @@
 package no.nav.pensjon.brev.api.model.maler.redigerbar
 
 import no.nav.pensjon.brev.api.model.AlderspensjonRegelverkType
+import no.nav.pensjon.brev.api.model.MetaforceSivilstand
+import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.vedlegg.DineRettigheterOgMulighetTilAaKlageDto
+import no.nav.pensjon.brevbaker.api.model.Kroner
+import java.time.LocalDate
 
 
 @Suppress("unused")
 data class InnvilgelseAvAlderspensjonDto(
     override val pesysData: PesysData,
-    override val saksbehandlerValg,
+    override val saksbehandlerValg: SaksbehandlerValg,
 ) : RedigerbarBrevdata<InnvilgelseAvAlderspensjonDto.SaksbehandlerValg, InnvilgelseAvAlderspensjonDto.PesysData> {
 
     data class SaksbehandlerValg(
         val abc: String,
-    )
+    ): BrevbakerBrevdata
 
     data class PesysData(
         val alderspensjonVedVirk: AlderspensjonVedVirk,
+        val barnetilleggVedVirk: BarnetilleggVedVirk,
+        val beregnetPensjonPerManedVedVirk: BeregnetPensjonPerManedVedVirk,
+        val ektefelletilleggVedVirk: EktefelletilleggVedVirk,
+        val kravVirkDatoFom: LocalDate,
+        val sivilstand: MetaforceSivilstand,
         val dineRettigheterOgMulighetTilAaKlageDto: DineRettigheterOgMulighetTilAaKlageDto,
-    )
+    ): BrevbakerBrevdata
 
     data class AlderspensjonVedVirk(
         val uttaksgrad: Int,
         val regelverkType: AlderspensjonRegelverkType,
+        val totalPensjon: Kroner,
+        val uforeKombinertMedAlder: Boolean,
+        val innvilgetFor67: Boolean,
+        val gjenlevendetilleggInnvilget: Boolean,
+        val gjenlevendetilleggKap19Innvilget: Boolean,
     )
 
-    data class DineRettigheterOgMulighet
+    data class BarnetilleggVedVirk(
+        val innvilgetFellesbarn: Boolean,
+        val innvilgetSaerkullsbarn: Boolean,
+    )
+
+    data class BeregnetPensjonPerManedVedVirk(
+        val barnetilleggFellesbarn: Kroner,
+        val barnetilleggSaerkullsbarn: Kroner,
+        val ektefelletillegg: Kroner,
+        val gjenlevendetilleggKap19: Kroner,
+    )
 
     data class EktefelletilleggVedVirk(
-        val innvilegetEktefelletillegg: Boolean,
+        val innvilgetEktefelletillegg: Boolean,
 
     )
 }
---
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/redigerbar/InnvilgelseAvAlderspensjon.kt
@@ -0,0 +1,69 @@
+package no.nav.pensjon.brev.maler.redigerbar
+
+import no.nav.pensjon.brev.api.model.Sakstype
+import no.nav.pensjon.brev.api.model.Sakstype.*
+import no.nav.pensjon.brev.api.model.TemplateDescription
+import no.nav.pensjon.brev.api.model.TemplateDescription.Brevkategori.FOERSTEGANGSBEHANDLING
+import no.nav.pensjon.brev.api.model.TemplateDescription.Brevkontekst.ALLE
+import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
+import no.nav.pensjon.brev.api.model.maler.redigerbar.InnvilgelseAvAlderspensjonDto
+import no.nav.pensjon.brev.api.model.maler.redigerbar.InnvilgelseAvAlderspensjonDtoSelectors.PesysDataSelectors.alderspensjonVedVirk
+import no.nav.pensjon.brev.api.model.maler.redigerbar.InnvilgelseAvAlderspensjonDtoSelectors.pesysData
+import no.nav.pensjon.brev.api.model.maler.redigerbar.VarselTilbakekrevingAvFeilutbetaltBeloepDtoSelectors.PesysDataSelectors.sakstype
+import no.nav.pensjon.brev.api.model.maler.redigerbar.VarselTilbakekrevingAvFeilutbetaltBeloepDtoSelectors.pesysData
+import no.nav.pensjon.brev.maler.fraser.common.Redigerbar.SaksType
+import no.nav.pensjon.brev.template.Expression
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.RedigerbarTemplate
+import no.nav.pensjon.brev.template.dsl.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.format
+import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brev.template.dsl.textExpr
+import no.nav.pensjon.brevbaker.api.model.Kroner
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Distribusjonstype.VEDTAK
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Brevtype.VEDTAKSBREV
+
+
+@TemplateModelHelpers
+object InnvilgelseAvAlderspensjon : RedigerbarTemplate<InnvilgelseAvAlderspensjonDto> {
+    override val kategori: TemplateDescription.Brevkategori = FOERSTEGANGSBEHANDLING
+    override val brevkontekst: TemplateDescription.Brevkontekst = ALLE
+    override val sakstyper: Set<Sakstype> = setOf(ALDER)
+    override val kode = Pesysbrevkoder.Redigerbar.PE_AP_INNVILGELSE
+    override val template = createTemplate(
+        name = kode.name,
+        letterDataType = InnvilgelseAvAlderspensjonDto::class,
+        languages = languages(Bokmal, Nynorsk, English),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Vedtak - innvilgelse av alderspensjon",
+            isSensitiv = false,
+            distribusjonstype = VEDTAK,
+            brevtype = VEDTAKSBREV
+        )
+    ) {
+        val uttaksgrad = pesysData.alderspensjonVedVirk
+        val regelverkType = pesysData.regelverkType
+        title{
+            showIf()
+        }
+            textExpr(
+            Bokmal to "Vi har innvilget søknaden din om ".expr() + uttaksgrad + " prosent alderspensjon",
+            Nynorsk to "Vi har innvilga søknaden din om ".expr() + uttaksgrad + " prosent alderspensjon",
+            English to "We have granted your application for ".expr() + uttaksgrad + " percent retirement pension",
+            )
+            includePhrase(SaksType(pesysData.sakstype))
+        }
+        outline {
+            paragraph {
+                textExpr(
+                )
+
+    }
+
+}
+
