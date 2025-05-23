c346b2a51f3874b9d84885bf049795104a535bdc
Jeremy Elsom <jeremy.elsom@nav.no>
2025-05-23 10:17:53 +0200

    WIP

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/InnvilgelseAvAlderspensjonDto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/InnvilgelseAvAlderspensjonDto.kt
@@ -1,20 +1,34 @@
 package no.nav.pensjon.brev.api.model.maler.redigerbar
 
-import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.api.model.AlderspensjonRegelverkType
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.vedlegg.DineRettigheterOgMulighetTilAaKlageDto
 
 
 @Suppress("unused")
 data class InnvilgelseAvAlderspensjonDto(
-    override val pesysData: PesysData, override val saksbehandlerValg: EmptyBrevdata
-) : RedigerbarBrevdata<EmptyBrevdata, InnvilgelseAvAlderspensjonDto, InnvilgelseAvAlderspensjonDto.PesysData> {
+    override val pesysData: PesysData,
+    override val saksbehandlerValg,
+) : RedigerbarBrevdata<InnvilgelseAvAlderspensjonDto.SaksbehandlerValg, InnvilgelseAvAlderspensjonDto.PesysData> {
+
+    data class SaksbehandlerValg(
+        val abc: String,
+    )
+
     data class PesysData(
         val alderspensjonVedVirk: AlderspensjonVedVirk,
         val dineRettigheterOgMulighetTilAaKlageDto: DineRettigheterOgMulighetTilAaKlageDto,
-    ),
+    )
 
     data class AlderspensjonVedVirk(
         val uttaksgrad: Int,
+        val regelverkType: AlderspensjonRegelverkType,
+    )
+
+    data class DineRettigheterOgMulighet
+
+    data class EktefelletilleggVedVirk(
+        val innvilegetEktefelletillegg: Boolean,
+
     )
 }
