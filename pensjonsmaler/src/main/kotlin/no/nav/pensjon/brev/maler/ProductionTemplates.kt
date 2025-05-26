367ec27385e59e0436950ced23f1c95ef68d5dde
Petter Hannevold <petter.hannevold@nav.no>
2025-05-26 12:29:33 +0200

    UFO-19 Lagt til varselbrev for utvidet rett til gjenlevendeytelse etter 2027 for folk bosatt i utlandet

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/ProductionTemplates.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/ProductionTemplates.kt
@@ -4,11 +4,7 @@ import no.nav.brev.brevbaker.AllTemplates
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.maler.adhoc.*
-import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VedtakOpphor1960til70
-import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VarselGjpForlengetArskull6061
-import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VedtakForlengelse6061
-import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VarselGjpOpphorArskill6070
-import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.VarselGjpForlengetArskull6270
+import no.nav.pensjon.brev.maler.adhoc.gjenlevenderett2027.*
 import no.nav.pensjon.brev.maler.alder.AvslagUttakFoerNormertPensjonsalderAuto
 import no.nav.pensjon.brev.maler.alder.AvslagUttakFoerNormertPensjonsalder
 import no.nav.pensjon.brev.maler.alder.AvslagUttakFoerNormertPensjonsalderAP2016
@@ -76,6 +72,7 @@ object ProductionTemplates : AllTemplates {
         VarselGjpOpphorArskill6070,
         VedtakOpphor1960til70,
         VedtakForlengelse6061,
+        VarselGjpForlengetArskull6061Utland,
     )
 
     private val redigerbare: Set<RedigerbarTemplate<out RedigerbarBrevdata<*, *>>> = setOf(
