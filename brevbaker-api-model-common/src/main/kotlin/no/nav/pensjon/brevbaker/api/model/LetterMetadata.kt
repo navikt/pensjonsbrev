92453c3c0e421ce18e72f0841d0709360dd4b4e9
Mads Opheim <mads.opheim@nav.no>
2025-05-27 10:57:48 +0200

    Går bort frå data class for brevrequest-klassane og for LetterMetadata

--- brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LetterMetadata.kt
+++ brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LetterMetadata.kt
@@ -1,12 +1,14 @@
 package no.nav.pensjon.brevbaker.api.model
 
+import java.util.Objects
+
 /**
  * @property displayTitle Visningstittel i arkivet og brevmeny. Brukes for visning til saksbehandler og bruker. Se https://confluence.adeo.no/display/BOA/QDIST010+-+DistribuerForsendelseTilDittNAV
  * @property isSensitiv Styrer om innholdet i brevet er sensitivt. Sensitive brev skal ikke vises ved nivå 3 innlogging
  * @property distribusjonstype Styrer viktigheten og typen varsel til bruker under distribusjon. Les mer på: https://confluence.adeo.no/display/BOA/RDIST002-1.+DistribuerJournalpost+-+For+Konsumenter
  * @property brevtype Endrer på enkelte standard-tekster i grunn-malen til brevet avhengig av om brevet er ett vedtak eller ikke.
  */
-data class LetterMetadata(
+class LetterMetadata(
     val displayTitle: String,
     val isSensitiv: Boolean,
     val distribusjonstype: Distribusjonstype,
@@ -14,4 +16,18 @@ data class LetterMetadata(
 ) {
     enum class Distribusjonstype { VEDTAK, VIKTIG, ANNET, }
     enum class Brevtype{ VEDTAKSBREV, INFORMASJONSBREV }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is LetterMetadata) return false
+        return displayTitle == other.displayTitle
+                && isSensitiv == other.isSensitiv
+                && distribusjonstype == other.distribusjonstype
+                && brevtype == other.brevtype
+    }
+
+    override fun hashCode() = Objects.hash(displayTitle, isSensitiv, distribusjonstype, brevtype)
+
+    override fun toString() =
+        "LetterMetadata(displayTitle='$displayTitle', isSensitiv=$isSensitiv, distribusjonstype=$distribusjonstype, brevtype=$brevtype)"
+
 }
