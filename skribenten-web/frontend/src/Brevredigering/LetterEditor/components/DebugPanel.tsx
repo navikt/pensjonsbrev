3840de5b84aef94b463aefec4b17b42a2d4d5dc0
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-06-10 12:06:02 +0200

    Vis EditedLetter i debug-panel

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/components/DebugPanel.tsx
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/components/DebugPanel.tsx
@@ -1,11 +1,13 @@
 import { css } from "@emotion/react";
-import { HStack } from "@navikt/ds-react";
+import { Accordion, ExpansionCard, HStack } from "@navikt/ds-react";
 import { useEffect, useState } from "react";
 
+import { isItemContentIndex, isNew, text as textOf } from "~/Brevredigering/LetterEditor/actions/common";
 import { useEditor } from "~/Brevredigering/LetterEditor/LetterEditor";
-import type { LiteralIndex } from "~/Brevredigering/LetterEditor/model/state";
+import type { Focus, LetterEditorState } from "~/Brevredigering/LetterEditor/model/state";
+import { isFritekst, isLiteral, isTextContent } from "~/Brevredigering/LetterEditor/model/utils";
 import { getCaretRect, getRange } from "~/Brevredigering/LetterEditor/services/caretUtils";
-import type { AnyBlock, Content, Item } from "~/types/brevbakerTypes";
+import type { AnyBlock, Block, Content, Item } from "~/types/brevbakerTypes";
 
 export function DebugPanel() {
   const { freeze, editorState } = useEditor();
@@ -65,52 +67,187 @@ export function DebugPanel() {
         <b>X: {caretRect?.x}</b>
         <b>Y: {caretRect?.y}</b>
       </HStack>
-      <HStack gap={"4"}>
-        Edited:
-        <ul>
-          {findEdits(editorState.redigertBrev.blocks).map((litIndex, index) => (
-            <li key={index}>
-              {Object.entries(litIndex).map(([key, value]) => (
-                <div key={key}>
-                  <b>{key}: </b>
-                  <span>{value}</span>
-                </div>
-              ))}
-            </li>
-          ))}
-        </ul>
-      </HStack>
+      <LetterTree state={editorState} />
     </div>
   );
 }
 
-function findEdits(blocks: AnyBlock[]): LiteralIndex[] {
-  return blocks.flatMap(findEditsBlock);
-}
+const LetterTree = ({ state: { focus, redigertBrev } }: { state: LetterEditorState }) => {
+  return (
+    <>
+      {redigertBrev.blocks.map((b, index) => (
+        <Block block={b} focus={focus} index={index} key={index} />
+      ))}
+    </>
+  );
+};
 
-function findEditsBlock(block: AnyBlock): LiteralIndex[] {
-  return block.content.flatMap(findEditsContent).map((ind) => ({ ...ind, blockIndex: block.id ?? -1 }));
-}
+const Block = ({ block, focus, index }: { block: AnyBlock; focus: Focus; index: number }) => {
+  const blockText = block.content
+    .map((c) => (isTextContent(c) ? textOf(c) : null))
+    .filter((c) => c !== null)
+    .join("");
+  const highlightColor = getHighlightColor(isNew(block), isEdited(block));
 
-function findEditsContent(content: Content): { contentIndex: number; itemIndex?: number; itemContentIndex?: number }[] {
+  const [isOpen, setIsOpen] = useState(focus.blockIndex === index);
+  useEffect(() => {
+    setIsOpen(focus.blockIndex === index);
+  }, [focus, index]);
+
+  return (
+    <ExpansionCard
+      aria-label={`${block.type} - ${index} - ${block.id}`}
+      css={css`
+        background: var(${highlightColor});
+      `}
+      onToggle={setIsOpen}
+      open={isOpen}
+      size={"small"}
+    >
+      <ExpansionCard.Header>
+        <ExpansionCard.Title>
+          <HStack gap={"4"}>
+            <span>{block.type}</span>
+            <span>Index: {index}</span>
+            <span>ID: {block.id ?? "NEW"}</span>
+          </HStack>
+        </ExpansionCard.Title>
+        <ExpansionCard.Description>{textExtract(blockText)}</ExpansionCard.Description>
+      </ExpansionCard.Header>
+      <ExpansionCard.Content>
+        <Accordion headingSize={"xsmall"} size={"small"}>
+          {block.content.map((c, index) => (
+            <Content content={c} focus={focus} index={index} key={index} />
+          ))}
+        </Accordion>
+      </ExpansionCard.Content>
+    </ExpansionCard>
+  );
+};
+
+const Content = ({ content, focus, index }: { content: Content; focus?: Focus; index: number }) => {
+  const extract = isTextContent(content) ? textExtract(textOf(content)) : "";
+  const highlightColor = getHighlightColor(isNew(content), isEdited(content));
+
+  const [isOpen, setIsOpen] = useState(focus?.contentIndex === index);
+  useEffect(() => {
+    setIsOpen(focus?.contentIndex === index);
+  }, [focus, index]);
+
+  return (
+    <Accordion.Item
+      css={css`
+        background: var(${highlightColor});
+      `}
+      onOpenChange={setIsOpen}
+      open={isOpen}
+    >
+      <Accordion.Header>
+        <HStack gap={"4"}>
+          <span>{content.type}</span>
+          <span>Index: {index}</span>
+          <span>ID: {content.id ?? "NEW"}</span>
+          <span>
+            {isLiteral(content) && isFritekst(content) && "Fritekst: "}
+            {extract}
+          </span>
+        </HStack>
+      </Accordion.Header>
+      <Accordion.Content>
+        <ContentBody content={content} focus={focus} />
+      </Accordion.Content>
+    </Accordion.Item>
+  );
+};
+
+const ContentBody = ({ content, focus }: { content: Content; focus?: Focus }) => {
   switch (content.type) {
-    case "VARIABLE": {
-      return [];
-    }
-    case "ITEM_LIST": {
-      return content.items.flatMap(findEditsItem).map((ind) => ({ ...ind, contentIndex: content.id ?? -1 }));
-    }
-    case "LITERAL": {
-      return content.editedText ? [{ contentIndex: content.id ?? -1 }] : [];
-    }
-    case "NEW_LINE": {
-      return content?.id === null ? [] : [{ contentIndex: content.id }];
-    }
+    case "NEW_LINE":
+      return "--new line--";
+    case "ITEM_LIST":
+      return (
+        <Accordion headingSize={"xsmall"} size={"small"}>
+          {content.items.map((i, index) => (
+            <ItemBody focus={focus} index={index} item={i} key={index} />
+          ))}
+        </Accordion>
+      );
+    case "LITERAL":
+    case "VARIABLE":
+      return textOf(content);
+  }
+};
+
+const ItemBody = ({ focus, index, item }: { focus?: Focus; index: number; item: Item }) => {
+  const itemContentFocus =
+    focus && isItemContentIndex(focus)
+      ? { blockIndex: focus.blockIndex, contentIndex: focus.itemContentIndex }
+      : undefined;
+
+  const [isOpen, setIsOpen] = useState(focus && isItemContentIndex(focus) && focus.itemIndex === index);
+  useEffect(() => {
+    setIsOpen(focus && isItemContentIndex(focus) && focus.itemIndex === index);
+  }, [focus, index]);
+
+  const itemText = item.content.map(textOf).join("");
+  const highlightColor = getHighlightColor(isNew(item), isEditedItem(item));
+
+  return (
+    <Accordion.Item
+      css={css`
+        background: var(${highlightColor});
+      `}
+      onOpenChange={setIsOpen}
+      open={isOpen}
+    >
+      <Accordion.Header>
+        <HStack gap={"4"}>
+          <span>Index: {index}</span>
+          <span>ID: {item.id ?? "NEW"}</span>
+          <span>{textExtract(itemText)}</span>
+        </HStack>
+      </Accordion.Header>
+      <Accordion.Content>
+        <Accordion headingSize={"xsmall"} size={"small"}>
+          {item.content.map((c, index) => (
+            <Content content={c} focus={itemContentFocus} index={index} key={index} />
+          ))}
+        </Accordion>
+      </Accordion.Content>
+    </Accordion.Item>
+  );
+};
+
+function getHighlightColor(isNew: boolean, isEdited: boolean): string {
+  return isNew ? "--a-surface-success-subtle" : isEdited ? "--a-surface-info-subtle" : "--a-surface-default";
+}
+
+function isEdited(content: Content | AnyBlock): boolean {
+  switch (content.type) {
+    case "NEW_LINE":
+      return isNew(content);
+    case "ITEM_LIST":
+      return isNew(content) || content.deletedItems.length > 0 || content.items.some(isEditedItem);
+    case "LITERAL":
+      return isNew(content) || content.editedText !== null || content.editedFontType !== null;
+    case "VARIABLE":
+      return false;
+    case "TITLE1":
+    case "TITLE2":
+    case "PARAGRAPH":
+      return (
+        isNew(content) ||
+        (content.originalType !== null ? content.type !== content.originalType : false) ||
+        content.deletedContent.length > 0 ||
+        content.content.some(isEdited)
+      );
   }
 }
 
-function findEditsItem(item: Item): { itemIndex: number; itemContentIndex: number }[] {
-  return item.content
-    .flatMap(findEditsContent)
-    .map((ind) => ({ itemIndex: item.id ?? -1, itemContentIndex: ind.contentIndex }));
+function isEditedItem(item: Item): boolean {
+  return isNew(item) || item.deletedContent.length > 0 || item.content.some(isEdited);
+}
+
+function textExtract(str: string, maxLength: number = 65): string {
+  return str.length > maxLength ? str.slice(0, 62) + "..." : str;
 }
