41a3d7720e9e18c7d015c15500c57f01f5899690
Hanne Æsøy Nes <48721817+hanneanes@users.noreply.github.com>
2025-06-11 13:46:09 +0200

    UFO-46: Mangla ny tekst for begrunnelse avslag vedtak (#1612)

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/gjenlevenderett2027/VedtakGjpOpphorArskull6070Utland.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/adhoc/gjenlevenderett2027/VedtakGjpOpphorArskull6070Utland.kt
@@ -143,26 +143,41 @@ object VedtakGjpOpphorArskull6070Utland : AutobrevTemplate<Gjenlevenderett2027Dt
 
             includePhrase(DineInntekterTabell(inntekt2019, inntekt2020, inntekt2021, inntekt2022, inntekt2023, gjennomsnittInntektG, inntekt2019G, inntekt2020G, inntekt2021G, inntekt2022G, inntekt2023G))
 
-            paragraph {
-                showIf(inntekt2022Over3g and inntekt2023Over3g) {
+            showIf(inntekt2022Over3g and inntekt2023Over3g) {
+                paragraph {
                     text(
                         Bokmal to "Din inntekt har ifølge våre opplysninger vært høyere enn inntektsgrensen i 2022 og 2023. ",
-                        English to "According to our information, your income was higher than the income limit in 2022 and 2023. "
+                        English to "According to our information, your income has been higher than the income cap for 2022 and 2023."
                     )
-                }.orShowIf(inntekt2022Over3g) {
+                }
+
+            }.orShowIf(inntekt2022Over3g) {
+                paragraph {
                     text(
                         Bokmal to "Din inntekt har ifølge våre opplysninger vært høyere enn inntektsgrensen i 2022. ",
-                        English to "According to our information, your income was higher than the income limit in 2022. "
+                        English to "According to our information, your income has been higher than the income cap for 2022."
                     )
-                }.orShowIf(inntekt2023Over3g) {
+                }
+            }.orShowIf(inntekt2023Over3g) {
+                paragraph {
                     text(
                         Bokmal to "Din inntekt har ifølge våre opplysninger vært høyere enn inntektsgrensen i 2023. ",
-                        English to "According to our information, your income was higher than the income limit in 2023."
+                        English to "According to our information, your income has been higher than the income cap for 2023."
                     )
-                }.orShow {
+                }
+            }.orShow {
+                paragraph {
+                    text(
+                        Bokmal to "Din inntekt har ifølge våre opplysninger vært høyere enn inntektsgrensen i perioden 2019–2023. ",
+                        English to "According to our information, your income has been higher than the income cap during the period 2019–2023. ",
+                    )
+                }
+                paragraph {
                     textExpr(
-                        Bokmal to "Din gjennomsnittlige inntekt mellom 2019 og 2023 har ifølge våre opplysninger vært høyere enn gjennomsnittlig G de fem årene (".expr() + 212_813.expr().format(CurrencyFormat) + " kroner)",
-                        English to "Your average income between 2019 and 2023 has, according to our information, been higher than the average G during those five years (NOK ".expr() + 212_813.expr().format(CurrencyFormat) + ")"
+                        Bokmal to "Vi har vurdert om gjennomsnittet av din inntekt som antall G for disse årene har vært høyere enn 2 G. Resultatet viser at din gjennomsnittlige inntekt har vært ".expr() + gjennomsnittInntektG.format(6) + " G. " +
+                                "Kravet om at inntekten må ha vært under to ganger grunnbeløpet i folketrygden (G) i gjennomsnitt i disse fem årene, er derfor ikke oppfylt.",
+                        English to "We have assessed whether the average of your income, expressed as a number of G for these years, has been higher than 2 G. The result shows that your average income has been ".expr() + gjennomsnittInntektG.format(6) + " G. " +
+                                "The requirement that the income must have been below twice the average National Insurance basic amount (G) on average during these five years is therefore not met."
                     )
                 }
             }
