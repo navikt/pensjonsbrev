51b5978ca8fca1053c62ca14100b287f4389b0a3
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2025-06-23 19:09:05 +0200

    Fix Switch is not exhaustive linting error

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/components/DebugPanel.tsx
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/components/DebugPanel.tsx
@@ -184,6 +184,8 @@ const ContentBody = ({ content, focus }: { content: Content; focus?: Focus }) =>
     case "LITERAL":
     case "VARIABLE":
       return textOf(content);
+    case "TABLE":
+      return null;
   }
 };
 
@@ -250,6 +252,8 @@ function isEdited(content: Content | AnyBlock): boolean {
         content.deletedContent.length > 0 ||
         content.content.some(isEdited)
       );
+    case "TABLE":
+      return false;
   }
   return false;
 }
