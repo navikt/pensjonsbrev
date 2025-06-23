62d00bdacbb22c9c60bbe98fd43a996399a59807
Espen Schei <espen.schei@nav.no>
2025-06-23 08:26:50 +0200

    Endrer brevtype til Vedtaksbrev for avslagsbrevene

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/alder/avslag/gradsendring/AvslagGradsendringFoerNormertPensjonsalder.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/alder/avslag/gradsendring/AvslagGradsendringFoerNormertPensjonsalder.kt
@@ -26,7 +26,6 @@ import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata
-import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Brevtype.INFORMASJONSBREV
 
 @TemplateModelHelpers
 object AvslagGradsendringFoerNormertPensjonsalder : RedigerbarTemplate<AvslagUttakFoerNormertPensjonsalderDto> {
@@ -41,7 +40,7 @@ object AvslagGradsendringFoerNormertPensjonsalder : RedigerbarTemplate<AvslagUtt
             displayTitle = "Vedtak - avslag endring av uttaksgrad - AP2025",
             isSensitiv = true,
             distribusjonstype = LetterMetadata.Distribusjonstype.VIKTIG,
-            brevtype = INFORMASJONSBREV, // todo: skal være vedtaksbrev (når attestering er ferdig)
+            brevtype = LetterMetadata.Brevtype.VEDTAKSBREV
         )
     ) {
         title {
