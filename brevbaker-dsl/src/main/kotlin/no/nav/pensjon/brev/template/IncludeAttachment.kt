770e3c4e4e48f2a558b73b7f55006aac73a12e9b
Mads Opheim <mads.opheim@nav.no>
2025-06-23 14:10:46 +0200

    NamedReference er jo ei eiga greie, så det kan fortene si eiga fil

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/IncludeAttachment.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/IncludeAttachment.kt
@@ -2,8 +2,6 @@ package no.nav.pensjon.brev.template
 
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
 import no.nav.pensjon.brev.template.dsl.PlainTextOnlyScope
-import no.nav.pensjon.brev.template.dsl.TextScope
-import no.nav.pensjon.brev.template.dsl.text
 import java.util.Objects
 
 fun <Lang : LanguageSupport, LetterData : Any> createAttachment(
@@ -25,41 +23,6 @@ fun <Lang : LanguageSupport, LetterData : Any> createAttachment(
     OutlineOnlyScope<Lang, LetterData>().apply(outline).elements,
     includeSakspart
 )
-
-private const val bokmalstart = "«"
-private const val nynorskstart = "«"
-private const val englishstart = "'"
-
-private const val bokmalend = "»"
-private const val nynorskend = "»"
-private const val englishend = "'"
-
-fun TextScope<BaseLanguages, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
-    text(Language.Bokmal to bokmalstart, Language.Nynorsk to nynorskstart, Language.English to englishstart)
-    attachment.title.forEach { addTextContent(it) }
-    text(Language.Bokmal to bokmalend, Language.Nynorsk to nynorskend, Language.English to englishend)
-}
-
-@JvmName("namedReferenceBokmalNynorsk")
-fun TextScope<LangBokmalNynorsk, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
-    text(Language.Bokmal to bokmalstart, Language.Nynorsk to nynorskstart)
-    attachment.title.forEach { addTextContent(it) }
-    text(Language.Bokmal to bokmalend, Language.Nynorsk to nynorskend)
-}
-
-@JvmName("namedReferenceBokmalEnglish")
-fun TextScope<LangBokmalEnglish, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
-    text(Language.Bokmal to bokmalstart, Language.English to englishstart)
-    attachment.title.forEach { addTextContent(it as TextElement<LangBokmalEnglish>) }
-    text(Language.Bokmal to bokmalend, Language.English to englishend)
-}
-
-@JvmName("namedReferenceBokmal")
-fun TextScope<LangBokmal, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
-    text(Language.Bokmal to bokmalstart)
-    attachment.title.forEach { addTextContent(it as TextElement<LangBokmal>) }
-    text(Language.Bokmal to bokmalend)
-}
 
 class IncludeAttachment<out Lang : LanguageSupport, AttachmentData : Any> internal constructor(
     val data: Expression<AttachmentData>,
---
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/NamedReference.kt
@@ -0,0 +1,46 @@
+package no.nav.pensjon.brev.template
+
+import no.nav.pensjon.brev.template.Language.Bokmal
+import no.nav.pensjon.brev.template.Language.English
+import no.nav.pensjon.brev.template.Language.Nynorsk
+import no.nav.pensjon.brev.template.dsl.TextScope
+import no.nav.pensjon.brev.template.dsl.text
+
+
+private fun start(language: Language) = when (language) {
+    Bokmal -> "«"
+    Nynorsk -> "«"
+    English -> "'"
+}
+private fun end(language: Language) = when (language) {
+    Bokmal -> "»"
+    Nynorsk -> "»"
+    English -> "'"
+}
+
+fun TextScope<BaseLanguages, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
+    text(Bokmal to start(Bokmal), Nynorsk to start(Nynorsk), English to start(English))
+    attachment.title.forEach { addTextContent(it) }
+    text(Bokmal to end(Bokmal), Nynorsk to end(Nynorsk), English to end(English))
+}
+
+@JvmName("namedReferenceBokmalNynorsk")
+fun TextScope<LangBokmalNynorsk, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
+    text(Bokmal to start(Bokmal), Nynorsk to start(Nynorsk))
+    attachment.title.forEach { addTextContent(it) }
+    text(Bokmal to end(Bokmal), Nynorsk to end(Nynorsk))
+}
+
+@JvmName("namedReferenceBokmalEnglish")
+fun TextScope<LangBokmalEnglish, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
+    text(Bokmal to start(Bokmal), English to start(English))
+    attachment.title.forEach { addTextContent(it as TextElement<LangBokmalEnglish>) }
+    text(Bokmal to end(Bokmal), English to end(English))
+}
+
+@JvmName("namedReferenceBokmal")
+fun TextScope<LangBokmal, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
+    text(Bokmal to start(Bokmal))
+    attachment.title.forEach { addTextContent(it as TextElement<LangBokmal>) }
+    text(Bokmal to end(Bokmal))
+}
