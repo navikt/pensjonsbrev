100bdb88a846362a25bc4c45e46126c3dbdf3d53
Mads Opheim <mads.opheim@nav.no>
2025-06-24 17:00:04 +0200

    Gjer om også Kroner, Telefonnnummer og Foedselsnummer til value classes

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/PrimitiveModule.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/PrimitiveModule.kt
@@ -9,10 +9,16 @@ import com.fasterxml.jackson.databind.node.IntNode
 import com.fasterxml.jackson.databind.node.TextNode
 import no.nav.pensjon.brevbaker.api.model.Days
 import no.nav.pensjon.brevbaker.api.model.DaysWrapper
+import no.nav.pensjon.brevbaker.api.model.Foedselsnummer
+import no.nav.pensjon.brevbaker.api.model.FoedselsnummerWrapper
+import no.nav.pensjon.brevbaker.api.model.Kroner
+import no.nav.pensjon.brevbaker.api.model.KronerWrapper
 import no.nav.pensjon.brevbaker.api.model.Months
 import no.nav.pensjon.brevbaker.api.model.MonthsWrapper
 import no.nav.pensjon.brevbaker.api.model.Percent
 import no.nav.pensjon.brevbaker.api.model.PercentWrapper
+import no.nav.pensjon.brevbaker.api.model.Telefonnummer
+import no.nav.pensjon.brevbaker.api.model.TelefonnummerWrapper
 import no.nav.pensjon.brevbaker.api.model.Year
 import no.nav.pensjon.brevbaker.api.model.YearWrapper
 
@@ -25,6 +31,9 @@ object PrimitiveModule : SimpleModule() {
         addDeserializer(Months::class.java, monthsDeserializer())
         addDeserializer(Days::class.java, daysDeserializer())
         addDeserializer(Percent::class.java, percentDeserializer())
+        addDeserializer(Telefonnummer::class.java, telefonnummerDeserializer())
+        addDeserializer(Foedselsnummer::class.java, foedselsnummerDeserializer())
+        addDeserializer(Kroner::class.java, kronerDeserializer())
     }
 
     private fun yearDeserializer() = object : StdDeserializer<Year>(Year::class.java) {
@@ -61,6 +70,33 @@ object PrimitiveModule : SimpleModule() {
                 is IntNode -> Percent(p.codec.treeToValue(node, Int::class.java))
                 is TextNode -> Percent(p.codec.treeToValue(node, String::class.java).toInt())
                 else -> Percent(p.codec.treeToValue(node, PercentWrapper::class.java).value)
+            }
+    }
+
+    private fun telefonnummerDeserializer() = object : StdDeserializer<Telefonnummer>(Telefonnummer::class.java) {
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+            when (val node = p.codec.readTree<JsonNode>(p)) {
+                is IntNode -> Telefonnummer(p.codec.treeToValue(node, Int::class.java).toString())
+                is TextNode -> Telefonnummer(p.codec.treeToValue(node, String::class.java))
+                else -> Telefonnummer(p.codec.treeToValue(node, TelefonnummerWrapper::class.java).value)
+            }
+    }
+
+    private fun foedselsnummerDeserializer() = object : StdDeserializer<Foedselsnummer>(Foedselsnummer::class.java) {
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+            when (val node = p.codec.readTree<JsonNode>(p)) {
+                is IntNode -> Foedselsnummer(p.codec.treeToValue(node, Int::class.java).toString())
+                is TextNode -> Foedselsnummer(p.codec.treeToValue(node, String::class.java))
+                else -> Foedselsnummer(p.codec.treeToValue(node, FoedselsnummerWrapper::class.java).value)
+            }
+    }
+
+    private fun kronerDeserializer() = object : StdDeserializer<Kroner>(Kroner::class.java) {
+        override fun deserialize(p: JsonParser, ctxt: DeserializationContext) =
+            when (val node = p.codec.readTree<JsonNode>(p)) {
+                is IntNode -> Kroner(p.codec.treeToValue(node, Int::class.java))
+                is TextNode -> Kroner(p.codec.treeToValue(node, String::class.java).toInt())
+                else -> Kroner(p.codec.treeToValue(node, KronerWrapper::class.java).value)
             }
     }
 }
