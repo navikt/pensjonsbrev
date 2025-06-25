9c8fe178b9c5478a9d4b19561d347937a11fe306
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-06-25 12:52:26 +0200

    Fiks teit feil

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/NamedReference.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/NamedReference.kt
@@ -6,14 +6,16 @@ import no.nav.pensjon.brev.template.dsl.TextScope
 fun <Lang : BaseLanguages> TextScope<in Lang, *>.namedReference(attachment: AttachmentTemplate<Lang, *>) {
     eval(QuotationMarks.start)
     attachment.title.forEach { addTextContent(it) }
-    eval(QuotationMarks.start)
+    eval(QuotationMarks.end)
 }
 
 @JvmName("namedReferenceBokmalEnglish")
 fun TextScope<LangBokmalEnglish, *>.namedReference(attachment: AttachmentTemplate<BaseLanguages, *>) {
+    eval(QuotationMarks.start)
     attachment.title.forEach {
         // Safe because we know that a template that support BaseLanguages will support Bokmal and English
         @Suppress("UNCHECKED_CAST")
         addTextContent(it as TextElement<LangBokmalEnglish>)
     }
+    eval(QuotationMarks.end)
 }
