be20b46d6d0d7225c919e670423b816237b8012f
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2025-07-04 11:47:55 +0200

    Vedtak om fjerning av omsorgspoeng (#1685)
    * Versjon
    
    * Legg inn brevet
    
    * Konsekvent i brevkode med å starte alders- eller andre pensjonsbrev med PE

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 195
+val apiModelVersion = 196
 
 val apiModelJavaTarget: String by System.getProperties()
 
