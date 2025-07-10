c9f52d2f07b896c0780b31aa67f16d35616ab1a4
Jeremy Elsom <jeremy.elsom@nav.no>
2025-07-10 09:21:27 +0200

    Bumped gradle til 200

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 188
+val apiModelVersion = 200
 
 val apiModelJavaTarget: String by System.getProperties()
 
