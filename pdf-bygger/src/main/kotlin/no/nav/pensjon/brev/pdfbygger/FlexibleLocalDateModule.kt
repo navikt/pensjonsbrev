d8a3965b2459d8ca36b552cbaf4e1ba018bb445d
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-07-31 10:22:19 +0200

    deserialisering i  skribenten og pdf-bygger også

--- pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/FlexibleLocalDateModule.kt
+++ pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/FlexibleLocalDateModule.kt
@@ -1,16 +1,26 @@
 package no.nav.pensjon.brev.pdfbygger
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.core.util.JacksonFeatureSet
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.exc.InvalidFormatException
 import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeFeature
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 // TODO: Kan fjernes så snart deployment er ferdig. Det er kun Brevbaker som per nå sender LetterMarkup,
 //       og den vil serialisere LocalDate som `yyyy-MM-dd` så snart den er deployet.
 object FlexibleLocalDateModule : SimpleModule() {
+
+   private val javaTimeDeserializer = LocalDateDeserializer.INSTANCE.withFeatures(
+        JacksonFeatureSet.fromDefaults(
+            JavaTimeFeature.entries.toTypedArray()
+        )
+    )
 
     // Kreves av Serializable (siden dette er et object)
     @Suppress("unused")
@@ -25,6 +35,10 @@ object FlexibleLocalDateModule : SimpleModule() {
             private val noShortFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
             override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): LocalDate {
+                if (!parser.hasToken(JsonToken.VALUE_STRING)) {
+                    return javaTimeDeserializer.deserialize(parser, ctxt)
+                }
+
                 val value = parser.text.trim()
                 return try {
                     LocalDate.parse(value, isoFormatter)
