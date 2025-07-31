c0f4d6369c09a82a00c603b0920a9b225e5e291f
Alexander Hoem Rosbach <82815189+routsi@users.noreply.github.com>
2025-07-31 10:27:52 +0200

    Fiks deserialisering av standard jackson serialiserte LocalDate (#1740)
    * Fiks deserialisering av standard jackson serialiserte LocalDate
    
    * deserialisering i  skribenten og pdf-bygger også

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/FlexibleLocalDateModule.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/FlexibleLocalDateModule.kt
@@ -1,16 +1,26 @@
 package no.nav.pensjon.brev.skribenten
 
 import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.core.JsonToken
+import com.fasterxml.jackson.core.util.JacksonFeatureSet
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.exc.InvalidFormatException
 import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeFeature
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 // TODO: Når alle brevredigeringer har `sistreservert` nyere enn 2025-07-30 (da LetterMarkup.Sakspart.dokumentDato ble endret til LocalDate),
 //       kan denne modulen fjernes og `JavaTimeModule` brukes direkte.
 object FlexibleLocalDateModule : SimpleModule() {
+
+    private val javaTimeDeserializer = LocalDateDeserializer.INSTANCE.withFeatures(
+        JacksonFeatureSet.fromDefaults(
+            JavaTimeFeature.entries.toTypedArray()
+        )
+    )
 
     // Kreves av Serializable (siden dette er et object)
     @Suppress("unused")
@@ -25,6 +35,10 @@ object FlexibleLocalDateModule : SimpleModule() {
         private val norwegianShortFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy")
 
         override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): LocalDate {
+            if (!parser.hasToken(JsonToken.VALUE_STRING)) {
+                return javaTimeDeserializer.deserialize(parser, ctxt)
+            }
+
             val value = parser.text.trim()
             return try {
                 LocalDate.parse(value, isoFormatter)
