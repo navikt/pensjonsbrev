209f03f1e2019373c5bbd0d3e0b855185438ad53
Mads Opheim <mads.opheim@nav.no>
2025-08-07 10:43:47 +0200

    Forbetrar testen som genererer opp alle malane til å kunne filtrere ut før køyring, og dermed bli meir oversiktleg særleg for pdf-generering

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/template/GenererAlleMaleneTest.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/template/GenererAlleMaleneTest.kt
@@ -14,31 +14,26 @@ import no.nav.pensjon.brev.maler.example.EksempelbrevRedigerbart
 import no.nav.pensjon.brev.maler.example.LetterExample
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-class GenererAlleMaleneTest {
 
-    private val filterForPDF = listOf<String>()
+val filterForPDF = listOf(LetterExample.kode)
+
+class GenererAlleMaleneTest {
 
     @Tag(TestTags.MANUAL_TEST)
     @ParameterizedTest(name = "{1}, {3}")
-    @MethodSource("alleMalene")
+    @MethodSource("filtrerteMaler")
     fun <T : Any> testPdf(
         template: LetterTemplate<LanguageSupport, T>,
         brevkode: Brevkode<*>,
         fixtures: T,
         spraak: Language,
     ) {
-        if (filterForPDF.isNotEmpty()) {
-            if (!filterForPDF.any { template.name.lowercase().contains(it.lowercase()) }) {
-                return
-            }
-        }
         if (!template.language.supports(spraak)) {
-            println("Mal ${template.name} fins ikke på språk $spraak, tester ikke denne")
+            println("Mal ${template.name} fins ikke på språk ${spraak.javaClass.simpleName.lowercase()}, tester ikke denne")
             return
         }
         val letter = LetterTestImpl(template, fixtures, spraak, Fixtures.felles)
@@ -55,7 +50,7 @@ class GenererAlleMaleneTest {
         spraak: Language,
     ) {
         if (!template.language.supports(spraak)) {
-            println("Mal ${template.name} fins ikke på språk ${spraak.javaClass.simpleName}, tester ikke denne")
+            println("Mal ${template.name} fins ikke på språk ${spraak.javaClass.simpleName.lowercase()}, tester ikke denne")
             return
         }
         LetterTestImpl(
@@ -69,58 +64,41 @@ class GenererAlleMaleneTest {
     private fun filnavn(brevkode: Brevkode<*>, spraak: Language) =
         "${brevkode.kode()}_${spraak.javaClass.simpleName}"
 
-
     @Test
     fun `alle maler skal bruke en unik brevkode`() {
         val malKoder = (ProductionTemplates.hentRedigerbareMaler() + ProductionTemplates.hentRedigerbareMaler())
             .map { it.kode.kode() }
 
         malKoder.sorted().zipWithNext { a, b ->
-            assert(a != b) {"Alle brevmaler må bruke egne unike brevkoder! Brevkode $a brukes i flere brev."} }
+            assert(a != b) { "Alle brevmaler må bruke egne unike brevkoder! Brevkode $a brukes i flere brev." }
+        }
     }
 
     companion object {
         @JvmStatic
-        fun alleMalene(): List<Arguments> {
+        fun filtrerteMaler(): List<Arguments> = finnMaler(filterForPDF)
+
+        @JvmStatic
+        fun alleMalene(): List<Arguments> = finnMaler(listOf())
+
+        @JvmStatic
+        fun finnMaler(filter: List<Brevkode<*>> = listOf()): List<Arguments> {
             FeatureToggleSingleton.init(FeatureToggleDummy)
             return listOf(Language.Nynorsk, Language.Bokmal, Language.English).flatMap { spraak ->
-                ProductionTemplates.hentAutobrevmaler().map {
-                    Arguments.of(
-                        it.template,
-                        it.kode,
-                        Fixtures.create(it.template.letterDataType),
-                        spraak,
-                    )
-                } + ProductionTemplates.hentRedigerbareMaler().map {
-                    Arguments.of(
-                        it.template,
-                        it.kode,
-                        Fixtures.create(it.template.letterDataType),
-                        spraak,
-                    )
-                } + LetterExample.let {
-                    Arguments.of(
-                        LetterExample.template,
-                        LetterExample.kode,
-                        Fixtures.create(LetterExample.template.letterDataType),
-                        spraak,
-                    )
-                } + EksempelbrevRedigerbart.let {
-                    Arguments.of(
-                        EksempelbrevRedigerbart.template,
-                        EksempelbrevRedigerbart.kode,
-                        Fixtures.create(EksempelbrevRedigerbart.template.letterDataType),
-                        spraak,
-                    )
-                }
+                (ProductionTemplates.hentAutobrevmaler() +
+                        ProductionTemplates.hentRedigerbareMaler()
+                        + LetterExample
+                        + EksempelbrevRedigerbart
+                        ).filter { filter.isEmpty() || filter.any { f -> it.kode.kode() == f.kode() } }
+                    .map { Arguments.of(it.template, it.kode, Fixtures.create(it.template.letterDataType), spraak) }
             }
         }
     }
 }
 
 object FeatureToggleDummy : FeatureToggleService {
-    val toggles: MutableMap<FeatureToggle, Boolean> = mutableMapOf()
+    private val toggles: MutableMap<FeatureToggle, Boolean> = mutableMapOf()
 
-    override fun isEnabled(toggle: FeatureToggle): Boolean = toggles.get(toggle) ?: false
+    override fun isEnabled(toggle: FeatureToggle): Boolean = toggles[toggle] ?: false
 
 }
