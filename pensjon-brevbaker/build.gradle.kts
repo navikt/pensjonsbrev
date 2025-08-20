e8bba4f494fd2b08c45a5e0ae27ff39a56e5b592
Mads Opheim <mads.opheim@nav.no>
2025-08-20 20:44:56 +0200

    Introduserer og tar i bruk ufoeremaler-modul

--- pensjon-brevbaker/build.gradle.kts
+++ pensjon-brevbaker/build.gradle.kts
@@ -106,6 +106,7 @@ dependencies {
     implementation(libs.connect.runtime)
 
     implementation(project(":pensjonsmaler"))
+    implementation(project(":ufoeremaler"))
     implementation(project(":etterlattemaler"))
     implementation(project(":brevbaker"))
     ksp(project(":template-model-generator"))
--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/BrevbakerApp.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/BrevbakerApp.kt
@@ -1,9 +1,21 @@
 package no.nav.pensjon.brev
 
 import io.ktor.server.application.Application
+import no.nav.brev.brevbaker.AllTemplates
+import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.maler.ProductionTemplates
+import no.nav.pensjon.brev.template.AutobrevTemplate
+import no.nav.pensjon.brev.template.RedigerbarTemplate
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-fun Application.brevbakerModulePensjon() = this.brevbakerModule(ProductionTemplates)
+fun Application.brevbakerModulePensjon() = this.brevbakerModule(pensjonOgUfoereProductionTemplates)
+
+val pensjonOgUfoereProductionTemplates = object : AllTemplates {
+    override fun hentAutobrevmaler() = ProductionTemplates.hentAutobrevmaler()
+
+    override fun hentRedigerbareMaler() = ProductionTemplates.hentRedigerbareMaler()
+
+}
