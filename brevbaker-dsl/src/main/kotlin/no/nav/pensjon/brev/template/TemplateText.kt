212f94a78fb19b67b1c109ca24bb9b58c84f14eb
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-08-29 12:25:04 +0200

    WIP

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/TemplateText.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/TemplateText.kt
@@ -3,6 +3,9 @@ package no.nav.pensjon.brev.template.dsl
 import no.nav.pensjon.brev.template.*
 import no.nav.pensjon.brev.template.ContentOrControlStructure.*
 import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType
+import no.nav.pensjon.brev.template.dsl.LiteralOrExpressionBuilder.*
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.expression.plus
 
 @LetterTemplateMarker
 class TextOnlyScope<Lang : LanguageSupport, LetterData : Any> internal constructor(): TextScope<Lang, LetterData>, ControlStructureScope<Lang, LetterData, Element.OutlineContent.ParagraphContent.Text<Lang>, TextOnlyScope<Lang, LetterData>> {
@@ -59,11 +62,21 @@ sealed interface PlainTextScope<Lang : LanguageSupport, LetterData : Any> : Temp
     fun eval(expression: StringExpression) {
         addTextContent(Content(Element.OutlineContent.ParagraphContent.Text.Expression(expression, FontType.PLAIN)))
     }
+
+    fun bokmal(block: LiteralOrExpressionBuilder.() -> LiteralOrExpression): Pair<Language.Bokmal, LiteralOrExpression> =
+        Language.Bokmal to LiteralOrExpressionBuilder().block()
+
+    fun nynorsk(block: LiteralOrExpressionBuilder.() -> LiteralOrExpression): Pair<Language.Nynorsk, LiteralOrExpression> =
+        Language.Nynorsk to LiteralOrExpressionBuilder().block()
+
+    fun english(block: LiteralOrExpressionBuilder.() -> LiteralOrExpression): Pair<Language.English, LiteralOrExpression> =
+        Language.English to LiteralOrExpressionBuilder().block()
 }
 
 // TextScope.text()
 //
 //
+@JvmName("oldText")
 fun <Lang1 : Language, ParameterType : Any> TextScope<LanguageSupport.Single<Lang1>, ParameterType>.text(
     lang1: Pair<Lang1, String>,
     fontType: FontType = FontType.PLAIN,
@@ -71,6 +84,7 @@ fun <Lang1 : Language, ParameterType : Any> TextScope<LanguageSupport.Single<Lan
     Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1, fontType).also { addTextContent(Content(it)) }
 }
 
+@JvmName("oldText")
 fun <Lang1 : Language, Lang2 : Language, ParameterType : Any> TextScope<LanguageSupport.Double<Lang1, Lang2>, ParameterType>.text(
     lang1: Pair<Lang1, String>,
     lang2: Pair<Lang2, String>,
@@ -79,6 +93,7 @@ fun <Lang1 : Language, Lang2 : Language, ParameterType : Any> TextScope<Language
     Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1, lang2, fontType).also { addTextContent(Content(it)) }
 }
 
+@JvmName("oldText")
 fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> TextScope<LanguageSupport.Triple<Lang1, Lang2, Lang3>, ParameterType>.text(
     lang1: Pair<Lang1, String>,
     lang2: Pair<Lang2, String>,
@@ -91,6 +106,7 @@ fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> 
 // TextScope.textExpr()
 //
 //
+
 fun <Lang1 : Language, ParameterType : Any> TextScope<LanguageSupport.Single<Lang1>, ParameterType>.textExpr(
     lang1: Pair<Lang1, StringExpression>,
     fontType: FontType = FontType.PLAIN,
@@ -119,12 +135,14 @@ fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> 
 // PlainTextScope.text()
 //
 //
+@JvmName("oldText")
 fun <Lang1 : Language, ParameterType : Any> PlainTextScope<LanguageSupport.Single<Lang1>, ParameterType>.text(
     lang1: Pair<Lang1, String>,
 ) {
     Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1).also { addTextContent(Content(it)) }
 }
 
+@JvmName("oldText")
 fun <Lang1 : Language, Lang2 : Language, ParameterType : Any> PlainTextScope<LanguageSupport.Double<Lang1, Lang2>, ParameterType>.text(
     lang1: Pair<Lang1, String>,
     lang2: Pair<Lang2, String>,
@@ -132,6 +150,7 @@ fun <Lang1 : Language, Lang2 : Language, ParameterType : Any> PlainTextScope<Lan
     Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1, lang2).also { addTextContent(Content(it)) }
 }
 
+@JvmName("oldText")
 fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> PlainTextScope<LanguageSupport.Triple<Lang1, Lang2, Lang3>, ParameterType>.text(
     lang1: Pair<Lang1, String>,
     lang2: Pair<Lang2, String>,
@@ -162,4 +181,161 @@ fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> 
     lang3: Pair<Lang3, StringExpression>,
 ) {
     Element.OutlineContent.ParagraphContent.Text.Expression.ByLanguage.create(lang1, lang2, lang3).also { addTextContent(Content(it)) }
+}
+
+// new text functions that uses LiteralOrExpressionBuilder
+// TextScope.text()
+//
+//
+
+fun <Lang1 : Language, ParameterType : Any> TextScope<LanguageSupport.Single<Lang1>, ParameterType>.text(
+    lang1: Pair<Lang1, LiteralOrExpression>,
+    fontType: FontType = FontType.PLAIN,
+    ) {
+    when (val value = lang1.second) {
+        is ExpressionWrapper ->
+            Element.OutlineContent.ParagraphContent.Text.Expression.ByLanguage.create(lang1.first to value.expr, fontType)
+                .also { addTextContent(Content(it)) }
+
+        is LiteralWrapper ->
+            Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1.first to value.str)
+                .also { addTextContent(Content(it)) }
+    }
+}
+
+fun <Lang1 : Language, Lang2 : Language, ParameterType : Any> TextScope<LanguageSupport.Double<Lang1, Lang2>, ParameterType>.text(
+    lang1: Pair<Lang1, LiteralOrExpression>,
+    lang2: Pair<Lang2, LiteralOrExpression>,
+    fontType: FontType = FontType.PLAIN,
+    ) {
+    val lang1Value = lang1.second
+    val lang2Value = lang2.second
+
+    if (lang1Value is LiteralWrapper && lang2Value is LiteralWrapper) {
+        Element.OutlineContent.ParagraphContent.Text.Literal.create(
+            lang1.first to lang1Value.str,
+            lang2.first to lang2Value.str,
+            fontType
+        ).also { addTextContent(Content(it)) }
+    } else {
+        Element.OutlineContent.ParagraphContent.Text.Expression.ByLanguage.create(
+            lang1.first to lang1.second.expr,
+            lang2.first to lang2.second.expr,
+            fontType
+        ).also { addTextContent(Content(it)) }
+    }
+}
+
+fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> TextScope<LanguageSupport.Triple<Lang1, Lang2, Lang3>, ParameterType>.text(
+    lang1: Pair<Lang1, LiteralOrExpression>,
+    lang2: Pair<Lang2, LiteralOrExpression>,
+    lang3: Pair<Lang3, LiteralOrExpression>,
+    fontType: FontType = FontType.PLAIN,
+    ) {
+    val lang1Value = lang1.second
+    val lang2Value = lang2.second
+    val lang3Value = lang3.second
+
+    if (lang1Value is LiteralWrapper && lang2Value is LiteralWrapper && lang3Value is LiteralWrapper) {
+        Element.OutlineContent.ParagraphContent.Text.Literal.create(
+            lang1.first to lang1Value.str,
+            lang2.first to lang2Value.str,
+            lang3.first to lang3Value.str,
+            fontType
+        ).also { addTextContent(Content(it)) }
+.also { addTextContent(Content(it)) }
+    } else {
+        Element.OutlineContent.ParagraphContent.Text.Literal.create(
+            lang1.first to (lang1.second as LiteralWrapper).str,
+            lang2.first to (lang2.second as LiteralWrapper).str,
+            lang3.first to (lang3.second as LiteralWrapper).str,
+            fontType
+        ).also { addTextContent(Content(it)) }
+    }
+}
+
+// PlainTextScope.text()
+//
+//
+
+fun <Lang1 : Language, ParameterType : Any> PlainTextScope<LanguageSupport.Single<Lang1>, ParameterType>.text(
+    lang1: Pair<Lang1, LiteralOrExpression>,
+) {
+    when (val value = lang1.second) {
+        is ExpressionWrapper ->
+            Element.OutlineContent.ParagraphContent.Text.Expression.ByLanguage.create(lang1.first to value.expr)
+                .also { addTextContent(Content(it)) }
+
+        is LiteralWrapper ->
+            Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1.first to value.str)
+                .also { addTextContent(Content(it)) }
+    }
+}
+
+fun <Lang1 : Language, Lang2 : Language, ParameterType : Any> PlainTextScope<LanguageSupport.Double<Lang1, Lang2>, ParameterType>.text(
+    lang1: Pair<Lang1, LiteralOrExpression>,
+    lang2: Pair<Lang2, LiteralOrExpression>,
+) {
+    val lang1Value = lang1.second
+    val lang2Value = lang2.second
+
+    if (lang1Value is LiteralWrapper && lang2Value is LiteralWrapper) {
+        Element.OutlineContent.ParagraphContent.Text.Literal.create(
+            lang1.first to lang1Value.str,
+            lang2.first to lang2Value.str
+        ).also { addTextContent(Content(it)) }
+    } else {
+        Element.OutlineContent.ParagraphContent.Text.Expression.ByLanguage.create(
+            lang1.first to lang1.second.expr,
+            lang2.first to lang2.second.expr
+        ).also { addTextContent(Content(it)) }
+    }
+}
+
+fun <Lang1 : Language, Lang2 : Language, Lang3 : Language, ParameterType : Any> PlainTextScope<LanguageSupport.Triple<Lang1, Lang2, Lang3>, ParameterType>.text(
+    lang1: Pair<Lang1, LiteralOrExpression>,
+    lang2: Pair<Lang2, LiteralOrExpression>,
+    lang3: Pair<Lang3, LiteralOrExpression>,
+) {
+    val lang1Value = lang1.second
+    val lang2Value = lang2.second
+    val lang3Value = lang3.second
+
+    if (lang1Value is LiteralWrapper && lang2Value is LiteralWrapper && lang3Value is LiteralWrapper) {
+        Element.OutlineContent.ParagraphContent.Text.Literal.create(
+            lang1.first to lang1Value.str,
+            lang2.first to lang2Value.str,
+            lang3.first to lang3Value.str
+        ).also { addTextContent(Content(it)) }
+            .also { addTextContent(Content(it)) }
+    } else {
+        Element.OutlineContent.ParagraphContent.Text.Literal.create(
+            lang1.first to (lang1.second as LiteralWrapper).str,
+            lang2.first to (lang2.second as LiteralWrapper).str,
+            lang3.first to (lang3.second as LiteralWrapper).str
+        ).also { addTextContent(Content(it)) }
+    }
+}
+
+class LiteralOrExpressionBuilder {
+    sealed class LiteralOrExpression() {
+        abstract val expr: StringExpression
+    }
+    class LiteralWrapper(val str: String) : LiteralOrExpression() {
+        override val expr: StringExpression
+            get() = str.expr()
+    }
+
+    class ExpressionWrapper(override val expr: StringExpression) : LiteralOrExpression()
+
+    operator fun StringExpression.unaryPlus() = ExpressionWrapper(this)
+    operator fun String.unaryPlus() = LiteralWrapper(this)
+    operator fun LiteralOrExpression.plus(other: StringExpression) = when(this) {
+        is ExpressionWrapper -> ExpressionWrapper(expr + other)
+        is LiteralWrapper -> ExpressionWrapper(str.expr() + other)
+    }
+    operator fun LiteralOrExpression.plus(other: String) = when(this) {
+        is ExpressionWrapper -> ExpressionWrapper(expr + other)
+        is LiteralWrapper -> LiteralWrapper(str + other)
+    }
 }
