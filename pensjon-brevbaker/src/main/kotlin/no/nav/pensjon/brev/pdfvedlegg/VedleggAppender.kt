147b4780f101977cc1cf04455e4e77da084e8fce
Mads Opheim <mads.opheim@nav.no>
2025-09-03 00:05:18 +0200

    Inlining

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/pdfvedlegg/VedleggAppender.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/pdfvedlegg/VedleggAppender.kt
@@ -1,7 +1,6 @@
 package no.nav.pensjon.brev.pdfvedlegg
 
 import no.nav.brev.brevbaker.PDFVedlegg
-import no.nav.brev.brevbaker.Side
 import no.nav.pensjon.brevbaker.api.model.LanguageCode
 import org.apache.pdfbox.Loader
 import org.apache.pdfbox.multipdf.PDFMergerUtility
@@ -15,17 +14,17 @@ internal object VedleggAppender {
         val sider = vedlegg.sider
 
         sider.forEach {
-            merger.leggTilSide(target, settOppSide( it, sider.size, spraak))
+            val side = lesInnPDF(it.filnavn, spraak).also { side ->
+                side.setValues(it.felt + ("page" to "${it.sidenummer}/${sider.size}"))
+            }
+            merger.leggTilSide(target, side)
         }
 
         return target
     }
 
-    private fun settOppSide(side: Side, antallSider: Int, spraak: LanguageCode): PDDocument =
-        lesInnPDF(side.filnavn, spraak).also { it.setValues(side.felt + ("page" to "${side.sidenummer}/$antallSider")) }
-
-    private fun lesInnPDF(filnavn: String, spraak: LanguageCode): PDDocument =
-        javaClass.getResource("/vedlegg/${spraak.name}/${filnavn}-${spraak.name}.pdf")?.let {
-            Loader.loadPDF(it.readBytes())
-        } ?: throw IllegalArgumentException("Fant ikke vedlegg $filnavn")
+    private fun lesInnPDF(filnavn: String, spraak: LanguageCode) =
+        javaClass.getResource("/vedlegg/${filnavn}-${spraak.name}.pdf")
+            ?.let { Loader.loadPDF(it.readBytes()) }
+            ?: throw IllegalArgumentException("Fant ikke vedlegg $filnavn")
 }
