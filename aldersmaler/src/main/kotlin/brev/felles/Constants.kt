0663f4a64748268f88c420f5ed41564a403a3c96
Espen Schei <espen.schei@outlook.com>
2025-09-10 10:06:26 +0200

    Konverterer autobrev INFO_FYLLER_67_AAR_SAERSKILT_SATS (#1930)
    * Konverterer autobrev INFO_FYLLER_67_AAR_SAERSKILT_SATS
    
    * Bump versjon av api

---
+++ aldersmaler/src/main/kotlin/brev/felles/Constants.kt
@@ -0,0 +1,14 @@
+package brev.felles
+
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brevbaker.api.model.Telefonnummer
+
+object Constants {
+    const val NAV_URL = "nav.no"
+    const val KONTAKT_URL = "$NAV_URL/kontakt"
+    const val PENSJON_URL = "$NAV_URL/pensjon"
+    const val NAV_KONTAKTSENTER_AAPNINGSTID = "09.00-15.00"
+    const val NAV_KONTAKTSENTER_OPEN_HOURS = "09:00-15:00"
+    private val navKontaktsenterPensjon = Telefonnummer("55553334")
+    val NAV_KONTAKTSENTER_TELEFON_PENSJON = navKontaktsenterPensjon.format()
+}
