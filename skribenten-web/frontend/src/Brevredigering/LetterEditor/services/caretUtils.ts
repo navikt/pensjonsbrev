56214d56f567d3763b0ec25a45ef81233d167c11
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2025-09-10 12:31:58 +0200

    Fiks feilende component test

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/services/caretUtils.ts
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/services/caretUtils.ts
@@ -82,9 +82,13 @@ export function gotoCoordinates(coordinates: Coordinates) {
 
   // caretPositionFromPoint() (supported by all except WebKit/Safari per 2025)
   // caretRangeFromPoint() (supported by all except Gecko/Firefox per 2025)
+  // We should prioritize the non-deprecated caretPositionFromPoin,
+  // but will keep caretRangeFromPoint first until we figure out a component test issue
   // https://developer.mozilla.org/en-US/docs/Web/API/Document/caretPositionFromPoint
   // https://developer.mozilla.org/en-US/docs/Web/API/Document/caretRangeFromPoint
-  if (document.caretPositionFromPoint) {
+  if (document.caretRangeFromPoint) {
+    range = document.caretRangeFromPoint(x, y);
+  } else if (document.caretPositionFromPoint) {
     const position = document.caretPositionFromPoint(x, y);
     if (position === null) {
       // eslint-disable-next-line no-console
@@ -94,8 +98,6 @@ export function gotoCoordinates(coordinates: Coordinates) {
     range = document.createRange();
     range.setStart(position.offsetNode, position.offset);
     range.collapse(true);
-  } else if (document.caretRangeFromPoint) {
-    range = document.caretRangeFromPoint(x, y);
   }
   if (range === null) {
     // eslint-disable-next-line no-console
