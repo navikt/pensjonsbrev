411d3ff2ab3b1167bcf9b1eaf6bf7a5b415edeab
Jeremy Elsom <jeremy.elsom@nav.no>
2025-09-11 13:35:55 +0200

    WIP

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/fixtures/alder/EndringAvUttaksgradAutoDto.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/fixtures/alder/EndringAvUttaksgradAutoDto.kt
@@ -2,19 +2,33 @@ package no.nav.pensjon.brev.fixtures.alder
 
 import no.nav.pensjon.brev.api.model.AlderspensjonRegelverkType
 import no.nav.pensjon.brev.api.model.maler.alderApi.EndringAvUttaksgradAutoDto
+import no.nav.pensjon.brev.api.model.vedlegg.MaanedligPensjonFoerSkattAP2025Dto
+import no.nav.pensjon.brev.fixtures.createMaanedligPensjonFoerSkatt
+import no.nav.pensjon.brev.maler.vedlegg.createOrienteringOmRettigheterOgPlikterDto
 import no.nav.pensjon.brevbaker.api.model.Kroner
 import java.time.LocalDate
 
 fun createEndringAvUttaksgradAutoDto() = EndringAvUttaksgradAutoDto(
-alderspensjonVedVirk = EndringAvUttaksgradAutoDto.AlderspensjonVedVirk(
-    privatAFPerBrukt = false,
-    skjermingstilleggInnvilget = false,
-    totalPensjon = Kroner(20000),
-    ufoereKombinertMedAlder = false,
-    uttaksgrad = 50,
-),
+    alderspensjonVedVirk = EndringAvUttaksgradAutoDto.AlderspensjonVedVirk(
+        privatAFPerBrukt = false,
+        skjermingstilleggInnvilget = false,
+        totalPensjon = Kroner(20000),
+        ufoereKombinertMedAlder = false,
+        uttaksgrad = 50,
+    ),
     harFlereBeregningsperioder = false,
     kravVirkDatoFom = LocalDate.of(2025, 1, 1),
     regelverkType = AlderspensjonRegelverkType.AP2011,
-    dineRettigheterOgMulighetTilAaKlageDto =
+    orienteringOmRettigheterOgPlikterDto = createOrienteringOmRettigheterOgPlikterDto(),
+    maanedligPensjonFoerSkattAP2025Dto = MaanedligPensjonFoerSkattAP2025Dto(
+        beregnetPensjonPerManedGjeldende = MaanedligPensjonFoerSkattAP2025Dto.AlderspensjonPerManed(
+            inntektspensjon = Kroner(1000),
+            totalPensjon = Kroner(2000),
+            garantipensjon = Kroner(1000),
+            minstenivaIndividuell = Kroner(1000),
+            virkDatoFom = LocalDate.now(),
+            virkDatoTom = null,
+        ),
+        maanedligPensjonFoerSkattDto = createMaanedligPensjonFoerSkatt(),
+    )
 )
