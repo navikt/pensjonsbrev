2d1db7272378884f353a58b9b5635b63e0e8edd4
Jeremy Elsom <jeremy.elsom@nav.no>
2025-09-11 13:22:23 +0200

    WIP

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/fixtures/alder/EndringAvUttaksgradAutoDto.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/fixtures/alder/EndringAvUttaksgradAutoDto.kt
@@ -1,7 +1,19 @@
 package no.nav.pensjon.brev.fixtures.alder
 
+import no.nav.pensjon.brev.api.model.AlderspensjonRegelverkType
 import no.nav.pensjon.brev.api.model.maler.alderApi.EndringAvUttaksgradAutoDto
+import no.nav.pensjon.brevbaker.api.model.Kroner
+import java.time.LocalDate
 
 fun createEndringAvUttaksgradAutoDto() = EndringAvUttaksgradAutoDto(
-
+alderspensjonVedVirk = EndringAvUttaksgradAutoDto.AlderspensjonVedVirk(
+    privatAFPerBrukt = false,
+    skjermingstilleggInnvilget = false,
+    totalPensjon = Kroner(20000),
+    ufoereKombinertMedAlder = false,
+    uttaksgrad = 50,
+),
+    harFlereBeregningsperioder = false,
+    kravVirkDatoFom = LocalDate.of(2025, 1, 1),
+    regelverkType = AlderspensjonRegelverkType.AP2011,
 )
