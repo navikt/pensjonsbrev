f476b5045da72f0b3aa41641b0486e1190185ccd
Jeremy Elsom <jeremy.elsom@nav.no>
2025-09-11 14:27:38 +0200

    WIP

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/maler/alder/EndringAvUttaksgradAutoTest.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/maler/alder/EndringAvUttaksgradAutoTest.kt
@@ -5,51 +5,30 @@ import no.nav.brev.brevbaker.TestTags
 import no.nav.brev.brevbaker.renderTestHtml
 import no.nav.brev.brevbaker.renderTestPDF
 import no.nav.pensjon.brev.*
+import no.nav.pensjon.brev.api.model.maler.alderApi.EndringAvUttaksgradAutoDto
 import no.nav.pensjon.brev.api.model.maler.alderApi.InfoAlderspensjonOvergang67AarAutoDto
 import no.nav.pensjon.brev.template.*
 import org.junit.jupiter.api.*
 
 @Tag(TestTags.MANUAL_TEST)
 class EndringAvUttaksgradAutoTest {
-
     @Test
-    fun testPdfNB() {
-        LetterTestImpl(
-            InfoAldersovergang67AarAuto.template,
-            Fixtures.create<InfoAlderspensjonOvergang67AarAutoDto>(),
-            Language.Bokmal,
-            Fixtures.fellesAuto
-        ).renderTestPDF("INFO_ALDERSOVERGANG_67_AAR_AUTO_BOKMAL")
-    }
+        fun testPdf() {
+            LetterTestImpl(
+                EndringAvUttaksgradAuto.template,
+                Fixtures.create<EndringAvUttaksgradAutoDto>(),
+                Language.Bokmal,
+                Fixtures.felles
+            ).renderTestPDF(InnvilgelseAvAlderspensjonAuto.kode.name)
+        }
 
-    @Test
-    fun testPdfNN() {
-        LetterTestImpl(
-            InfoAldersovergang67AarAuto.template,
-            Fixtures.create<InfoAlderspensjonOvergang67AarAutoDto>(),
-            Language.Nynorsk,
-            Fixtures.fellesAuto
-        ).renderTestPDF("INFO_ALDERSOVERGANG_67_AAR_AUTO_NYNORSK")
+        @Test
+        fun testHtml() {
+            LetterTestImpl(
+                InnvilgelseAvAlderspensjonAuto.template,
+                Fixtures.create<InnvilgelseAvAlderspensjonAutoDto>(),
+                Language.Nynorsk,
+                Fixtures.felles
+            ).renderTestHtml(InnvilgelseAvAlderspensjonAuto.kode.name)
+        }
     }
-
-    @Test
-    fun testPdfEN() {
-        LetterTestImpl(
-            InfoAldersovergang67AarAuto.template,
-            Fixtures.create<InfoAlderspensjonOvergang67AarAutoDto>(),
-            Language.English,
-            Fixtures.fellesAuto
-        ).renderTestPDF("INFO_ALDERSOVERGANG_67_AAR_AUTO_ENGLISH")
-    }
-
-
-    @Test
-    fun testHtml() {
-        LetterTestImpl(
-            InfoAldersovergang67AarAuto.template,
-            Fixtures.create<InfoAlderspensjonOvergang67AarAutoDto>(),
-            Language.Bokmal,
-            Fixtures.fellesAuto
-        ).renderTestHtml("INFO_ALDERSOVERGANG_67_AAR_AUTO_BOKMAL")
-    }
-}
