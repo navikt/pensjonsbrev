569ca90dd590b6309299ea2ce5ca341f7bbfc63c
Espen Schei <espen.schei@outlook.com>
2025-09-15 14:40:59 +0200

    Konverterer infobrevet mf-000238 til brevbaker (#1979)
    * Konverterer infobrevet mf-000238 til brevbaker
    
    Informasjon til deg som har ektefelle/partner/samboer som snart fyller 60 år
    
    * Legg til fixture for InfoAldersovergangEps60AarAutoDto
    
    * Legg til fixture for EmptyBrevData
    
    * Legg tilbake brev i automaler

--- aldersmaler/src/test/kotlin/brev/Fixtures.kt
+++ aldersmaler/src/test/kotlin/brev/Fixtures.kt
@@ -1,5 +1,8 @@
 package no.nav.pensjon.brev
 
+import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.api.model.maler.auto.InfoAldersovergangEps60AarAutoDto
+import no.nav.pensjon.brev.api.model.maler.auto.Ytelse
 import kotlin.reflect.KClass
 
 object Fixtures {
@@ -13,7 +16,8 @@ object Fixtures {
     @Suppress("UNCHECKED_CAST")
     fun <T : Any> create(letterDataType: KClass<T>): T =
         when (letterDataType) {
-
+            InfoAldersovergangEps60AarAutoDto::class -> InfoAldersovergangEps60AarAutoDto(ytelse = Ytelse.ALDER) as T
+            EmptyBrevdata::class -> EmptyBrevdata as T
             else -> throw IllegalArgumentException("Don't know how to construct: ${letterDataType.qualifiedName}")
         }
 }
