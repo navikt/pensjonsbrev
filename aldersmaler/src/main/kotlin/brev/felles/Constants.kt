34e8c1718035d9f9f19f9239aaeb2598f83293ac
Espen Schei <espen.schei@outlook.com>
2025-09-17 11:04:01 +0200

    Benytt felles kontakt url (#1998)

--- aldersmaler/src/main/kotlin/brev/felles/Constants.kt
+++ aldersmaler/src/main/kotlin/brev/felles/Constants.kt
@@ -7,7 +7,6 @@ object Constants {
     const val NAV_URL = "nav.no"
     const val KONTAKT_URL = "$NAV_URL/kontakt"
     const val PENSJON_URL = "$NAV_URL/pensjon"
-    const val SKRIVTILOSS_URL = "$NAV_URL/skriv-til-oss"
     const val NAV_KONTAKTSENTER_AAPNINGSTID = "09.00-15.00"
     const val NAV_KONTAKTSENTER_OPEN_HOURS = "09:00-15:00"
     private val navKontaktsenterPensjon = Telefonnummer("55553334")
