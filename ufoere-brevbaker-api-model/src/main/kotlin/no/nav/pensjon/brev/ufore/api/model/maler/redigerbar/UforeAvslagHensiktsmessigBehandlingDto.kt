f39d3b6b86bd0a18465d9e3a9794538ae12b8a92
Knut.Mork <knut.mork@nav.no>
2025-09-19 14:06:33 +0200

    Avslagsbrev UT - lagt til dato for krav mottatt

--- ufoere-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/UforeAvslagHensiktsmessigBehandlingDto.kt
+++ ufoere-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/UforeAvslagHensiktsmessigBehandlingDto.kt
@@ -4,6 +4,7 @@ import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagHensiktsmessigBehandlingDto.*
 import no.nav.pensjon.brevbaker.api.model.DisplayText
+import java.time.LocalDate
 
 data class UforeAvslagHensiktsmessigBehandlingDto(
     override val pesysData: UforeAvslagHensiktsmessigBehandlingPendata,
@@ -16,6 +17,7 @@ data class UforeAvslagHensiktsmessigBehandlingDto(
     ) : BrevbakerBrevdata
 
     data class UforeAvslagHensiktsmessigBehandlingPendata(
+        val kravMottattDato: LocalDate,
         val vurdering: String
     ) : BrevbakerBrevdata
 }
