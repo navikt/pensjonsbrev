38bc564accb0c4f58d398a707fb32a6cfb936bb2
Knut.Mork <knut.mork@nav.no>
2025-09-22 12:21:18 +0200

    Avslagsbrev UT - Påstartet nytt brev og refaktorert felles kode

--- ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/maler/UforeAvslagHensiktsmessigTiltak.kt
+++ ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/maler/UforeAvslagHensiktsmessigTiltak.kt
@@ -5,9 +5,8 @@ import no.nav.pensjon.brev.api.model.Sakstype
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.maler.fraser.Felles.*
 import no.nav.pensjon.brev.maler.vedlegg.vedleggDineRettigheterOgMulighetTilAaKlageUfoereStatisk
+import no.nav.pensjon.brev.template.*
 import no.nav.pensjon.brev.template.Language.Bokmal
-import no.nav.pensjon.brev.template.RedigerbarTemplate
-import no.nav.pensjon.brev.template.createTemplate
 import no.nav.pensjon.brev.template.dsl.expression.format
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
@@ -23,7 +22,7 @@ import no.nav.pensjon.brevbaker.api.model.LetterMetadata
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Distribusjonstype.VEDTAK
 
 @TemplateModelHelpers
-object UforeAvslagHensiktsmessigBehandling : RedigerbarTemplate<UforeAvslagHensiktsmessigBehandlingDto> {
+object UforeAvslagHensiktsmessigTiltak : RedigerbarTemplate<UforeAvslagHensiktsmessigBehandlingDto> {
 
     override val featureToggle = FeatureToggles.uforeAvslagDemo.toggle
 
@@ -36,7 +35,7 @@ object UforeAvslagHensiktsmessigBehandling : RedigerbarTemplate<UforeAvslagHensi
     override val template = createTemplate(
         languages = languages(Bokmal),
         letterMetadata = LetterMetadata(
-            displayTitle = "Avslag uføretrygd - Hensiktsmessig behandling",
+            displayTitle = "Avslag uføretrygd - Hensiktsmessig tiltak",
             isSensitiv = false,
             distribusjonstype = VEDTAK,
             brevtype = LetterMetadata.Brevtype.VEDTAKSBREV
