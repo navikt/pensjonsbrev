910f183c3d9f2af1d01ec3065e6386e82fac2294
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2025-09-22 13:20:22 +0200

    Deler vedlegget Informasjon om medlemskap og helserettigheter i to, og slår saman titlar (#2013)
    * Deler vedlegget informasjon om medlemskap og helserettigheter i to, det er jo eigentleg to vedlegg
    
    * Legg all tittel-teksten inn i tittelen
    
    * Bruker ny datastruktur
    
    * Rullar tilbake dsl-endringar som ikkje vart naudsynte etter nye endringar
    
    * Tar bort eksisterande dto som ikkje lenger gir meining
    
    * Versjon

---
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/maler/vedlegg/InformasjonOmMedlemskapOgHelserettigheterUtenforEOESTest.kt
@@ -0,0 +1,36 @@
+package no.nav.pensjon.brev.maler.vedlegg
+
+import no.nav.brev.brevbaker.LetterTestImpl
+import no.nav.brev.brevbaker.createVedleggTestTemplate
+import no.nav.brev.brevbaker.renderTestHtml
+import no.nav.pensjon.brev.Fixtures
+import no.nav.pensjon.brev.api.model.Sakstype
+import no.nav.pensjon.brev.template.Language
+import no.nav.pensjon.brev.template.dsl.expression.expr
+import no.nav.pensjon.brev.template.dsl.languages
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class InformasjonOmMedlemskapOgHelserettigheterUtenforEOESTest {
+
+    @ParameterizedTest
+    @MethodSource("sakstyperOgSpraak")
+    fun `test vedlegg vedleggInformasjonOmMedlemskapOgHelserettigheter`(sakstype: Sakstype, spraak: Language) {
+        val template = createVedleggTestTemplate(
+            vedleggInformasjonOmMedlemskapOgHelserettigheterUtenforEOES,
+            Unit.expr(),
+            languages(Language.Bokmal, Language.Nynorsk, Language.English),
+        )
+        LetterTestImpl(
+            template, Unit, spraak, Fixtures.fellesAuto
+        ).renderTestHtml(this::class.simpleName + "_${sakstype}_${spraak::class.simpleName}")
+    }
+
+    companion object {
+        @JvmStatic
+        fun sakstyperOgSpraak() = listOf(Language.Bokmal, Language.Nynorsk, Language.English).map {
+            Arguments.of(Sakstype.ALDER, it)
+        }
+    }
+}
