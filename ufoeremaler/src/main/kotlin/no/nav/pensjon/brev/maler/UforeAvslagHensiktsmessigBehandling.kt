f907476a40193da6f06cc073dcec50ca8cb5c4d4
Knut.Mork <knut.mork@nav.no>
2025-09-22 14:50:53 +0200

    Avslagsbrev UT - Endret standardtekst

--- ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/maler/UforeAvslagHensiktsmessigBehandling.kt
+++ ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/maler/UforeAvslagHensiktsmessigBehandling.kt
@@ -105,6 +105,7 @@ object UforeAvslagHensiktsmessigBehandling : RedigerbarTemplate<UforeAvslagDto> 
                 text(bokmal { +"Vedtaket er gjort etter folketrygdloven §§ 12-5 til 12-7." })
             }
 
+            includePhrase(HvaSkjerNa)
             includePhrase(RettTilAKlage)
             includePhrase(RettTilInnsyn)
             includePhrase(HarDuSporsmal)
