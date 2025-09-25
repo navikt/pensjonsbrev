6cfec5d35a8400fdc7d1572aee12abff5cbe1fb6
Knut.Mork <knut.mork@nav.no>
2025-09-25 08:39:10 +0200

    UT Avslagsbrev: Nye varianter for yrkesskade

---
+++ ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/maler/uforeavslag/UforeAvslagYrkesskadeGodkjent.kt
@@ -0,0 +1,83 @@
+package no.nav.pensjon.brev.maler.uforeavslag
+
+import no.nav.pensjon.brev.FeatureToggles
+import no.nav.pensjon.brev.api.model.Sakstype
+import no.nav.pensjon.brev.api.model.TemplateDescription
+import no.nav.pensjon.brev.maler.fraser.Felles.*
+import no.nav.pensjon.brev.maler.vedlegg.vedleggDineRettigheterOgMulighetTilAaKlageUfoereStatisk
+import no.nav.pensjon.brev.template.Language.Bokmal
+import no.nav.pensjon.brev.template.RedigerbarTemplate
+import no.nav.pensjon.brev.template.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.format
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brev.ufore.api.model.Ufoerebrevkoder.Redigerbar.UT_AVSLAG_YRKESSKADE_GODKJENT
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagDto
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagDtoSelectors.SaksbehandlervalgSelectors.brukVurderingFraVilkarsvedtak
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagDtoSelectors.UforeAvslagPendataSelectors.kravMottattDato
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagDtoSelectors.UforeAvslagPendataSelectors.vurdering
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagDtoSelectors.pesysData
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagDtoSelectors.saksbehandlerValg
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata.Distribusjonstype.VEDTAK
+
+@TemplateModelHelpers
+object UforeAvslagYrkesskadeGodkjent : RedigerbarTemplate<UforeAvslagDto> {
+
+    override val featureToggle = FeatureToggles.uforeAvslag.toggle
+
+    override val kode = UT_AVSLAG_YRKESSKADE_GODKJENT
+    override val kategori = TemplateDescription.Brevkategori.FOERSTEGANGSBEHANDLING
+    override val brevkontekst = TemplateDescription.Brevkontekst.VEDTAK
+    override val sakstyper = setOf(Sakstype.UFOREP)
+
+
+    override val template = createTemplate(
+        languages = languages(Bokmal),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Avslag uføretrygd - 12-17",
+            isSensitiv = false,
+            distribusjonstype = VEDTAK,
+            brevtype = LetterMetadata.Brevtype.VEDTAKSBREV
+        ),
+    )
+    {
+        title {
+            text (bokmal { + "Nav har avslått søknaden din om uføretrygd"})
+        }
+        outline {
+            paragraph {
+                text(bokmal { +"Vi har avslått søknaden din om særbestemmelser for yrkesskade eller yrkessykdom i uføretrygden, som vi fikk den " + pesysData.kravMottattDato.format() + "." })
+            }
+            title1 {
+                text(bokmal { +"Derfor får du ikke uføretrygd med yrkesskade fordel" })
+            }
+            showIf(saksbehandlerValg.brukVurderingFraVilkarsvedtak) {
+                paragraph {
+                    text(bokmal { +pesysData.vurdering })
+                }
+            }.orShow {
+                paragraph {
+                    text(bokmal { +fritekst("Konkret begrunnelse der det er nødvendig.") })
+                }
+            }
+            paragraph {
+                text(bokmal { +"Du har en godkjent yrkesskade eller yrkessykdom. " +
+                        "Vi har vurdert at den nedsatte inntektsevnen din ikke skyldes den godkjente yrkesskaden eller yrkessykdommen din. " +
+                        "Det er andre sykdomsforhold som er årsaken til den nedsatte inntektsevnen din." })
+            }
+            paragraph {
+                text(bokmal { + "Du oppfyller ikke vilkårene, og vi avslår derfor søknaden din."})
+            }
+            paragraph {
+                text(bokmal { +"Vedtaket er gjort etter folketrygdloven §§ 12-17." })
+            }
+
+            includePhrase(RettTilAKlage)
+            includePhrase(RettTilInnsyn)
+            includePhrase(HarDuSporsmal)
+        }
+        includeAttachment(vedleggDineRettigheterOgMulighetTilAaKlageUfoereStatisk)
+    }
+}
