86930bacc2d8b352a3b84f367de8a165e7e05389
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2025-10-02 15:08:42 +0200

    Skjul tanstack devtools i test

--- skribenten-web/frontend/src/main.tsx
+++ skribenten-web/frontend/src/main.tsx
@@ -2,7 +2,6 @@ import "@navikt/ds-css";
 import "./appStyles.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { enablePatches } from "immer";
 import React from "react";
@@ -45,7 +44,6 @@ declare module "@tanstack/react-router" {
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
