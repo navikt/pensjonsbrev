6218fca274cdc3f6035896132094161d1ac604bc
Viktor Solberg <viktorgsolberg@gmail.com>
2025-10-03 11:37:38 +0200

    justerer brevtekster for vedtaksbrev for etteroppgjør (#2124)
    * justerer brevtekster for vedtaksbrev for etteroppgjør
    
    * Justerer tekster i beregningsvedlegg
    
    ---------
    
    Co-authored-by: Hanne Olsen <hanne.olsen@nav.no>

--- etterlattemaler/src/main/kotlin/no/nav/pensjon/etterlatte/maler/vedlegg/omstillingsstoenad/etteroppgjoer/OpplysningerOmEtteroppgjoeretVedlegg.kt
+++ etterlattemaler/src/main/kotlin/no/nav/pensjon/etterlatte/maler/vedlegg/omstillingsstoenad/etteroppgjoer/OpplysningerOmEtteroppgjoeretVedlegg.kt
@@ -94,9 +94,9 @@ private fun OutlineOnlyScope<LangBokmalNynorskEnglish, BeregningsVedleggData>.op
         }
         paragraph {
             text(
-                bokmal { +"Det er opplysninger fra Skatteetaten og a-ordningen om din faktiske inntekt for " + etteroppgjoersAar.format() + " som er brukt i beregningen. Hvis du har svart på varselet du fikk tidligere, har vi vurdert det du har skrevet og kommentert dette i avsnittet «Beløp trukket fra din pensjonsgivende inntekt»." },
-                nynorsk { +"Utrekninga er gjort ut frå opplysningar om di faktiske inntekt for " + etteroppgjoersAar.format() + " frå Skatteetaten og a-ordninga.  Dersom du har svart på varselet du fekk tidlegare, har vi vurdert det du har skrive og kommentert dette i avsnittet «Beløp trekt frå di pensjonsgivande inntekt»." },
-                english { +"The recalculation is based on information from the Tax Administration and the A-scheme regarding your actual income for " + etteroppgjoersAar.format() + ". If you responded to the notice you received earlier, we have reviewed your input and commented on it in the section «Amount deducted from your pensionable income»." },
+                bokmal { +"I beregningen har vi brukt opplysninger om din faktiske inntekt for " + etteroppgjoersAar.format() + " fra Skatteetaten og a-ordningen. Hvis du har svart på varselet du mottok tidligere, har vi vurdert det du skrev. Eventuelle kommentarer til dette finner du i avsnittet “Beløp trukket fra din pensjonsgivende inntekt”." },
+                nynorsk { +"I utrekninga har vi brukt opplysningar om den faktiske inntekta di for " + etteroppgjoersAar.format() + " frå Skatteetaten og a-ordninga. Dersom du har svart på varselet du fekk tidlegare, har vi vurdert det du skreiv. Eventuelle merknader til dette finn du i avsnittet “Beløp trekt frå di pensjonsgivande inntekt”." },
+                english { +"In the calculation, we have used information about your actual income for " + etteroppgjoersAar.format() + " from the Norwegian Tax Administration and the A-scheme. If you responded to the notice you received earlier, we have considered what you wrote. Any comments related to this can be found in the section “Amount deducted from your pensionable income”." },
             )
         }
     }.orShow {
