5ee29e3d3996314cb6fe69c8b9f0160eb3eabfa6
Alexander Hoem Rosbach <82815189+routsi@users.noreply.github.com>
2025-10-06 09:17:34 +0200

    Signatur/nullable rolle lettermarkup (#2130)
    * LetterMarkup.Signatur nullable rolle tekst
    
    * Nullable rolle felt i LetterMarkup
    
    * Bump uføre og pensjon api-model
    
    * Bump alder api-model
    
    * SignaturImpl må også ha nullable!

--- aldersmaler/build.gradle.kts
+++ aldersmaler/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val alderApiModelVersion = 8
+val alderApiModelVersion = 9
 
 val apiModelJavaTarget: String by System.getProperties()
 
--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 276
+val apiModelVersion = 277
 
 val apiModelJavaTarget: String by System.getProperties()
 
