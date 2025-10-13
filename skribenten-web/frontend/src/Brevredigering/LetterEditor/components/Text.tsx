edf888d08054ca86dc18f8e71065c309129863b1
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2025-10-13 11:34:36 +0200

    Adds data-literal-index on span for VARIABLES and NEW_LINE

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/components/Text.tsx
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/components/Text.tsx
@@ -19,11 +19,12 @@ export const Text = ({ content, literalIndex }: TextProperties) => {
 
   switch (content.type) {
     case NEW_LINE: {
-      return <br />;
+      return <br data-literal-index={JSON.stringify(literalIndex)} data-noneditable="1" />;
     }
     case VARIABLE: {
       return (
         <span
+          contentEditable={false}
           css={css`
             border-radius: 4px;
             border: ${isFocused ? "2px" : "1px"} solid ${isFocused ? "blue" : "var(--a-border-default)"};
@@ -36,6 +37,8 @@ export const Text = ({ content, literalIndex }: TextProperties) => {
             ${content.fontType === FontType.BOLD && "font-weight: bold;"}
             ${content.fontType === FontType.ITALIC && "font-style: italic;"}
           `}
+          data-literal-index={JSON.stringify(literalIndex)}
+          data-noneditable="1"
           onClick={() => {
             setEditorState((oldState) => ({
               ...oldState,
