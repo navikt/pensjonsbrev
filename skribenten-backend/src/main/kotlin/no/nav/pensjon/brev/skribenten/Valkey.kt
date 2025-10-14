48e51e66b7d72d937932e37ecd7b3774b5215e3d
Mads Opheim <mads.opheim@nav.no>
2025-10-14 22:40:10 +0200

    Refaktorisering, prøver å forenkle

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/Valkey.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/Valkey.kt
@@ -8,15 +8,17 @@ import io.valkey.HostAndPort
 import io.valkey.JedisPool
 import io.valkey.params.SetParams
 import no.nav.pensjon.brev.skribenten.db.databaseObjectMapper
-import java.net.URI
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.collections.set
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
+
+private val defaultTtl = 30.minutes
 
 interface CacheImplementation {
     fun <K> hasValue(key: K): Boolean
     fun <K, T> get(key: K, clazz: Class<T>): T?
-    fun <K, V> update(key: K, value: V, sekunder: Int = 2000)
-    suspend fun <K, V> cached(key: K, clazz: Class<V>, fetch: suspend (K) -> V?): V?
+    fun <K, V> update(key: K, value: V, ttl: Duration = defaultTtl)
+    suspend fun <K, V> cached(key: K, clazz: Class<V>, ttl: Duration = defaultTtl, fetch: suspend (K) -> V?): V?
 }
 
 class Valkey(config: ApplicationConfig, instanceName: String) : CacheImplementation {
@@ -24,25 +26,26 @@ class Valkey(config: ApplicationConfig, instanceName: String) : CacheImplementat
 
     private val jedisPool = setupJedis(config, instanceName)
 
-    override suspend fun <K, V> cached(key: K, clazz: Class<V>, fetch: suspend (K) -> V?): V? = if (hasValue(key)) {
+    override suspend fun <K, V> cached(key: K, clazz: Class<V>, ttl: Duration, fetch: suspend (K) -> V?): V? = if (hasValue(key)) {
         get(key, clazz)
     } else {
-        fetch(key)?.also { update(key, it) }
+        fetch(key)?.also { update(key, it, ttl) }
     }
 
     override fun <K> hasValue(key: K) = jedisPool.resource.use {
         it.get(objectMapper.write(key)) != null
     }
 
-    override fun <K, T> get(key: K, clazz: Class<T>): T? = jedisPool.resource.use { it.get(objectMapper.write(key))?.let { objectMapper.readValue(it, clazz) } }
+    override fun <K, T> get(key: K, clazz: Class<T>): T? =
+        jedisPool.resource.use { it.get(objectMapper.write(key))?.let { k -> objectMapper.readValue(k, clazz) } }
 
-    override fun <K, V> update(key: K, value: V, sekunder: Int) {
+    override fun <K, V> update(key: K, value: V, ttl: Duration) {
         jedisPool.resource.use {
             it.set(
                 objectMapper.write(key),
                 objectMapper.write(value),
                 SetParams().apply {
-                    ex(sekunder.toLong())
+                    ex(ttl.inWholeSeconds)
                     nx()
                 },
             )
@@ -56,16 +59,17 @@ class InMemoryCache : CacheImplementation {
 
     override fun <K> hasValue(key: K) = cache.get(objectMapper.write(key)) != null
 
-    override fun <K, V> get(key: K, clazz: Class<V>) = cache.get(objectMapper.write(key))?.let { objectMapper.readValue(it, clazz) }
+    override fun <K, V> get(key: K, clazz: Class<V>) =
+        cache.get(objectMapper.write(key))?.let { objectMapper.readValue(it, clazz) }
 
-    override fun <K, V> update(key: K, value: V, sekunder: Int) {
+    override fun <K, V> update(key: K, value: V, ttl: Duration) {
         cache[objectMapper.write(key)] = objectMapper.write(value)
     }
 
-    override suspend fun <K, V> cached(key: K, clazz: Class<V>, fetch: suspend (K) -> V?): V? = if (hasValue(key)) {
+    override suspend fun <K, V> cached(key: K, clazz: Class<V>, ttl: Duration, fetch: suspend (K) -> V?): V? = if (hasValue(key)) {
         get(key, clazz)
     } else {
-        fetch(key)?.also { update(key, it) }
+        fetch(key)?.also { update(key, it, ttl) }
     }
 }
 
@@ -73,7 +77,6 @@ private fun <V> ObjectMapper.write(value: V) = if (value is String) value else w
 
 
 private fun setupJedis(config: ApplicationConfig, instanceName: String): JedisPool {
-    val uri = URI(config.getString("VALKEY_URI_$instanceName"))
     val host = config.getString("VALKEY_HOST_$instanceName")
     val port = config.getString("VALKEY_PORT_$instanceName").toInt()
     val username = config.getString("VALKEY_USERNAME_$instanceName")
