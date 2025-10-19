e3e99300ee8fee3ce7e36fc366ef5cdfead1f58f
Mads Opheim <mads.opheim@nav.no>
2025-10-19 17:43:09 +0200

    Flyttar den generelle biten over til testfixtures

---
+++ brevbaker/src/testFixtures/kotlin/no/nav/brev/brevbaker/BrevmodulTest.kt
@@ -0,0 +1,69 @@
+package no.nav.brev.brevbaker
+
+import no.nav.pensjon.brev.api.model.TemplateDescription
+import no.nav.pensjon.brev.api.model.maler.Brevkode
+import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
+import no.nav.pensjon.brev.api.model.maler.EmptyRedigerbarBrevdata
+import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
+import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brevbaker.api.model.DisplayText
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import kotlin.collections.filterNot
+import kotlin.reflect.KProperty
+
+
+abstract class TemplatesTest(val templates: AllTemplates, val auto: Collection<Brevkode.Automatisk>, val redigerbare: Collection<Brevkode.Redigerbart>) {
+    @Test
+    fun `alle autobrev fins i templates`() {
+        val brukteKoder = templates.hentAutobrevmaler().map { it.kode }
+        val ubrukteKoder = auto.filterNot { brukteKoder.contains(it) }
+        assertEquals(ubrukteKoder, listOf<Brevkode.Automatisk>())
+    }
+
+    @Test
+    fun `alle redigerbare brev fins i templates`() {
+        val brukteKoder = templates.hentRedigerbareMaler().map { it.kode }
+        val ubrukteKoder = redigerbare.filterNot { brukteKoder.contains(it) }
+        assertEquals(ubrukteKoder, listOf<Brevkode.Redigerbart>())
+    }
+
+    @Test
+    fun `alle redigerbare brev har displaytext for alle saksbehandlervalg`() {
+        templates.hentRedigerbareMaler().map { mal ->
+            val clazz = mal.template.letterDataType.java
+            val saksbehandlervalg = clazz.declaredFields.map { it.type }.filter { field -> SaksbehandlerValgBrevdata::class.java.isAssignableFrom(field) }.map { it.kotlin }
+            saksbehandlervalg.flatMap { it.members }.filter { it is KProperty<*> }.forEach { field ->
+                val hasDisplayText = field.annotations.filterIsInstance<DisplayText>().any()
+                assertTrue(hasDisplayText, "Alle saksbehandlervalg må ha displaytext, ${field.name} i klasse ${clazz.name} mangler det")
+            }
+        }
+    }
+
+    @Test
+    fun `alle maler med brevdata har annotasjon som gjoer at vi genererer selectors`() {
+        (templates.hentAutobrevmaler() + templates.hentRedigerbareMaler())
+            .filterNot { it.template.letterDataType in setOf(EmptyBrevdata::class, EmptyRedigerbarBrevdata::class)  }
+            .forEach {
+                assertTrue(
+                    it.javaClass.declaredAnnotations.any { annotation -> annotation.annotationClass == TemplateModelHelpers::class },
+                    "Alle maler annoteres med @TemplateModelHelpers, for å få generert selectors. Det har ikke ${it.javaClass.simpleName}"
+                )
+            }
+    }
+
+    // Dette er ei hypotese vi på brevteamet har. Oppdater eller fjern testen hvis dere skulle finne et scenario hvor hypotesa ikke holder
+    @Test
+    fun `brev som er deklarert med brevtype vedtaksbrev skal ha brevkontekst vedtak`() {
+        assertEquals(
+            emptyList<String>(),
+            templates.hentRedigerbareMaler()
+                .filter { it.template.letterMetadata.brevtype == LetterMetadata.Brevtype.VEDTAKSBREV }
+                .filterNot { it.brevkontekst == TemplateDescription.Brevkontekst.VEDTAK }
+                .map { it.javaClass.simpleName }
+            ,
+        )
+    }
+}
