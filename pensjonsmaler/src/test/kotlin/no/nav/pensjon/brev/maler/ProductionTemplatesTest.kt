bd4a9e897d2cc973701d8e3b19395b91eb856c8d
Mads Opheim <mads.opheim@nav.no>
2025-10-19 18:07:53 +0200

    Generaliserer også pensjonsbrev-modulen

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/maler/ProductionTemplatesTest.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/maler/ProductionTemplatesTest.kt
@@ -1,71 +1,65 @@
 package no.nav.pensjon.brev.maler
 
-import no.nav.pensjon.brev.api.model.TemplateDescription
+import no.nav.brev.brevbaker.AllTemplates
+import no.nav.brev.brevbaker.TemplatesTest
+import no.nav.brev.brevbaker.TestTags
+import no.nav.pensjon.brev.Fixtures
 import no.nav.pensjon.brev.api.model.maler.Brevkode
-import no.nav.pensjon.brev.api.model.maler.EmptyBrevdata
-import no.nav.pensjon.brev.api.model.maler.EmptyRedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
-import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
-import no.nav.pensjon.brev.template.RedigerbarTemplate
-import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
-import no.nav.pensjon.brevbaker.api.model.DisplayText
-import no.nav.pensjon.brevbaker.api.model.LetterMetadata
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
-import kotlin.reflect.KProperty
+import no.nav.pensjon.brev.maler.example.EksempelbrevRedigerbart
+import no.nav.pensjon.brev.maler.example.LetterExample
+import no.nav.pensjon.brev.template.Language
+import no.nav.pensjon.brev.template.LanguageSupport
+import no.nav.pensjon.brev.template.LetterTemplate
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 
-class ProductionTemplatesTest {
+val filterForPDF = listOf(LetterExample.kode)
 
-    @Test
-    fun `alle autobrev fins i templates`() {
-        val brukteKoder = ProductionTemplates.hentAutobrevmaler().map { it.kode }
-        val ubrukteKoder = Pesysbrevkoder.AutoBrev.entries.filterNot { brukteKoder.contains(it) }
-        Assertions.assertEquals(ubrukteKoder, listOf<Brevkode.Automatisk>())
-    }
+class ProductionTemplatesTest : TemplatesTest(
+    templates = ProductionTemplates,
+    auto = Pesysbrevkoder.AutoBrev.entries,
+    redigerbare = Pesysbrevkoder.Redigerbar.entries
+) {
 
-    @Test
-    fun `alle redigerbare brev fins i templates`() {
-        val brukteKoder = ProductionTemplates.hentRedigerbareMaler().map { it.kode }
-        val ubrukteKoder = Pesysbrevkoder.Redigerbar.entries.filterNot { brukteKoder.contains(it) }
-        Assertions.assertEquals(ubrukteKoder, listOf<Brevkode.Redigerbart>())
-    }
+    @Tag(TestTags.MANUAL_TEST)
+    @ParameterizedTest(name = "{1}, {3}")
+    @MethodSource("filtrerteMaler")
+    override fun <T : Any> testPdf(
+        template: LetterTemplate<LanguageSupport, T>,
+        brevkode: Brevkode<*>,
+        fixtures: T,
+        spraak: Language,
+    ) = renderPdf(template, brevkode, fixtures, spraak)
 
-    @Test
-    fun `alle redigerbare brev har displaytext for alle saksbehandlervalg`() {
-        ProductionTemplates.hentRedigerbareMaler().map { mal ->
-            val clazz = mal.template.letterDataType.java
-            val saksbehandlervalg = clazz.declaredFields.map { it.type }.filter { field -> SaksbehandlerValgBrevdata::class.java.isAssignableFrom(field) }.map { it.kotlin }
-            saksbehandlervalg.flatMap { it.members }.filter { it is KProperty<*> }.forEach { field ->
-                val hasDisplayText = field.annotations.filterIsInstance<DisplayText>().any()
-                assertTrue(hasDisplayText, "Alle saksbehandlervalg må ha displaytext, ${field.name} i klasse ${clazz.name} mangler det")
+    @ParameterizedTest(name = "{1}, {3}")
+    @MethodSource("alleMalene")
+    override fun <T : Any> testHtml(
+        template: LetterTemplate<LanguageSupport, T>,
+        brevkode: Brevkode<*>,
+        fixtures: T,
+        spraak: Language,
+    ) = renderHtml(template, brevkode, fixtures, spraak)
+
+    companion object {
+        @JvmStatic
+        fun filtrerteMaler(): List<Arguments> = finnMaler(filterForPDF)
+
+        @JvmStatic
+        fun alleMalene(): List<Arguments> = finnMaler(listOf())
+
+        @JvmStatic
+        fun finnMaler(filter: List<Brevkode<*>> = listOf()): List<Arguments> {
+            val alleMaler = object : AllTemplates {
+                override fun hentAutobrevmaler() = ProductionTemplates.hentAutobrevmaler() + LetterExample
+                override fun hentRedigerbareMaler() =
+                    ProductionTemplates.hentRedigerbareMaler() + EksempelbrevRedigerbart
             }
+            return finnMaler(
+                alleMaler, Fixtures, filter
+            )
         }
-    }
-
-    @Test
-    fun `alle maler med brevdata har annotasjon som gjoer at vi genererer selectors`() {
-        (ProductionTemplates.hentAutobrevmaler() + ProductionTemplates.hentRedigerbareMaler())
-            .filterNot { it.template.letterDataType in setOf(EmptyBrevdata::class, EmptyRedigerbarBrevdata::class) }
-            .forEach {
-                assertTrue(
-                    it.javaClass.declaredAnnotations.any { annotation -> annotation.annotationClass == TemplateModelHelpers::class },
-                    "Alle maler annoteres med @TemplateModelHelpers, for å få generert selectors. Det har ikke ${it.javaClass.simpleName}"
-                )
-            }
-    }
-
-    // Dette er ei hypotese vi på brevteamet har. Oppdater eller fjern testen hvis dere skulle finne et scenario hvor hypotesa ikke holder
-    @Test
-    fun `brev som er deklarert med brevtype vedtaksbrev skal ha brevkontekst vedtak`() {
-        assertEquals(
-            emptyList<String>(),
-            ProductionTemplates.hentRedigerbareMaler()
-                .filter { it.template.letterMetadata.brevtype == LetterMetadata.Brevtype.VEDTAKSBREV }
-                .filterNot { it.brevkontekst == TemplateDescription.Brevkontekst.VEDTAK }
-                .map { it.javaClass.simpleName }
-            ,
-        )
     }
 }
