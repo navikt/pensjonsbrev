2157ec75a4626a96fdb91d89033f8c520cefe475
Mads Opheim <mads.opheim@nav.no>
2025-10-20 14:49:51 +0200

    Med endringar i api-modell-modulane kjem byggejobbane til å tryne viss vi ikkje oppdaterer versjon, sjølv om endringane kun er i test

--- pensjonsmaler/build.gradle.kts
+++ pensjonsmaler/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 295
+val apiModelVersion = 296
 
 val apiModelJavaTarget: String by System.getProperties()
 
--- ufoeremaler/build.gradle.kts
+++ ufoeremaler/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val ufoereApiModelVersion = 33
+val ufoereApiModelVersion = 34
 
 val apiModelJavaTarget: String by System.getProperties()
 
