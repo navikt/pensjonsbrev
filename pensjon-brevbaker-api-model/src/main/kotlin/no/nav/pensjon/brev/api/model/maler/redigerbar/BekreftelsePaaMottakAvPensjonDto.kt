1691eb21feb52803d9d692b284949f69056c73cd
Jeremy Elsom <jeremy.elsom@nav.no>
2025-10-21 09:32:37 +0200

    WIP

--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/BekreftelsePaaMottakAvPensjonDto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/BekreftelsePaaMottakAvPensjonDto.kt
@@ -5,8 +5,8 @@ import no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brevbaker.api.model.Foedselsnummer
 
-data class BekreftelsePaaMottakAvPensjonDto (
-    override val pesysData: PesysData, override  val saksbehandlerValg: EmptySaksbehandlerValg
+data class BekreftelsePaaMottakAvPensjonDto(
+    override val pesysData: PesysData, override val saksbehandlerValg: EmptySaksbehandlerValg
 ) : RedigerbarBrevdata<EmptySaksbehandlerValg, BekreftelsePaaMottakAvPensjonDto.PesysData> {
 
     data class PesysData(
--- pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/BekreftelsePaaMottakAvUfoeretrygdDto.kt
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/BekreftelsePaaMottakAvUfoeretrygdDto.kt
@@ -1,10 +1,17 @@
 package no.nav.pensjon.brev.api.model.maler.redigerbar
 
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
+import no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brevbaker.api.model.Foedselsnummer
 
-data class BekreftelsePaaMottakAvUfoeretrygdDto (
-    val navn: String,
-    val foedselsnummer: Foedselsnummer,
-    val statsborgerskap: String
-): BrevbakerBrevdata
+data class BekreftelsePaaMottakAvUfoeretrygdDto(
+    override val pesysData: PesysData, override val saksbehandlerValg: EmptySaksbehandlerValg
+) : RedigerbarBrevdata<EmptySaksbehandlerValg, BekreftelsePaaMottakAvUfoeretrygdDto.PesysData> {
+
+    data class PesysData(
+        val navn: String,
+        val foedselsnummer: Foedselsnummer,
+        val statsborgerskap: String
+    ) : BrevbakerBrevdata
+}
