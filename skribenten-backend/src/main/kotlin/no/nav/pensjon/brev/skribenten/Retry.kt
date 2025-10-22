04628c79f7e8d155344f20bdd433b36bc98846eb
Mads Opheim <mads.opheim@nav.no>
2025-10-22 14:54:36 +0200

    Forenklar retry ved å bruke Kotlin.Result heller enn eigenmekka klasse

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/Retry.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/Retry.kt
@@ -3,33 +3,20 @@ package no.nav.pensjon.brev.skribenten
 import kotlin.coroutines.cancellation.CancellationException
 
 
-sealed class RetryResult<T> {
-    data class Success<T>(
-        val content: T,
-    ) : RetryResult<T>()
-
-    data class Failure<T>(
-        val exceptions: List<Exception> = emptyList(),
-    ) : RetryResult<T>() {
-        fun samlaExceptions(): Exception = exceptions.last().also { last ->
-            exceptions.dropLast(1).reversed().forEach { last.addSuppressed(it) }
-        }
-    }
-}
-
 fun <T> retryOgPakkUt(times: Int, block: () -> T) = retryInner(times, emptyList(), block).let {
-    when (it) {
-        is RetryResult.Success -> it.content
-        is RetryResult.Failure -> throw it.samlaExceptions()
+    if (it.isSuccess) {
+        return@let it.getOrNull()
+    } else {
+        throw it.exceptionOrNull()!!
     }
 }
 
-private fun <T> retryInner(times: Int, exceptions: List<Exception>, block: () -> T): RetryResult<T> = try {
-    RetryResult.Success(block())
+private fun <T> retryInner(times: Int, exceptions: List<Exception>, block: () -> T): Result<T> = try {
+    Result.success(block())
 } catch (ex: Exception) {
     if (ex is CancellationException) throw ex
     if (times < 1) {
-        RetryResult.Failure(exceptions + ex)
+        Result.failure(ex.also { exceptions.forEach { ex.addSuppressed(it) } })
     } else {
         retryInner(times - 1, exceptions + ex, block)
     }
