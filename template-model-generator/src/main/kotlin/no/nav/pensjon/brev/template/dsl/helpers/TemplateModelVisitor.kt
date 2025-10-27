0b1ab48268df91ad8b4bc47d7eab812f55824172
Mads Opheim <mads.opheim@nav.no>
2025-10-27 15:23:16 +0100

    Treng ikkje å få åtvaringra frå modeldescription-oppbygginga om det tomme vedlegg-objektet

--- template-model-generator/src/main/kotlin/no/nav/pensjon/brev/template/dsl/helpers/TemplateModelVisitor.kt
+++ template-model-generator/src/main/kotlin/no/nav/pensjon/brev/template/dsl/helpers/TemplateModelVisitor.kt
@@ -10,7 +10,8 @@ private val SKIPPED_NO_WARN_PACKAGES: Set<String> = setOf("kotlin", "java.util",
 private val SKIPPED_NO_WARN_CLASSES: Set<String> = setOf(
     "no.nav.pensjon.brev.api.model.maler.EmptyRedigerbarBrevdata",
     "no.nav.pensjon.brev.api.model.maler.EmptyBrevdata",
-    "no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg"
+    "no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg",
+    "no.nav.pensjon.brev.api.model.maler.EmptyVedlegg"
 )
 
 private fun KSPLogger.logSkipped(classDeclaration: KSClassDeclaration) {
