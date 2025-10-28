af313c58492cf3db0d1b6704a7f7b27ddc19cf73
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2025-10-28 16:02:59 +0100

    Gjer ikkje så mykje om vi bommar på eit og anna valkey-oppslag (#2266)
    Så loggar det som info

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/Cache.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/Cache.kt
@@ -58,7 +58,7 @@ class Valkey(config: Config) : Cache() {
         }
     } catch (e: Exception) {
         if (e is CancellationException) throw e
-        logger.warn("Fikk feilmelding fra Valkey under forsøk på å hente verdi, returnerer null", e)
+        logger.info("Fikk feilmelding fra Valkey under forsøk på å hente verdi, returnerer null", e)
         null
     }
 
@@ -77,7 +77,7 @@ class Valkey(config: Config) : Cache() {
             }
         } catch (e: Exception) {
             if (e is CancellationException) throw e
-            logger.warn("Fikk feilmelding fra Valkey under forsøk på å oppdatere verdi", e)
+            logger.info("Fikk feilmelding fra Valkey under forsøk på å oppdatere verdi", e)
         }
     }
 
