518d774e496016b0652c323c78535cf4a01cdcd5
Scott Levkowetz <43199515+Scolev@users.noreply.github.com>
2025-10-29 14:21:53 +0100

    Flytter avslag tidliguttak/gradsendring til ny modul
    * Flytt brev om sivilstandsendring til alder-brevbaker-api-model og aldersmaler
    
    * WIP flytter avslag gradsendring og avslag uttak før normert til ny aldersmodul
    
    * flytter vedlegg
    
    * Bruker riktig vedlegg
    
    * Fjerner NormertPensjonsalderFormatering fra fellesmodul
    
    * Legger til vedlegg for opplysninger brukt i beregningen for å generere selectors
    
    * Opprydding i import etter flytting
    
    * Legg til feilaktig slettede DTOer etter merge
    
    * Fiks import vedlegg
    
    * Fiks import vedlegg
    
    * Fiks import test
    
    * Fjern import fra gammel modul
    
    * Legg til manglende import
    
    * Fjerner toggles fra brev
    
    * Bump version
    
    * Bump version
    
    * Bump version
    
    * VedleggDto implementerer vedleggdata
    
    ---------
    
    Co-authored-by: Chris Searle <chris@chrissearle.org>

---
+++ aldersmaler/src/main/kotlin/brev/felles/NormertPensjonsalderFormatering.kt
@@ -0,0 +1,24 @@
+package brev.felles
+
+import no.nav.pensjon.brev.model.alder.avslag.NormertPensjonsalder
+import no.nav.pensjon.brev.template.Expression
+import no.nav.pensjon.brev.template.Language
+import no.nav.pensjon.brev.template.Language.*
+import no.nav.pensjon.brev.template.LocalizedFormatter
+import no.nav.pensjon.brev.template.dsl.expression.format
+
+fun Expression<NormertPensjonsalder>.aarOgMaanederFormattert() = format(NormertPensjonsalderFormatter)
+
+object NormertPensjonsalderFormatter : LocalizedFormatter<NormertPensjonsalder>() {
+    override fun apply(first: NormertPensjonsalder, second: Language): String {
+        val aar = first.aar
+        val maaneder = first.maaneder
+        return when (second) {
+            Bokmal -> "$aar år" + if (maaneder > 0) " og $maaneder måneder" else ""
+            Nynorsk -> "$aar år" + if (maaneder > 0) " og $maaneder månadar" else ""
+            English -> "$aar years" + if (maaneder > 0) " and $maaneder months" else ""
+        }
+    }
+
+    override fun stableHashCode(): Int = "NormertPensjonsalderFormatter".hashCode()
+}
