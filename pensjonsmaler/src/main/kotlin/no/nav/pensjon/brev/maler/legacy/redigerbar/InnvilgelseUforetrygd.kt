087393b2c0f4135f1c66fca34b5486a660abe715
Knut.Mork <knut.mork@nav.no>
2025-11-05 17:00:34 +0100

    Ryddet i imports

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/InnvilgelseUforetrygd.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/InnvilgelseUforetrygd.kt
@@ -21,9 +21,6 @@ import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata
-import kotlin.and
-import kotlin.or
-import kotlin.text.format
 
 @TemplateModelHelpers
 object InnvilgelseUforetrygd : RedigerbarTemplate<AvslagUfoeretrygdDto> {
