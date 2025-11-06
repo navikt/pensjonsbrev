3704a367c42707edf1b8a1164ac284ea62753974
Jeremy Elsom <jeremy.elsom@nav.no>
2025-11-06 15:47:46 +0100

    Lagt til VedtakOmInnvilgelseAvOmsorgspoengDto.kt

---
+++ pensjon-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/VedtakOmInnvilgelseAvOmsorgspoengDto.kt
@@ -0,0 +1,18 @@
+package no.nav.pensjon.brev.api.model.maler.redigerbar
+
+import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
+import no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
+
+
+@Suppress("unused")
+data class VedtakOmInnvilgelseAvOmsorgspoengDto(
+    override val saksbehandlerValg: EmptySaksbehandlerValg,
+    override val pesysData: PesysData,
+) : RedigerbarBrevdata<EmptySaksbehandlerValg, VedtakOmInnvilgelseAvOmsorgspoengDto.PesysData> {
+
+    data class PesysData(
+        val omsorgsopptjeningsaar: String,
+        val orienteringOmSaksbehandlingstidDto: OrienteringOmSaksbehandlingstidDto
+    ) : BrevbakerBrevdata
+}
