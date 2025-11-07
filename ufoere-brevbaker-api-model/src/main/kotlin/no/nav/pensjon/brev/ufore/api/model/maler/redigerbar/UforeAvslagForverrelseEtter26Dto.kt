36a02d417d1a3792baaf87a76cee52cd992923b7
Knut.Mork <knut.mork@nav.no>
2025-11-07 08:53:12 +0100

    Rydda i imports

--- ufoere-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/UforeAvslagForverrelseEtter26Dto.kt
+++ ufoere-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/UforeAvslagForverrelseEtter26Dto.kt
@@ -3,13 +3,15 @@ package no.nav.pensjon.brev.ufore.api.model.maler.redigerbar
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagForverrelseEtter26Dto.Saksbehandlervalg
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagForverrelseEtter26Dto.UforeAvslagPendata
 import no.nav.pensjon.brevbaker.api.model.DisplayText
 import java.time.LocalDate
 
 data class UforeAvslagForverrelseEtter26Dto(
     override val pesysData: UforeAvslagPendata,
     override val saksbehandlerValg: Saksbehandlervalg
-) : RedigerbarBrevdata<UforeAvslagForverrelseEtter26Dto.Saksbehandlervalg, UforeAvslagForverrelseEtter26Dto.UforeAvslagPendata> {
+) : RedigerbarBrevdata<Saksbehandlervalg, UforeAvslagPendata> {
 
     data class Saksbehandlervalg(
         @DisplayText("Bruk vurdering fra vilkårsvedtak")
--- ufoere-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/UforeAvslagUforetidspunkt26Dto.kt
+++ ufoere-brevbaker-api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/UforeAvslagUforetidspunkt26Dto.kt
@@ -3,13 +3,15 @@ package no.nav.pensjon.brev.ufore.api.model.maler.redigerbar
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagUforetidspunkt26Dto.Saksbehandlervalg
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagUforetidspunkt26Dto.UforeAvslagPendata
 import no.nav.pensjon.brevbaker.api.model.DisplayText
 import java.time.LocalDate
 
 data class UforeAvslagUforetidspunkt26Dto(
     override val pesysData: UforeAvslagPendata,
     override val saksbehandlerValg: Saksbehandlervalg
-) : RedigerbarBrevdata<UforeAvslagUforetidspunkt26Dto.Saksbehandlervalg, UforeAvslagUforetidspunkt26Dto.UforeAvslagPendata> {
+) : RedigerbarBrevdata<Saksbehandlervalg, UforeAvslagPendata> {
 
     data class Saksbehandlervalg(
         @DisplayText("Bruk vurdering fra vilkårsvedtak")
