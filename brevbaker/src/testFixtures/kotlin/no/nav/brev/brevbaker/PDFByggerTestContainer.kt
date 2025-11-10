4c010859b330955f2c9f99522dd108262f930ee2
Mads Opheim <mads.opheim@nav.no>
2025-11-10 11:00:27 +0100

    Hjelpemetode

--- brevbaker/src/testFixtures/kotlin/no/nav/brev/brevbaker/PDFByggerTestContainer.kt
+++ brevbaker/src/testFixtures/kotlin/no/nav/brev/brevbaker/PDFByggerTestContainer.kt
@@ -6,22 +6,25 @@ import org.testcontainers.utility.DockerImageName
 
 object PDFByggerTestContainer {
 
-    // DIGEST blir i GitHub Actions-byggejobbane sendt inn som miljøvariabel. Lokalt kan vi bruke nyeste bygde.
-    // TODO: differensier lokal køyring
-    private val fullImageName = System.getenv("DIGEST")
+    private val pdfContainer: GenericContainer<*> = konfigurerPdfbyggerContainer()
+
+    private fun konfigurerPdfbyggerContainer(): GenericContainer<*> {
+        // DIGEST blir i GitHub Actions-byggejobbane sendt inn som miljøvariabel. Lokalt kan vi bruke nyeste bygde.
+        // TODO: differensier lokal køyring
+        val fullImageName = System.getenv("DIGEST")
             ?.takeIf { it.isNotBlank() }
             ?.let { "ghcr.io/navikt/pensjonsbrev/pdf-bygger:$it" }
             ?: "pensjonsbrev-pdf-bygger:latest"
-
-    private val pdfContainer: GenericContainer<*> = GenericContainer(DockerImageName.parse(fullImageName))
-        .withExposedPorts(8080)
-        .withEnv("PDF_COMPILE_TIMEOUT_SECONDS", "200")
-        .withEnv(
-            "JAVA_TOOL_OPTIONS",
-            "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5016 -Djdk.lang.Process.launchMechanism=vfork"
-        )
-        .withEnv("PDF_BYGGER_COMPILE_TMP_DIR", "/tmp")
-        .waitingFor(Wait.forHttp("/isAlive").forStatusCode(200))
+        return GenericContainer(DockerImageName.parse(fullImageName))
+            .withExposedPorts(8080)
+            .withEnv("PDF_COMPILE_TIMEOUT_SECONDS", "200")
+            .withEnv(
+                "JAVA_TOOL_OPTIONS",
+                "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5016 -Djdk.lang.Process.launchMechanism=vfork"
+            )
+            .withEnv("PDF_BYGGER_COMPILE_TMP_DIR", "/tmp")
+            .waitingFor(Wait.forHttp("/isAlive").forStatusCode(200))
+    }
 
     @Suppress("HttpUrlsUsage") // Kun for lokal kjøring
     fun mappedUrl() = "http://${pdfContainer.host}:${pdfContainer.getMappedPort(8080)}"
