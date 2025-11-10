b296c42a64655af42cafaf801e0d02ba5d2e43da
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2025-11-10 13:30:34 +0100

    Feilhandtering ved funksjonell feil under henting av brev eller attestering (#2351)
    * Feilhandtering ved funksjonell feil under henting av brev eller attestering
    
    * Bruker logger

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/SakBrev.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/SakBrev.kt
@@ -112,9 +112,14 @@ fun Route.sakBrev(dto2ApiService: Dto2ApiService, brevredigeringService: Brevred
             brevredigeringService.hentBrev(sak.saksId, brevId, reserver)
                 ?.onOk { brev ->
                     call.respond(HttpStatusCode.OK, dto2ApiService.toApi(brev))
-                }?.onError { message, statusCode ->
-                    call.application.log.error("$statusCode - Feil ved henting av brev: $message")
-                    call.respond(HttpStatusCode.InternalServerError, "Feil ved henting av brev.")
+                }?.onError { message, statusCode, tittel ->
+                    if (statusCode == HttpStatusCode.UnprocessableEntity) {
+                        logger.info("$statusCode - Funksjonell feil ved henting av brev: $message")
+                        call.respond(HttpStatusCode.UnprocessableEntity, BrevExceptionDto(tittel ?: "Feil ved henting av brev", message))
+                    } else {
+                        logger.error("$statusCode - Feil ved henting av brev: $message")
+                        call.respond(HttpStatusCode.InternalServerError, "Feil ved henting av brev.")
+                    }
                 }
                 ?: call.respond(HttpStatusCode.NotFound, "Fant ikke brev med id: $brevId")
         }
@@ -146,9 +151,14 @@ fun Route.sakBrev(dto2ApiService: Dto2ApiService, brevredigeringService: Brevred
 
                 brevredigeringService.hentBrevAttestering(sak.saksId, brevId, reserver)
                     ?.onOk { call.respond(HttpStatusCode.OK, dto2ApiService.toApi(it)) }
-                    ?.onError { message, statusCode ->
-                        call.application.log.error("$statusCode - Feil ved henting av brev: $message")
-                        call.respond(HttpStatusCode.InternalServerError, "Feil ved henting av brev.")
+                    ?.onError { message, statusCode, tittel ->
+                        if (statusCode == HttpStatusCode.UnprocessableEntity) {
+                            logger.info("$statusCode - Funksjonell feil ved attestering av brev: $message")
+                            call.respond(HttpStatusCode.UnprocessableEntity, BrevExceptionDto(tittel ?: "Feil ved attestering av brev", message))
+                        } else {
+                            logger.error("$statusCode - Feil ved attestering av brev: $message")
+                            call.respond(HttpStatusCode.InternalServerError, "Feil ved attestering av brev.")
+                        }
                     }
                     ?: call.respond(HttpStatusCode.NotFound, "Fant ikke brev med id: $brevId")
             }
