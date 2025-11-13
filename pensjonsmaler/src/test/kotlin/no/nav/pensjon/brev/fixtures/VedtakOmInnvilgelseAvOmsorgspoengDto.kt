e08253e972e6bcd61e85e533fdc2744db576de51
Jeremy Elsom <jeremy.elsom@nav.no>
2025-11-13 10:15:25 +0100

    Opprettet VedtakOmInnvilgelseAvOmsorgspoengDto.kt

--- pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/fixtures/VedtakOmInnvilgelseAvOmsorgspoengDto.kt
+++ pensjonsmaler/src/test/kotlin/no/nav/pensjon/brev/fixtures/VedtakOmInnvilgelseAvOmsorgspoengDto.kt
@@ -1,10 +1,15 @@
 package no.nav.pensjon.brev.fixtures
 
+import no.nav.pensjon.brev.Fixtures
 import no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.redigerbar.VedtakOmInnvilgelseAvOmsorgspoengDto
 
 fun createVedtakOmInnvilgelseAvOmsorgspoengDto() =
     VedtakOmInnvilgelseAvOmsorgspoengDto(
         saksbehandlerValg = EmptySaksbehandlerValg,
-        pesysData = createVedtakOmInnvilgelseAvOmsorgspoengDto()
+        pesysData = VedtakOmInnvilgelseAvOmsorgspoengDto.PesysData(
+            brukerNavn = "Per Pensjon",
+            omsorgsopptjeningsaar = "2025",
+            orienteringOmSaksbehandlingstidDto = Fixtures.create()
+        )
     )
