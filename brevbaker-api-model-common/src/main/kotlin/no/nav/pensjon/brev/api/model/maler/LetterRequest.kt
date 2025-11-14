cc562eafbfcd0515d434e08ec9fb8b9696c76c97
Mads Opheim <mads.opheim@nav.no>
2025-11-14 18:12:11 +0100

    Startar å splitte request-objektet for hhv autobrev og redigerbare, det trengs for å kunne skilje meir seinare

--- brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/LetterRequest.kt
+++ brevbaker-api-model-common/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/LetterRequest.kt
@@ -1,5 +1,6 @@
 package no.nav.pensjon.brev.api.model
 
+import no.nav.pensjon.brev.api.model.maler.AutobrevData
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
@@ -26,6 +27,46 @@ class BestillBrevRequest<T : Brevkode<T>>(
     override fun hashCode() = Objects.hash(kode, letterData, felles, language)
 
     override fun toString() = "BestillBrevRequest(kode=$kode, letterData=$letterData, felles=$felles, language=$language)"
+}
+
+@Suppress("unused")
+class BestillAutobrevRequest<T : Brevkode<T>>(
+    val kode: T,
+    val letterData: AutobrevData,
+    val felles: Felles,
+    val language: LanguageCode,
+) : BrevRequest<T> {
+    override fun equals(other: Any?): Boolean {
+        if (other !is BestillAutobrevRequest<*>) return false
+        return kode == other.kode
+                && letterData == other.letterData
+                && felles == other.felles
+                && language == other.language
+    }
+
+    override fun hashCode() = Objects.hash(kode, letterData, felles, language)
+
+    override fun toString() = "BestillAutobrevRequest(kode=$kode, letterData=$letterData, felles=$felles, language=$language)"
+}
+
+@Suppress("unused")
+class BestillRedigerbartBrevRequest<T : Brevkode<T>>(
+    val kode: T,
+    val letterData: RedigerbarBrevdata<*, *>,
+    val felles: Felles,
+    val language: LanguageCode,
+) : BrevRequest<T> {
+    override fun equals(other: Any?): Boolean {
+        if (other !is BestillRedigerbartBrevRequest<*>) return false
+        return kode == other.kode
+                && letterData == other.letterData
+                && felles == other.felles
+                && language == other.language
+    }
+
+    override fun hashCode() = Objects.hash(kode, letterData, felles, language)
+
+    override fun toString() = "BestillRedigerbartBrevRequest(kode=$kode, letterData=$letterData, felles=$felles, language=$language)"
 }
 
 @Suppress("unused")
