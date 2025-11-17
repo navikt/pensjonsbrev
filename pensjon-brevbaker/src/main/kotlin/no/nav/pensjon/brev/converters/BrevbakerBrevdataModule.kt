a6c05b55bad1a158d35d31eac09d01ac36b7fc29
Mads Opheim <mads.opheim@nav.no>
2025-11-17 12:58:35 +0100

    Brevbaker-appen skal kun trengje å kunne deserialisere to modell-typar: modell for autobrev og modell for redigerbart brev

--- pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/converters/BrevbakerBrevdataModule.kt
+++ pensjon-brevbaker/src/main/kotlin/no/nav/pensjon/brev/converters/BrevbakerBrevdataModule.kt
@@ -5,16 +5,12 @@ import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
 import no.nav.pensjon.brev.api.model.maler.AutobrevData
-import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
 
 object BrevbakerBrevdataModule : SimpleModule() {
     private fun readResolve(): Any = BrevbakerBrevdataModule
-
-    // TODO: Håper å klare å fase ut denne.
-    private class GenericBrevdata : LinkedHashMap<String, Any>(), BrevbakerBrevdata, FagsystemBrevdata, SaksbehandlerValgBrevdata
 
     private class GenericSaksbehandlervalg : LinkedHashMap<String, Any>(), SaksbehandlerValgBrevdata
 
@@ -25,13 +21,8 @@ object BrevbakerBrevdataModule : SimpleModule() {
     private data class GenericRedigerbarBrevdata(override val saksbehandlerValg: GenericSaksbehandlervalg, override val pesysData: GenericFagsystemBrevdata) : RedigerbarBrevdata<GenericSaksbehandlervalg, GenericFagsystemBrevdata>
 
     init {
-        addDeserializer(BrevbakerBrevdata::class.java, BrevdataDeserializer)
         addDeserializer(AutobrevData::class.java, AutobrevdataDeserializer)
         addDeserializer(RedigerbarBrevdata::class.java, RedigerbarBrevdataDeserializer)
-    }
-
-    private object BrevdataDeserializer : JsonDeserializer<BrevbakerBrevdata>() {
-        override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): BrevbakerBrevdata = ctxt.readValue(parser, GenericBrevdata::class.java)
     }
 
     private object AutobrevdataDeserializer : JsonDeserializer<AutobrevData>() {
