aae576a17527e3817c2774c4422bceb0eaa536ed
Håkon Heggholmen <hakon.heggholmen@nav.no>
2025-11-17 10:28:49 +0100

    Fjerner cache disable som var ment for test.
    Co-authored-by: routsi <82815189+routsi@users.noreply.github.com>

--- aldersmaler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/aldersovergang/EndringAvAlderspensjonFordiDuFyller75AarAutoDto.kt
+++ aldersmaler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/aldersovergang/EndringAvAlderspensjonFordiDuFyller75AarAutoDto.kt
@@ -1,6 +1,6 @@
 package no.nav.pensjon.brev.alder.maler.aldersovergang
 
-import no.nav.pensjon.brev.alder.maler.createMaanedligPensjonFoerSkatt
+import no.nav.pensjon.brev.alder.maler.vedlegg.createMaanedligPensjonFoerSkatt
 import no.nav.pensjon.brev.alder.maler.vedlegg.createOpplysningerBruktIBeregningAlderDto
 import no.nav.pensjon.brev.maler.vedlegg.createOrienteringOmRettigheterOgPlikterDto
 import no.nav.pensjon.brev.alder.model.AlderspensjonRegelverkType
