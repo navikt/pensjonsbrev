eeea205384b82b81781cb7d43d76fe2d43d32f0f
Knut Mork <knut.mork@nav.no>
2025-12-01 10:05:52 +0100

    UT Avslag 12-2 utland: Fiks manglende avtaleland og artikkel (#2467)
    * Viser riktig tekst dersom ved manglende avtaleland og artikkel
    
    * Spesialhåndtering ift flere avtaleverk med Storbritannia.

--- ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/uforeavslag/UforeAvslagMedlemskapUtland.kt
+++ ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/uforeavslag/UforeAvslagMedlemskapUtland.kt
@@ -5,6 +5,8 @@ import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.template.Language.Bokmal
 import no.nav.pensjon.brev.template.RedigerbarTemplate
 import no.nav.pensjon.brev.template.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.and
+import no.nav.pensjon.brev.template.dsl.expression.equalTo
 import no.nav.pensjon.brev.template.dsl.expression.format
 import no.nav.pensjon.brev.template.dsl.expression.ifNull
 import no.nav.pensjon.brev.template.dsl.expression.isOneOf
@@ -223,9 +225,15 @@ object UforeAvslagMedlemskapUtland : RedigerbarTemplate<UforeAvslagUtlandDto> {
                     paragraph {
                         text(bokmal { +"Vedtaket har vi også gjort etter EØS-forordning 883/2004 artikkel 6 og artikkel 51, og forskrift om beregning av uføretrygd etter EØS-avtalen av 12. februar 2015." })
                     }
-                }.orShowIf(pesysData.avtaletype.notNull()) {
-                    paragraph {
-                        text(bokmal { + "Vedtaket er også gjort etter trygdeavtalen med " + pesysData.avtaletype.ifNull("avtaletype") + " artikkel " + pesysData.artikkel.ifNull("X") + "." })
+                }.orShowIf(pesysData.avtaletype.notNull() and pesysData.artikkel.notNull()) {
+                    showIf(pesysData.avtaletype.equalTo("Storbritannia")) {
+                        paragraph {
+                            text(bokmal { + "Vedtaket har vi også gjort etter konvensjonen mellom Island, Liechtenstein, Norge og Storbritannia artikkel 10 og artikkel 46." })
+                        }
+                    }.orShow {
+                        paragraph {
+                            text(bokmal { + "Vedtaket har vi også gjort etter trygdeavtalen med " + pesysData.avtaletype.ifNull("avtaletype") + " artikkel " + pesysData.artikkel.ifNull("X") + "." })
+                        }
                     }
                 }.orShow {
                     paragraph {
