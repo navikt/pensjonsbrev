9d51ccc13d2e050441ceabcaff89cfbaeec1bbb4
Knut.Mork <knut.mork@nav.no>
2025-12-01 14:57:30 +0100

    Endring i avslag for 12-7

--- ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/uforeavslag/UforeAvslagInntektsevne50.kt
+++ ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/uforeavslag/UforeAvslagInntektsevne50.kt
@@ -14,11 +14,15 @@ import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brev.ufore.api.model.Ufoerebrevkoder.Redigerbar.UT_AVSLAG_INNTEKTSEVNE_50
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDto
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.SaksbehandlervalgInntektSelectors.VisVurderingFraVilkarvedtak
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.SaksbehandlervalgInntektSelectors.visUnntaksregelFremtidigInntekt
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.SaksbehandlervalgInntektSelectors.visVurderingIEU
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.SaksbehandlervalgInntektSelectors.visVurderingIFU
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.inntektEtterUforhet
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.inntektForUforhet
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.kravMottattDato
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.uforetidspunkt
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.vurdering
+import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.vurderingIEU
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.UforeAvslagInntektPendataSelectors.vurderingIFU
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.pesysData
 import no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.UforeAvslagInntektDtoSelectors.saksbehandlerValg
@@ -65,16 +69,9 @@ object UforeAvslagInntektsevne50 : RedigerbarTemplate<UforeAvslagInntektDto> {
             paragraph {
                 text(bokmal { +"Vi sammenligner inntekten din før og etter at du ble ufør for å avgjøre i hvor stor grad inntektsevnen din er nedsatt." })
             }
-
-            // TODO: Ikke ferdig
-
-            showIf(saksbehandlerValg.VisVurderingFraVilkarvedtak) {
-                paragraph {
-                    text(bokmal { +pesysData.vurdering })
-                }
-            }
             paragraph {
-                text(bokmal { + fritekst("Individuell vurdering") })
+                text(bokmal { +"Uføretidspunktet ditt er satt til " + pesysData.uforetidspunkt.format() + ". " +
+                        "På dette tidspunktet vurderer vi at dine helseutfordringer førte til at din arbeidsevne ble varig nedsatt. " })
             }
 
             paragraph {
@@ -83,22 +80,58 @@ object UforeAvslagInntektsevne50 : RedigerbarTemplate<UforeAvslagInntektDto> {
                 showIf(saksbehandlerValg.visVurderingIFU) {
                     text( bokmal { + pesysData.vurderingIFU })
                 }.orShow {
-                    text( bokmal { + fritekst("Begrunnelse for fastsatt IFU.") })
+                    text( bokmal { + fritekst("Begrunnelse for IFU") })
                 }
                 text(bokmal { + " Oppjustert til dagens verdi tilsvarer dette en inntekt på " + fritekst("oppjustert IFU") + " kroner. " +
-                    "Du har en inntekt på " + pesysData.inntektEtterUforhet.format(CurrencyFormat) + " kroner, " +
-                    "og vi har derfor fastsatt din nedsatte inntektsevne til " +
-                    fritekst("sett inn fastsatt uføregrad før avrunding") + " prosent. " +
-                    "For å få uføretrygd, må inntektsevnen din som hovedregel være varig nedsatt med minst 50 prosent."})
+                    "Inntekt etter uførhet er satt til " + pesysData.inntektEtterUforhet.format(CurrencyFormat) + " kroner. " }
+                )
+                showIf(saksbehandlerValg.visVurderingIEU) {
+                    text( bokmal { + pesysData.vurderingIEU })
+                }.orShow {
+                    text( bokmal { + fritekst("Begrunnelse for IEU") })
+                }
             }
             paragraph {
-                text(bokmal { + "Du omfattes heller ikke av unntaksreglene for yrkesskade, yrkessykdom eller personer som har fått arbeidsavklaringspenger."})
+                text(bokmal { + "For å få uføretrygd må inntektsevnen din som hovedregel være varig nedsatt med minst 50 prosent."})
             }
+            paragraph {
+                text(bokmal { + "Vi har sammenlignet din inntekt før og etter at du ble ufør og kommet til at din uføregrad er " + fritekst("grad før avrunding") + " prosent."})
+            }
+            paragraph {
+                text(bokmal { + "Uføregraden din er under 50 prosent, og du omfattes heller ikke av unntaksreglene ved yrkesskade, yrkessykdom eller for personer som mottar arbeidsavklaringspenger på søknadstidspunktet."})
+            }
+
+            showIf(saksbehandlerValg.visUnntaksregelFremtidigInntekt) {
+                title1 {
+                    text(bokmal { +"Unntaksregel om fremtidig inntekt" })
+                }
+                paragraph {
+                    text(bokmal { +"Etter unntaksregel kan inntekt før uførhet settes ut fra den fremtidige inntekten. " +
+                            "Unntaksbestemmelsen skal sikre at personen får korrekt uføregrad ut fra hvilken stillingsandel vedkommende klarer å arbeide i. " })
+                }
+                paragraph {
+                    text(bokmal { +"Arbeidsforholdet må være dokumentert med en arbeidsavtale med en klart angitt stillingsprosent og oppstartsdato. " })
+                }
+                paragraph {
+                    text(bokmal { +"Vi vurderer at denne unntaksbestemmelsen ikke kan brukes i ditt tilfelle, fordi X (begrunnelse, " +
+                            "f.eks. ikke klart angitt stillingsprosent, jobber utover stillingsprosenten, ekstravakter/overtid, bonus, selvstendig næringsdrivende etc). " })
+                }
+            }
+
             paragraph {
                 text(bokmal { + "Du oppfyller ikke vilkårene, og vi avslår derfor søknaden din om uføretrygd."})
             }
             paragraph {
-                text(bokmal { +"Vedtaket er gjort etter folketrygdloven § 12-7." })
+                text(bokmal { +"Vedtaket har vi gjort etter folketrygdloven § 12-7." })
+            }
+
+            showIf(saksbehandlerValg.VisVurderingFraVilkarvedtak) {
+                paragraph {
+                    text(bokmal { +pesysData.vurdering })
+                }
+            }
+            paragraph {
+                text(bokmal { + fritekst("Individuell vurdering") })
             }
 
             includePhrase(Felles.HvaSkjerNa)
