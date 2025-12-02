1fd492f891cfe9439ef464e1068c503e0a114010
Mads Opheim <mads.opheim@nav.no>
2025-12-02 13:46:22 +0100

    Rettar datatype

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/IncludeAttachment.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/IncludeAttachment.kt
@@ -1,6 +1,5 @@
 package no.nav.pensjon.brev.template
 
-import no.nav.pensjon.brev.api.model.maler.EmptyVedleggData
 import no.nav.pensjon.brev.api.model.maler.FellesVedleggData
 import no.nav.pensjon.brev.api.model.maler.VedleggData
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
@@ -60,7 +59,7 @@ class AttachmentTemplate<out Lang : LanguageSupport, AttachmentData : VedleggDat
 class AlltidValgbartVedlegg<out Lang : LanguageSupport>(
     val vedlegg: AttachmentTemplate<Lang, FellesVedleggData>,
     val kode: AlltidValgbartVedleggKode
-) : HasModel<EmptyVedleggData>, StableHash by StableHash.of(vedlegg, StableHash.of(kode.kode())) {
+) : HasModel<FellesVedleggData>, StableHash by StableHash.of(vedlegg, StableHash.of(kode.kode())) {
     fun asIncludeAttachment(felles: Felles) = IncludeAttachment(FellesVedleggData(felles).expr(), vedlegg)
     override fun equals(other: Any?): Boolean {
         if (other !is AlltidValgbartVedlegg<*>) return false
