7a0bdcf80389799c94d82f2e7400bb2d9d7c30c7
Scott Hallvard Levkowetz <scott.hallvard.levkowetz@nav.no>
2025-12-08 13:20:08 +0100

    Rydd imports

--- aldersmaler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/aldersovergang/omregning/OmregningAlderUfore2016Dto.kt
+++ aldersmaler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/aldersovergang/omregning/OmregningAlderUfore2016Dto.kt
@@ -2,7 +2,6 @@ package no.nav.pensjon.brev.alder.maler.aldersovergang.omregning
 
 import no.nav.brev.brevbaker.vilkaarligDato
 import no.nav.pensjon.brev.alder.maler.vedlegg.createMaanedligPensjonFoerSkatt
-import no.nav.pensjon.brev.alder.maler.vedlegg.createMaanedligPensjonFoerSkattAlderspensjonDto
 import no.nav.pensjon.brev.alder.maler.vedlegg.createOpplysningerBruktIBeregningAlderAP2025Dto
 import no.nav.pensjon.brev.alder.maler.vedlegg.createOpplysningerBruktIBeregningAlderDto
 import no.nav.pensjon.brev.alder.model.BorMedSivilstand
@@ -10,7 +9,6 @@ import no.nav.pensjon.brev.alder.model.InformasjonOmMedlemskap
 import no.nav.pensjon.brev.alder.model.Sivilstand
 import no.nav.pensjon.brev.alder.model.aldersovergang.omregning.InngangOgEksportVurdering
 import no.nav.pensjon.brev.alder.model.aldersovergang.omregning.OmregningAlderUfore2016Dto
-import no.nav.pensjon.brev.alder.model.aldersovergang.omregning.OmregningAlderUfore2016RedigerbarDtoSelectors.pesysData
 import no.nav.pensjon.brev.alder.model.aldersovergang.omregning.PersongrunnlagAvdod
 import no.nav.pensjon.brev.maler.vedlegg.createOrienteringOmRettigheterOgPlikterDto
 import no.nav.pensjon.brevbaker.api.model.Kroner
