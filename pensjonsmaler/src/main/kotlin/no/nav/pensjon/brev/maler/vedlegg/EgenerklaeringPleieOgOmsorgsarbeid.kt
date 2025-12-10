595363eedee587dcf0df44020e10ba20baa81c11
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2025-12-10 14:51:14 +0100

    Oppdaterer syntaksen for formText, formChoice og choice (#2523)
    * Fjernar unødig kommentar
    
    * Oppdaterer syntaksen for formText så han tar inn tekst på samme måte som vi gjer elles
    
    * Oppdaterer syntaksen for formChoice så han tar inn tekst på samme måte som vi gjer elles
    
    * Oppdaterer syntaksen for choice så han tar inn tekst på samme måte som vi gjer elles.
    
    Bruker samme mekanisme som vi gjer for andre tekst-blokker, så gjenbruker dermed createTextContent-metodane

--- pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/EgenerklaeringPleieOgOmsorgsarbeid.kt
+++ pensjonsmaler/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/EgenerklaeringPleieOgOmsorgsarbeid.kt
@@ -70,29 +70,29 @@ private fun OutlineOnlyScope<LangBokmalNynorskEnglish, *>.vedlegg(returadresse: 
 
     paragraph {
         formText(
-            Size.LONG, newText(
-                Bokmal to "Navn på pleietrengende:",
-                Nynorsk to "Namn på pleietrengande:",
-                English to "I have provided care work for:",
-            )
+            Size.LONG, { text(
+                bokmal { +"Navn på pleietrengende:" },
+                nynorsk { +"Namn på pleietrengande:" },
+                english { +"I have provided care work for:" }
+            ) }
         )
 
         formChoice(
-            newText(
-                Bokmal to "Arbeidet har vart i:",
-                Nynorsk to "Arbeidet har vart i:",
-                English to "The work has lasted for:"
-            )
+            { text(
+                bokmal { +"Arbeidet har vart i:" },
+                nynorsk { +"Arbeidet har vart i:" },
+                english { +"The work has lasted for:" }
+            ) }
         ) {
             choice(
-                Bokmal to "minst seks måneder",
-                Nynorsk to "minst seks månader",
-                English to "at least six months"
+                bokmal { +"minst seks måneder" },
+                nynorsk { +"minst seks månader" },
+                english { +"at least six months" }
             )
             choice(
-                Bokmal to "under seks måneder",
-                Nynorsk to "under seks månader",
-                English to "less than six months"
+                bokmal { +"under seks måneder" },
+                nynorsk { +"under seks månader" },
+                english { +"less than six months" }
             )
         }
     }
@@ -108,27 +108,29 @@ private fun OutlineOnlyScope<LangBokmalNynorskEnglish, *>.vedlegg(returadresse: 
         formText(
             size = Size.SHORT,
             vspace = false,
-            prompt = newText(
-                Bokmal to "Oppgi dato for opphøret:",
-                Nynorsk to "Dato for opphøyr:",
-                English to "State date if ceased:"
-            )
+            prompt = {
+                text (
+                    bokmal { +"Oppgi dato for opphøret:" },
+                    nynorsk { +"Dato for opphøyr:" },
+                    english { +"State date if ceased:" }
+                )
+            }
         )
         formText(
             size = Size.LONG,
             vspace = false,
-            prompt = newText(
-                Bokmal to "Oppgi årsaken til opphøret:",
-                Nynorsk to "Grunnen til opphøyr: ",
-                English to "State reason if ceased"
-            )
+            prompt = { text(
+                bokmal { +"Oppgi årsaken til opphøret:" },
+                nynorsk { +"Grunnen til opphøyr: " },
+                english { +"State reason if ceased" }
+            ) }
         )
 
-        formText(size = Size.SHORT, prompt = newText(Bokmal to "Dato:", Nynorsk to "Dato:", English to "Date"))
+        formText(size = Size.SHORT, prompt = { text(bokmal { +"Dato:" }, nynorsk { +"Dato:" }, english { +"Date" }) })
         formText(
             size = Size.LONG,
             vspace = false,
-            prompt = newText(Bokmal to "Underskrift:", Nynorsk to "Underskrift:", English to "Signature:")
+            prompt = { text(bokmal { +"Underskrift:" }, nynorsk { +"Underskrift:" }, english { +"Signature:" }) }
         )
     }
 
