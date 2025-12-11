00e42dca23cad54ff293fc5ad19a0b13e8ff9cf0
Mads Opheim <mads.opheim@nav.no>
2025-12-11 16:15:11 +0100

    outline utanfor parentes

--- aldersmaler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/vedlegg/alltidValgbare/SkjemaForBankopplysninger.kt
+++ aldersmaler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/vedlegg/alltidValgbare/SkjemaForBankopplysninger.kt
@@ -15,107 +15,106 @@ val skjemaForBankopplysninger = createAttachment<LangBokmalEnglish, EmptyVedlegg
             english { +"Bank details" }
         )
     },
-    includeSakspart = false,
-    outline = {
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"Norsk personnummer (11 siffer):" },
-                        english { +"Norwegian Identity number (11 digits):" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"Navn:" },
-                        english { +"Name:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"Adresse:" },
-                        english { +"Address:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"IBAN-nummer/ kontonummer:" },
-                        english { +"IBAN-number/ account number:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"SWIFT/BIC-kode:" },
-                        english { +"SWIFT/ BIC-code:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            text(
-                bokmal { +"Dette er koden man oppgir ved utenlandsbetalinger. Koden finner du som regel på banken sine hjemmesider." },
-                english { +"This is the code you enter when making international payments. You can usually find the code on your bank's website." }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"Bankens navn:" },
-                        english { +"Name of your bank:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"Bankens adresse:" },
-                        english { +"Bank address:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            formText(
-                Size.FILL,
-                {
-                    text(
-                        bokmal { +"Underskrift:" },
-                        english { +"Signature:" }
-                    )
-                }
-            )
-        }
-        paragraph {
-            text(
-                bokmal { +"Kopi av legitimasjon (pass, førerkort eller ID-kort med bilde) må vedlegges."},
-                english { +"Copy of valid identity document (e.g. passport, driving license, bank card with photo) must be attached." }
-            )
-        }
+    includeSakspart = false
+) {
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"Norsk personnummer (11 siffer):" },
+                    english { +"Norwegian Identity number (11 digits):" }
+                )
+            }
+        )
     }
-)
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"Navn:" },
+                    english { +"Name:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"Adresse:" },
+                    english { +"Address:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"IBAN-nummer/ kontonummer:" },
+                    english { +"IBAN-number/ account number:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"SWIFT/BIC-kode:" },
+                    english { +"SWIFT/ BIC-code:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        text(
+            bokmal { +"Dette er koden man oppgir ved utenlandsbetalinger. Koden finner du som regel på banken sine hjemmesider." },
+            english { +"This is the code you enter when making international payments. You can usually find the code on your bank's website." }
+        )
+    }
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"Bankens navn:" },
+                    english { +"Name of your bank:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"Bankens adresse:" },
+                    english { +"Bank address:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        formText(
+            Size.FILL,
+            {
+                text(
+                    bokmal { +"Underskrift:" },
+                    english { +"Signature:" }
+                )
+            }
+        )
+    }
+    paragraph {
+        text(
+            bokmal { +"Kopi av legitimasjon (pass, førerkort eller ID-kort med bilde) må vedlegges." },
+            english { +"Copy of valid identity document (e.g. passport, driving license, bank card with photo) must be attached." }
+        )
+    }
+}
