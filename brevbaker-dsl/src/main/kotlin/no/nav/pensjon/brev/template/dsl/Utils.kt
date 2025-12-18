9038c8615e5b9d4473437cc988141cd6b650ba00
Mads Opheim <mads.opheim@nav.no>
2025-12-18 11:04:15 +0100

    Flyttar newText-metodar som primært vart brukt i test over i testFixtures.
    Den eine plassen vi bruker det i koden kan vi ha den metoden vi faktisk bruker som privat hjelpemetode

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/dsl/Utils.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/dsl/Utils.kt
@@ -1,30 +1,6 @@
 package no.nav.pensjon.brev.template.dsl
 
 import no.nav.pensjon.brev.template.*
-import no.nav.pensjon.brev.template.ContentOrControlStructure.Content
-import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType
-import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType.PLAIN
-
-fun <Lang1 : Language> newText(
-    lang1: Pair<Lang1, String>,
-    fontType: FontType = PLAIN,
-): TextElement<LanguageSupport.Single<Lang1>> =
-    Content(Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1, fontType))
-
-fun <Lang1 : Language, Lang2 : Language> newText(
-    lang1: Pair<Lang1, String>,
-    lang2: Pair<Lang2, String>,
-    fontType: FontType = PLAIN,
-): TextElement<LanguageSupport.Double<Lang1, Lang2>> =
-    Content(Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1, lang2, fontType))
-
-fun <Lang1 : Language, Lang2 : Language, Lang3 : Language> newText(
-    lang1: Pair<Lang1, String>,
-    lang2: Pair<Lang2, String>,
-    lang3: Pair<Lang3, String>,
-    fontType: FontType = PLAIN,
-): TextElement<LanguageSupport.Triple<Lang1, Lang2, Lang3>> =
-    Content(Element.OutlineContent.ParagraphContent.Text.Literal.create(lang1, lang2, lang3, fontType))
 
 fun <Lang1 : Language> languages(lang1: Lang1): LanguageSupport.Single<Lang1> =
     LanguageCombination.Single(lang1)
