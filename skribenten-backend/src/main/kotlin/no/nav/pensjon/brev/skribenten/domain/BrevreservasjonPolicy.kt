b7c994ff7c66b129d303daa6daf30cd341dba3a5
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2025-12-19 11:56:30 +0100

    Gjør brevreservasjon timeout statisk

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/domain/BrevreservasjonPolicy.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/domain/BrevreservasjonPolicy.kt
@@ -7,7 +7,11 @@ import java.time.Instant
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.toJavaDuration
 
-class BrevreservasjonPolicy(val timeout: Duration = 10.minutes.toJavaDuration()) {
+class BrevreservasjonPolicy {
+
+    companion object {
+        val timeout: Duration = 10.minutes.toJavaDuration()
+    }
 
     fun kanReservere(brev: Brevredigering, fra: Instant, saksbehandler: NavIdent): Result<Boolean, ReservertAvAnnen> {
         val eksisterende = brev.reservasjon
