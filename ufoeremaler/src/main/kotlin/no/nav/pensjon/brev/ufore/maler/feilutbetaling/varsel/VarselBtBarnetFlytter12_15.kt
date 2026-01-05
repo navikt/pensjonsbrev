9d05dde116b0af43875c8bd258a7e6208a4ea3a6
petterhannevold <104101317+petterhannevold@users.noreply.github.com>
2026-01-05 15:41:23 +0100

    Lagt varselbrev feilutbetaling i vedtakskontekst (#2603)

--- ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/feilutbetaling/varsel/VarselBtBarnetFlytter12_15.kt
+++ ufoeremaler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/feilutbetaling/varsel/VarselBtBarnetFlytter12_15.kt
@@ -2,7 +2,7 @@ package no.nav.pensjon.brev.ufore.maler.feilutbetaling.varsel
 
 import no.nav.pensjon.brev.api.model.Sakstype.UFOREP
 import no.nav.pensjon.brev.api.model.TemplateDescription.Brevkategori.FEILUTBETALING
-import no.nav.pensjon.brev.api.model.TemplateDescription.Brevkontekst.ALLE
+import no.nav.pensjon.brev.api.model.TemplateDescription.Brevkontekst.VEDTAK
 import no.nav.pensjon.brev.template.Language.Bokmal
 import no.nav.pensjon.brev.template.Language.Nynorsk
 import no.nav.pensjon.brev.template.LocalizedFormatter.CurrencyFormat
@@ -28,7 +28,7 @@ object VarselBtBarnetFlytter12_15: RedigerbarTemplate<FeilutbetalingSpesifikkVar
 
     override val kode = Ufoerebrevkoder.Redigerbar.UT_FEILUTBETALING_VARSEL_BARN_FLYTTER
     override val kategori = FEILUTBETALING
-    override val brevkontekst = ALLE
+    override val brevkontekst = VEDTAK
     override val sakstyper = setOf(UFOREP)
 
     override val template = createTemplate(
