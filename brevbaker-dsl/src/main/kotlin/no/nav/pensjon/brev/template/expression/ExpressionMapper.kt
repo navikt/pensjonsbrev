51e5eb921934eccc6dff17789a2c6babe4158db4
Mads Opheim <mads.opheim@nav.no>
2026-01-05 13:17:16 +0100

    IntToYear og IntToKroner kan vera interne

--- brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/expression/ExpressionMapper.kt
+++ brevbaker-dsl/src/main/kotlin/no/nav/pensjon/brev/template/expression/ExpressionMapper.kt
@@ -4,19 +4,19 @@ import no.nav.pensjon.brev.template.StableHash
 import no.nav.pensjon.brevbaker.api.model.Kroner
 import no.nav.pensjon.brevbaker.api.model.Year
 
-interface ExpressionMapper<In, Out>: StableHash {
+sealed interface ExpressionMapper<In, Out>: StableHash {
     val name: String
     val apply: (In) -> Out
 
     override fun stableHashCode(): Int = StableHash.of(name).stableHashCode()
 }
 
-object IntToKroner : ExpressionMapper<Int, Kroner> {
+internal object IntToKroner : ExpressionMapper<Int, Kroner> {
     override val name: String = "IntToKroner"
     override val apply = ::Kroner
 }
 
-object IntToYear : ExpressionMapper<Int, Year> {
+internal object IntToYear : ExpressionMapper<Int, Year> {
     override val name: String = "IntToYear"
     override val apply = ::Year
 }
