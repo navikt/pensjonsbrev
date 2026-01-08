bc9dc1da1c3445c55825df8c2b86a3d23e9fc373
Andreas Balevik <andreas.balevik@nav.no>
2026-01-08 10:06:19 +0100

    dynamisk grunnbeloep i brev om aktivitetsplikt 4, 6, 10 mnd

--- etterlattemaler/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadAktivitetspliktInformasjon4mnd.kt
+++ etterlattemaler/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/OmstillingsstoenadAktivitetspliktInformasjon4mnd.kt
@@ -1,5 +1,6 @@
 package no.nav.pensjon.etterlatte.fixtures
 
+import no.nav.pensjon.brevbaker.api.model.Kroner
 import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.aktivitetsplikt.Aktivitetsgrad
 import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.aktivitetsplikt.NasjonalEllerUtland
 import no.nav.pensjon.etterlatte.maler.omstillingsstoenad.aktivitetsplikt.OmstillingsstoenadAktivitetspliktInformasjon4mndInnholdDTO
@@ -9,6 +10,7 @@ fun createOmstillingsstoenadAktivitetspliktInformasjon4mndInnholdDTO() =
     OmstillingsstoenadAktivitetspliktInformasjon4mndInnholdDTO(
         aktivitetsgrad = Aktivitetsgrad.OVER_50_PROSENT,
         utbetaling = true,
-        redusertEtterInntekt = true,
-        nasjonalEllerUtland = NasjonalEllerUtland.NASJONAL
+        redusertEtterInntekt = false,
+        nasjonalEllerUtland = NasjonalEllerUtland.NASJONAL,
+        halvtGrunnbeloep = Kroner(130160 / 2)
     )
