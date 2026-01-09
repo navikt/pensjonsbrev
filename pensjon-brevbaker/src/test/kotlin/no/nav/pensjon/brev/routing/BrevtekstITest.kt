c5b1d737c9a914c5bdce22488197c8078f934177
Mads Opheim <mads.opheim@nav.no>
2026-01-09 15:54:35 +0100

    Mergefeil

--- pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/routing/BrevtekstITest.kt
+++ pensjon-brevbaker/src/test/kotlin/no/nav/pensjon/brev/routing/BrevtekstITest.kt
@@ -1,7 +1,5 @@
 package no.nav.pensjon.brev.routing
 
-import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.containsSubstring
 import io.ktor.client.call.body
 import io.ktor.client.request.accept
 import io.ktor.client.request.post
