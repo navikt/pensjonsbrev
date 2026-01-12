2e4b142291cd2800629932890eee41de43e4f0aa
Mads Opheim <mads.opheim@nav.no>
2026-01-12 13:25:05 +0100

    Uføremalane brukar per no berre uføre-sakstypen

--- ufoere/maler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/Sakstype.kt
+++ ufoere/maler/src/main/kotlin/no/nav/pensjon/brev/ufore/maler/Sakstype.kt
@@ -3,36 +3,8 @@ package no.nav.pensjon.brev.ufore.maler
 import no.nav.pensjon.brev.api.model.ISakstype
 
 enum class Sakstype : ISakstype {
-    AFP,
-    AFP_PRIVAT,
-    ALDER,
-    BARNEP,
-    FAM_PL,
-    GAM_YRK,
-    GENRL,
-    GJENLEV,
-    GRBL,
-    KRIGSP,
-    OMSORG,
     UFOREP,
     ;
 
     override fun kode() = name
-
-    companion object {
-        val all: Set<Sakstype> = entries.toSet()
-        val pensjon: Set<Sakstype> = setOf(
-            AFP,
-            AFP_PRIVAT,
-            ALDER,
-            BARNEP,
-            FAM_PL,
-            GAM_YRK,
-            GENRL,
-            GJENLEV,
-            GRBL,
-            KRIGSP,
-            OMSORG
-        )
-    }
 }
