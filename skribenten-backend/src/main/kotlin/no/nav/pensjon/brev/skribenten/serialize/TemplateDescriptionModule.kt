ced89d59c282b78ec479372b733d44a2d0ff50e4
Mads Opheim <mads.opheim@nav.no>
2026-01-13 15:36:12 +0100

    Sett opp deserialisering for brevbaker-appen

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/serialize/TemplateDescriptionModule.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/serialize/TemplateDescriptionModule.kt
@@ -1,8 +1,11 @@
 package no.nav.pensjon.brev.skribenten.serialize
 
+import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.module.SimpleModule
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.skribenten.model.Pen
@@ -12,7 +15,14 @@ object TemplateDescriptionModule : SimpleModule() {
     private fun readResolve(): Any = TemplateDescriptionModule
 
     init {
+        addSerializer(TemplateDescription.IBrevkategori::class.java, BrevkategoriSerializer())
         addDeserializer(TemplateDescription.IBrevkategori::class.java, BrevkategoriDeserializer())
+    }
+
+    private class BrevkategoriSerializer : JsonSerializer<TemplateDescription.IBrevkategori>() {
+        override fun serialize(brevkategori: TemplateDescription.IBrevkategori, generator: JsonGenerator, serializers: SerializerProvider?) {
+            generator.writeString(brevkategori.kode())
+        }
     }
 
     private class BrevkategoriDeserializer : JsonDeserializer<TemplateDescription.IBrevkategori>() {
