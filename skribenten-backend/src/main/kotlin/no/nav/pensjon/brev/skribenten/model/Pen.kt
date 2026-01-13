5c81651d146ed165828896a0532757ed63776475
Mads Opheim <mads.opheim@nav.no>
2026-01-13 15:23:10 +0100

    Deserialiserer i skribenten til denne enumen, for å vera sikker på at overstyring og alt sånt blir rett

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/model/Pen.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/model/Pen.kt
@@ -166,4 +166,25 @@ object Pen {
     ) {
         data class Error(val brevIkkeStoettet: String?, val tekniskgrunn: String?, val beskrivelse: String?)
     }
+
+    enum class Brevkategori(override val kategoritekst: String) : TemplateDescription.IBrevkategori {
+        ETTEROPPGJOER("Etteroppgjør"),
+        FEILUTBETALING("Feilutbetaling"),
+        FOERSTEGANGSBEHANDLING("Førstegangsbehandling"),
+        FRITEKSTBREV("Fritekstbrev"),
+        INFORMASJONSBREV("Informasjonsbrev"),
+        INNHENTE_OPPLYSNINGER("Innhente opplysninger"),
+        KLAGE_OG_ANKE("Klage og anke"),
+        LEVEATTEST("Leveattest"),
+        OMSORGSOPPTJENING("Omsorgsopptjening"),
+        POSTERINGSGRUNNLAG("Posteringsgrunnlag"),
+        SLUTTBEHANDLING("Sluttbehandling"),
+        UFOEREPENSJON("Uførepensjon"),
+        VARSEL("Varsel"),
+        VEDTAK_EKSPORT("Vedtak - eksport"),
+        VEDTAK_ENDRING_OG_REVURDERING("Vedtak - endring og revurdering"),
+        VEDTAK_FLYTTE_MELLOM_LAND("Vedtak - flytte mellom land");
+
+        override fun kode() = name
+    }
 }
