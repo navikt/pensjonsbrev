4b6cba7c583d6ebf6b5b153865a5aff67ac01c18
Mads Opheim <mads.opheim@nav.no>
2026-01-16 09:32:04 +0100

    Loggar warnings viss det er tex-feil under pdf-generering

--- brevbaker/pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/latex/LatexCompileService.kt
+++ brevbaker/pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/latex/LatexCompileService.kt
@@ -109,8 +109,10 @@ class LatexCompileService(
                 process.onExit().await()
 
                 if (process.exitValue() == 0) {
-                    logger.info("Compilation successful. Output: ${output.toFile().readText()}")
-                    logger.info("Compilation successful. Errors: ${error.toFile().readText()}")
+                    val errors = error.toFile().readText()
+                    if (errors.isNotBlank()) {
+                        logger.warn("PDF-generering gikk bra, men ga følgende TeX-feil: $errors")
+                    }
                     Execution.Success(pdf = workingDir.resolve("${File(texFilename).nameWithoutExtension}.pdf"))
                 } else {
                     Execution.Failure.Compilation(
