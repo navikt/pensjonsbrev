efa4f4c8eee288e024590fd45281eb3e5937ea79
Jeremy Elsom <jeremy.elsom@nav.no>
2026-02-05 11:27:13 +0100

    Opprettet selsector: pe.vedtaksdata_beregningsdata_beregning_beregningytelsekomp_ektefellegarantitillegg_egtinnvilget

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/vedtaksbrev/vedtaksdata/beregningsdata/beregningufore/beregningytelseskomp/BeregningYtelsesKomp.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/vedtaksbrev/vedtaksdata/beregningsdata/beregningufore/beregningytelseskomp/BeregningYtelsesKomp.kt
@@ -4,6 +4,7 @@ data class BeregningYtelsesKomp(
     val uforetrygdordiner: UforetrygdOrdiner?,
     val barnetilleggfelles: BarnetilleggFelles?,
     val barnetilleggserkull: BarnetilleggSerkull?,
+    val ektefellegarantitillegg: Ektefellegarantitillegg?,
     val ektefelletillegg: Ektefelletillegg?,
     val gjenlevendetillegg: Gjenlevendetillegg?,
 )
---
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/vedtaksbrev/vedtaksdata/beregningsdata/beregningufore/beregningytelseskomp/Ektefellegarantitillegg.kt
@@ -0,0 +1,8 @@
+package no.nav.pensjon.brev.api.model.maler.legacy.vedtaksbrev.vedtaksdata.beregningsdata.beregningufore.beregningytelseskomp
+
+import no.nav.pensjon.brevbaker.api.model.Kroner
+
+
+data class Ektefellegarantitillegg(
+    val egtinnvilget: Boolean?,
+)
