ba9365f881b0d8aab66b3949e07232d54f489c83
Mads Opheim <mads.opheim@nav.no>
2026-02-11 12:02:08 +0100

    Unyttig kommentar

--- pensjon/brevbaker/build.gradle.kts
+++ pensjon/brevbaker/build.gradle.kts
@@ -102,7 +102,6 @@ dependencies {
 
     implementation(libs.bundles.metrics)
 
-    // JUnit 5
     testImplementation(libs.bundles.junit)
     testImplementation(libs.ktor.server.test.host) {
         exclude("org.jetbrains.kotlin", "kotlin-test")
