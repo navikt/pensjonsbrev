971db1145c4e75e17dc7fc4e0fb015b345829d2f
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2026-02-19 13:48:21 +0100

    Oppdater kommentarer

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,26 +1,25 @@
 export default {
   /*
-   * Top entries run in parallell. We run `biome format` before any of the
-   * other non-writing commands, to make sure we don't get concurrency
-   * problems.
+   * Top entries run in parallell. We run `biome format` before any of the other
+   * non-writing commands, to make sure we don't get concurrency problems.
    */
   "!(*.{ts,tsx,cts,mts,css})": [
-    "biome format --write --no-errors-on-unmatched", // reformats and writes to disk
+    "biome format --write --no-errors-on-unmatched", // format and write to disk
     "biome check --no-errors-on-unmatched",
   ],
   "*.{ts,cts,mts}": [
-    "biome format --write --no-errors-on-unmatched", // reformats and writes to disk
+    "biome format --write --no-errors-on-unmatched", // format and write to disk
     "biome check --no-errors-on-unmatched",
     () => "tsc --noEmit", // use function to pick up tsconfig.json
   ],
   "*.tsx": [
-    "biome format --write --no-errors-on-unmatched", // reformats and writes to disk
+    "biome format --write --no-errors-on-unmatched", // format and write to disk
     "biome check --no-errors-on-unmatched",
     () => "tsc --noEmit", // use function to pick up tsconfig.json
     "stylelint",
   ],
   "*.css": [
-    "biome format --write --no-errors-on-unmatched", // reformats and writes to disk
+    "biome format --write --no-errors-on-unmatched", // format and write to disk
     "biome check --no-errors-on-unmatched",
     "stylelint",
   ],
