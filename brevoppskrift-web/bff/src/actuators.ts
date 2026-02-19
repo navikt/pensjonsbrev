815e28a9125e8e38a31e5a48e34ee3e5f06e716a
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2026-02-19 17:28:55 +0100

    Synkroniser kode mellom skribent-bff og brevoppskrift-bff

--- brevoppskrift-web/bff/src/actuators.ts
+++ brevoppskrift-web/bff/src/actuators.ts
@@ -1,10 +1,10 @@
 import type { Express } from "express";
 
 export function setupActuators(server: Express) {
-  server.get("/internal/health/liveness", (_request, response) => {
+  server.get("/internal/health/liveness", (_request, response): void => {
     response.status(200).end();
   });
-  server.get("/internal/health/readiness", (_request, response) => {
+  server.get("/internal/health/readiness", (_request, response): void => {
     response.status(200).end();
   });
 }
