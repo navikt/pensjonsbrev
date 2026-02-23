4b53fbb3e3b22caab5b73cd7af5ff256b8aaa9ea
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2026-02-23 16:32:27 +0100

    Legg på logging for Outcome.Failure

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/ApiResponse.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/routes/ApiResponse.kt
@@ -15,6 +15,7 @@ import no.nav.pensjon.brev.skribenten.domain.Reservasjon
 import no.nav.pensjon.brev.skribenten.model.Dto
 import no.nav.pensjon.brev.skribenten.services.Dto2ApiService
 import no.nav.pensjon.brev.skribenten.usecase.Outcome
+import org.slf4j.LoggerFactory
 
 @JvmName("apiRespondBrevredigering")
 suspend fun RoutingContext.apiRespond(
@@ -60,6 +61,8 @@ suspend fun RoutingContext.apiRespond(
     }
 }
 
+private val logger = LoggerFactory.getLogger("ApiResponse")
+
 suspend fun <T> RoutingContext.respondOutcome(
     dto2ApiService: Dto2ApiService,
     outcome: Outcome<T, BrevredigeringError>?,
@@ -68,49 +71,52 @@ suspend fun <T> RoutingContext.respondOutcome(
     when (outcome) {
         is Outcome.Success -> call.successResponse(outcome.value)
 
-        is Outcome.Failure -> when (outcome.error) {
-            is BrevreservasjonPolicy.ReservertAvAnnen ->
-                call.respond(HttpStatusCode.Locked, dto2ApiService.toApi(outcome.error.eksisterende))
+        is Outcome.Failure -> {
+            logger.info("Outcome failure: $outcome")
+            when (outcome.error) {
+                is BrevreservasjonPolicy.ReservertAvAnnen ->
+                    call.respond(HttpStatusCode.Locked, dto2ApiService.toApi(outcome.error.eksisterende))
 
-            is RedigerBrevPolicy.KanIkkeRedigere.ArkivertBrev ->
-                call.respond(HttpStatusCode.Conflict, "Brev er arkivert med journalpostId: ${outcome.error.journalpostId}")
+                is RedigerBrevPolicy.KanIkkeRedigere.ArkivertBrev ->
+                    call.respond(HttpStatusCode.Conflict, "Brev er arkivert med journalpostId: ${outcome.error.journalpostId}")
 
-            // TODO: Muligens burde dette være internal server error siden det burde indikere at koden ikke forsøkte å reserver eller at feil fra reservasjon ble ignorert
-            is RedigerBrevPolicy.KanIkkeRedigere.IkkeReservert ->
-                call.respond(HttpStatusCode.Conflict, "Brev er ikke reservert for redigering av deg")
+                // TODO: Muligens burde dette være internal server error siden det burde indikere at koden ikke forsøkte å reserver eller at feil fra reservasjon ble ignorert
+                is RedigerBrevPolicy.KanIkkeRedigere.IkkeReservert ->
+                    call.respond(HttpStatusCode.Conflict, "Brev er ikke reservert for redigering av deg")
 
-            is RedigerBrevPolicy.KanIkkeRedigere.LaastBrev ->
-                call.respond(HttpStatusCode.Locked, "Brev er låst for redigering")
+                is RedigerBrevPolicy.KanIkkeRedigere.LaastBrev ->
+                    call.respond(HttpStatusCode.Locked, "Brev er låst for redigering")
 
-            is OpprettBrevPolicy.KanIkkeOppretteBrev.BrevmalFinnesIkke ->
-                call.respond(HttpStatusCode.BadRequest, "Brevmal finnes ikke: ${outcome.error.brevkode}")
+                is OpprettBrevPolicy.KanIkkeOppretteBrev.BrevmalFinnesIkke ->
+                    call.respond(HttpStatusCode.BadRequest, "Brevmal finnes ikke: ${outcome.error.brevkode}")
 
-            is OpprettBrevPolicy.KanIkkeOppretteBrev.BrevmalKreverVedtaksId ->
-                call.respond(HttpStatusCode.BadRequest, "Brevmal krever vedtaksId: ${outcome.error.brevkode}")
+                is OpprettBrevPolicy.KanIkkeOppretteBrev.BrevmalKreverVedtaksId ->
+                    call.respond(HttpStatusCode.BadRequest, "Brevmal krever vedtaksId: ${outcome.error.brevkode}")
 
-            is OpprettBrevPolicy.KanIkkeOppretteBrev.IkkeTilgangTilEnhet ->
-                call.respond(HttpStatusCode.BadRequest, "Ikke tilgang til enhet: ${outcome.error.enhetsId}")
+                is OpprettBrevPolicy.KanIkkeOppretteBrev.IkkeTilgangTilEnhet ->
+                    call.respond(HttpStatusCode.BadRequest, "Ikke tilgang til enhet: ${outcome.error.enhetsId}")
 
-            is KlarTilSendingPolicy.IkkeKlarTilSending.FritekstFelterUredigert ->
-                call.respond(
-                    status = HttpStatusCode.UnprocessableEntity,
-                    message = BrevExceptionDto(tittel = "Brev ikke klart", melding = "Brevet inneholder fritekst-felter som ikke er endret")
-                )
+                is KlarTilSendingPolicy.IkkeKlarTilSending.FritekstFelterUredigert ->
+                    call.respond(
+                        status = HttpStatusCode.UnprocessableEntity,
+                        message = BrevExceptionDto(tittel = "Brev ikke klart", melding = "Brevet inneholder fritekst-felter som ikke er endret")
+                    )
 
-            is AttesterBrevPolicy.KanIkkeAttestere.HarIkkeAttestantrolle ->
-                call.respond(HttpStatusCode.Forbidden, "Bruker ${outcome.error.navIdent} har ikke attestantrolle")
+                is AttesterBrevPolicy.KanIkkeAttestere.HarIkkeAttestantrolle ->
+                    call.respond(HttpStatusCode.Forbidden, "Bruker ${outcome.error.navIdent} har ikke attestantrolle")
 
-            is AttesterBrevPolicy.KanIkkeAttestere.KanIkkeAttestereInformasjonsbrev ->
-                call.respond(HttpStatusCode.BadRequest, "Kan ikke attestere informasjonsbrev ${outcome.error.brevId}")
+                is AttesterBrevPolicy.KanIkkeAttestere.KanIkkeAttestereInformasjonsbrev ->
+                    call.respond(HttpStatusCode.BadRequest, "Kan ikke attestere informasjonsbrev ${outcome.error.brevId}")
 
-            is AttesterBrevPolicy.KanIkkeAttestere.IkkeKlarTilAttestering ->
-                call.respond(HttpStatusCode.BadRequest, "Brev ${outcome.error.brevId} er ikke klar til attestering")
+                is AttesterBrevPolicy.KanIkkeAttestere.IkkeKlarTilAttestering ->
+                    call.respond(HttpStatusCode.BadRequest, "Brev ${outcome.error.brevId} er ikke klar til attestering")
 
-            is AttesterBrevPolicy.KanIkkeAttestere.KanIkkeAttestereEgetBrev ->
-                call.respond(HttpStatusCode.Forbidden, "Bruker ${outcome.error.navIdent} kan ikke attestere sitt eget brev ${outcome.error.brevId}")
+                is AttesterBrevPolicy.KanIkkeAttestere.KanIkkeAttestereEgetBrev ->
+                    call.respond(HttpStatusCode.Forbidden, "Bruker ${outcome.error.navIdent} kan ikke attestere sitt eget brev ${outcome.error.brevId}")
 
-            is AttesterBrevPolicy.KanIkkeAttestere.AlleredeAttestertAvAnnen ->
-                call.respond(HttpStatusCode.Conflict, "Brev ${outcome.error.brevId} er allerede attestert av ${outcome.error.attestertAv}")
+                is AttesterBrevPolicy.KanIkkeAttestere.AlleredeAttestertAvAnnen ->
+                    call.respond(HttpStatusCode.Conflict, "Brev ${outcome.error.brevId} er allerede attestert av ${outcome.error.attestertAv}")
+            }
         }
 
         null -> call.respond(HttpStatusCode.NotFound, "Fant ikke brev")
