8f5f2ec530d26e81f8cf6ef860d2b207307099d6
Jeremy Elsom <jeremy.elsom@nav.no>
2026-02-24 15:38:36 +0100

    WIP

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/PE.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/PE.kt
@@ -57,14 +57,11 @@ data class PE(
         val pe_ut_kravlinjekode_vedtakresultat_forekomst_bt_innv: Int,
         val harOpptjeningUTMedOpptjeningBruktAaretFoerOgFoerstegangstjeneste: Boolean,
         val foedselsdatoTilBarnTilleggErInnvilgetFor: List<LocalDate>,
-        val pe_grunnlag_persongrunnlagsliste_uforehistorikkgarantigrad: Int,
         val pe_sivilstand_ektefelle_partner_samboer_bormed_ut_alle_spraak_entall: String,
         val pe_ut_tbu4071_btfbinnvilget: Boolean,
         val pe_ut_inntekt_hoeyere_lavere: String,
         val pe_ut_fradrag_hoeyere_lavere: String,
         val pe_ut_bruttoetterreduksjonbt_hoeyere_lavere: String,
-        val pe_vedtaksdata_beregningsdata_beregningufore_beregningytelseskomp_gjenlevendetillegg_gtnetto: Kroner,
-        val pe_vedtaksdata_beregningsdata_beregningufore_beregningytelseskomp_gjenlevendetillegg_gtbrutto: Kroner,
         val pe_saksdata_sakapogup: Boolean,
         )
 }
