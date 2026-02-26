5078358b0478c795012ec59d7a3777b6817b96ca
Jeremy Elsom <jeremy.elsom@nav.no>
2026-02-26 13:11:36 +0100

    Fjernet ubrukte imports

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/redigerbar/OmregningUfoerepensjonTilUfoeretrygdDto.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/redigerbar/OmregningUfoerepensjonTilUfoeretrygdDto.kt
@@ -4,10 +4,7 @@ import no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.legacy.PE
-import no.nav.pensjon.brev.api.model.vedlegg.DineRettigheterOgMulighetTilAaKlageDto
 import no.nav.pensjon.brev.api.model.vedlegg.MaanedligUfoeretrygdFoerSkattDto
-import no.nav.pensjon.brev.api.model.vedlegg.OpplysningerBruktIBeregningUTDto
-import no.nav.pensjon.brev.api.model.vedlegg.OrienteringOmRettigheterUfoereDto
 
 data class OmregningUfoerepensjonTilUfoeretrygdDto(
     override val saksbehandlerValg: EmptySaksbehandlerValg,
