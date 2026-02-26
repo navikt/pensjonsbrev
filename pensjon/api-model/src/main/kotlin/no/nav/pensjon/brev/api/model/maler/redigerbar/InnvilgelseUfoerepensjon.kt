1eed8a5129490ca5086a4d8ed0e1cfcc9d87554d
Jeremy Elsom <jeremy.elsom@nav.no>
2026-02-26 12:53:41 +0100

    WIP

---
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/InnvilgelseUfoerepensjon.kt
@@ -0,0 +1,4 @@
+package no.nav.pensjon.brev.api.model.maler.redigerbar
+
+class InnvilgelseUfoerepensjon {
+}
---
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/InnvilgelseUfoerepensjon.kt
@@ -0,0 +1,4 @@
+package no.nav.pensjon.brev.maler.legacy.redigerbar
+
+object InnvilgelseUfoerepensjon {
+}
