6561756197bd52260f6d5572662e2020863d910e
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2026-03-02 15:59:03 +0100

    Modul for planlegge pensjon (#2859)
    * Introduserer modul for planlegge pensjon
    
    * Tilpassar testar til å takle nye modular som startar som tomme
    
    * Oppdaterer også codeowners
    
    * Oppsett for funksjonsbrytar
    
    * Legg inn pensjonskalkulator som codeowners på ny modul
    
    * Endringar i malar må jo trigge redeploy av brevbaker

---
+++ planlegge-pensjon-maler/build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+val apiModelJavaTarget: String by System.getProperties()
+
+plugins {
+    kotlin("jvm")
+    alias(libs.plugins.ksp) apply true
+}
+
+group = "no.nav.pensjon.brev"
+version = "0.0.1-SNAPSHOT"
+base.archivesName.set("planlegge-pensjon-maler")
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation(project(":brevbaker:core"))
+    ksp(project(":brevbaker:template-model-generator"))
+
+    testImplementation(libs.bundles.junit)
+    testImplementation(testFixtures(project(":brevbaker:core")))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(apiModelJavaTarget))
+    }
+    sourceSets {
+        main {
+            kotlin.srcDir("build/generated/ksp/main/kotlin")
+        }
+        test {
+            kotlin.srcDir("build/generated/ksp/test/kotlin")
+        }
+    }
+}
+
+tasks {
+    register("verifyPackages") {
+        inputs.files(fileTree("src/main/kotlin").matching { include("**/*.kt") })
+        doLast {
+            inputs.files.forEach { file ->
+                val text = file.readText()
+                val pkg = Regex("""package\s+([a-zA-Z0-9\._]+)""")
+                    .find(text)?.groupValues?.get(1)
+
+                val requiredPrefix = "no.nav.pensjon.brev.planleggepensjon"
+                if (pkg == null) {
+                    throw GradleException("File $file is missing package directive!")
+                } else if (!pkg.startsWith(requiredPrefix)) {
+                    throw GradleException("Invalid package: $pkg in file $file. Package should start with $requiredPrefix to avoid runtime class conflict.")
+                }
+            }
+        }
+    }
+
+    compileJava {
+        targetCompatibility = apiModelJavaTarget
+    }
+    compileTestJava {
+        targetCompatibility = apiModelJavaTarget
+    }
+}
+
+
+tasks {
+    test {
+        useJUnitPlatform {
+            excludeTags = setOf("integration-test", "manual-test")
+        }
+    }
+
+    val test by testing.suites.existing(JvmTestSuite::class)
+    named<Test>("integrationTest") {
+        testClassesDirs = files(test.map { it.sources.output.classesDirs })
+        classpath = files(test.map { it.sources.runtimeClasspath })
+    }
+    named<Test>("manualTest") {
+        testClassesDirs = files(test.map { it.sources.output.classesDirs })
+        classpath = files(test.map { it.sources.runtimeClasspath })
+    }
+}
