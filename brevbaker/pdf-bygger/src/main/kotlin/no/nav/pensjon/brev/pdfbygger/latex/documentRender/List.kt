977d43352113eff793c383ee3636f647bbecbc89
Mads Opheim <mads.opheim@nav.no>
2026-03-09 17:38:17 +0100

    Fullfører

--- brevbaker/pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/latex/documentRender/List.kt
+++ brevbaker/pdf-bygger/src/main/kotlin/no/nav/pensjon/brev/pdfbygger/latex/documentRender/List.kt
@@ -1,15 +1,17 @@
 package no.nav.pensjon.brev.pdfbygger.latex.documentRender
 
+import no.nav.brev.Listetype
 import no.nav.pensjon.brev.pdfbygger.latex.LatexAppendable
 import no.nav.pensjon.brevbaker.api.model.LetterMarkup.ParagraphContent.ItemList
 
 internal fun LatexAppendable.renderList(list: ItemList) {
     if (list.items.isNotEmpty()) {
-        appendCmd("begin", "letteritemize")
+        val command = if (list.listType == Listetype.PUNKTLISTE) "letteritemize" else "letterenumerate"
+        appendCmd("begin", command)
         list.items.forEach { item ->
             append("""\item """, escape = false)
             renderTextContent(item.content)
         }
-        appendCmd("end", "letteritemize")
+        appendCmd("end", command)
     }
 }
