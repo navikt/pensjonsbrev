31b2fe79fdfe705e55f6d7f06228d741afdeeedf
Mads Opheim <mads.opheim@nav.no>
2026-03-13 13:02:02 +0100

    Fiksar route

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/eksterntApi/Route.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/eksterntApi/Route.kt
@@ -3,7 +3,10 @@ package no.nav.pensjon.brev.skribenten.eksterntApi
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.auth.authenticate
 import io.ktor.server.response.respond
-import io.ktor.server.routing.*
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.route
 import no.nav.pensjon.brev.skribenten.auth.ADGroups
 import no.nav.pensjon.brev.skribenten.auth.JwtConfig
 import no.nav.pensjon.brev.skribenten.auth.PrincipalHasGroup
@@ -22,30 +25,33 @@ fun Route.externalAPI(authConfig: JwtConfig, externalAPIService: ExternalAPIServ
             onRejection { respond(emptyList<String>()) }
         }
         route("/external/api/v1") {
-            get("/brev") {
-                val saksIder = call.queryParameters.getAll("saksId")
-                    ?.flatMap { it.split(",") }
-                    ?.mapNotNull { it.toLongOrNull() }
-                    ?.map { SaksId(it) }
-                    ?: emptyList()
+            route("/brev") {
+                get {
+                    val saksIder = call.queryParameters.getAll("saksId")
+                        ?.flatMap { it.split(",") }
+                        ?.mapNotNull { it.toLongOrNull() }
+                        ?.map { SaksId(it) }
+                        ?: emptyList()
 
-                call.respond(externalAPIService.hentAlleBrevForSaker(saksIder.toSet()))
-            }
-            post<ExternalAPI.OpprettBrevRequest>("/brev") { request ->
-                externalAPIService.opprettBrev(request).onSuccess {
-                    call.respond(HttpStatusCode.Created, it)
-                }.onError { error -> when (error) {
-                    is OpprettBrevPolicy.KanIkkeOppretteBrev.BrevmalKreverVedtaksId ->
-                        call.respond(HttpStatusCode.BadRequest, "Brevmal krever vedtaksId: ${error.brevkode}")
-
-                    is OpprettBrevPolicy.KanIkkeOppretteBrev.IkkeTilgangTilEnhet ->
-                        call.respond(HttpStatusCode.BadRequest, "Ikke tilgang til enhet: ${error.enhetsId}")
-
-                    else -> {
-                        logger.warn("Feil ved opprettelse av brev: {}", error)
-                        call.respond(HttpStatusCode.InternalServerError, "Feil ved opprettelse av brev")
-                    }
+                    call.respond(externalAPIService.hentAlleBrevForSaker(saksIder.toSet()))
                 }
+                post<ExternalAPI.OpprettBrevRequest>("/brev") { request ->
+                    externalAPIService.opprettBrev(request).onSuccess {
+                        call.respond(HttpStatusCode.Created, it)
+                    }.onError { error ->
+                        when (error) {
+                            is OpprettBrevPolicy.KanIkkeOppretteBrev.BrevmalKreverVedtaksId ->
+                                call.respond(HttpStatusCode.BadRequest, "Brevmal krever vedtaksId: ${error.brevkode}")
+
+                            is OpprettBrevPolicy.KanIkkeOppretteBrev.IkkeTilgangTilEnhet ->
+                                call.respond(HttpStatusCode.BadRequest, "Ikke tilgang til enhet: ${error.enhetsId}")
+
+                            else -> {
+                                logger.warn("Feil ved opprettelse av brev: {}", error)
+                                call.respond(HttpStatusCode.InternalServerError, "Feil ved opprettelse av brev")
+                            }
+                        }
+                    }
                 }
             }
         }
