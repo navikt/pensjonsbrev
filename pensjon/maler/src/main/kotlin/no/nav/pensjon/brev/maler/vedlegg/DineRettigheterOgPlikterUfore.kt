1fb14c04cb0a23eff206b79bcfb1fec4105184b7
Knut.Mork <knut.mork@nav.no>
2026-03-16 12:26:20 +0100

    Forbedret nynorsk tekst

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/DineRettigheterOgPlikterUfore.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/vedlegg/DineRettigheterOgPlikterUfore.kt
@@ -51,91 +51,91 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         }
         paragraph {
             text( bokmal { +"Du må blant annet gi oss beskjed hvis: " },
-                nynorsk() { +"Du må blant anna gi oss beskjed hvis: " }
+                nynorsk { +"Du må blant anna gi oss beskjed dersom: " }
             )
             list {
                 item {
                     text(
                         bokmal { +"inntekten din endrer seg. Dette gjelder også inntekten til samboer eller ektefelle hvis du har barnetillegg for felles barn. Du kan informere Nav om endret inntekt ved å bruke inntektsplanleggeren på $NAV_URL" },
-                        nynorsk { +"inntekta di endrar seg. Dette gjeld også inntekta til sambuar eller ektefelle hvis du har barnetillegg for felles barn. Du kan informere Nav om endra inntekt ved å bruke inntektsplanleggeren på $NAV_URL" }
+                        nynorsk { +"inntekta di endrar seg. Dette gjeld også inntekta til sambuar eller ektefelle dersom du har barnetillegg for felles barn. Du kan informere Nav om endra inntekt ved å bruke inntektsplanleggaren på $NAV_URL" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"du endrer adresse" },
-                        nynorsk() { +"du endrar adresse" }
+                        nynorsk { +"du endrar adresse" }
                     )
                 }
                 item {
                     text(
-                        bokmal { +"u skal begynne å arbeide i utlandet" },
-                        nynorsk() { +"du skal begynne å arbeide i utlandet" }
+                        bokmal { +"du skal begynne å arbeide i utlandet" },
+                        nynorsk { +"du skal byrje å arbeide i utlandet" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"du skal oppholde deg utenfor Norge lengre enn seks måneder" },
-                        nynorsk() { +"du skal opphalde deg utanfor Noreg lengre enn seks månader" }
+                        nynorsk { +"du skal opphalde deg utanfor Noreg lenger enn seks månader" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"du skal flytte til et annet land" },
-                        nynorsk() { +"du skal flytte til eit anna land" }
+                        nynorsk { +"du skal flytte til eit anna land" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"sivilstanden din endrer seg" },
-                        nynorsk() { +"sivilstanda di endrar seg" }
+                        nynorsk { +"sivilstanden din endrar seg" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"det skjer endringer i omsorgsituasjonen for barna du forsørger" },
-                        nynorsk() { +"det skjer endringar i omsorgsituasjonen for barna du forsørger" }
+                        nynorsk { +"det skjer endringar i omsorgssituasjonen for barna du forsørgjer" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"barna du forsørger skal flytte til et annet land" },
-                        nynorsk() { +"barna du forsørger skal flytte til eit anna land" }
+                        nynorsk { +"barna du forsørgjer skal flytte til eit anna land" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"barna du forsørger skal oppholde seg i et annet land mer enn 90 dager i løpet av en tolvmånedersperiode" },
-                        nynorsk() { +"barna du forsørger skal opphalde seg i eit anna land meir enn 90 dagar i løpet av ein tolvmånadersperiode" }
+                        nynorsk { +"barna du forsørgjer skal opphalde seg i eit anna land meir enn 90 dagar i løpet av ein tolvmånadersperiode" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"tjenestepensjon fra offentlig eller private ordninger endrer seg" },
-                        nynorsk() { +"tjenestepensjon fra offentlige eller private ordningar endrar seg" }
+                        nynorsk { +"tenestepensjon frå offentlege eller private ordningar endrar seg" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"individuelle pensjonsordninger, livrente og gavepensjon endrer seg" },
-                        nynorsk() { +"individuelle pensjonsordningar, livrente og gavepensjon endrar seg" }
+                        nynorsk { +"individuelle pensjonsordningar, livrente og gåvepensjon endrar seg" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"ytelser og pensjon fra andre land endrer seg" },
-                        nynorsk() { +"ytelser og pensjon fra andre land endrar seg" }
+                        nynorsk { +"ytingar og pensjon frå andre land endrar seg" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"du, samboer eller ektefelle blir innlagt på institusjon" },
-                        nynorsk() { +"du, sambuar eller ektefelle blir innlagt på institusjon" }
+                        nynorsk { +"du, sambuar eller ektefelle blir innlagd på institusjon" }
                     )
                 }
                 item {
                     text(
                         bokmal { +" du, samboer eller ektefelle sitter i varetekt, soner straff eller er under forvaring" },
-                        nynorsk() { +" du, sambuar eller ektefelle sitter i varetekt, soner straff eller er under forvaring" }
+                        nynorsk { +"du, sambuar eller ektefelle sit i varetekt, sonar straff eller er under forvaring" }
                     )
                 }
             }
@@ -143,7 +143,7 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
 
         paragraph {
             text(bokmal { +"Du kan lese om plikt til å melde fra i folketrygdloven § 21-3." },
-                nynorsk() { +"Du kan lese om plikt til å melde fra i folketrygdloven § 21-3." }
+                nynorsk { +"Du kan lese om plikt til å melde frå i folketrygdlova § 21-3." }
             )
         }
 
@@ -156,7 +156,7 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         paragraph {
             text(
                 bokmal { +"Vi har plikt til å veilede deg om dine rettigheter og plikter i saken din, både før, under og etter saksbehandlingen. Dersom du har spørsmål eller er usikker på noe, vil vi gjøre vårt beste for å hjelpe deg." },
-                nynorsk { +"Vi har plikt til å veilede deg om dine rettigheter og plikter i saka di, både før, under og etter saksbehandlinga. Dersom du har spørsmål eller er usikker på noko, vil vi gjere vårt beste for å hjelpe deg." }
+                nynorsk { +"Vi har plikt til å rettleie deg om rettane og pliktene dine i saka di, både før, under og etter saksbehandlinga. Dersom du har spørsmål eller er usikker på noko, vil vi gjere vårt beste for å hjelpe deg." }
             )
         }
         paragraph {
@@ -169,19 +169,19 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         title2 {
             text(
                 bokmal { +"Du kan få hjelp fra andre" },
-                nynorsk { +"Du kan få hjelp fra andre" }
+                nynorsk { +"Du kan få hjelp frå andre" }
             )
         }
         paragraph {
             text(
                 bokmal { +"Du kan be om hjelp fra andre under hele saksbehandlingen, for eksempel av advokat, rettshjelper, en organisasjon du er medlem av eller en annen myndig person. Hvis den som hjelper deg ikke er advokat, må du gi denne personen en skriftlig fullmakt. Bruk gjerne skjemaet du finner på $FULLMAKT_URL." },
-                nynorsk { +"Du kan be om hjelp fra andre under hele saksbehandlinga, for eksempel av advokat, rettshjelper, en organisasjon du er medlem av eller en annen myndig person. Hvis den som hjelper deg ikke er advokat, må du gi denne personen en skriftlig fullmakt. Bruk gjerne skjemaet du finner på $FULLMAKT_URL." }
+                nynorsk { +"Du kan be om hjelp frå andre under heile saksbehandlinga, for eksempel av advokat, rettshjelpar, ein organisasjon du er medlem av, eller ein annan myndig person. Dersom den som hjelper deg, ikkje er advokat, må du gi denne personen ei skriftleg fullmakt. Bruk gjerne skjemaet du finn på $FULLMAKT_URL." }
             )
         }
         paragraph {
             text(
                 bokmal { +"Du kan lese hvem som kan hjelpe deg i forvaltningsloven § 12." },
-                nynorsk { +"Du kan lese hvem som kan hjelpe deg i forvaltningslova § 12." }
+                nynorsk { +"Du kan lese kven som kan hjelpe deg i forvaltningslova § 12." }
             )
         }
 
@@ -194,7 +194,7 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         paragraph {
             text(
                 bokmal { +"Med få unntak har du rett til å se dokumentene i saken din. Du kan logge deg inn via nav.no for å se dokumenter i saken din. Du kan også ringe oss på telefon $NAV_KONTAKTSENTER_TELEFON." },
-                nynorsk { +"Med få unntak har du rett til å se dokumentene i saka di. Du kan logge deg inn via nav.no for å se dokumenter i saka di. Du kan også ringe oss på telefon $NAV_KONTAKTSENTER_TELEFON." }
+                nynorsk { +"Med få unntak har du rett til å sjå dokumenta i saka di. Du kan logge deg inn via nav.no for å sjå dokument i saka di. Du kan også ringje oss på telefon $NAV_KONTAKTSENTER_TELEFON." }
             )
         }
         paragraph {
@@ -219,19 +219,19 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         paragraph {
             text(
                 bokmal { +"Du kan klage på vedtaket innen seks uker fra du mottok det." },
-                nynorsk { +"Du kan klage på vedtaket innen seks veker fra du mottok det." }
+                nynorsk { +"Du kan klage på vedtaket innan seks veker frå du fekk det." }
             )
         }
         paragraph {
             text(
                 bokmal { +"Du kan sende oss en skriftlig klage, bruk gjerne skjemaet som du finner på ${KLAGE_URL}. Kan du ikke skrive klagen selv, kan vi hjelpe deg. Du er velkommen til å ringe oss på telefon $NAV_KONTAKTSENTER_TELEFON." },
-                nynorsk { +"Du kan sende oss en skriftlig klage, bruk gjerne skjemaet som du finner på ${KLAGE_URL}. Kan du ikke skrive klagen selv, kan vi hjelpe deg. Du er velkommen til å ringe oss på telefon $NAV_KONTAKTSENTER_TELEFON." }
+                nynorsk { +"Du kan sende oss ei skriftleg klage, bruk gjerne skjemaet som du finn på ${KLAGE_URL}. Kan du ikkje skrive klaga sjølv, kan vi hjelpe deg. Du er velkomen til å ringje oss på telefon $NAV_KONTAKTSENTER_TELEFON." }
             )
         }
         paragraph {
             text(
                 bokmal { +"I klagen " },
-                nynorsk { +" I klagen " }
+                nynorsk { +"I klaga " }
             )
             text(
                 bokmal { +"må" },
@@ -252,13 +252,13 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
                 item {
                     text(
                         bokmal { +"hvilket vedtak du klager på" },
-                        nynorsk { +"hvilket vedtak du klagar på" }
+                        nynorsk { +"kva vedtak du klagar på" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"hvilken endring i vedtaket du ber om" },
-                        nynorsk { +"hvilken endring i vedtaket du ber om" }
+                        nynorsk { +"kva endring i vedtaket du ber om" }
                     )
                 }
             }
@@ -281,13 +281,13 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
                 item {
                     text(
                         bokmal { +"skrive hvorfor du mener vedtaket er feil" },
-                        nynorsk { +"skrive hvorfor du mener vedtaket er feil" }
+                        nynorsk { +"skrive kvifor du meiner vedtaket er feil" }
                     )
                 }
                 item {
                     text(
                         bokmal { +"fortelle oss hvilke erklæringer og andre dokumenter som du legger ved klagen" },
-                        nynorsk { +"fortelle oss hvilke erklæringer og andre dokumenter som du legger ved klagen" }
+                        nynorsk { +"nemne erklæringar og andre dokument som du legg ved klaga" }
                     )
                 }
             }
@@ -295,32 +295,32 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         paragraph {
             text(
                 bokmal { +"Husk å signere klagen, ellers må vi sende den i retur til deg." },
-                nynorsk { +"Husk å signere klagen, ellers må vi sende den i retur til deg." }
+                nynorsk { +"Hugs å skrive under klaga, elles må vi sende henne i retur til deg." }
             )
         }
         paragraph {
             text(
                 bokmal { +"Du kan ha krav på fri rettshjelp etter rettshjelploven. Informasjon om denne ordningen kan du få hos statsforvalteren, advokater eller Nav." },
-                nynorsk { +"Du kan ha krav på fri rettshjelp etter rettshjelploven. Informasjon om denne ordningen kan du få hos statsforvalteren, advokater eller Nav." }
+                nynorsk { +"Du kan ha krav på fri rettshjelp etter rettshjelplova. Informasjon om denne ordninga kan du få hos statsforvaltaren, advokatar eller Nav." }
             )
         }
         paragraph {
             text(
                 bokmal { +"Du kan sende klagen direkte til Nav eller gjennom trygdemyndighetene i landet du bor i dersom du bor i utlandet." },
-                nynorsk { +"Du kan sende klagen direkte til Nav eller gjennom trygdemyndighetene i landet du bor i dersom du bor i utlandet." }
+                nynorsk { +"Du kan sende klaga direkte til Nav eller gjennom trygdemyndigheitene i landet du bur i dersom du bur i utlandet." }
             )
         }
 
         title2 {
             text(
                 bokmal { +"Dette skjer når vi får klagen" },
-                nynorsk { +"Dette skjer når vi får klagen" }
+                nynorsk { +"Dette skjer når vi får klaga" }
             )
         }
         paragraph {
             text(
                 bokmal { +"Kontoret som har fattet vedtaket vil vurdere saken din på nytt. Hvis du ikke får medhold i klagen din, blir klagen sendt videre til Nav klageinstans for ny vurdering og avgjørelse." },
-                nynorsk { +"Kontoret som har fattet vedtaket vil vurdere saka di på nytt. Hvis du ikke får medhold i klagen din, blir klagen sendt videre til Nav klageinstans for ny vurdering og avgjørelse." }
+                nynorsk { +"Kontoret som har gjort vedtaket, vurderer då saka di på nytt. Dersom du ikkje får medhald i klaga di, blir klaga send vidare til Nav klageinstans for ny vurdering og avgjerd." }
             )
         }
         paragraph {
@@ -332,13 +332,13 @@ val vedleggDineRettigheterOgPlikterUfore = createAttachment<LangBokmalNynorsk, E
         paragraph {
             text(
                 bokmal { +"Får du medhold, kan du få dekket vesentlige utgifter som har vært nødvendige for å få endret vedtaket. Du kan lese om saksomkostninger i forvaltningsloven § 36." },
-                nynorsk { +"Får du medhold, kan du få dekket vesentlige utgifter som har vært nødvendige for å få endret vedtaket. Du kan lese om saksomkostninger i forvaltningslova § 36." }
+                nynorsk { +"Får du medhald, kan du få dekt vesentlege utgifter som har vore nødvendige for å få endra vedtaket. Du kan lese om saksomkostningar i forvaltningslova § 36." }
             )
         }
         paragraph {
             text(
                 bokmal { +"Du kan lese om dine rettigheter til å klage i folketrygdloven § 21-12." },
-                nynorsk { +"Du kan lese om dine rettigheter til å klage i folketrygdloven § 21-12." }
+                nynorsk { +"Du kan lese om rettane dine til å klage i folketrygdlova § 21-12." }
             )
         }
     }
