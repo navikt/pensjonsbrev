cd33c4c34e1306a1e7ebd3549637a21dfff5ac79
Jeremy Elsom <jeremy.elsom@nav.no>
2026-03-17 13:24:39 +0100

    Endret OpphoerGjenlevendepensjonDto.kt til enum

--- pensjon/maler/src/test/kotlin/no/nav/pensjon/brev/fixtures/OpphoerGjenlevendepensjonDto.kt
+++ pensjon/maler/src/test/kotlin/no/nav/pensjon/brev/fixtures/OpphoerGjenlevendepensjonDto.kt
@@ -2,17 +2,12 @@ package no.nav.pensjon.brev.fixtures
 
 import no.nav.pensjon.brev.api.model.maler.EmptyFagsystemdata
 import no.nav.pensjon.brev.api.model.maler.redigerbar.OpphoerGjenlevendepensjonDto
-import no.nav.pensjon.brev.api.model.maler.redigerbar.OpphoerGjenlevendepensjonDto.*
 
 fun createOpphoerGjenlevendepensjonDto() =
     OpphoerGjenlevendepensjonDto(
-        saksbehandlerValg = SaksbehandlerValg(
-            gifterSeg = false,
-            inngaaPartnerskap = false,
-            blirSamboerBarn = false,
-            erSamboerBarn = false,
-            blirSamboerTidligereGift = false,
-            tilbakekreving = false
+        saksbehandlerValg = OpphoerGjenlevendepensjonDto.SaksbehandlerValg(
+            folketrygdlovenAlternativ = OpphoerGjenlevendepensjonDto.SaksbehandlerValg.FolketrygdlovenAlternativ.gifterSeg,
+            opphoerMedTilbakekreving = false,
         ),
-        pesysData = EmptyFagsystemdata
+        pesysData = EmptyFagsystemdata,
     )
