f02392176c2fe96b66f543c6b63c14235f867f39
Mads Opheim <mads.opheim@nav.no>
2026-03-17 11:43:33 +0100

    Rydding etter copilot-qa

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/brevredigering/domain/FerdigRedigertPolicy.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/brevredigering/domain/FerdigRedigertPolicy.kt
@@ -1,7 +1,6 @@
 package no.nav.pensjon.brev.skribenten.brevredigering.domain
 
 import no.nav.pensjon.brev.skribenten.Features
-import no.nav.pensjon.brev.skribenten.UnleashToggle
 import no.nav.pensjon.brev.skribenten.common.Outcome
 import no.nav.pensjon.brev.skribenten.common.Outcome.Companion.failure
 import no.nav.pensjon.brev.skribenten.common.Outcome.Companion.success
@@ -12,7 +11,7 @@ import no.nav.pensjon.brevbaker.api.model.ElementTags
 class FerdigRedigertPolicy {
 
     suspend fun erFerdigRedigert(brev: Brevredigering): Outcome<Unit, IkkeFerdigRedigert> =
-        if (brev.redigertBrev.alleFritekstFelterErRedigert() && ((!Features.hindreDuplikateAvsnitt.isEnabled()) || (Features.hindreDuplikateAvsnitt.isEnabled() && brev.redigertBrev.alleDuplikateAvsnittErHaandtert()))) {
+        if (brev.redigertBrev.alleFritekstFelterErRedigert() && ((!Features.hindreDuplikateAvsnitt.isEnabled()) || brev.redigertBrev.alleDuplikateAvsnittErHaandtert())) {
             success(Unit)
         } else {
             failure(IkkeFerdigRedigert.FritekstFelterUredigert)
