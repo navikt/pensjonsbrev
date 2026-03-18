09c29e514ddedcefd3d1dc0d656eee12b1aee21a
stainawarjar <210375207+stainawarjar@users.noreply.github.com>
2026-03-18 15:47:56 +0100

    Trekk adressevisning ut i egen komponent

--- skribenten-web/frontend/src/components/EndreMottakerMedApiHåndtering.tsx
+++ skribenten-web/frontend/src/components/EndreMottakerMedApiHåndtering.tsx
@@ -1,131 +1,87 @@
 import { PencilIcon, XMarkOctagonFillIcon } from "@navikt/aksel-icons";
-import { Box, Button, HStack, Label, VStack } from "@navikt/ds-react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { type AxiosError } from "axios";
-import { useState } from "react";
+import { Button, HStack, Label, VStack } from "@navikt/ds-react";
 
-import { getBrev } from "~/api/brev-queries";
-import { endreMottaker, fjernOverstyrtMottaker, hentAlleBrevInfoForSak, hentPdfForBrev } from "~/api/sak-api-endpoints";
 import { EndreMottakerModal } from "~/components/endreMottaker/EndreMottakerModal";
-import { type BrevInfo, type Mottaker } from "~/types/brev";
-import { mapEndreMottakerValueTilMottaker } from "~/utils/AdresseUtils";
+import { useEndreMottaker } from "~/hooks/useEndreMottaker";
+import { type BrevInfo } from "~/types/brev";
 
 import OppsummeringAvMottaker from "./OppsummeringAvMottaker";
 
 const EndreMottakerMedOppsummeringOgApiHåndtering = (props: {
   saksId: string;
   brev: BrevInfo;
-  endreAsIcon?: boolean;
-  overrideOppsummering?: (edit: React.ReactNode) => React.ReactNode;
   withOppsummeringTitle?: boolean;
   kanTilbakestilleMottaker?: boolean;
   withGap?: boolean;
 }) => {
-  const queryClient = useQueryClient();
-  const mottakerMutation = useMutation<BrevInfo, AxiosError, Mottaker>({
-    mutationFn: (mottaker) => endreMottaker(props.saksId, props.brev.id, { mottaker: mottaker }),
-    onSuccess: (response) => {
-      queryClient.setQueryData(hentAlleBrevInfoForSak.queryKey(props.saksId), (currentBrevInfo: BrevInfo[]) =>
-        currentBrevInfo.map((brevInfo) => (brevInfo.id === response.id ? response : brevInfo)),
-      );
-      queryClient.setQueryData(getBrev.queryKey(props.brev.id), response);
-      queryClient.invalidateQueries({
-        queryKey: hentPdfForBrev.queryKey(props.brev.id),
-      });
-      setModalÅpen(false);
-    },
-  });
+  const {
+    modalÅpen,
+    åpneModal,
+    lukkModal,
+    endreMottaker,
+    resetEndreMottaker,
+    endreMottakerError,
+    endreMottakerIsPending,
+    fjernMottaker,
+    fjernMottakerIsPending,
+    fjernMottakerIsError,
+  } = useEndreMottaker(props.saksId, props.brev.id);
 
-  const fjernMottakerMutation = useMutation<void, AxiosError>({
-    mutationFn: () => fjernOverstyrtMottaker({ saksId: props.saksId, brevId: props.brev.id }),
-    onSuccess: () => {
-      queryClient.setQueryData(hentAlleBrevInfoForSak.queryKey(props.saksId), (currentBrevInfo: BrevInfo[]) =>
-        currentBrevInfo.map((brevInfo) =>
-          brevInfo.id === props.brev.id ? { ...props.brev, mottaker: null } : brevInfo,
-        ),
-      );
-      queryClient.invalidateQueries({
-        queryKey: hentPdfForBrev.queryKey(props.brev.id),
-      });
-    },
-  });
-
-  const [modalÅpen, setModalÅpen] = useState<boolean>(false);
   return (
     <VStack gap={props.withGap ? "space-8" : "space-0"}>
       {modalÅpen && (
         <EndreMottakerModal
-          error={mottakerMutation.error}
-          isPending={mottakerMutation.isPending}
-          onBekreftNyMottaker={(mottaker) => {
-            mottakerMutation.mutate(mapEndreMottakerValueTilMottaker(mottaker));
-          }}
-          onClose={() => setModalÅpen(false)}
-          resetOnBekreftState={() => mottakerMutation.reset()}
+          error={endreMottakerError}
+          isPending={endreMottakerIsPending}
+          onBekreftNyMottaker={endreMottaker}
+          onClose={lukkModal}
+          resetOnBekreftState={resetEndreMottaker}
           åpen={modalÅpen}
         />
       )}
-      {props.overrideOppsummering ? (
-        <HStack align="center" gap="space-8">
-          {props.overrideOppsummering(
-            <Box asChild borderRadius="4">
-              <Button
-                icon={<PencilIcon />}
-                onClick={() => setModalÅpen(true)}
-                size="xsmall"
-                type="button"
-                variant="tertiary"
-              />
-            </Box>,
-          )}
-        </HStack>
-      ) : (
-        <VStack gap="space-8">
-          {props.withOppsummeringTitle && (
-            <HStack align="center" justify="space-between">
-              <Label size="small">Mottaker</Label>
-              <Button
-                icon={<PencilIcon />}
-                iconPosition="right"
-                onClick={() => setModalÅpen(true)}
-                size="xsmall"
-                type="button"
-                variant="tertiary"
-              >
-                Endre
-              </Button>
-            </HStack>
-          )}
-          {!props.withOppsummeringTitle && (
+      <VStack gap="space-8">
+        {props.withOppsummeringTitle && (
+          <HStack align="center" justify="space-between">
+            <Label size="small">Mottaker</Label>
             <Button
               icon={<PencilIcon />}
               iconPosition="right"
-              onClick={() => setModalÅpen(true)}
+              onClick={åpneModal}
               size="xsmall"
               type="button"
               variant="tertiary"
             >
               Endre
             </Button>
-          )}
-          <OppsummeringAvMottaker mottaker={props.brev.mottaker} saksId={props.saksId} withTitle={false} />
-        </VStack>
-      )}
-      {props.kanTilbakestilleMottaker && "Kan tilbakestille mottaker"}
-
+          </HStack>
+        )}
+        {!props.withOppsummeringTitle && (
+          <Button
+            icon={<PencilIcon />}
+            iconPosition="right"
+            onClick={åpneModal}
+            size="xsmall"
+            type="button"
+            variant="tertiary"
+          >
+            Endre
+          </Button>
+        )}
+        <OppsummeringAvMottaker mottaker={props.brev.mottaker} saksId={props.saksId} withTitle={false} />
+      </VStack>
       {props.brev.mottaker && props.kanTilbakestilleMottaker && (
         <HStack>
           <Button
             css={{ margin: "0 calc(-1 * var(--ax-space-8))" }}
-            loading={fjernMottakerMutation.isPending}
-            onClick={() => fjernMottakerMutation.mutate()}
+            loading={fjernMottakerIsPending}
+            onClick={fjernMottaker}
             size="xsmall"
             type="button"
             variant="tertiary"
           >
             Tilbakestill mottaker
           </Button>
-          {fjernMottakerMutation.isError && (
+          {fjernMottakerIsError && (
             <XMarkOctagonFillIcon
               css={{
                 alignSelf: "center",
