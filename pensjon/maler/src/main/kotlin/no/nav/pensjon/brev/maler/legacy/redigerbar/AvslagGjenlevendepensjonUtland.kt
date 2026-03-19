f298eac1dcea2a7ccd863f389bd12e481dac3a8a
Jeremy Elsom <jeremy.elsom@nav.no>
2026-03-19 14:54:13 +0100

    WIP

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/AvslagGjenlevendepensjonUtland.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/AvslagGjenlevendepensjonUtland.kt
@@ -5,18 +5,44 @@ import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
 import no.nav.pensjon.brev.api.model.maler.redigerbar.AvslagGjenlevendepensjonUtlandDto
 import no.nav.pensjon.brev.model.Brevkategori
+import no.nav.pensjon.brev.template.Language.Bokmal
+import no.nav.pensjon.brev.template.Language.English
+import no.nav.pensjon.brev.template.Language.Nynorsk
 import no.nav.pensjon.brev.template.RedigerbarTemplate
+import no.nav.pensjon.brev.template.createTemplate
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
+import no.nav.pensjon.brev.template.dsl.languages
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brevbaker.api.model.LetterMetadata
+
+//PE_GP_04_033 Vedtak avslag av gjenlevendepensjon utland
 
 @TemplateModelHelpers
 object AvslagGjenlevendepensjonUtland : RedigerbarTemplate<AvslagGjenlevendepensjonUtlandDto> {
 
     //override val featureToggle = FeatureToggles.brevmalAvslagGjenlevendepensjonUtland.toggle
 
-    override val kode = Pesysbrevkoder.Redigerbar.GP_OPPHOER_GJENLEVENDEPENSJON
-    override val kategori = Brevkategori.VEDTAK_ENDRING_OG_REVURDERING
+    override val kode = Pesysbrevkoder.Redigerbar.GP_AVSLAG_GJENLEVENDEPENSJON_UTLAND
+    override val kategori = Brevkategori.FOERSTEGANGSBEHANDLING
     override val brevkontekst = TemplateDescription.Brevkontekst.VEDTAK
     override val sakstyper = setOf(Sakstype.GJENLEV)
+
+    override val template = createTemplate(
+        languages = languages(Bokmal, Nynorsk, English),
+        letterMetadata = LetterMetadata(
+            displayTitle = "Vedtak - avslag ytelse til gjenlevende (bosatt utland)",
+            distribusjonstype = LetterMetadata.Distribusjonstype.VEDTAK,
+            brevtype = LetterMetadata.Brevtype.VEDTAKSBREV
+        )
+    ) {
+        title {
+            text(
+                bokmal { +"Gjenlevendepensjon - melding om vedtak" },
+                nynorsk { +"Gjenlevendepensjon - melding om vedtak" },
+                english { +"Survivor's pension - notification of decision" }
+            )
+        }
+
 
 
 
