e31f1de61f9d91e6d4e9bd0a51989e52e3955dfb
Jeremy Elsom <jeremy.elsom@nav.no>
2026-03-19 12:24:44 +0100

    WIP

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/AvslagGjenlevendepensjonDto.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/AvslagGjenlevendepensjonDto.kt
@@ -1,15 +1,16 @@
 package no.nav.pensjon.brev.api.model.maler.redigerbar
 
-import no.nav.pensjon.brev.api.model.maler.EmptyFagsystemdata
+import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
 import no.nav.pensjon.brevbaker.api.model.DisplayText
+import java.time.LocalDateTime
 
 @Suppress("unused")
 data class AvslagGjenlevendepensjonDto (
     override val saksbehandlerValg: SaksbehandlerValg,
-    override val pesysData: EmptyFagsystemdata
-) : RedigerbarBrevdata<AvslagGjenlevendepensjonDto.SaksbehandlerValg, EmptyFagsystemdata> {
+    override val pesysData: PesysData,
+) : RedigerbarBrevdata<AvslagGjenlevendepensjonDto.SaksbehandlerValg, AvslagGjenlevendepensjonDto.PesysData> {
     data class SaksbehandlerValg(
         @DisplayText("Velg folketrygdloven paragraf:")
         val folketrygdlovenParagraf: FolketrygdlovenParagraf,
@@ -31,4 +32,7 @@ data class AvslagGjenlevendepensjonDto (
             paragraf17_10,
         }
     }
+    data class PesysData(
+        val mottattDato: LocalDateTime,  //TODO  ikke støtte for PEbrevgruppe2
+    ) : FagsystemBrevdata
 }
