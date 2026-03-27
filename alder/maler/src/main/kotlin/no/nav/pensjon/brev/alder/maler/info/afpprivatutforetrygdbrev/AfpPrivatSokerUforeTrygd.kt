e79a410f77ca03bcada7fefeeaebe2cf19772852
Kim.Andre.Ness <kim.andre.ness@nav.no>
2026-03-27 08:01:10 +0100

    Fjern ubrukt import

--- alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/info/afpprivatutforetrygdbrev/AfpPrivatSokerUforeTrygd.kt
+++ alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/info/afpprivatutforetrygdbrev/AfpPrivatSokerUforeTrygd.kt
@@ -2,7 +2,6 @@ package no.nav.pensjon.brev.alder.maler.info.afpprivatutforetrygdbrev
 
 import no.nav.pensjon.brev.alder.maler.Brevkategori
 import no.nav.pensjon.brev.alder.maler.brev.FeatureToggles
-import no.nav.pensjon.brev.alder.maler.info.afpprivatutforetrygdbrev.UforetrygdSokerAfpPrivat.fritekst
 import no.nav.pensjon.brev.alder.maler.vedlegg.forbeholdTilBeregningeneAfpTIlUforeTrygd
 import no.nav.pensjon.brev.alder.model.Aldersbrevkoder
 import no.nav.pensjon.brev.alder.model.Sakstype
@@ -10,7 +9,6 @@ import no.nav.pensjon.brev.alder.model.info.afpprivatutforetrygdbrev.AfpPrivatSo
 import no.nav.pensjon.brev.alder.model.info.afpprivatutforetrygdbrev.AfpPrivatSokerUforeTrygdDtoSelectors.SaksBehandlerValgSelectors.harSoktUforeTrygd
 import no.nav.pensjon.brev.alder.model.info.afpprivatutforetrygdbrev.AfpPrivatSokerUforeTrygdDtoSelectors.saksbehandlerValg
 import no.nav.pensjon.brev.api.model.FeatureToggle
-import no.nav.pensjon.brev.template.dsl.expression.not
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.TemplateDescription.ISakstype
 import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Table.ColumnAlignment.RIGHT
@@ -18,6 +16,7 @@ import no.nav.pensjon.brev.template.Language.Bokmal
 import no.nav.pensjon.brev.template.Language.Nynorsk
 import no.nav.pensjon.brev.template.RedigerbarTemplate
 import no.nav.pensjon.brev.template.createTemplate
+import no.nav.pensjon.brev.template.dsl.expression.not
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
