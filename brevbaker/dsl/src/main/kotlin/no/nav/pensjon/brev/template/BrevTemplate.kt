3bc7a9543ab4dbed2571fee7ad653c9b7bc77117
martinstorvoll <martin.storvoll@nav.no>
2026-05-12 15:19:23 +0200

    Fjerner import

--- brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/BrevTemplate.kt
+++ brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/BrevTemplate.kt
@@ -11,7 +11,6 @@ import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
 import no.nav.pensjon.brev.template.Expression.Literal
-import no.nav.pensjon.brev.template.dsl.LiteralOrExpressionBuilder
 import no.nav.pensjon.brev.template.dsl.TemplateRootScope
 import no.nav.pensjon.brevbaker.api.model.LanguageCode
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata
