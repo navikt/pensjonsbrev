ef625634d4375e298986c6541e361c6428ebbb2c
martinstorvoll <martin.storvoll@nav.no>
2026-05-13 15:41:22 +0200

    Planlegge pensjon: Lagt til tabeller for AFP og Opptjening  (#3185)

---
+++ planlegge-pensjon-maler/src/main/kotlin/no/nav/pensjon/brev/planleggepensjon/simulering/tabeller/OpptjeningKapittel20Tabell.kt
@@ -0,0 +1,78 @@
+package no.nav.pensjon.brev.planleggepensjon.simulering.tabeller
+
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjon
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.delingstall
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.garantipensjonsnivaaBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.garantitilleggBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.kapittel20AndelTeller
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.kapittel20Trygdetid
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.pensjonsbeholdningEtterUttakBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.pensjonsbeholdningFoerUttakBeloep
+import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Table.ColumnAlignment.RIGHT
+import no.nav.pensjon.brev.template.Expression
+import no.nav.pensjon.brev.template.LangBokmal
+import no.nav.pensjon.brev.template.OutlinePhrase
+import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
+import no.nav.pensjon.brev.template.dsl.expression.format
+import no.nav.pensjon.brev.template.dsl.text
+
+data class OpptjeningKapittel20Tabell(
+    val alderspensjon: Expression<SimuleringV1MaanedligAlderspensjon>,
+) : OutlinePhrase<LangBokmal>() {
+    override fun OutlineOnlyScope<LangBokmal, Unit>.template() {
+        paragraph {
+            table(header = {
+                column(columnSpan = 3) {
+                    text(bokmal { +"Opptjening etter kapittel 20" })
+                }
+                column(alignment = RIGHT, columnSpan = 1) {
+                    text(bokmal { +"" })
+                }
+            }) {
+                ifNotNull(alderspensjon.kapittel20AndelTeller) {
+                    row {
+                        cell { text(bokmal { +"Andelsbrøk" }) }
+                        cell { text(bokmal { +it.format() + "/10" }) }
+                    }
+                }
+                ifNotNull(alderspensjon.delingstall) {
+                    row {
+                        cell { text(bokmal { +"Delingstall ved uttak" }) }
+                        cell { text(bokmal { +it.format(2) }) }
+                    }
+                }
+                ifNotNull(alderspensjon.garantipensjonsnivaaBeloep) {
+                    row {
+                        cell { text(bokmal { +"Garantipensjonsnivå" }) }
+                        cell { text(bokmal { +it.format() }) }
+                    }
+                }
+                ifNotNull(alderspensjon.garantitilleggBeloep) {
+                    row {
+                        cell { text(bokmal { +"Garantitillegg" }) }
+                        cell { text(bokmal { +it.format() }) }
+                    }
+                }
+                ifNotNull(alderspensjon.pensjonsbeholdningFoerUttakBeloep) {
+                    row {
+                        cell { text(bokmal { +"Pensjonsbeholdning før uttak" }) }
+                        cell { text(bokmal { +it.format() }) }
+                    }
+                }
+                ifNotNull(alderspensjon.pensjonsbeholdningEtterUttakBeloep) {
+                    row {
+                        cell { text(bokmal { +"Pensjonsbeholdning etter uttak" }) }
+                        cell { text(bokmal { +it.format() }) }
+                    }
+                }
+                ifNotNull(alderspensjon.kapittel20Trygdetid) {
+                    row {
+                        cell { text(bokmal { +"Trygdetid" }) }
+                        cell { text(bokmal { +it.format() + " år" }) }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ planlegge-pensjon-maler/src/main/kotlin/no/nav/pensjon/brev/planleggepensjon/simulering/tabeller/SumTabell.kt
@@ -0,0 +1,87 @@
+package no.nav.pensjon.brev.planleggepensjon.simulering.tabeller
+
+import no.nav.pensjon.brev.model.format
+import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfp
+import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfpSelectors.kompensasjonstillegg
+import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfpSelectors.kronetillegg
+import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfpSelectors.livsvarig
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjon
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.basispensjonBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.garantipensjonBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.garantitilleggBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.gjenlevendetillegg
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.grunnpensjonBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.inntektspensjonBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.pensjonstillegg
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.restpensjonBeloep
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.skjermingstillegg
+import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.tilleggspensjonBeloep
+import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Table.ColumnAlignment.RIGHT
+import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType.BOLD
+import no.nav.pensjon.brev.template.Expression
+import no.nav.pensjon.brev.template.LangBokmal
+import no.nav.pensjon.brev.template.OutlinePhrase
+import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
+import no.nav.pensjon.brev.template.dsl.expression.ifNull
+import no.nav.pensjon.brev.template.dsl.expression.plus
+import no.nav.pensjon.brev.template.dsl.text
+import no.nav.pensjon.brevbaker.api.model.BrevbakerType.Kroner
+
+data class SumTabell(
+    val alderspensjon: Expression<SimuleringV1MaanedligAlderspensjon>,
+    val privatAfp: Expression<PrivatAfp>,
+) : OutlinePhrase<LangBokmal>() {
+    override fun OutlineOnlyScope<LangBokmal, Unit>.template() {
+        paragraph {
+            table(header = {
+                column(columnSpan = 3) {
+                    text(bokmal { +"Alderspensjon og AFP" })
+                }
+                column(alignment = RIGHT, columnSpan = 1) {
+                    text(bokmal { +"Kr per måned" })
+                }
+            }) {
+                row {
+                    cell { text(bokmal { +"Alderspensjon" }) }
+                    cell {
+                        val sumAlderspensjon = alderspensjon.grunnpensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.tilleggspensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.pensjonstillegg.ifNull(Kroner(0)) +
+                                alderspensjon.inntektspensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.garantipensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.garantitilleggBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.skjermingstillegg.ifNull(Kroner(0)) +
+                                alderspensjon.basispensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.restpensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.gjenlevendetillegg.ifNull(Kroner(0))
+                        text(bokmal { +sumAlderspensjon.format() })
+                    }
+                }
+                row {
+                    cell { text(bokmal { +"Livsvarig del" }) }
+                    cell { text(bokmal { +privatAfp.livsvarig.format() }) }
+                }
+                row {
+                    cell { text(bokmal { +"Sum pensjon" }, fontType = BOLD) }
+                    cell {
+                        val sumAlderspensjon = alderspensjon.grunnpensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.tilleggspensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.pensjonstillegg.ifNull(Kroner(0)) +
+                                alderspensjon.inntektspensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.garantipensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.garantitilleggBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.skjermingstillegg.ifNull(Kroner(0)) +
+                                alderspensjon.basispensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.restpensjonBeloep.ifNull(Kroner(0)) +
+                                alderspensjon.gjenlevendetillegg.ifNull(Kroner(0))
+                        val sumPensjon = sumAlderspensjon +
+                                privatAfp.kompensasjonstillegg +
+                                privatAfp.kronetillegg +
+                                privatAfp.livsvarig
+                        text(bokmal { +sumPensjon.format() }, fontType = BOLD)
+                    }
+                }
+            }
+        }
+    }
+}
