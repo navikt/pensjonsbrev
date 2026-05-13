0150008a8477eb0211ee024e80a0db8853667165
martinstorvoll <martin.storvoll@nav.no>
2026-05-13 17:44:55 +0200

    Fikset sum

--- planlegge-pensjon-maler/src/main/kotlin/no/nav/pensjon/brev/planleggepensjon/simulering/tabeller/SumTabell.kt
+++ planlegge-pensjon-maler/src/main/kotlin/no/nav/pensjon/brev/planleggepensjon/simulering/tabeller/SumTabell.kt
@@ -2,6 +2,8 @@ package no.nav.pensjon.brev.planleggepensjon.simulering.tabeller
 
 import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfp
+import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfpSelectors.kompensasjonstillegg
+import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfpSelectors.kronetillegg
 import no.nav.pensjon.brev.planleggepensjon.simulering.PrivatAfpSelectors.livsvarig
 import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjon
 import no.nav.pensjon.brev.planleggepensjon.simulering.SimuleringV1MaanedligAlderspensjonSelectors.garantipensjonBeloep
@@ -52,8 +54,13 @@ data class SumTabell(
                     }
                 }
                 row {
-                    cell { text(bokmal { +"Livsvarig del" }) }
-                    cell { text(bokmal { +privatAfp.livsvarig.format() }) }
+                    cell { text(bokmal { +"AFP i privat sektor" }) }
+                    cell {
+                        val sumAfp = privatAfp.kronetillegg.ifNull(Kroner(0)) +
+                                privatAfp.livsvarig.ifNull(Kroner(0)) +
+                                privatAfp.kompensasjonstillegg.ifNull(Kroner(0))
+                        text(bokmal { +sumAfp.format() })
+                    }
                 }
                 row {
                     cell { text(bokmal { +"Sum pensjon" }, fontType = BOLD) }
@@ -66,7 +73,10 @@ data class SumTabell(
                                 alderspensjon.garantitilleggBeloep.ifNull(Kroner(0)) +
                                 alderspensjon.skjermingstillegg.ifNull(Kroner(0)) +
                                 alderspensjon.gjenlevendetillegg.ifNull(Kroner(0))
-                        val sumPensjon = sumAlderspensjon + privatAfp.livsvarig
+                        val sumAfp = privatAfp.kronetillegg.ifNull(Kroner(0)) +
+                                privatAfp.livsvarig.ifNull(Kroner(0)) +
+                                privatAfp.kompensasjonstillegg.ifNull(Kroner(0))
+                        val sumPensjon = sumAlderspensjon + sumAfp
                         text(bokmal { +sumPensjon.format() }, fontType = BOLD)
                     }
                 }
