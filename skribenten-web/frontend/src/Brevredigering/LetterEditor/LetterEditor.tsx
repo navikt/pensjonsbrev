47d99058432cb10cafb0fcc74376071044f93fa7
Mussie Mesfin <mussie.equbatsion.mesfin@nav.no>
2026-05-20 19:51:03 +0200

    Remove redundant check in "freeze"

--- skribenten-web/frontend/src/Brevredigering/LetterEditor/LetterEditor.tsx
+++ skribenten-web/frontend/src/Brevredigering/LetterEditor/LetterEditor.tsx
@@ -48,11 +48,8 @@ export const LetterEditor = ({
 
   const [vilTilbakestilleMal, setVilTilbakestilleMal] = useState(false);
 
-  const canUndo = !freeze && editorState.saveStatus !== "SAVE_PENDING" && editorState.history.entryPointer >= 0;
-  const canRedo =
-    !freeze &&
-    editorState.saveStatus !== "SAVE_PENDING" &&
-    editorState.history.entryPointer < editorState.history.entries.length - 1;
+  const canUndo = !freeze && editorState.history.entryPointer >= 0;
+  const canRedo = !freeze && editorState.history.entryPointer < editorState.history.entries.length - 1;
 
   const undo = useCallback(() => {
     setEditorState((current) => {
