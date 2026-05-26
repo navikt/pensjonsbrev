42b5aa06fafdf2ed62ff02ed34a104b2d4030184
Mads Opheim <mads.opheim@nav.no>
2026-05-26 13:12:25 +0200

    ToString, equals og hashCode

--- brevbaker/api-model-common/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/SaksbehandlervalgVerdi.kt
+++ brevbaker/api-model-common/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/SaksbehandlervalgVerdi.kt
@@ -13,12 +13,30 @@ sealed interface SaksbehandlervalgVerdi {
 
     class Bool(val bool: Boolean) : SaksbehandlervalgVerdi {
         override val type = Type.BOOL
+        override fun toString() = "SaksbehandlervalgVerdi.Bool(bool=$bool)"
+        override fun equals(other: Any?): Boolean {
+            if (other !is Bool) return false
+            return bool == other.bool
+        }
+        override fun hashCode() = bool.hashCode()
     }
     class Integer(val int: Int?) : SaksbehandlervalgVerdi {
         override val type = Type.INTEGER
+        override fun toString() = "SaksbehandlervalgVerdi.Integer(int=$int)"
+        override fun equals(other: Any?): Boolean {
+            if (other !is Integer) return false
+            return int == other.int
+        }
+        override fun hashCode() = int.hashCode()
     }
     class Enum<T : SaksbehandlerValgEnum>(val enum: T?) : SaksbehandlervalgVerdi {
         override val type = Type.ENUM
+        override fun toString() = "SaksbehandlervalgVerdi.Enum(enum=$enum)"
+        override fun equals(other: Any?): Boolean {
+            if (other !is Enum<*>) return false
+            return enum == other.enum
+        }
+        override fun hashCode() = enum.hashCode()
     }
 }
 
