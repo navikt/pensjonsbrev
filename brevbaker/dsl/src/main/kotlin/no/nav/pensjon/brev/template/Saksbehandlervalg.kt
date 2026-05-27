14226d35aa6dad22570599848d7d12efb8dae584
Mads Opheim <mads.opheim@nav.no>
2026-05-27 13:32:59 +0200

    Fjernar ubrukt

--- brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/Saksbehandlervalg.kt
+++ brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/Saksbehandlervalg.kt
@@ -9,8 +9,6 @@ import no.nav.pensjon.brev.template.selectorTake2
 import kotlin.also
 
 class SBWrapper(val displayText: String, val scope: TemplateRootScope<*, *>) {
-    fun bool(default: Boolean = false) = Expression.FromScope.Saksbehandlervalg(displayText, default).also { scope.saksbehandlervalg[displayText] = SaksbehandlervalgVerdi.Bool(default) }
-    fun int(default: Int? = null) = Expression.FromScope.Saksbehandlervalg(displayText, default).also { scope.saksbehandlervalg[displayText] = SaksbehandlervalgVerdi.Integer(default) }
     fun <T : SaksbehandlerValgEnum> enum(default: T?) = Expression.FromScope.Saksbehandlervalg(displayText, default).also { scope.saksbehandlervalg[displayText] = SaksbehandlervalgVerdi.Enum(default) }
 }
 
