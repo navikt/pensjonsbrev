79bd5ab7c7af594709bd12c52a9f8861ef4e3711
Mads Opheim <mads.opheim@nav.no>
2026-05-27 13:41:35 +0200

    Ryddar meir

--- brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/Saksbehandlervalg.kt
+++ brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/Saksbehandlervalg.kt
@@ -23,65 +23,18 @@ fun <T : SaksbehandlervalgVerdi, D : RedigerbarBrevdata<SaksbehandlervalgIDSL, *
     selector = { saksbehandlerValg.get(displayText) as T }
 )
 
-fun Expression<SaksbehandlervalgVerdi>.bool(): Expression.UnaryInvoke<SaksbehandlervalgVerdi, Boolean> {
-    val selector1: TemplateModelSelector<SaksbehandlervalgVerdi, Boolean> = object : TemplateModelSelector<SaksbehandlervalgVerdi, Boolean> {
+fun Expression<SaksbehandlervalgVerdi>.bool(): Expression.UnaryInvoke<SaksbehandlervalgVerdi, Boolean> =
+    Expression.UnaryInvoke(this, UnaryOperation.Select(object : TemplateModelSelector<SaksbehandlervalgVerdi, Boolean> {
         override val className = SaksbehandlervalgVerdi::class.qualifiedName!!
         override val propertyName = "bool"
         override val propertyType = "kotlian.Boolean"
         override val selector: SaksbehandlervalgVerdi.() -> Boolean = { this.unwrap() as Boolean }
-    }
-    val operation: UnaryOperation.Select<SaksbehandlervalgVerdi, Boolean> = UnaryOperation.Select(selector1)
-    return Expression.UnaryInvoke(this, operation)
-}
+    }))
 
-fun Expression<SaksbehandlervalgVerdi>.int(): Expression.UnaryInvoke<SaksbehandlervalgVerdi, Int> {
-    val selector1: TemplateModelSelector<SaksbehandlervalgVerdi, Int> = object : TemplateModelSelector<SaksbehandlervalgVerdi, Int> {
+fun Expression<SaksbehandlervalgVerdi>.int(): Expression.UnaryInvoke<SaksbehandlervalgVerdi, Int> =
+    Expression.UnaryInvoke(this, UnaryOperation.Select(object : TemplateModelSelector<SaksbehandlervalgVerdi, Int> {
         override val className = SaksbehandlervalgVerdi::class.qualifiedName!!
         override val propertyName = "int"
         override val propertyType = "Int"
         override val selector: SaksbehandlervalgVerdi.() -> Int = { this.unwrap() as Int }
-    }
-    val operation: UnaryOperation.Select<SaksbehandlervalgVerdi, Int> = UnaryOperation.Select(selector1)
-    return Expression.UnaryInvoke(this, operation)
-}
-
-
-// .let {
-//    Expression.UnaryInvoke(
-//        null,
-//        it
-//    )
-//}
-
-
-//fun <D : RedigerbarBrevdata<SaksbehandlervalgIDSL, *>> Expression<D>.alleSaksbehandlervalgSelector() {
-//    return UnaryOperation.Select(
-//        SaksbehandlervalgSelector<SaksbehandlervalgIDSL>(
-//            className = "no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL",
-//            propertyName = displayText,
-//            propertyType = T::class.qualifiedName!!,
-//            selector = { SaksbehandlervalgIDSL::get }
-//        )
-//    )
-//}
-
-
-//val saksbehandlervalgSelector = SimpleSelector<no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.InnhentingOpplysningerSamboerDto, no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL>(
-//    className = "no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.InnhentingOpplysningerSamboerDto",
-//    propertyName = "saksbehandlerValg",
-//    propertyType = "no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL",
-//    selector = no.nav.pensjon.brev.ufore.api.model.maler.redigerbar.InnhentingOpplysningerSamboerDto::saksbehandlerValg
-//)
-
-
-
-//val innerSelector = Expression.UnaryInvoke(
-//    this,
-//    UnaryOperation.Select(
-//        SaksbehandlervalgSelector<SaksbehandlervalgVerdi.Bool>(
-//            className = "no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL",
-//            propertyName = displayText,
-//            propertyType = "kotlin.Boolean",
-//            selector = { SaksbehandlervalgIDSL::get }
-//        )
-//    )
+    }))
