806e4ab6cb7c4dd310d50aeef1b9d74ea51ef9ff
Jeremy Elsom <jeremy.elsom@nav.no>
2026-05-28 13:37:35 +0200

    Fin pussing

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/gjenlevende/GjenlevendepensjonBeregningTabell.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/gjenlevende/GjenlevendepensjonBeregningTabell.kt
@@ -5,7 +5,7 @@ import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Table.ColumnAlignment.LEFT
 import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Table.ColumnAlignment.RIGHT
 import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType
-import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType.BOLD
+import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType.*
 import no.nav.pensjon.brev.template.Expression
 import no.nav.pensjon.brev.template.LangBokmalEnglish
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
@@ -179,7 +179,7 @@ internal fun <LetterData : Any> TableScope<LangBokmalEnglish, LetterData>.sumBru
     netto: Expression<Kroner>,
     bold: Boolean = true,
 ) {
-    val font = if (bold) BOLD else FontType.PLAIN
+    val font = if (bold) BOLD else PLAIN
     row {
         cell {
             text(
@@ -197,7 +197,7 @@ internal fun <LetterData : Any> TableScope<LangBokmalEnglish, LetterData>.sumNet
     netto: Expression<Kroner>,
     bold: Boolean = true,
 ) {
-    val font = if (bold) BOLD else FontType.PLAIN
+    val font = if (bold) BOLD else PLAIN
     row {
         cell {
             text(
