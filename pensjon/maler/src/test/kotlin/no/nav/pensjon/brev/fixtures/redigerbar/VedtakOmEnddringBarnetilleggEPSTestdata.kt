a7776cf9842426e2d90e7620f9629dbea31d67a3
Knut.Mork <knut.mork@nav.no>
2026-06-01 14:43:32 +0200

    Nytt brev for endring bt for eps - regelverksendringer 2026

---
+++ pensjon/maler/src/test/kotlin/no/nav/pensjon/brev/fixtures/redigerbar/VedtakOmEnddringBarnetilleggEPSTestdata.kt
@@ -0,0 +1,30 @@
+package no.nav.pensjon.brev.fixtures.redigerbar
+
+import no.nav.pensjon.brev.Fixtures
+import no.nav.pensjon.brev.api.model.maler.legacy.VedtakOmEndringBarnetilleggEPSAutoDto
+import no.nav.pensjon.brev.api.model.maler.legacy.VedtakOmEndringBarnetilleggEPSData
+import no.nav.pensjon.brevbaker.api.model.BrevbakerType.Kroner
+
+fun createVedtakOmEndringBarnetilleggEPSAutoDto() =
+    VedtakOmEndringBarnetilleggEPSAutoDto(
+        vedtakData = VedtakOmEndringBarnetilleggEPSData(
+            nettoUforetrygdUtenTillegg = Kroner(10000),
+            nettoBarnetilleggFB = Kroner(2000),
+            totalbelop = Kroner(12000),
+            inntektBruker = Kroner(200000),
+            inntektEPS = Kroner(150000),
+            redusertBarnetillegg = true,
+            nullBarnetillegg = false,
+            gInntil = Kroner(600000),
+            samletInntekt = Kroner(350000),
+            grenseUtbetaltBarnetillegg = Kroner(24000),
+            nyttBarnetillegg = Kroner(2000),
+            fribelop = Kroner(50000),
+            arligUtbetalingBarnetilleggFB = Kroner(24000),
+            utbetaltBarnetilleggHittilIAr = Kroner(12000),
+            utbetalingBarnetilleggResten = Kroner(6000),
+            pe = Fixtures.create(),
+            maanedligUfoeretrygdFoerSkatt = Fixtures.createVedlegg(),
+            orienteringOmRettigheterUfoere = Fixtures.createVedlegg(),
+        )
+    )
