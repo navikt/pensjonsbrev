488ce434f3dbb9c0c32b9ac3ab172d92ed270932
Mads Opheim <mads.opheim@nav.no>
2026-06-01 10:44:22 +0200

    Abstraherer litt

--- ufoere/api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/InnhentingOpplysningerSamboerDto.kt
+++ ufoere/api-model/src/main/kotlin/no/nav/pensjon/brev/ufore/api/model/maler/redigerbar/InnhentingOpplysningerSamboerDto.kt
@@ -2,9 +2,10 @@ package no.nav.pensjon.brev.ufore.api.model.maler.redigerbar
 
 import no.nav.pensjon.brev.api.model.maler.EmptyFagsystemdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdataMedSaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL
 
 data class InnhentingOpplysningerSamboerDto(
     override val pesysData: EmptyFagsystemdata,
     override val saksbehandlerValg: SaksbehandlervalgIDSL
-) : RedigerbarBrevdata<SaksbehandlervalgIDSL, EmptyFagsystemdata>
+) : RedigerbarBrevdataMedSaksbehandlerValg<EmptyFagsystemdata>
