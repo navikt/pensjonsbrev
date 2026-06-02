85e53329c0ab066fdabdef94fad2e89345eadd41
Mads Opheim <mads.opheim@nav.no>
2026-06-02 13:39:09 +0200

    Revert "Testar også ut enum"
    This reverts commit 0490cd82bb1acca4b0874194c7f636f3ec4c174d.

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/VarselRevurderingAvPensjon.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/VarselRevurderingAvPensjon.kt
@@ -3,23 +3,20 @@ package no.nav.pensjon.brev.api.model.maler.redigerbar
 import no.nav.pensjon.brev.api.model.Sakstype
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
-import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdataMedSaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
-import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgEnum
-import no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL
 import no.nav.pensjon.brevbaker.api.model.DisplayText
 
 @Suppress("unused")
 data class VarselRevurderingAvPensjonDto(
-    override val saksbehandlerValg: SaksbehandlervalgIDSL,
+    override val saksbehandlerValg: SaksbehandlerValg,
     override val pesysData: PesysData,
-) : RedigerbarBrevdataMedSaksbehandlerValg<VarselRevurderingAvPensjonDto.PesysData> {
+) : RedigerbarBrevdata<VarselRevurderingAvPensjonDto.SaksbehandlerValg, VarselRevurderingAvPensjonDto.PesysData> {
     data class SaksbehandlerValg(
         @DisplayText("Tittelvalg") val tittelValg: TittelValg
     ) : SaksbehandlerValgBrevdata{
-        enum class TittelValg(override val displayText: String) : SaksbehandlerValgEnum {
-            @DisplayText("Revurdering av rett") RevurderingAvRett("Revurdering av rett"),
-            @DisplayText("Revurdering reduksjon") RevurderingReduksjon("Revurdering reduksjon"),
+        enum class TittelValg {
+            @DisplayText("Revurdering av rett") RevurderingAvRett,
+            @DisplayText("Revurdering reduksjon") RevurderingReduksjon
         }
     }
 
