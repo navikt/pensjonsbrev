98f6bdf79aabce9be12d844c2fddf958791bd7f9
Mads Opheim <mads.opheim@nav.no>
2026-06-02 13:41:55 +0200

    Reapply "Testar også ut enum"
    This reverts commit 85e53329c0ab066fdabdef94fad2e89345eadd41.

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/VarselRevurderingAvPensjon.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/VarselRevurderingAvPensjon.kt
@@ -3,20 +3,23 @@ package no.nav.pensjon.brev.api.model.maler.redigerbar
 import no.nav.pensjon.brev.api.model.Sakstype
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdataMedSaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
+import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgEnum
+import no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL
 import no.nav.pensjon.brevbaker.api.model.DisplayText
 
 @Suppress("unused")
 data class VarselRevurderingAvPensjonDto(
-    override val saksbehandlerValg: SaksbehandlerValg,
+    override val saksbehandlerValg: SaksbehandlervalgIDSL,
     override val pesysData: PesysData,
-) : RedigerbarBrevdata<VarselRevurderingAvPensjonDto.SaksbehandlerValg, VarselRevurderingAvPensjonDto.PesysData> {
+) : RedigerbarBrevdataMedSaksbehandlerValg<VarselRevurderingAvPensjonDto.PesysData> {
     data class SaksbehandlerValg(
         @DisplayText("Tittelvalg") val tittelValg: TittelValg
     ) : SaksbehandlerValgBrevdata{
-        enum class TittelValg {
-            @DisplayText("Revurdering av rett") RevurderingAvRett,
-            @DisplayText("Revurdering reduksjon") RevurderingReduksjon
+        enum class TittelValg(override val displayText: String) : SaksbehandlerValgEnum {
+            @DisplayText("Revurdering av rett") RevurderingAvRett("Revurdering av rett"),
+            @DisplayText("Revurdering reduksjon") RevurderingReduksjon("Revurdering reduksjon"),
         }
     }
 
