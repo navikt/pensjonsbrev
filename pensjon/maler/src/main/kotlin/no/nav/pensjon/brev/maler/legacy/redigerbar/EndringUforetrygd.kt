28800c01b3f4c36f99af64b5ce87dcef188a1c7a
eirikstadheim <82815956+eirikstadheim@users.noreply.github.com>
2026-06-04 15:36:57 +0200

    Gjør det enklere å forstå hvordan barnetillegg er regnet ut (#3268)

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/EndringUforetrygd.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/EndringUforetrygd.kt
@@ -1486,6 +1486,27 @@ object EndringUforetrygd : RedigerbarTemplate<EndringUfoeretrygdDto> {
                     }
                 }
 
+                showIf(barnetilleggInnvilget) {
+                    paragraph {
+                        text(
+                            bokmal { +"Inntekten din består av uføretrygd for de " + fritekst("antall mnd gjenstående månedene av året.") },
+                            nynorsk { +"Inntekta di består av uføretrygd for dei " + fritekst("antall mnd gjenstående månedene av året.") }
+                        )
+                        showIf(barnetilleggFellesInnvilget) {
+                            text(
+                                bokmal { +" Din " + pe.sivilstand_ektefelle_partner_samboer_bormed_ut() + "s inntekt består av inntekt de " + fritekst("antall mnd") + " gjenstående månedene av året, fratrukket " + fritekst("antall mnd") + "/12 av folketrygdens grunnbeløp. Folketrygdens grunnbeløp er for tiden " + grunnbelop.format() + "." },
+                                nynorsk { +" Din " + pe.sivilstand_ektefelle_partner_samboer_bormed_ut_nn_entall() + "s inntekt består av inntekt de " + fritekst("antall mnd") + " gjenstående månedene av året, fratrukket " + fritekst("antall mnd") + "/12 av grunnbeløpet i folketrygda. Grunnbeløpet i folketrygda er for tida " + grunnbelop.format() + "." }
+                            )
+                        }
+                        showIf(barnetilleggFellesInnvilget and barnetilleggSerkullInnvilget) {
+                            text(
+                                bokmal { +"Barnetillegget for barn som ikke bor med begge foreldre er kun beregnet utfra din inntekt" },
+                                nynorsk { +"Barnetillegget for barn som ikkje bur med begge foreldre er kun berekna utfra inntekta di" },
+                            )
+                        }
+                    }
+                }
+
                 showIf(barnetilleggFellesInnvilget) {
                     paragraph {
                         text(
