068e3d007efdaafa647b0135f074ff818d31688e
Monika Sperstad Koller <monika.koller@nav.no>
2026-06-05 13:20:37 +0200

    Oppdatere tekst om mottatt vedtak

--- alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/afp/fraser/AfpEtteroppgjoerAvslutning.kt
+++ alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/afp/fraser/AfpEtteroppgjoerAvslutning.kt
@@ -94,13 +94,11 @@ object AfpEtteroppgjoerAvslutning : OutlinePhrase<LangBokmalNynorsk>() {
             paragraph {
                 text(
                     bokmal {
-                        +"Hvis du mener vedtaket er feil, kan du klage innen seks uker fra den datoen du " +
-                            "mottok vedtaket. Klagen skal være skriftlig. Du finner skjema og informasjon på " +
+                        +"Hvis du mener vedtaket er feil, kan du klage innen seks uker fra vedtaket har kommet fram til deg. Klagen skal være skriftlig. Du finner skjema og informasjon på " +
                             "$KLAGE_URL."
                     },
                     nynorsk {
-                        +"Dersom du meiner at vedtaket er feil, kan du klage innan seks veker frå den datoen " +
-                            "då du fekk vedtaket. Klaga skal vere skriftleg. Du finn skjema og informasjon på " +
+                        +"Dersom du meiner at vedtaket er feil, kan du klage innan seks veker frå vedtaket har komme fram til deg. Klaga skal vere skriftleg. Du finn skjema og informasjon på " +
                             "$KLAGE_URL."
                     },
                 )
@@ -136,8 +134,7 @@ object AfpEtteroppgjoerAvslutning : OutlinePhrase<LangBokmalNynorsk>() {
                 text(
                     bokmal {
                         +"Hvis du mener at det er andre forhold ved vedtaket som ikke er riktig, har du " +
-                            "anledning til å klage på vedtaket. Fristen for å klage er seks uker fra du " +
-                            "mottar dette brevet."
+                            "anledning til å klage på vedtaket. Fristen for å klage er seks uker fra vedtaket har kommet fram til deg."
                     },
                     nynorsk {
                         +"Dersom du meiner at det er andre forhold ved vedtaket som ikkje er rette, har du " +
