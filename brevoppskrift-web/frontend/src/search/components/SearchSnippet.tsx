113c7366bd382a17d6fc33a1c86b4eb483f4bd55
Håkon Heggholmen <hakon.heggholmen@nav.no>
2026-06-08 12:02:57 +0200

    linting.

--- brevoppskrift-web/frontend/src/search/components/SearchSnippet.tsx
+++ brevoppskrift-web/frontend/src/search/components/SearchSnippet.tsx
@@ -51,16 +51,21 @@ export function SearchSnippet({ hit, needle }: { hit: ContentHit; needle?: strin
               css={css`
                 white-space: normal;
                 word-break: break-word;
-                ${!isPrimary &&
-                css`
+                ${
+                  !isPrimary &&
+                  css`
                   margin: var(--ax-space-2) 0;
-                `}
+                `
+                }
               `}
               key={absoluteIndex}
               size="small"
               textColor={isPrimary ? "default" : "subtle"}
             >
-              <LineContent line={isPrimary ? line : truncateLine(line, SNIPPET_CHARS)} needle={isPrimary ? needle : undefined} />
+              <LineContent
+                line={isPrimary ? line : truncateLine(line, SNIPPET_CHARS)}
+                needle={isPrimary ? needle : undefined}
+              />
             </BodyShort>
           );
         })}
