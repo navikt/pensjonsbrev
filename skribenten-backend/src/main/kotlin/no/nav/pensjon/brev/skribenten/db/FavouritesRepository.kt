2cd6d990897b3d1774cfb1958a2869efd0b75be8
Mads Opheim <mads.opheim@nav.no>
2026-06-09 16:46:47 +0200

    Konsekvent på å bruke value class heller enn interface

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/db/FavouritesRepository.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/db/FavouritesRepository.kt
@@ -1,6 +1,6 @@
 package no.nav.pensjon.brev.skribenten.db
 
-import no.nav.pensjon.brev.api.model.maler.Brevkode
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevkode
 import no.nav.pensjon.brev.skribenten.model.NavIdent
 import org.jetbrains.exposed.v1.core.eq
 import org.jetbrains.exposed.v1.jdbc.deleteWhere
@@ -9,13 +9,13 @@ import org.jetbrains.exposed.v1.jdbc.selectAll
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 
 class FavouritesRepository {
-    fun getFavourites(userId: NavIdent): List<Brevkode.Redigerbart> =
+    fun getFavourites(userId: NavIdent): List<RedigerbarBrevkode> =
         transaction {
             Favourites.selectAll().where { Favourites.userId eq userId }.map { row -> row[Favourites.letterCode] }
         }
 
 
-    fun addFavourite(userID: NavIdent, letterId: Brevkode.Redigerbart) {
+    fun addFavourite(userID: NavIdent, letterId: RedigerbarBrevkode) {
         transaction {
             Favourites.insert {
                 it[userId] = userID
@@ -24,7 +24,7 @@ class FavouritesRepository {
         }
     }
 
-    fun removeFavourite(userID: NavIdent, letterId: Brevkode.Redigerbart) {
+    fun removeFavourite(userID: NavIdent, letterId: RedigerbarBrevkode) {
         transaction {
             Favourites.deleteWhere {
                 userId eq userID
