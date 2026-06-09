5ce5ea1954669133dcd6b0f6941e970f7c6f9340
eirikstadheim <eirik.stadheim@nav.no>
2026-06-09 14:25:28 +0200

    Fritekst på etterbetaling også utenfor tabell

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OktMinsteIFUReduksjonsprosent.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/fraser/OktMinsteIFUReduksjonsprosent.kt
@@ -332,11 +332,20 @@ object OktMinsteIFUReduksjonsprosent {
                             nynorsk { +"Fordi du har hatt inntekt over inntektsgrensa" },
                         )
                     }
-                    paragraph {
-                        text(
-                            bokmal { +"Fram til 1. juli i år har vi brukt din gamle reduksjonsprosent i beregningene av uføretrygden din. Når lovendringen trer i kraft, skal ny reduksjonsprosent ha virkning tilbake i tid fra 1. januar i år. Du vil derfor få en etterbetaling på " + data.etterbetalingJuli.format() + " innen kort tid." },
-                            nynorsk { +"Fram til 1. juli i år har vi brukt din gamle reduksjonsprosent i berekningane av uføretrygda di. Når lovendringa trer i kraft, skal ny reduksjonsprosent ha tilbakeverkande kraft frå 1. januar i år. Du vil derfor få ei etterbetaling på " + data.etterbetalingJuli.format() + " innan kort tid." },
-                        )
+                    showIf(data.redigerbar) {
+                        paragraph {
+                            text(
+                                bokmal { +"Fram til 1. juli i år har vi brukt din gamle reduksjonsprosent i beregningene av uføretrygden din. Når lovendringen trer i kraft, skal ny reduksjonsprosent ha virkning tilbake i tid fra 1. januar i år. Du vil derfor få en etterbetaling på " + fritekst("Beløp etterbetaling") + " innen kort tid." },
+                                nynorsk { +"Fram til 1. juli i år har vi brukt din gamle reduksjonsprosent i berekningane av uføretrygda di. Når lovendringa trer i kraft, skal ny reduksjonsprosent ha tilbakeverkande kraft frå 1. januar i år. Du vil derfor få ei etterbetaling på " + fritekst("Beløp etterbetaling") + " innan kort tid." },
+                            )
+                        }
+                    }.orShow {
+                        paragraph {
+                            text(
+                                bokmal { +"Fram til 1. juli i år har vi brukt din gamle reduksjonsprosent i beregningene av uføretrygden din. Når lovendringen trer i kraft, skal ny reduksjonsprosent ha virkning tilbake i tid fra 1. januar i år. Du vil derfor få en etterbetaling på " + data.etterbetalingJuli.format() + " innen kort tid." },
+                                nynorsk { +"Fram til 1. juli i år har vi brukt din gamle reduksjonsprosent i berekningane av uføretrygda di. Når lovendringa trer i kraft, skal ny reduksjonsprosent ha tilbakeverkande kraft frå 1. januar i år. Du vil derfor få ei etterbetaling på " + data.etterbetalingJuli.format() + " innan kort tid." },
+                            )
+                        }
                     }
                 }.orShow {
                     title2 {
