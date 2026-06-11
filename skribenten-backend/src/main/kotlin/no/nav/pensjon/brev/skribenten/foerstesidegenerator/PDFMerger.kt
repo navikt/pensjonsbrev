8febce76d1fc38dab243e87d4285a66993ff92b6
Mads Opheim <mads.opheim@nav.no>
2026-06-11 19:09:54 +0200

    Tryggare handtering med use

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/foerstesidegenerator/PDFMerger.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/foerstesidegenerator/PDFMerger.kt
@@ -9,10 +9,8 @@ object PDFMerger {
     fun merge(first: ByteArray, second: ByteArray): ByteArray {
         PDDocument().use { target ->
             val merger = PDFMergerUtility()
-            val foerstesidePdf = Loader.loadPDF(second)
-            val originalPdf = Loader.loadPDF(first)
-            merger.appendDocument(target, originalPdf).also { originalPdf.close() }
-            merger.appendDocument(target, foerstesidePdf).also { foerstesidePdf.close() }
+            Loader.loadPDF(first).use { merger.appendDocument(target, it) }
+            Loader.loadPDF(second).use { merger.appendDocument(target, it) }
             val outputStream = ByteArrayOutputStream()
             target.save(outputStream)
             return outputStream.toByteArray()
