68e8ca55f65730484adccb0375e950aedb6e5253
Mads Opheim <mads.opheim@nav.no>
2026-06-11 20:34:15 +0200

    Støttar scenariet kor vi sender inn enginet

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/HttpClientFactory.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/HttpClientFactory.kt
@@ -2,12 +2,23 @@ package no.nav.pensjon.brev.skribenten.services
 
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.HttpClientEngineConfig
+import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.CIOEngineConfig
 
 object HttpClientFactory {
     fun lagHttpClient(config: HttpClientConfig<CIOEngineConfig>.() -> Unit): HttpClient =
-        HttpClient(CIO) {
+        lagHttpClient(CIO, config)
+
+    fun lagHttpClient(engine: HttpClientEngine, config: HttpClientConfig<*>.() -> Unit): HttpClient =
+        HttpClient(engine) {
+            config()
+        }
+
+    fun <C : HttpClientEngineConfig, T : HttpClientEngineFactory<C>> lagHttpClient(engine: T, config: HttpClientConfig<C>.() -> Unit): HttpClient =
+        HttpClient(engine) {
             config()
         }
 }
