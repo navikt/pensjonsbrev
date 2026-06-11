b5c2809275e04e1259629c50d84497767b62ec77
Mads Opheim <mads.opheim@nav.no>
2026-06-11 20:42:31 +0200

    Legg på lukk-klient-mekanismen

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/HttpClientFactory.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/services/HttpClientFactory.kt
@@ -7,18 +7,25 @@ import io.ktor.client.engine.HttpClientEngineConfig
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.CIOEngineConfig
+import io.ktor.utils.io.core.Closeable
 
-object HttpClientFactory {
+object HttpClientFactory : Closeable {
+    val clients = mutableListOf<HttpClient>()
+
     fun lagHttpClient(config: HttpClientConfig<CIOEngineConfig>.() -> Unit): HttpClient =
         lagHttpClient(CIO, config)
 
     fun lagHttpClient(engine: HttpClientEngine, config: HttpClientConfig<*>.() -> Unit): HttpClient =
         HttpClient(engine) {
             config()
-        }
+        }.also { clients.add(it) }
 
-    fun <C : HttpClientEngineConfig, T : HttpClientEngineFactory<C>> lagHttpClient(engine: T, config: HttpClientConfig<C>.() -> Unit): HttpClient =
+    private fun <C : HttpClientEngineConfig, T : HttpClientEngineFactory<C>> lagHttpClient(engine: T, config: HttpClientConfig<C>.() -> Unit): HttpClient =
         HttpClient(engine) {
             config()
-        }
+        }.also { clients.add(it) }
+
+    override fun close() {
+        clients.forEach { it.close() }
+    }
 }
