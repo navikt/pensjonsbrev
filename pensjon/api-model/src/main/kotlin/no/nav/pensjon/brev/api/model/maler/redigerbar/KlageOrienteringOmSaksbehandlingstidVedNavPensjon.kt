7618c98833fca4e19cc6a7bb9f55f97dab719926
Jeremy Elsom <jeremy.elsom@nav.no>
2026-06-15 14:49:50 +0200

    Opprettet Dto

---
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/redigerbar/KlageOrienteringOmSaksbehandlingstidVedNavPensjon.kt
@@ -0,0 +1,17 @@
+package no.nav.pensjon.brev.api.model.maler.redigerbar
+
+import no.nav.pensjon.brev.api.model.maler.EmptySaksbehandlerValg
+import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
+import no.nav.pensjon.brevbaker.api.model.BrevbakerType.Foedselsnummer
+
+class KlageOrienteringOmSaksbehandlingstidVedNavPensjon(
+    override val pesysData: PesysData, override val saksbehandlerValg: EmptySaksbehandlerValg
+) : RedigerbarBrevdata<EmptySaksbehandlerValg, KlageOrienteringOmSaksbehandlingstidVedNavPensjon.PesysData> {
+
+    data class PesysData(
+        val foedselsdato: Foedselsnummer, // PE_PersonSak_PSfnr_tssid
+        val navn: String, // PE_PersonSak_PSNavn
+        val kontaktinformasjon: String // PE_kontaktinformasjon_NavnAvsenderEnhet
+    ) : FagsystemBrevdata
+}
