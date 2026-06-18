f2789db552aa6b11bf26e073a687f22f9af1f2c2
eirikstadheim <eirik.stadheim@nav.no>
2026-06-18 12:25:26 +0200

    Viser ikke "For deg..." dersom etterbetaling blir negativt beløp. Bruker samme logikk på tittel for enkelhets skyld

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/VedtakOmOktMinsteIFULavereReduksjonsprosentRedigerbar.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/redigerbar/VedtakOmOktMinsteIFULavereReduksjonsprosentRedigerbar.kt
@@ -42,7 +42,7 @@ import no.nav.pensjon.brev.template.RedigerbarTemplate
 import no.nav.pensjon.brev.template.createTemplate
 import no.nav.pensjon.brev.template.dsl.expression.expr
 import no.nav.pensjon.brev.template.dsl.expression.greaterThan
-import no.nav.pensjon.brev.template.dsl.expression.or
+import no.nav.pensjon.brev.template.dsl.expression.lessThan
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
@@ -74,7 +74,7 @@ object VedtakOmOktMinsteIFULavereReduksjonsprosentRedigerbar : RedigerbarTemplat
                     bokmal { +"Vedtaksbrev - Du får en etterbetaling av uføretrygd " },
                     nynorsk { +"Vedtaksbrev - Du får ein etterbetaling av uføretrygd " },
                 )
-            }.orShowIf(data.endringNettoUforetrygdUtenTillegg or data.endringNettoBarnetillegg or data.endringNettoGjenlevendetillegg) {
+            }.orShowIf(data.etterbetalingJuli.lessThan(0)) {
                 text(
                     bokmal { +"Vedtaksbrev - Endring av uføretrygd" },
                     nynorsk { +"Vedtaksbrev - Endring av uføretrygd" },
