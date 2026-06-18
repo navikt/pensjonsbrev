0736482bb689abf52ccd068bd80d7cfea5dbdc4f
Monika Sperstad Køller <44394008+monikaspko@users.noreply.github.com>
2026-06-18 09:58:20 +0200

    Endre kategori fra VEDTAK_ENDRING_OG_REVURDERING til ETTEROPPGJOER (#3346)

--- alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/afp/VedtakAfpEtteroppgjoerEtterbetalingEtterSvar.kt
+++ alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/afp/VedtakAfpEtteroppgjoerEtterbetalingEtterSvar.kt
@@ -64,7 +64,7 @@ object VedtakAfpEtteroppgjoerEtterbetalingEtterSvar : RedigerbarTemplate<VedtakA
 
     override val kode = Aldersbrevkoder.Redigerbar.PE_AFP_ETTEROPPGJOER_ETTERBETALING_ETTER_SVAR
 
-    override val kategori = Brevkategori.VEDTAK_ENDRING_OG_REVURDERING
+    override val kategori = Brevkategori.ETTEROPPGJOER
     override val brevkontekst = TemplateDescription.Brevkontekst.VEDTAK
     override val sakstyper = setOf(Sakstype.AFP)
 
