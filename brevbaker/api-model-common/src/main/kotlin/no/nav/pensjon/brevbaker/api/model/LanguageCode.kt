3f7d91a2a588458048a64c1915b6c3bff0cabc52
Mads Opheim <mads.opheim@nav.no>
2026-06-18 18:10:59 +0200

    Flyttar locale-konverteringa over på LanguageCode for å kunne gjenbruke

--- brevbaker/api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LanguageCode.kt
+++ brevbaker/api-model-common/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/LanguageCode.kt
@@ -2,6 +2,14 @@
 
 package no.nav.pensjon.brevbaker.api.model
 
+import java.util.Locale
+
 enum class LanguageCode {
-    BOKMAL, NYNORSK, ENGLISH
+    BOKMAL, NYNORSK, ENGLISH;
+
+    fun locale(): Locale = when (this) {
+        BOKMAL -> Locale.forLanguageTag("no")
+        NYNORSK -> Locale.forLanguageTag("no")
+        LanguageCode.ENGLISH -> Locale.UK
+    }
 }
