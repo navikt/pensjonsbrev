a913b0ba124a7896b7e2c6e3ae1c039d185ff374
Mads Opheim <mads.opheim@nav.no>
2026-07-08 09:49:14 +0200

    Gjer interne klassar til data classes, og sparar oss med det for tostring, equals og hashcode

--- brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/SaksbehandlervalgVerdi.kt
+++ brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/SaksbehandlervalgVerdi.kt
@@ -15,50 +15,22 @@ sealed interface SaksbehandlervalgVerdi<out T> {
     val displayText: String
 
     @BrevbakerDSLInternal
-    class Bool @InternKonstruktoer constructor(override val defaultValue: Boolean, override val displayText: String) : SaksbehandlervalgVerdi<Boolean> {
+    data class Bool @InternKonstruktoer constructor(override val defaultValue: Boolean, override val displayText: String) : SaksbehandlervalgVerdi<Boolean> {
         override val type = Type.BOOL
-        override fun toString() = "SaksbehandlervalgVerdi.Bool(bool=$defaultValue)"
-        override fun equals(other: Any?): Boolean {
-            if (other !is Bool) return false
-            return defaultValue == other.defaultValue
-        }
-
-        override fun hashCode() = Bool::class.java.hashCode() + defaultValue.hashCode()
     }
 
     @BrevbakerDSLInternal
-    class Integer @InternKonstruktoer constructor(override val defaultValue: Int?, override val displayText: String) : SaksbehandlervalgVerdi<Int?> {
+    data class Integer @InternKonstruktoer constructor(override val defaultValue: Int?, override val displayText: String) : SaksbehandlervalgVerdi<Int?> {
         override val type = Type.INTEGER
-        override fun toString() = "SaksbehandlervalgVerdi.Integer(int=$defaultValue)"
-        override fun equals(other: Any?): Boolean {
-            if (other !is Integer) return false
-            return defaultValue == other.defaultValue
-        }
-
-        override fun hashCode() = Integer::class.java.hashCode() + (defaultValue?.hashCode() ?: 0)
     }
 
     @BrevbakerDSLInternal
-    class Enum<T : SaksbehandlerValgEnum> @InternKonstruktoer constructor(override val defaultValue: T?, override val displayText: String, val clazz: Class<out kotlin.Enum<*>?>) : SaksbehandlervalgVerdi<T?> {
+    data class Enum<T : SaksbehandlerValgEnum> @InternKonstruktoer constructor(override val defaultValue: T?, override val displayText: String, val clazz: Class<out kotlin.Enum<*>?>) : SaksbehandlervalgVerdi<T?> {
         override val type = Type.ENUM
-        override fun toString() = "SaksbehandlervalgVerdi.Enum(enum=$defaultValue)"
-        override fun equals(other: Any?): Boolean {
-            if (other !is Enum<*>) return false
-            return defaultValue == other.defaultValue
-        }
-
-        override fun hashCode() = Enum::class.java.hashCode() + (defaultValue?.hashCode() ?: 0)
     }
 
     @BrevbakerDSLInternal
-    class Text @InternKonstruktoer constructor(override val defaultValue: String?, override val displayText: String) : SaksbehandlervalgVerdi<String?> {
+    data class Text @InternKonstruktoer constructor(override val defaultValue: String?, override val displayText: String) : SaksbehandlervalgVerdi<String?> {
         override val type = Type.TEXT
-        override fun toString() = "SaksbehandlervalgVerdi.Text(text=$defaultValue)"
-        override fun equals(other: Any?): Boolean {
-            if (other !is Text) return false
-            return defaultValue == other.defaultValue
-        }
-
-        override fun hashCode() = Text::class.java.hashCode() + (defaultValue?.hashCode() ?: 0)
     }
 }
