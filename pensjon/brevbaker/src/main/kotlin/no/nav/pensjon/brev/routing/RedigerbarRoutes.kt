a60e17db9a0ab866b1d16127f790a059ea142149
Mads Opheim <mads.opheim@nav.no>
2026-07-08 08:48:10 +0200

    Kastar litt exceptions for å teste logginga

--- pensjon/brevbaker/src/main/kotlin/no/nav/pensjon/brev/routing/RedigerbarRoutes.kt
+++ pensjon/brevbaker/src/main/kotlin/no/nav/pensjon/brev/routing/RedigerbarRoutes.kt
@@ -22,6 +22,12 @@ fun Route.redigerbarRoutes(
         }
 
         post<BestillBrevRequest<Brevkode.Redigerbart>>("/markup-usage") { brevbestilling ->
+            try {
+                throw IllegalStateException("For testing")
+            } catch (e: Exception) {
+                e.addSuppressed(NumberFormatException("test"))
+                throw IllegalArgumentException("outer", e)
+            }
             val markup = redigerbareBrev.renderLetterMarkupWithDataUsage(brevbestilling)
             call.respond(markup)
         }
