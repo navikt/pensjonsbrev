b4f8682261799f0ad73bc1fb45497364cd367aba
Mads Opheim <mads.opheim@nav.no>
2026-07-08 21:22:39 +0200

    Fiksar testfixtures

--- brevbaker/dsl/src/testFixtures/kotlin/no/nav/brev/brevbaker/SaksbehandlervalgIDSLTestImpl.kt
+++ brevbaker/dsl/src/testFixtures/kotlin/no/nav/brev/brevbaker/SaksbehandlervalgIDSLTestImpl.kt
@@ -1,38 +1,8 @@
-@file:OptIn(InternKonstruktoer::class, BrevbakerDSLInternal::class)
-
 package no.nav.brev.brevbaker
 
-import no.nav.brev.InternKonstruktoer
-import no.nav.pensjon.brev.template.BrevbakerDSLInternal
+import no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL
 
-// TODO fiks
-//fun lagSaksbehandlervalg(vararg verdier: Pair<String, *>) = lagSaksbehandlervalg(verdier.toMap())
-//fun lagSaksbehandlervalg(verdier: Map<String, *> = emptyMap<String, Any>()) = verdier
-//
-//fun <T> T.tilSaksbehandlervalgverdiEnum(displayText: String): SaksbehandlervalgVerdi.Enum<*> where T : SaksbehandlerValgEnum, T: Enum<T> =
-//    SaksbehandlervalgVerdi.Enum(
-//        defaultValue = this,
-//        displayText = displayText,
-//        clazz = this::class.java
-//    )
-//
-//fun saksbehandlervalgVerdiBool(verdi: Boolean, displayText: String) = SaksbehandlervalgVerdi.Bool(
-//    defaultValue = verdi,
-//    displayText = displayText
-//)
-//
-//fun saksbehandlervalgVerdiInteger(verdi: Int?, displayText: String) = SaksbehandlervalgVerdi.Integer(
-//    defaultValue = verdi,
-//    displayText = displayText
-//)
-//
-//fun saksbehandlervalgVerdiText(verdi: String?, displayText: String) = SaksbehandlervalgVerdi.Text(
-//    defaultValue = verdi,
-//    displayText = displayText
-//)
-//
-//inline fun <reified T> saksbehandlervalgVerdiEnum(verdi: T, displayText: String) where T : SaksbehandlerValgEnum, T : Enum<T> = SaksbehandlervalgVerdi.Enum(
-//    defaultValue = verdi,
-//    displayText = displayText,
-//    clazz = T::class.java
-//)
+class SaksbehandlervalgIDSLTestImpl : SaksbehandlervalgIDSL, LinkedHashMap<String, Any?>()
+
+fun lagSaksbehandlervalg(vararg verdier: Pair<String, *>) = lagSaksbehandlervalg(verdier.toMap())
+fun lagSaksbehandlervalg(verdier: Map<String, *> = emptyMap<String, Any>()) = SaksbehandlervalgIDSLTestImpl().also { it.putAll(verdier) }
