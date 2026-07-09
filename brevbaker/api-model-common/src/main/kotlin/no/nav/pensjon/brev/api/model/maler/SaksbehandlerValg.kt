534b7328585164d5064b4436b0fa5ea05129ec84
Alexander Hoem Rosbach <alexander.hoem.rosbach@nav.no>
2026-07-09 16:12:51 +0200

    Unngå cast, legg til Map<String, Any?> som type for SaksbehandlerValgIDSL

--- brevbaker/api-model-common/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/SaksbehandlerValg.kt
+++ brevbaker/api-model-common/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/SaksbehandlerValg.kt
@@ -1,6 +1,6 @@
 package no.nav.pensjon.brev.api.model.maler
 
-interface SaksbehandlervalgIDSL : SaksbehandlerValgBrevdata
+interface SaksbehandlervalgIDSL : SaksbehandlerValgBrevdata, Map<String, Any?>
 
 interface SaksbehandlerValgEnum {
     val displayText: String
