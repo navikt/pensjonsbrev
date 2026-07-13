fff58404beeb978e7a52ff886d013b3b6e01320f
Mads Opheim <mads.opheim@nav.no>
2026-07-13 12:12:54 +0200

    Forenkling

--- pensjon/brevbaker/src/main/kotlin/no/nav/pensjon/brev/converters/BrevbakerBrevdataModule.kt
+++ pensjon/brevbaker/src/main/kotlin/no/nav/pensjon/brev/converters/BrevbakerBrevdataModule.kt
@@ -1,8 +1,5 @@
 package no.nav.pensjon.brev.converters
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
 import no.nav.pensjon.brev.api.model.maler.BrevbakerBrevdata
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
@@ -17,19 +14,12 @@ object BrevbakerBrevdataModule : SimpleModule() {
 
     private data class GenericRedigerbarBrevdata(override val saksbehandlerValg: GenericBrevdata, override val pesysData: GenericBrevdata) : RedigerbarBrevdata<GenericBrevdata, GenericBrevdata>
 
+    private class SaksbehandlervalgMap : SaksbehandlervalgIDSL, LinkedHashMap<String, Any?>()
+
     init {
-        addDeserializer(BrevbakerBrevdata::class.java, BrevdataDeserializer)
-        addDeserializer(RedigerbarBrevdata::class.java, RedigerbarBrevdataDeserializer)
+        addAbstractTypeMapping<BrevbakerBrevdata, GenericBrevdata>()
+        addAbstractTypeMapping<RedigerbarBrevdata<*, *>, GenericRedigerbarBrevdata>()
         addAbstractTypeMapping<SaksbehandlervalgIDSL, SaksbehandlervalgMap>()
     }
 
-    private object BrevdataDeserializer : JsonDeserializer<BrevbakerBrevdata>() {
-        override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): BrevbakerBrevdata = ctxt.readValue(parser, GenericBrevdata::class.java)
-    }
-    private object RedigerbarBrevdataDeserializer : JsonDeserializer<RedigerbarBrevdata<*,*>>() {
-        override fun deserialize(parser: JsonParser, ctxt: DeserializationContext): RedigerbarBrevdata<*,*> =
-            ctxt.readValue(parser, GenericRedigerbarBrevdata::class.java)
-    }
 }
-
-private class SaksbehandlervalgMap : SaksbehandlervalgIDSL, LinkedHashMap<String, Any?>()
