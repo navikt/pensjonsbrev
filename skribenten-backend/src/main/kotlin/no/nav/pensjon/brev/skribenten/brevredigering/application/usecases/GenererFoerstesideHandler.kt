65dbbaf413b28a74e9a40be4e4484e4ffed16e44
Mads Opheim <mads.opheim@nav.no>
2026-07-20 12:11:45 +0200

    Oppdaterer todo

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/brevredigering/application/usecases/GenererFoerstesideHandler.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/brevredigering/application/usecases/GenererFoerstesideHandler.kt
@@ -37,6 +37,7 @@ class GenererFoerstesideHandler(
                 brukerType = Bruker.BrukerType.PERSON
             ),
             tema = request.tema,
+            avsender = null, // TODO: finn ut om vi skal sende med noko her
             behandlingstema = null, // TODO: korleis finn vi denne?
             arkivtittel = tittel,
             vedleggsliste = listOf(), // TODO: må finne ut av kva vi sender her
