8dbf82a368451bf62bc0d8883b685e1ee0f40633
Mads Opheim <mads.opheim@nav.no>
2026-08-13 10:01:29 +0200

    Fjernar @DisplayText som ikkje lenger er nyttig

--- pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/redigerbar/PeriodisertInntektBarnetillegg.kt
+++ pensjon/api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/legacy/redigerbar/PeriodisertInntektBarnetillegg.kt
@@ -4,15 +4,8 @@ import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgEnum
 import no.nav.pensjon.brevbaker.api.model.DisplayText
 
 enum class PeriodisertInntektBarnetillegg(override val displayText: String) : SaksbehandlerValgEnum {
-    @DisplayText("Periodisert inntekt")
     PERIODISERT_INNTEKT("Periodisert inntekt"),
-
-    @DisplayText("Inntekt gjelder for hele året")
     INNTEKT_HELE_ARET("Inntekt gjelder for hele året"),
-
-    @DisplayText("Barn fyller 18 ila året")
     BARN_FYLLER_18("Barn fyller 18 ila året"),
-
-    @DisplayText("Skriv begrunnelse selv")
     INGEN("Skriv begrunnelse selv")
 }
