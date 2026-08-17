90c0ec4d4f1965096c7f3864e49140bde990844d
Håkon <32221678+hconhegg@users.noreply.github.com>
2026-08-17 12:11:01 +0200

    Bumper (#3668)

--- pensjon/maler/build.gradle.kts
+++ pensjon/maler/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val apiModelVersion = 400
+val apiModelVersion = 401
 
 val apiModelJavaTarget: String by System.getProperties()
 
--- ufoere/maler/build.gradle.kts
+++ ufoere/maler/build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val ufoereApiModelVersion = 90
+val ufoereApiModelVersion = 91
 
 val apiModelJavaTarget: String by System.getProperties()
 
