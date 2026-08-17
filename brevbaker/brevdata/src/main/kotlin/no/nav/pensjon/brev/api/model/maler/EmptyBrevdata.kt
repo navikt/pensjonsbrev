671c023e2d7f91497ea1f191c6c2654128b12852
Mads Opheim <mads.opheim@nav.no>
2026-08-17 14:15:26 +0200

    Fjernar EmptySaksbehandlerValg

--- brevbaker/brevdata/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/EmptyBrevdata.kt
+++ brevbaker/brevdata/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/EmptyBrevdata.kt
@@ -6,6 +6,4 @@ data object EmptyAutobrevdata : AutobrevData
 
 data object EmptyFagsystemdata : FagsystemBrevdata
 
-data object EmptySaksbehandlerValg : SaksbehandlerValgBrevdata
-
 data object EmptyVedleggData : VedleggData
