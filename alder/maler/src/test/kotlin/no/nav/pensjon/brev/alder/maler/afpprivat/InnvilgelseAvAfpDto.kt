2ccd4611eb6c02b7dd253aae5dd4ec9da1ff6494
Monika Sperstad Koller <monika.koller@nav.no>
2026-08-19 15:26:19 +0200

    Oppdatere testdata

--- alder/maler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/afpprivat/InnvilgelseAvAfpDto.kt
+++ alder/maler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/afpprivat/InnvilgelseAvAfpDto.kt
@@ -9,22 +9,18 @@ import java.time.LocalDate
 fun createInnvilgelseAvAfpDto(): InnvilgelseAvAfpDto =
     InnvilgelseAvAfpDto(
         saksbehandlerValg = lagSaksbehandlervalg(),
-        pesysData = createPesysdata(),
-    )
-
-
-fun createPesysdata(): InnvilgelseAvAfpDto.PesysData =
-    InnvilgelseAvAfpDto.PesysData(
-        kravMottattDato = LocalDate.of(2026, 1, 15),
-        virkningFom = LocalDate.of(2026, 3, 1),
-        brukerUnder70Aar = true,
-        bosattINorge = true,
-        afpBeregning = InnvilgelseAvAfpDto.PesysData.AfpBeregning(
-            totalPensjon = Kroner(18500),
-            livsvarigBrutto = Kroner(12000),
-            kronetilleggBrutto = Kroner(2000),
-            kompensasjonstilleggBrutto = Kroner(4500),
-        ),
-        oversiktOverPensjonen = createOversiktOverPensjonenAfpPrivatDto(),
+        pesysData = InnvilgelseAvAfpDto.PesysData(
+            kravMottattDato = LocalDate.of(2026, 1, 15),
+            virkningFom = LocalDate.of(2026, 3, 1),
+            brukerUnder70Aar = true,
+            bosattINorge = true,
+            afpBeregning = InnvilgelseAvAfpDto.PesysData.AfpBeregning(
+                totalPensjon = Kroner(18500),
+                livsvarigBrutto = Kroner(12000),
+                kronetilleggBrutto = Kroner(2000),
+                kompensasjonstilleggBrutto = Kroner(4500),
+            ),
+            oversiktOverPensjonen = createOversiktOverPensjonenAfpPrivatDto(),
+        )
     )
 
