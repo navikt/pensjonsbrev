3d70256727fd3fcb515d556c1e92cf26674b8ba6
Mads Opheim <mads.opheim@nav.no>
2026-08-19 14:37:04 +0200

    Typane for saksbehandlervalg og fagsystemdata er ikkje så viktige her, det viktige er berre redigerbarbrevdata

--- brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/DslExtensionForRedigerbareBrev.kt
+++ brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/DslExtensionForRedigerbareBrev.kt
@@ -1,31 +1,29 @@
 package no.nav.pensjon.brev.template
 
-import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
-import no.nav.pensjon.brev.api.model.maler.SaksbehandlerValgBrevdata
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
 import no.nav.pensjon.brev.template.dsl.ParagraphOnlyScope
 import no.nav.pensjon.brev.template.dsl.TemplateGlobalScope
 
 sealed interface DslExtensionForRedigerbareBrev {
-    fun <LetterData : RedigerbarBrevdata<out SaksbehandlerValgBrevdata, out FagsystemBrevdata>> TemplateGlobalScope<LetterData>.fritekst(
+    fun <LetterData : RedigerbarBrevdata<*, *>> TemplateGlobalScope<LetterData>.fritekst(
         beskrivelse: String,
     ): Fritekst =
         beskrivelse.takeIf { it.trim().isNotEmpty() }
             ?.let { Fritekst(it) }
             ?: throw IllegalArgumentException("Fritekstfelt må ha initiell tekst for at vi ikke skal lure bruker.")
 
-    fun <LetterData : RedigerbarBrevdata<out SaksbehandlerValgBrevdata, out FagsystemBrevdata>> TemplateGlobalScope<LetterData>.redigerbarData(
+    fun <LetterData : RedigerbarBrevdata<*, *>> TemplateGlobalScope<LetterData>.redigerbarData(
         variabel: StringExpression,
     ): RedigerbarData = RedigerbarData(variabel)
 
-    fun <Lang : LanguageSupport> ParagraphOnlyScope<Lang, out RedigerbarBrevdata<out SaksbehandlerValgBrevdata, out FagsystemBrevdata>>.includePhrase(
+    fun <Lang : LanguageSupport> ParagraphOnlyScope<Lang, out RedigerbarBrevdata<*, *>>.includePhrase(
         phrase: RedigerbarParagraphPhrase<Lang>,
     ) {
         phrase.apply(this)
     }
 
-    fun <Lang : LanguageSupport> OutlineOnlyScope<Lang, out RedigerbarBrevdata<out SaksbehandlerValgBrevdata, out FagsystemBrevdata>>.includePhrase(
+    fun <Lang : LanguageSupport> OutlineOnlyScope<Lang, out RedigerbarBrevdata<*, *>>.includePhrase(
         phrase: RedigerbarOutlinePhrase<Lang>,
     ) {
         phrase.apply(this)
