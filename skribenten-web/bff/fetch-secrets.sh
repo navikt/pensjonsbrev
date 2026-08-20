7cbf6e041b4f9718c45e2e3ec6c671c889d9aa04
Mads Opheim <mads.opheim@nav.no>
2026-08-20 14:32:23 +0200

    Rettar env-hardkoding, og fjernar unødig mkdir

--- skribenten-web/bff/fetch-secrets.sh
+++ skribenten-web/bff/fetch-secrets.sh
@@ -14,9 +14,7 @@ if [ -z "$team_name" ]; then
   echo "ERROR: Could not find a team matching 'pensjonsbrev' via 'nais status'. Make sure you are logged in with 'nais login' and have access to the team." && exit 1
 fi
 
-mkdir -p secrets
-
 # AzureAD
-secret_name="$(nais app env skribenten-web --environment dev-gcp --team pensjonsbrev --verbose --output=json | jq -r '[.[] | select(.source.kind=="SECRET") | .source.name] | unique | .[] | select(startswith("azure-skribenten-web"))')"
+secret_name="$(nais app env skribenten-web --environment $KUBE_CLUSTER --team pensjonsbrev --verbose --output=json | jq -r '[.[] | select(.source.kind=="SECRET") | .source.name] | unique | .[] | select(startswith("azure-skribenten-web"))')"
 nais secret get "${secret_name}" --environment $KUBE_CLUSTER --with-values --reason "local development" --output json | jq '.data | from_entries' | jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' > .env
 echo ".env file created."
