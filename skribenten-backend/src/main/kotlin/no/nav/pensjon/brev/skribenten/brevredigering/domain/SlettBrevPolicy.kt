2f8d0ddb3036491941719a1b780250111a36ff46
Mads Opheim <mads.opheim@nav.no>
2026-08-24 14:35:05 +0200

    Sjekkar dødsdato

--- skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/brevredigering/domain/SlettBrevPolicy.kt
+++ skribenten-backend/src/main/kotlin/no/nav/pensjon/brev/skribenten/brevredigering/domain/SlettBrevPolicy.kt
@@ -3,12 +3,22 @@ package no.nav.pensjon.brev.skribenten.brevredigering.domain
 import no.nav.pensjon.brev.skribenten.common.Outcome
 import no.nav.pensjon.brev.skribenten.common.Outcome.Companion.failure
 import no.nav.pensjon.brev.skribenten.common.Outcome.Companion.success
+import no.nav.pensjon.brev.skribenten.fagsystem.Behandlingsnummer
 import no.nav.pensjon.brev.skribenten.model.JournalpostId
+import no.nav.pensjon.brev.skribenten.services.PdlService
+import no.nav.pensjon.brevbaker.api.model.BrevbakerType
 
-class SlettBrevPolicy {
-    fun kanSlette(brev: Brevredigering): Outcome<Unit, BrevredigeringError> {
+class SlettBrevPolicy(val pdlService: PdlService) {
+    suspend fun kanSlette(
+        brev: Brevredigering,
+        pid: BrevbakerType.Pid,
+        behandlingsnumre: List<Behandlingsnummer>,
+    ): Outcome<Unit, BrevredigeringError> {
         return when {
-            brev.journalpostId != null -> failure(KanIkkeSlette.ArkivertBrev(brev.journalpostId!!))
+            brev.journalpostId != null && pdlService.hentBrukerContext(
+                pid,
+                behandlingsnumre
+            )?.doedsdato == null -> failure(KanIkkeSlette.ArkivertBrev(brev.journalpostId!!))
             else -> success(Unit)
         }
     }
