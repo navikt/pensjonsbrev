61b4180f96e82ea6010b6e8762f12f5f890338c4
Mussie Mesfin <mussie009@gmail.com>
2026-08-26 14:09:36 +0200

    Refresh vedlegg caches when the letter template is reset

--- skribenten-web/frontend/src/components/TilbakestillMalModal.tsx
+++ skribenten-web/frontend/src/components/TilbakestillMalModal.tsx
@@ -2,6 +2,7 @@ import { BodyLong, Button, HStack, Modal } from "@navikt/ds-react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { getBrev, tilbakestillBrev } from "~/api/brev-queries";
+import { redigerbareVedleggKeys } from "~/api/redigerbareVedlegg-endpoints";
 import { type BrevResponse } from "~/types/brev";
 
 const TilbakestillMalModal = (props: {
@@ -15,6 +16,8 @@ const TilbakestillMalModal = (props: {
     mutationFn: () => tilbakestillBrev(props.brevId),
     onSuccess: (response) => {
       queryClient.setQueryData(getBrev.queryKey(props.brevId), response);
+      // Tilbakestilling av brevet merger også inn malendringer i vedleggene, så de kan ha endret seg.
+      queryClient.invalidateQueries({ queryKey: redigerbareVedleggKeys.all });
       props.resetEditor(response);
       props.onClose();
     },
