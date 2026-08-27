654996ea0868298c2c7b53087b981457c841b9a6
Håkon <32221678+hconhegg@users.noreply.github.com>
2026-08-27 09:39:05 +0200

    Retter fra stor til liten n i parse av team navn i fetch-secrets.

--- brevbaker/locust/fetch-secrets.sh
+++ brevbaker/locust/fetch-secrets.sh
@@ -9,7 +9,7 @@ which nais || (
   echo "ERROR: You need to install the nais CLI tool on your machine: https://doc.nais.io/operate/how-to/naisdevice/nais-cli/" && exit 1
 ) || exit 1
 
-team_name="$(nais status -ojson | jq -r '.[].team.Name | select(contains("pensjonsbrev"))')"
+team_name="$(nais status -ojson | jq -r '.[].team.name | select(contains("pensjonsbrev"))')"
 if [ -z "$team_name" ]; then
   echo "ERROR: Could not find a team matching 'pensjonsbrev' via 'nais status'. Make sure you are logged in with 'nais login' and have access to the team." && exit 1
 fi
