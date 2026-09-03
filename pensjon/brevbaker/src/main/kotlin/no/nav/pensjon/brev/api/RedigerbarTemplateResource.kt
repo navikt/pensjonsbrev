b8126841c44885bac46b2855440ed3c31ad8f5f0
Mads Opheim <mads.opheim@nav.no>
2026-09-03 09:08:48 +0200

    Slettar det gamle, no ubrukte endepunktet for valgbare vedlegg

--- pensjon/brevbaker/src/main/kotlin/no/nav/pensjon/brev/api/RedigerbarTemplateResource.kt
+++ pensjon/brevbaker/src/main/kotlin/no/nav/pensjon/brev/api/RedigerbarTemplateResource.kt
@@ -22,7 +22,7 @@ class RedigerbarTemplateResource<Kode : Brevkode<Kode>, out T : BrevTemplate<Bre
     override val name: String,
     templates: Set<T>,
     pdfByggerService: PDFByggerService,
-    val alltidValgbareVedlegg: Set<AlltidValgbartVedlegg<*>>,
+    alltidValgbareVedlegg: Set<AlltidValgbartVedlegg<*>>,
 ) : TemplateResource<Kode, T, BestillRedigertBrevRequest<Kode>>, TemplateLibrary<Kode, T> by TemplateLibraryImpl(templates) {
     private val brevbaker = Brevbaker(pdfByggerService)
     private val letterFactory: LetterFactory<Kode> = LetterFactory(alltidValgbareVedlegg)
