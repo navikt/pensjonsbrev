25e60891ae76475e456cf859a26e47d39abc408f
Jeremy Elsom <jeremy.elsom@nav.no>
2026-09-04 10:16:53 +0200

    Brevmalen ferdigstilt

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/klageOgAnke/AnkeOrienteringOmSaksbehandling.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/klageOgAnke/AnkeOrienteringOmSaksbehandling.kt
@@ -2,6 +2,7 @@ package no.nav.pensjon.brev.maler.klageOgAnke
 
 import no.nav.pensjon.brev.api.model.maler.EmptyRedigerbarBrevdataMedSaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.Pesysbrevkoder
+import no.nav.pensjon.brev.maler.fraser.common.Constants.NAV_KLAGEINSTANS
 import no.nav.pensjon.brev.maler.fraser.common.Felles.fulltNavn
 import no.nav.pensjon.brev.model.format
 import no.nav.pensjon.brev.template.Element.OutlineContent.ParagraphContent.Text.FontType.BOLD
@@ -13,10 +14,8 @@ import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
 import no.nav.pensjon.brevbaker.api.model.LetterMetadata
-import no.nav.pensjon.brevbaker.api.model.selectors.brevbakerFelles.avsenderEnhet
 import no.nav.pensjon.brevbaker.api.model.selectors.brevbakerFelles.bruker
 import no.nav.pensjon.brevbaker.api.model.selectors.brevbakerFelles.bruker.foedselsnummer
-import no.nav.pensjon.brevbaker.api.model.selectors.brevbakerFelles.navEnhet.navn
 
 @TemplateModelHelpers
 object AnkeOrienteringOmSaksbehandling : RedigerbarTemplate<EmptyRedigerbarBrevdataMedSaksbehandlerValg> {
@@ -34,32 +33,42 @@ object AnkeOrienteringOmSaksbehandling : RedigerbarTemplate<EmptyRedigerbarBrevd
             brevtype = LetterMetadata.Brevtype.INFORMASJONSBREV,
         )
     ) {
-        title { text(bokmal { +"Anke - " + fritekst("ytelse") + " - Orientering om saksbehandling" }, english { +"Appeal - " + fritekst("ytelse") + " - Briefing on case proceedings" }) }
+        title {
+            text(
+                bokmal { +"Anke - " + fritekst("ytelse") + " - Orientering om saksbehandling" },
+                english { +"Appeal - " + fritekst("ytelse") + " - Briefing on case proceedings" })
+        }
 
         outline {
             paragraph {
                 text(bokmal { +"Ankende part: " }, english { +"Appellant: " }, BOLD)
                 text(bokmal { +felles.bruker.fulltNavn() + " " }, english { +felles.bruker.fulltNavn() + " " })
-                text(bokmal { +felles.bruker.foedselsnummer.format() }, english { +felles.bruker.foedselsnummer.format() })
+                text(
+                    bokmal { +felles.bruker.foedselsnummer.format() },
+                    english { +felles.bruker.foedselsnummer.format() })
             }
             paragraph {
                 text(bokmal { +"Ankemotpart: " }, english { +"Other party: " }, BOLD)
-                text(bokmal { +"Nav Klageinstans"}, english { +"Nav Klageinstans" } )
+                text(bokmal { +NAV_KLAGEINSTANS }, english { +NAV_KLAGEINSTANS })
             }
             paragraph {
                 text(
                     bokmal { +"Vi har mottatt anken din av " + fritekst("ankedato") + " over vedtaket av " + fritekst("dato på klagevedtaktet") + "." },
-                    english { +"We have received your appeal of " + fritekst("ankedato") + " against the decision of " + fritekst("dato på klagevedtaktet") + "." })
+                    english {
+                        +"We have received your appeal of " + fritekst("ankedato") + " against the decision of "
+                        +fritekst("dato på klagevedtaktet") + "."
+                    }
+                )
             }
             title1() { text(bokmal { +"Ny prøving" }, english { +"New examination" }) }
             paragraph {
                 text(
                     bokmal {
-                        +"Nav Klageinstans skal prøve vedtaket som er påanket på nytt."
+                        +"$NAV_KLAGEINSTANS skal prøve vedtaket som er påanket på nytt."
                         +" Hvis dette ikke fører til at vedtaket blir omgjort, blir saken sendt til Trygderetten til avgjørelse."
                         +" Dette følger av lov om anke til Trygderetten paragraf 13."
                     },
-                    english { +"Nav Klageinstans will reconsider the decision that is being appealed." }
+                    english { +"$NAV_KLAGEINSTANS will reconsider the decision that is being appealed." }
                             + " If this does not result in the decision being amended, the case will be passed to the National Insurance Court for decision."
                             + " This is pursuant to Section 13 of the Act governing appeals to the National Insurance Court."
                 )
@@ -81,8 +90,32 @@ object AnkeOrienteringOmSaksbehandling : RedigerbarTemplate<EmptyRedigerbarBrevd
                     }
                 )
             }
-
-            )
+            title1() { text(bokmal { +"Behandlingstid" }, english { +"Processing time" }) }
+            paragraph {
+                text(
+                    bokmal {
+                        +"Saksbehandlingstiden er vanligvis " + fritekst("antall dager/uker/måneder") + "."
+                        +" Hvis saken din ikke er ferdigbehandlet av oss i løpet av denne tiden, vil du få nærmere beskjed."
+                    },
+                    english {
+                        +"The processing time is normally " + fritekst("antall dager/uker/måneder") + "."
+                        +"If the processing of your case has not been completed within that time, you will be notified."
+                    }
+                )
+            }
+            title1() { text(bokmal { +"Meld fra om endringer" }, english { +"Please notify us of changes" }) }
+            paragraph {
+                text(
+                    bokmal {
+                        +"Vi ber om at du holder oss orientert om forhold som kan ha betydning for avgjørelsen av saken din."
+                        +" Det kan være endringer i medisinske forhold, arbeid, inntekt, sivilstand og lignende."
+                    },
+                    english {
+                        +"Please keep us informed about circumstances that can affect the decision on your case."
+                        +" These might be changes in circumstances relating to health, work, income, civil status and similar."
+                    }
+                )
+            }
         }
     }
 }
