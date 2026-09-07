d213fcb057a200277c0366f9b603efabb10723da
Mads Opheim <mads.opheim@nav.no>
2026-09-07 08:58:00 +0200

    Gjer fagsystembrevdata for simuleringsbrevet lik mtp nullability som saksbehandlervalg
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>

--- planlegge-pensjon-maler/src/main/kotlin/no/nav/pensjon/brev/planleggepensjon/simulering/ApSimuleringBrev.kt
+++ planlegge-pensjon-maler/src/main/kotlin/no/nav/pensjon/brev/planleggepensjon/simulering/ApSimuleringBrev.kt
@@ -1,7 +1,7 @@
 package no.nav.pensjon.brev.planleggepensjon.simulering
 
-import no.nav.pensjon.brev.api.model.ISakstype
 import no.nav.pensjon.brev.api.model.IBrevkategori
+import no.nav.pensjon.brev.api.model.ISakstype
 import no.nav.pensjon.brev.api.model.TemplateDescription
 import no.nav.pensjon.brev.api.model.maler.Brevkode
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
@@ -10,8 +10,6 @@ import no.nav.pensjon.brev.planleggepensjon.Brevkategori
 import no.nav.pensjon.brev.planleggepensjon.FeatureToggles
 import no.nav.pensjon.brev.planleggepensjon.PlanleggePensjonBrevkoder
 import no.nav.pensjon.brev.planleggepensjon.simulering.selectors.apSimuleringBrevDto.pesysData
-import no.nav.pensjon.brev.planleggepensjon.simulering.selectors.apSimuleringBrevDto.saksbehandlerValg
-import no.nav.pensjon.brev.planleggepensjon.simulering.selectors.apSimuleringDto.simulering
 import no.nav.pensjon.brev.planleggepensjon.simulering.selectors.apSimuleringDtoData.simulering
 import no.nav.pensjon.brev.planleggepensjon.simulering.selectors.simulering.afpOffentligLivsvarig
 import no.nav.pensjon.brev.planleggepensjon.simulering.selectors.simulering.afpOffentligTidsbegrenset
@@ -21,10 +19,8 @@ import no.nav.pensjon.brev.template.Language
 import no.nav.pensjon.brev.template.LetterTemplate
 import no.nav.pensjon.brev.template.RedigerbarTemplate
 import no.nav.pensjon.brev.template.createTemplate
-import no.nav.pensjon.brev.template.dsl.expression.ifNull
 import no.nav.pensjon.brev.template.dsl.expression.notNull
 import no.nav.pensjon.brev.template.dsl.expression.or
-import no.nav.pensjon.brev.template.dsl.expression.safe
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brev.template.dsl.text
@@ -51,9 +47,9 @@ object ApSimuleringBrev : RedigerbarTemplate<ApSimuleringBrevDto> {
         )
     ) {
         title {
-            showIf(pesysData.simulering.safe { afpPrivat.notNull() }.ifNull(saksbehandlerValg.simulering.afpPrivat.notNull())) {
+            showIf(pesysData.simulering.afpPrivat.notNull()) {
                 text(bokmal { +"Beregning av alderspensjon og AFP i privat sektor" })
-            }.orShowIf(pesysData.simulering.safe { afpOffentligTidsbegrenset.notNull() }.ifNull(saksbehandlerValg.simulering.afpOffentligTidsbegrenset.notNull()) or pesysData.simulering.safe { afpOffentligLivsvarig.notNull() }.ifNull(saksbehandlerValg.simulering.afpOffentligLivsvarig.notNull())) {
+            }.orShowIf(pesysData.simulering.afpOffentligTidsbegrenset.notNull() or pesysData.simulering.afpOffentligLivsvarig.notNull()) {
                 text(bokmal { +"Beregning av AFP i offentlig sektor etterfulgt av alderspensjon" })
             }.orShow {
                 text(bokmal { +"Beregning av alderspensjon" })
