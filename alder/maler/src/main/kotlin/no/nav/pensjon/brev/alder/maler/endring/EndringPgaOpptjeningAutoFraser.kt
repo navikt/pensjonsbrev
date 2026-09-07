af6d8f5d4c5195a1ac583abbf2de8528864c6e3d
Knut.Mork <knut.mork@nav.no>
2026-09-07 12:46:42 +0200

    Riktig terminologi for bunnfradrag på nynorsk

--- alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/endring/EndringPgaOpptjeningAutoFraser.kt
+++ alder/maler/src/main/kotlin/no/nav/pensjon/brev/alder/maler/endring/EndringPgaOpptjeningAutoFraser.kt
@@ -579,7 +579,7 @@ data class AvsnittArbeidsinntekt(
             paragraph {
                 text(
                     bokmal { +"Uføretrygden din kan fortsatt bli redusert på grunn av inntekt. Du finner informasjon om bunnfradraget i vedtak om uføretrygd." },
-                    nynorsk { +"Uføretrygda di kan framleis bli redusert på grunn av inntekt. Du finn informasjon om bunnfradraget i vedtak om uføretrygd." },
+                    nynorsk { +"Uføretrygda di kan framleis bli redusert på grunn av inntekt. Du finn informasjon om botnfrådraget i vedtak om uføretrygd." },
                     english { +"Your disability benefit may still be reduced as a result of income. You can find information on the income limit in the decision on disability benefit." }
                 )
             }
