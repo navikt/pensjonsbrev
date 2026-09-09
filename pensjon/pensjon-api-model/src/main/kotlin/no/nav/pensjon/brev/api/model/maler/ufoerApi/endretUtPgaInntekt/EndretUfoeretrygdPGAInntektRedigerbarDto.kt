e16be24a9681316fe7dda826f1df78c884d8ed70
Mads Opheim <mads.opheim@nav.no>
2026-09-09 16:32:50 +0200

    Fiksar ny mal
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>

--- pensjon/pensjon-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ufoerApi/endretUtPgaInntekt/EndretUfoeretrygdPGAInntektRedigerbarDto.kt
+++ pensjon/pensjon-api-model/src/main/kotlin/no/nav/pensjon/brev/api/model/maler/ufoerApi/endretUtPgaInntekt/EndretUfoeretrygdPGAInntektRedigerbarDto.kt
@@ -1,13 +1,13 @@
 package no.nav.pensjon.brev.api.model.maler.ufoerApi.endretUtPgaInntekt
 
-import no.nav.pensjon.brev.api.model.maler.BrevdataMedSaksbehandlerValg
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
+import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.api.model.maler.SaksbehandlervalgIDSL
 
 data class EndretUfoeretrygdPGAInntektRedigerbarDto(
     override val saksbehandlerValg: SaksbehandlervalgIDSL,
     override val pesysData: PesysData,
-) : BrevdataMedSaksbehandlerValg<EndretUfoeretrygdPGAInntektRedigerbarDto.PesysData> {
+) : RedigerbarBrevdata<EndretUfoeretrygdPGAInntektRedigerbarDto.PesysData> {
     data class PesysData(
         val data: EndretUTPgaInntektDtoV2,
     ) : FagsystemBrevdata
