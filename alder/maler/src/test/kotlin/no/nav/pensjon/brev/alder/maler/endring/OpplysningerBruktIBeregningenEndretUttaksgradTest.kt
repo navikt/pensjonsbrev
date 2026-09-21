4f9a3904b9c1f2d23f489f72259ae5fbf718fb91
Mads Opheim <71336041+madsop-nav@users.noreply.github.com>
2026-09-21 15:26:35 +0200

    Slett vedleggtestar som allereie er dekka av generelle testar (#3897)
    * Fjernar manuelt introduserte testar som gjer det samme som no blir dekka av generelle testar
    
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>
    
    * Enda litt fleire vedleggstestar som allereie er dekka
    
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>
    
    * Skjønner ikkje korfor desse to testane skal vera manuelle, gjer dei heller automatiske
    
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>
    
    * Denne testen har gjort sitt, vi får ikkje lenger innpakka variantar
    
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>
    
    ---------
    
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>

--- alder/maler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/endring/OpplysningerBruktIBeregningenEndretUttaksgradTest.kt
+++ alder/maler/src/test/kotlin/no/nav/pensjon/brev/alder/maler/endring/OpplysningerBruktIBeregningenEndretUttaksgradTest.kt
@@ -1,55 +1,12 @@
 package no.nav.pensjon.brev.alder.maler.endring
 
-import no.nav.brev.brevbaker.LetterTestImpl
-import no.nav.brev.brevbaker.createVedleggTestTemplate
-import no.nav.brev.brevbaker.renderTestHtml
 import no.nav.brev.brevbaker.vilkaarligDato
-import no.nav.pensjon.brev.alder.Fixtures
 import no.nav.pensjon.brev.alder.model.AlderspensjonRegelverkType
 import no.nav.pensjon.brev.alder.model.Beregningsmetode
 import no.nav.pensjon.brev.alder.model.endring.OpplysningerBruktIBeregningenEndretUttaksgradDto
-import no.nav.pensjon.brev.api.model.maler.EmptyAutobrevdata
-import no.nav.pensjon.brev.template.Language
-import no.nav.pensjon.brev.template.dsl.expression.expr
-import no.nav.pensjon.brev.template.dsl.languages
 import no.nav.pensjon.brevbaker.api.model.BrevbakerType.Kroner
 import no.nav.pensjon.brevbaker.api.model.BrevbakerType.Percent
 import no.nav.pensjon.brevbaker.api.model.BrevbakerType.Year
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
-
-class OpplysningerBruktIBeregningenEndretUttaksgradTest {
-
-    @ParameterizedTest(name = "{index} => template={0}, etterlatteBrevKode={1}, fixtures={2}, spraak={3}")
-    @MethodSource("sakstyperOgSpraak")
-    fun `lag vedlegg`(regelverk: AlderspensjonRegelverkType, spraak: Language) {
-        val template = createVedleggTestTemplate(
-            vedleggOpplysningerBruktIBeregningenEndretUttaksgrad,
-            createOpplysningerBruktIBeregningenEndretUttaksgradDto(regelverk).expr(),
-            languages(Language.Bokmal, Language.Nynorsk, Language.English),
-        )
-        LetterTestImpl(
-            template,
-            EmptyAutobrevdata,
-            spraak,
-            Fixtures.fellesAuto
-        ).renderTestHtml(this::class.simpleName + "_${regelverk}_${spraak::class.simpleName}")
-    }
-
-    companion object {
-        @JvmStatic
-        fun sakstyperOgSpraak() = listOf(Language.Nynorsk, Language.Bokmal, Language.English)
-            .flatMap { spraak ->
-                AlderspensjonRegelverkType.entries.map {
-                    Arguments.of(
-                        it,
-                        spraak
-                    )
-                }
-            }
-    }
-}
 
 fun createOpplysningerBruktIBeregningenEndretUttaksgradDto(alderspensjonRegelverkType: AlderspensjonRegelverkType = AlderspensjonRegelverkType.AP2025) =
     OpplysningerBruktIBeregningenEndretUttaksgradDto(
