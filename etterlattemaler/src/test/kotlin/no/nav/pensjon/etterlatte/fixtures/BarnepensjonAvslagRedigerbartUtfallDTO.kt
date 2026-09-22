3330c65b96e64742cda3dbb64f9bbebf765c0161
harrybjorntvedtliam-code <harry.bjorntvedt.liam@nav.no>
2026-09-22 12:47:14 +0200

    Legger til dødsdato i barnepensjon avslag fraser og redigerbare utfall

--- etterlattemaler/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/BarnepensjonAvslagRedigerbartUtfallDTO.kt
+++ etterlattemaler/src/test/kotlin/no/nav/pensjon/etterlatte/fixtures/BarnepensjonAvslagRedigerbartUtfallDTO.kt
@@ -2,10 +2,27 @@ package no.nav.pensjon.etterlatte.fixtures
 
 import no.nav.pensjon.etterlatte.maler.barnepensjon.avslag.BarnepensjonAvslagRedigerbartUtfallDTO
 import no.nav.pensjon.etterlatte.maler.barnepensjon.avslag.BarnepensjonAvslagRedigerbartUtfallData
+import java.time.LocalDate
 
-fun createBarnepensjonAvslagRedigerbartUtfallDTO(): BarnepensjonAvslagRedigerbartUtfallDTO =
+fun createBarnepensjonAvslagRedigerbartUtfallDTO(
+    erSluttbehandling: Boolean = true,
+): BarnepensjonAvslagRedigerbartUtfallDTO =
     BarnepensjonAvslagRedigerbartUtfallDTO(
         data = BarnepensjonAvslagRedigerbartUtfallData(
-            erSluttbehandling = true,
-        )
+            avdoedNavn = "Ola Nordmann",
+            avdoedDoedsdato = LocalDate.of(2020, 1, 1),
+            erSluttbehandling = erSluttbehandling,
+        ),
+    )
+
+fun createBarnepensjonAvslagRedigerbartUtfallDTOUtenDoedsdato(
+    erSluttbehandling: Boolean,
+): BarnepensjonAvslagRedigerbartUtfallDTO =
+    BarnepensjonAvslagRedigerbartUtfallDTO(
+        data =
+            BarnepensjonAvslagRedigerbartUtfallData(
+                avdoedNavn = "Ola Nordmann",
+                avdoedDoedsdato = null,
+                erSluttbehandling = erSluttbehandling,
+            ),
     )
