54ebd8cded678b21b9dc9a976eb0ba083003c948
Mads Opheim <mads.opheim@nav.no>
2026-09-23 10:22:07 +0200

    Flyttar redigerbar-selectors til ny pakke
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>

--- brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/dsl/RedigerbarBrevdataSelectors.kt
+++ brevbaker/dsl/src/main/kotlin/no/nav/pensjon/brev/template/dsl/RedigerbarBrevdataSelectors.kt
@@ -1,6 +1,6 @@
 @file:OptIn(InternKonstruktoer::class)
 
-package no.nav.pensjon.brev.api.model.maler.selectors.redigerbarBrevdata
+package no.nav.pensjon.brev.template.dsl
 
 import no.nav.brev.InternKonstruktoer
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
@@ -8,7 +8,6 @@ import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
 import no.nav.pensjon.brev.template.Expression
 import no.nav.pensjon.brev.template.TemplateModelSelector
 import no.nav.pensjon.brev.template.UnaryOperation
-import no.nav.pensjon.brev.template.dsl.TemplateGlobalScope
 
 // Håndskrevet fordi RedigerbarBrevdata er generisk: KSP-generatoren lager én selectors-fil per
 // klasse-deklarasjon og kan derfor ikke substituere Data per mal. Pakken etterligner konvensjonen
@@ -26,13 +25,14 @@ private object PesysDataSelector : TemplateModelSelector<RedigerbarBrevdata<Fags
 private fun <Data : FagsystemBrevdata> pesysDataSelector(): TemplateModelSelector<RedigerbarBrevdata<Data>, Data> =
     PesysDataSelector as TemplateModelSelector<RedigerbarBrevdata<Data>, Data>
 
-val <Data : FagsystemBrevdata> TemplateGlobalScope<RedigerbarBrevdata<Data>>.pesysData: Expression<Data>
+// R er nødvendig så lenge hver mal har sin egen dto-klasse som implementerer RedigerbarBrevdata, som vi nå prøver å komme bort fra
+val <Data : FagsystemBrevdata, R : RedigerbarBrevdata<Data>> TemplateGlobalScope<R>.pesysData: Expression<Data>
     get() = Expression.UnaryInvoke(
         Expression.FromScope.Argument(),
         UnaryOperation.Select(pesysDataSelector())
     )
 
-val <Data : FagsystemBrevdata> Expression<RedigerbarBrevdata<Data>>.pesysData: Expression<Data>
+val <Data : FagsystemBrevdata, R : RedigerbarBrevdata<Data>> Expression<R>.pesysData: Expression<Data>
     get() = Expression.UnaryInvoke(
         this,
         UnaryOperation.Select(pesysDataSelector())
--- brevbaker/template-model-generator/src/test/kotlin/no/nav/pensjon/brev/template/dsl/helpers/testcases/RedigerbarBrevdataAsModel.kt
+++ brevbaker/template-model-generator/src/test/kotlin/no/nav/pensjon/brev/template/dsl/helpers/testcases/RedigerbarBrevdataAsModel.kt
@@ -3,13 +3,13 @@ package no.nav.pensjon.brev.template.dsl.helpers.testcases
 import no.nav.brev.InternKonstruktoer
 import no.nav.pensjon.brev.api.model.maler.FagsystemBrevdata
 import no.nav.pensjon.brev.api.model.maler.RedigerbarBrevdata
-import no.nav.pensjon.brev.api.model.maler.selectors.redigerbarBrevdata.pesysData
 import no.nav.pensjon.brev.template.Expression
 import no.nav.pensjon.brev.template.HasModel
 import no.nav.pensjon.brev.template.dsl.helpers.SimpleTemplateScope
 import no.nav.pensjon.brev.template.dsl.helpers.TemplateModelHelpers
 import no.nav.pensjon.brev.template.dsl.helpers.testcases.selectors.redigerbarBrevdataAsModel.pesysData.*
 import no.nav.pensjon.brev.template.dsl.helpers.testcases.selectors.redigerbarBrevdataAsModel.sak.*
+import no.nav.pensjon.brev.template.dsl.pesysData
 
 /**
  * Verify that a template which uses RedigerbarBrevdata directly as its model gets selectors
