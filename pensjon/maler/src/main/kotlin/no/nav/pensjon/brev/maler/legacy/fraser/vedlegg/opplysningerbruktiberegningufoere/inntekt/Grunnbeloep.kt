6e8dc672a10762fc24efdf754317351fb7bcc426
Knut.Mork <knut.mork@nav.no>
2026-09-24 23:57:50 +0200

    Gjenbruker erIkkeInntektsendringBrevkode i Grunnbeløp

--- pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/fraser/vedlegg/opplysningerbruktiberegningufoere/inntekt/Grunnbeloep.kt
+++ pensjon/maler/src/main/kotlin/no/nav/pensjon/brev/maler/legacy/fraser/vedlegg/opplysningerbruktiberegningufoere/inntekt/Grunnbeloep.kt
@@ -1,23 +1,18 @@
 package no.nav.pensjon.brev.maler.legacy.fraser.vedlegg.opplysningerbruktiberegningufoere.inntekt
 
 import no.nav.pensjon.brev.api.model.maler.legacy.pegruppe10.PEgruppe10
-import no.nav.pensjon.brev.maler.legacy.pebrevkode
+import no.nav.pensjon.brev.maler.legacy.vedlegg.erIkkeInntektsendringBrevkode
 import no.nav.pensjon.brev.template.Expression
 import no.nav.pensjon.brev.template.LangBokmalNynorsk
 import no.nav.pensjon.brev.template.OutlinePhrase
 import no.nav.pensjon.brev.template.dsl.OutlineOnlyScope
-import no.nav.pensjon.brev.template.dsl.expression.and
-import no.nav.pensjon.brev.template.dsl.expression.notEqualTo
 import no.nav.pensjon.brev.template.dsl.text
 
 data class Grunnbeloep(
     val pe: Expression<PEgruppe10>,
 ) : OutlinePhrase<LangBokmalNynorsk>() {
     override fun OutlineOnlyScope<LangBokmalNynorsk, Unit>.template() {
-        showIf(
-            pe.pebrevkode().notEqualTo("PE_UT_05_100")
-                    and pe.pebrevkode().notEqualTo("PE_UT_07_100")
-        ) {
+        showIf(pe.erIkkeInntektsendringBrevkode()) {
             paragraph {
                 text(
                     bokmal { + "Folketrygdens grunnbeløp endres hvert år, og uføretrygden din blir justert ut fra dette." },
