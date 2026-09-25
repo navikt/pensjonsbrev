4f4be5101a7c1cea0c34a70468518eb3228401f3
Mads Opheim <mads.opheim@nav.no>
2026-09-25 15:07:36 +0200

    Går bort frå data class i publisert modul
    Signed-off-by: Mads Opheim <mads.opheim@nav.no>

--- brevbaker/brevbaker-api/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/RedigerbareVedleggTitler.kt
+++ brevbaker/brevbaker-api/src/main/kotlin/no/nav/pensjon/brevbaker/api/model/RedigerbareVedleggTitler.kt
@@ -1,7 +1,16 @@
 package no.nav.pensjon.brevbaker.api.model
 
 import no.nav.pensjon.brevbaker.api.model.BrevbakerType.VedleggId
+import java.util.Objects
 
-data class RedigerbareVedleggTitler(val vedlegg: List<Vedlegg>) {
-    data class Vedlegg(val id: VedleggId, val tittel: String)
+class RedigerbareVedleggTitler(val vedlegg: List<Vedlegg>) {
+    override fun equals(other: Any?) = other is RedigerbareVedleggTitler && vedlegg == other.vedlegg
+    override fun hashCode() = vedlegg.hashCode()
+    override fun toString(): String = "RedigerbareVedleggTitler(vedlegg=$vedlegg)"
+
+    class Vedlegg(val id: VedleggId, val tittel: String) {
+        override fun equals(other: Any?) = other is Vedlegg && id == other.id && tittel == other.tittel
+        override fun hashCode() = Objects.hash(id, tittel)
+        override fun toString(): String = "Vedlegg(id=$id, tittel=$tittel)"
+    }
 }
